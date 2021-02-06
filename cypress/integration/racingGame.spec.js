dd33157550650bde152cb6f1f7f6a7264ecfe053
jho2301 <jho2301@gmail.com>
2021-02-06 17:39:22 +0900

    test: 자동차 이름을 부여하면 시도할 횟수 입력창이 노출된다

--- cypress/integration/racingGame.spec.js
+++ cypress/integration/racingGame.spec.js
@@ -2,4 +2,10 @@ describe('레이싱 게임', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500');
   });
+
+  it('자동차 이름을 부여하면 시도할 횟수 입력창이 노출된다.', () => {
+    cy.get('#input-car-name').type('aaa,bbb,ccc');
+    cy.get('#submit-car-name').click();
+    cy.get('#section-race-times').should('be.visible');
+  });
 });
