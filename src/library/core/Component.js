701ebc544dd5728011232deb7cc4f1902e3b53a5
jho2301 <jho2301@gmail.com>
2021-02-06 15:21:04 +0900

    feat: Component 부모클래스 구현

---
+++ src/library/core/Component.js
@@ -0,0 +1,12 @@
+export default class Component {
+  $target;
+  props;
+
+  constructor($target, props) {
+    this.$target = $target;
+    this.props = props;
+    this.render();
+  }
+
+  render() {}
+}
