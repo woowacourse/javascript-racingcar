72fc8243b431e2e4ec36699f6fa91eb3570ee236
Kyle <mkitigy@gmail.com>
2021-02-07 17:49:31 +0900

    feat: add test functions (racing car game) - render result - reset game when clicking reset button

--- cypress/integration/racing_car_game.spec.js
+++ cypress/integration/racing_car_game.spec.js
@@ -1,9 +1,32 @@
-describe('Racing car game test', () => {
-  // it('Can render initial display.', () => {});
+import { setGameData } from '../utils/test_value.js';
+import {
+  CAR_INPUT,
+  CAR_BTN,
+  COUNT_INPUT,
+  COUNT_BTN,
+  COUNT_CONTAINER,
+  PROGRESSIVE_CONTAINER,
+  RESET_BTN,
+  RESULT_CONTAINER,
+} from '../../src/js/constants/index.js';
 
-  it('Can check game count input.', () => {
-    // non-digit
-    // negative
-    // decimal
+describe('Racing car game test', () => {
+  before(() => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+    setGameData(CAR_INPUT, CAR_BTN, 'a,b');
+    setGameData(COUNT_INPUT, COUNT_BTN, '1');
+  });
+
+  it('Can render result.', () => {
+    cy.get(COUNT_CONTAINER).should('exist');
+    cy.get(PROGRESSIVE_CONTAINER).should('exist');
+    cy.get(RESULT_CONTAINER).should('exist');
+  });
+
+  it('Can reset game when clicking reset button.', () => {
+    cy.get(RESET_BTN).click();
+    cy.get(COUNT_CONTAINER).should('not.exist');
+    cy.get(PROGRESSIVE_CONTAINER).should('not.exist');
+    cy.get(RESULT_CONTAINER).should('not.exist');
   });
 });
--- src/js/constants/DOM.js
+++ src/js/constants/DOM.js
@@ -2,3 +2,7 @@ export const CAR_INPUT = '.car-name-input';
 export const CAR_BTN = '.car-name-btn';
 export const COUNT_INPUT = '.count-input';
 export const COUNT_BTN = '.count-btn';
+export const COUNT_CONTAINER = '.count-container';
+export const PROGRESSIVE_CONTAINER = '.progressive-container';
+export const RESULT_CONTAINER = '.result-container';
+export const RESET_BTN = '.reset-btn';
