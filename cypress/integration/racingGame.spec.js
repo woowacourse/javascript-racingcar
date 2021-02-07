708a554deb75656ca635b0c1b66aece2f3550262
365kim <365listener@gmail.com>
2021-02-07 16:43:57 +0900

    test: 양의 정수가 아닌 시도횟수 입력 시 경고메세지 출력 테스트 코드 작성

--- cypress/integration/racingGame.spec.js
+++ cypress/integration/racingGame.spec.js
@@ -6,7 +6,7 @@ describe('racing-game', () => {
   it('"EAST, WEST, SOUTH, NORTH"를 입력하면 화면에 해당 자동차 이름들을 표시하는 테스트를 한다.', () => {
     const carNames = ['EAST', 'WEST', 'SOUTH', 'NORTH'];
 
-    typeAndSubmit(carNames);
+    typeCarNameAndSubmit(carNames);
     cy.get('.car-player')
       .should('have.length', carNames.length)
       .each(($div, index, $lis) => {
@@ -21,45 +21,55 @@ describe('racing-game', () => {
     const alertStub = cy.stub();
 
     cy.on('window:alert', alertStub);
-    typeAndSubmit(longCarName).then(() => {
+    typeCarNameAndSubmit(longCarName).then(() => {
       expect(alertStub.getCall(0)).to.be.calledWith(
         '이름은 5글자 이하로 입력해 주세요.',
       );
       cy.get('#car-name-input').should('have.text', '');
     });
-    typeAndSubmit(blankCarName).then(() => {
+    typeCarNameAndSubmit(blankCarName).then(() => {
       expect(alertStub.getCall(0)).to.be.calledWith(
         '공백만으로는 이름을 구성할 수 없습니다.',
       );
       cy.get('#car-name-input').should('have.text', '');
     });
-    typeAndSubmit(emptyCarName).then(() => {
+    typeCarNameAndSubmit(emptyCarName).then(() => {
       expect(alertStub.getCall(0)).to.be.calledWith(
         '공백만으로는 이름을 구성할 수 없습니다.',
       );
       cy.get('#car-name-input').should('have.text', '');
     });
   });
-
-  // it('네자리 이상의 숫자가 입력됐을 경우 경고메세지가 뜬다.', () => {
-  const stub = cy.stub();
-
-  cy.on('window:alert', stub);
-
-  cy.get('#total').then(() => {
-    cy.get('.digits')
-      .contains('1')
-      .click()
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith(
-          '숫자는 세 자리까지만 입력 가능합니다!',
-        );
-      });
-    cy.get('#total').should('have.text', '111/111');
-  });
-  // });
 });
-const typeAndSubmit = (carNames) => {
+
+it('양의 정수만을 시도횟수로 입력할 수 있다.', () => {
+  const negativeRacingCount = -7;
+  const emptyRacingCount = '';
+  const alertStub = cy.stub();
+
+  cy.on('window:alert', alertStub);
+
+  typeRacingCountAndSubmit(negativeRacingCount).then(() => {
+    expect(alertStub.getCall(0)).to.be.calledWith(
+      '1 이상의 숫자를 입력해주세요.',
+    );
+    cy.get('#racing-count-input').should('have.text', '');
+  });
+
+  typeRacingCountAndSubmit(emptyRacingCount).then(() => {
+    expect(alertStub.getCall(0)).to.be.calledWith(
+      '1 이상의 숫자를 입력해주세요.',
+    );
+    cy.get('#racing-count-input').should('have.text', '');
+  });
+});
+
+const typeCarNameAndSubmit = (carName) => {
   cy.get('#car-name-input').type(carNames.join(','));
   cy.get('#car-name-submit').click();
+};
+
+const typeRacingCountAndSubmit = (racingCount) => {
+  cy.get('#racing-count-input').type(racingCount);
+  cy.get('#racing-count-submit').click();
 };
