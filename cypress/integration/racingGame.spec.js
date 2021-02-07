598a47dc1c35ff244b23d6abea68d73b385ef054
365kim <365listener@gmail.com>
2021-02-07 16:25:01 +0900

    test: 자동차 이름이 올바르지 않을 경우의 테스트 코드 작성

--- cypress/integration/racingGame.spec.js
+++ cypress/integration/racingGame.spec.js
@@ -5,9 +5,8 @@ describe('racing-game', () => {
 
   it('"EAST, WEST, SOUTH, NORTH"를 입력하면 화면에 해당 자동차 이름들을 표시하는 테스트를 한다.', () => {
     const carNames = ['EAST', 'WEST', 'SOUTH', 'NORTH'];
-    cy.get('#car-name-input').type('EAST, WEST, SOUTH, NORTH');
-    cy.get('#car-name-submit').click();
 
+    typeAndSubmit(carNames);
     cy.get('.car-player')
       .should('have.length', carNames.length)
       .each(($div, index, $lis) => {
@@ -15,21 +14,52 @@ describe('racing-game', () => {
       });
   });
 
+  it('올바르지 않은 자동차 이름을 입력한 경우 경고메세지를 출력한다.', () => {
+    const longCarName = ['YUJOYOONHO'];
+    const blankCarName = ['   '];
+    const emptyCarName = [''];
+    const alertStub = cy.stub();
+
+    cy.on('window:alert', alertStub);
+    typeAndSubmit(longCarName).then(() => {
+      expect(alertStub.getCall(0)).to.be.calledWith(
+        '이름은 5글자 이하로 입력해 주세요.',
+      );
+      cy.get('#car-name-input').should('have.text', '');
+    });
+    typeAndSubmit(blankCarName).then(() => {
+      expect(alertStub.getCall(0)).to.be.calledWith(
+        '공백만으로는 이름을 구성할 수 없습니다.',
+      );
+      cy.get('#car-name-input').should('have.text', '');
+    });
+    typeAndSubmit(emptyCarName).then(() => {
+      expect(alertStub.getCall(0)).to.be.calledWith(
+        '공백만으로는 이름을 구성할 수 없습니다.',
+      );
+      cy.get('#car-name-input').should('have.text', '');
+    });
+  });
+
   // it('네자리 이상의 숫자가 입력됐을 경우 경고메세지가 뜬다.', () => {
-  //   const stub = cy.stub();
+  const stub = cy.stub();
 
-  //   cy.on('window:alert', stub);
+  cy.on('window:alert', stub);
 
-  //   cy.get('#total').then(() => {
-  //     cy.get('.digits')
-  //       .contains('1')
-  //       .click()
-  //       .then(() => {
-  //         expect(stub.getCall(0)).to.be.calledWith(
-  //           '숫자는 세 자리까지만 입력 가능합니다!',
-  //         );
-  //       });
-  //     cy.get('#total').should('have.text', '111/111');
-  //   });
+  cy.get('#total').then(() => {
+    cy.get('.digits')
+      .contains('1')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(
+          '숫자는 세 자리까지만 입력 가능합니다!',
+        );
+      });
+    cy.get('#total').should('have.text', '111/111');
+  });
   // });
 });
+const typeAndSubmit = (carNames) => {
+  cy.get('#car-name-input').type(carNames.join(','));
+  cy.get('#car-name-submit').click();
+};
