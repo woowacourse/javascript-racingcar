a88ec8231edd26693bf6429b0e4cddc7fc18b6d1
yujo <yujo@student.42seoul.kr>
2021-02-07 17:46:26 +0900

    test: getRandomNumber 함수가 정상적으로 랜덤값을 생성하는지 테스트 하는 코드 작성

--- cypress/integration/racingGame.spec.js
+++ cypress/integration/racingGame.spec.js
@@ -1,7 +1,21 @@
+const { should } = require('chai');
+
 describe('racing-game', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/');
   });
+
+  const typeCarNameAndSubmit = (
+    carNames = ['EAST', 'WEST', 'SOUTH', 'NORTH'],
+  ) => {
+    cy.get('#car-name-input').type(carNames.join(','));
+    cy.get('#car-name-submit').click();
+  };
+
+  const typeRacingCountAndSubmit = (racingCount = 4) => {
+    cy.get('#racing-count-input').type(racingCount);
+    cy.get('#racing-count-submit').click();
+  };
 
   it('"EAST, WEST, SOUTH, NORTH"를 입력하면 화면에 시도횟수 입력창을 표시하는지 테스트 한다.', () => {
     typeCarNameAndSubmit();
@@ -34,49 +48,50 @@ describe('racing-game', () => {
       cy.get('#car-name-input').should('have.text', '');
     });
   });
-});
 
-it('양의 정수만을 시도횟수로 입력할 수 있다.', () => {
-  const negativeRacingCount = -7;
-  const emptyRacingCount = '';
-  const alertStub = cy.stub();
+  it('양의 정수만을 시도횟수로 입력할 수 있다.', () => {
+    const negativeRacingCount = -7;
+    const emptyRacingCount = '';
+    const alertStub = cy.stub();
 
-  cy.on('window:alert', alertStub);
+    cy.on('window:alert', alertStub);
+    typeCarNameAndSubmit();
+    typeRacingCountAndSubmit(negativeRacingCount).then(() => {
+      expect(alertStub.getCall(0)).to.be.calledWith(
+        '1 이상의 숫자를 입력해주세요.',
+      );
+      cy.get('#racing-count-input').should('have.text', '');
+    });
 
-  typeRacingCountAndSubmit(negativeRacingCount).then(() => {
-    expect(alertStub.getCall(0)).to.be.calledWith(
-      '1 이상의 숫자를 입력해주세요.',
-    );
-    cy.get('#racing-count-input').should('have.text', '');
+    typeRacingCountAndSubmit(emptyRacingCount).then(() => {
+      expect(alertStub.getCall(0)).to.be.calledWith(
+        '1 이상의 숫자를 입력해주세요.',
+      );
+      cy.get('#racing-count-input').should('have.text', '');
+    });
   });
 
-  typeRacingCountAndSubmit(emptyRacingCount).then(() => {
-    expect(alertStub.getCall(0)).to.be.calledWith(
-      '1 이상의 숫자를 입력해주세요.',
-    );
-    cy.get('#racing-count-input').should('have.text', '');
+  it('시도횟수가 올바르게 입력된 경우 경주 화면이 보이는지 테스트 한다.', () => {
+    typeCarNameAndSubmit();
+    typeRacingCountAndSubmit();
+    cy.get('.car-player')
+      .should('have.length', carNames.length)
+      .each(($div, index, $lis) => {
+        return cy.get($div).should('have.text', carNames[index]);
+      });
   });
-});
 
-it('시도횟수가 올바르게 입력된 경우 경주 화면이 보이는지 테스트 한다.', () => {
-  typeCarNameAndSubmit();
-  typeRacingCountAndSubmit();
+  it('자동차 경주가 정상적으로 진행되는지 테스트 한다.', () => {
+    const possibleScores = Array.from({ length: 10 }).map((v, i) => i + 1);
 
-  cy.get('.car-player')
-    .should('have.length', carNames.length)
-    .each(($div, index, $lis) => {
+    for (let i = 0; i < 10; i++) {
+      expect(possibleScores).to.include(getRandomNumber());
+    }
+
+    typeCarNameAndSubmit();
+    typeRacingCountAndSubmit();
+    cy.get('.car-player').each(($div, index, $lis) => {
       return cy.get($div).should('have.text', carNames[index]);
     });
+  });
 });
-
-const typeCarNameAndSubmit = (
-  carNames = ['EAST', 'WEST', 'SOUTH', 'NORTH'],
-) => {
-  cy.get('#car-name-input').type(carNames.join(','));
-  cy.get('#car-name-submit').click();
-};
-
-const typeRacingCountAndSubmit = (racingCount = 4) => {
-  cy.get('#racing-count-input').type(racingCount);
-  cy.get('#racing-count-submit').click();
-};
