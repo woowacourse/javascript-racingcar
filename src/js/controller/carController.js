837dfaa93a0eb5b9077b946bea1523834dfb0341
zereight <fullstackmachinelearning@gmail.com>
2021-02-08 21:49:16 +0900

    docs : README.md 수정

--- src/js/controller/carController.js
+++ src/js/controller/carController.js
@@ -1,15 +1,22 @@
 // 자동차와 관련된 검증, 조작 및 뷰?
 
 // 자동차 이름 검증 함수
+
+// 공백 이름 검증 함수
+function isAlphanumeric(input) {
+  const regex = /^[a-zA-Z0-9]+S/;
+}
+
 // 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
 const isValidCarNames = function(carNamesInput) {
   return carNamesInput.every(carName => carName.length <= 5);
 }
 
+
 export const handleCarNamesSubmit = function() {
   const carNamesInput = document.querySelector("#car-names-input").value.split(",");
   // 자동차 이름 검증
-  if (!isValidCarNames(carNamesInput)) {
+  if (!isValidCarNames(carNamesInput) && 공백체크) {
     alert('유효한 자동차이름이 아닙니다.');
     return;
   }
