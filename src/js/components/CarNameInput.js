800fe88b8ee5a2e910f6f56477213ca2b23dc2f5
2sooy <1401660@naver.com>
2021-02-08 20:59:39 +0900

    feat: 자동차 이름을 하나만 입력한 경우 처리

--- src/js/components/CarNameInput.js
+++ src/js/components/CarNameInput.js
@@ -1,11 +1,11 @@
-import { ERROR_MESSAGE } from '../util/errorMessage.js';
-import { CAR_NAME_MAX_LENGTH } from '../util/constant.js';
+import { ERROR_MESSAGE } from "../util/errorMessage.js";
+import { CAR_NAME_MAX_LENGTH } from "../util/constant.js";
 
 export default class CarNameInput {
   constructor({ setCars }) {
-    this.$target = document.querySelector('.car-name-input-containter');
-    this.$carNameInput = this.$target.querySelector('input[type=text]');
-    this.$carNameSummitBtn = this.$target.querySelector('button');
+    this.$target = document.querySelector(".car-name-input-containter");
+    this.$carNameInput = this.$target.querySelector("input[type=text]");
+    this.$carNameSummitBtn = this.$target.querySelector("button");
     this.carNames = [];
     this.setCars = setCars;
 
@@ -13,19 +13,32 @@ export default class CarNameInput {
   }
 
   bindEvents() {
-    this.$carNameSummitBtn.addEventListener('click', this.handleSubmitCarName.bind(this));
+    this.$carNameSummitBtn.addEventListener(
+      "click",
+      this.handleSubmitCarName.bind(this),
+    );
   }
 
   handleSubmitCarName() {
     const inputCarName = this.$carNameInput.value;
+    const carNames = inputCarName.split(",").map((name) => name.trim());
 
     if (this.isEmptyCarName(inputCarName)) {
       alert(ERROR_MESSAGE.EMPTY_CAR_NAME_INPUT);
       return;
     }
+
+    if (this.isOneCarName(carNames)) {
+      alert(ERROR_MESSAGE.ONE_CAR_NAME_INPUT);
+      return;
+    }
   }
 
   isEmptyCarName(inputCarName) {
-    return inputCarName.trim() === '' ? true : false;
+    return inputCarName.trim() === "";
+  }
+
+  isOneCarName(carNames) {
+    return carNames.length < 2;
   }
 }
