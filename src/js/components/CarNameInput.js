4ddb8a1e5db5ca6c0a6a2ce5fe61dd42cc839058
SunnyK <sunnyk5780@gmail.com>
2021-02-08 21:11:37 +0900

    feat: 입력된 자동차 이름 중 빈 문자열이 있는 경우 처리

--- src/js/components/CarNameInput.js
+++ src/js/components/CarNameInput.js
@@ -1,11 +1,11 @@
-import { ERROR_MESSAGE } from "../util/errorMessage.js";
-import { CAR_NAME_MAX_LENGTH } from "../util/constant.js";
+import { ERROR_MESSAGE } from '../util/errorMessage.js';
+import { CAR_NAME_MAX_LENGTH } from '../util/constant.js';
 
 export default class CarNameInput {
   constructor({ setCars }) {
-    this.$target = document.querySelector(".car-name-input-containter");
-    this.$carNameInput = this.$target.querySelector("input[type=text]");
-    this.$carNameSummitBtn = this.$target.querySelector("button");
+    this.$target = document.querySelector('.car-name-input-containter');
+    this.$carNameInput = this.$target.querySelector('input[type=text]');
+    this.$carNameSummitBtn = this.$target.querySelector('button');
     this.carNames = [];
     this.setCars = setCars;
 
@@ -13,17 +13,14 @@ export default class CarNameInput {
   }
 
   bindEvents() {
-    this.$carNameSummitBtn.addEventListener(
-      "click",
-      this.handleSubmitCarName.bind(this),
-    );
+    this.$carNameSummitBtn.addEventListener('click', this.handleSubmitCarName.bind(this));
   }
 
   handleSubmitCarName() {
     const inputCarName = this.$carNameInput.value;
-    const carNames = inputCarName.split(",").map((name) => name.trim());
+    const carNames = inputCarName.split(',').map((name) => name.trim());
 
-    if (this.isEmptyCarName(inputCarName)) {
+    if (this.isEmptyCarName(inputCarName.trim())) {
       alert(ERROR_MESSAGE.EMPTY_CAR_NAME_INPUT);
       return;
     }
@@ -32,13 +29,22 @@ export default class CarNameInput {
       alert(ERROR_MESSAGE.ONE_CAR_NAME_INPUT);
       return;
     }
+
+    if (this.isContainEmptyString(carNames)) {
+      alert(ERROR_MESSAGE.EMPTY_STRING_CAR_NAME_INPUT);
+      return;
+    }
   }
 
   isEmptyCarName(inputCarName) {
-    return inputCarName.trim() === "";
+    return inputCarName === '';
   }
 
   isOneCarName(carNames) {
     return carNames.length < 2;
+  }
+
+  isContainEmptyString(carNames) {
+    return carNames.some((carName) => this.isEmptyCarName(carName));
   }
 }
