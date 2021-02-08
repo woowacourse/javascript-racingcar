f7b920dd3599390341160ef272e056cd40410693
365kim <365listener@gmail.com>
2021-02-08 16:07:35 +0900

    feat: 다시시작 버튼 눌렀을 때 게임을 초기화하는 기능

--- src/js/game/printGameResult.js
+++ src/js/game/printGameResult.js
@@ -1,7 +1,19 @@
 import { getWinners } from '../utils/getWinners.js';
 
+const restartGame = () => {
+  const $racingCountSection = document.querySelector('#racing-count-section');
+  const $gameProcessSection = document.querySelector('#game-process-section');
+  const $gameResultSection = document.querySelector('#game-result-section');
+
+  $racingCountSection.setAttribute('hidden', true);
+  $gameProcessSection.setAttribute('hidden', true);
+  $gameResultSection.setAttribute('hidden', true);
+};
+
 export const printGameResult = () => {
   const $gameResultText = document.querySelector('#game-result-text');
+  const $gameRestartButton = document.querySelector('#game-restart-button');
 
   $gameResultText.innerHTML = `🏆 최종 우승자: ${getWinners()} 🏆`;
+  $gameRestartButton.addEventListener('click', restartGame);
 };
