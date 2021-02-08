326f50098046b4db84658149d9c6c5051e9b1c8d
zereight <fullstackmachinelearning@gmail.com>
2021-02-08 21:52:34 +0900

    feat : 자동차 이름 검증 기능 추가 (최소 1자이상)

--- src/js/controller/carController.js
+++ src/js/controller/carController.js
@@ -8,19 +8,21 @@ function isAlphanumeric(input) {
 }
 
 // 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
-const isValidCarNames = function(carNamesInput) {
-  return carNamesInput.every(carName => carName.length <= 5);
-}
+const isValidCarNames = function (carNamesInput) {
+  return carNamesInput.every(
+    (carName) => 1 <= carName.length && carName.length <= 5
+  );
+};
 
-
-export const handleCarNamesSubmit = function() {
-  const carNamesInput = document.querySelector("#car-names-input").value.split(",");
+export const handleCarNamesSubmit = function () {
+  const carNamesInput = document
+    .querySelector("#car-names-input")
+    .value.split(",");
   // 자동차 이름 검증
   if (!isValidCarNames(carNamesInput) && 공백체크) {
-    alert('유효한 자동차이름이 아닙니다.');
+    alert("유효한 자동차이름이 아닙니다.");
     return;
   }
-  
+
   // Car 생성
-  
 };
