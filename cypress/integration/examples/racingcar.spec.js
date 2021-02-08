b96b19c138bae48d4d750c1d3f3698aafb19c1cc
ChayanRin <k852012@naver.com>
2021-02-08 16:10:56 +0900

    test : 자동차 이름 입력 순서대로 자동차를 생성하는 테스트코드 구현

--- cypress/integration/examples/racingcar.spec.js
+++ cypress/integration/examples/racingcar.spec.js
@@ -1,14 +1,24 @@
-describe("ui-click-and-input", () => {
+describe("ui-input-click-show", () => {
   before(() => {
     cy.visit("http://localhost:5500/");
   });
 
-  it("자동차 섹션, 횟수 섹션, 진행과정 섹션이 순서대로 보여진다", () => {
+  it("자동차 섹션을 입력하고 버튼을 클릭하면 횟수 영역이 보여진다", () => {
     cy.get("#car-input").type("a,b,c,d");
     cy.get("#car-btn").click();
     cy.get("#count").should("have.css", "display", "block");
+  });
+
+  it("횟수를 입력하고 버튼을 클릭하면 진행 영역이 보여진다", () => {
     cy.get("#count-input").type(5);
     cy.get("#count-btn").click();
     cy.get("#process").should("have.css", "display", "block");
+  });
+
+  it("자동차 이름을 입력한 순서대로 자동차들을 생성한다", () => {
+    const cars = ["a", "b", "c", "d"];
+    cy.get(".car-player").each((v, i, arr) => {
+      cy.get(v).should("have.text", cars[i]);
+    });
   });
 });
