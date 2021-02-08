44fd06015402ef35e7c102335249c50e03f0493c
0imbean0 <tjsqls2003@gmail.com>
2021-02-08 21:35:46 +0900

    feat: HTML요소에 클래스 이름을 추가하고 선택자 상수를 정의한다

---
+++ src/js/util/constants.js
@@ -0,0 +1,27 @@
+const SELECTOR = {
+  TITLE: {
+    CONTAINER: ".title-container",
+  },
+
+  CARNAME: {
+    CONTAINER: ".carname-input-container",
+    INPUT: ".carname-input-container__input",
+    BUTTON: ".carname-input-container__button",
+  },
+
+  LAP: {
+    CONTAINER: ".lap-input-container",
+    INPUT: ".lap-input-container__input",
+    BUTTON: ".lap-input-container__button",
+  },
+
+  GAME_PROGRESS: {
+    CONTAINER: ".game-progress-container",
+  },
+
+  GAME_RESULT: {
+    CONTAINER: ".game-result-container",
+  },
+};
+
+export { SELECTOR };
