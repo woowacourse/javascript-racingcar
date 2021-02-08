89894b600f4b3662e756d966f71414eeda29fbd5
zereight <fullstackmachinelearning@gmail.com>
2021-02-08 22:17:56 +0900

    feat : 자동차 이름 검증 기능 추가 (중복 이름 체크)

--- src/js/controller/carController.js
+++ src/js/controller/carController.js
@@ -3,13 +3,18 @@
 // 자동차 이름 검증 함수
 
 // 공백 이름 검증 함수
+// 리팩토링 할 때 수정 (함수 표현 방식)
 function isAlphanumeric(input) {
   return /^[a-zA-Z0-9]+$/.test(input);
 }
 
+function isNotDuplicatedArray(array){
+  return Array.from(new Set(array)).length === array.length;
+}
+
 // 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
-const isValidCarNames = function (carNamesInput) {
-  return carNamesInput.every(
+const isValidCarNames = function(carNamesInput) {
+  return isNotDuplicatedArray(carNamesInput) && carNamesInput.every(
     (carName) =>
       1 <= carName.length && carName.length <= 5 && isAlphanumeric(carName)
   );
