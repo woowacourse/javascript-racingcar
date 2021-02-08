bf35d36f6a062b01c7c2a0d3b79739cd014b20a6
sunhpark42 <sunhpark42@gmail.com>
2021-02-08 17:59:55 +0900

    Test : display try count form after input car names and click button

--- cypress/integration/PlayRacingCarTest.spec.js
+++ cypress/integration/PlayRacingCarTest.spec.js
@@ -9,4 +9,12 @@ describe('자동차 경주 게임 테스트', () => {
     cy.get('#display-game-progress').should('not.be.visbile');
     cy.get('#display-game-result').should('not.be.visbile');
   });
+
+  it('자동차 이름을 입력하고, 확인버튼을 누르면, 시도 횟수 입력폼이 나타난다.', () => {
+    cy.get('#input-car-names > div > input').type('EAST, WEST, SOUTH, NORTH');
+    cy.get('#input-car-names > div > button').click();
+    cy.get('#input-try-count').should('be.visible');
+    cy.get('#display-game-progress').should('not.be.visbile');
+    cy.get('#display-game-result').should('not.be.visbile');
+  })
 });
