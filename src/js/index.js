a8cb09534fbc5fdcd5b8c2f81705cc922c577698
zigsong <wldms5764@gmail.com>
2021-02-08 18:17:13 +0900

    feat: 자동차 이름 5자 이하 예외 처리

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,38 @@
 export default function RacingCar() {
-    const carNames = [];
+  const carNames = [];
 
-    const resetUI = () => {
-        document.querySelector('.try-count-form').style.display = 'none';
-        document.querySelector('.progress-container').style.display = 'none';
-        document.querySelector('.result-container').style.display = 'none';
+  const resetUI = () => {
+    document.querySelector('.try-count-form').style.display = 'none';
+    document.querySelector('.progress-container').style.display = 'none';
+    document.querySelector('.result-container').style.display = 'none';
+  };
+
+  const getCarNames = () => {
+    const carNameInput = document.querySelector('.car-name').value;
+
+    for (let name of carNameInput.split(',')) {
+      if (name.trim().length > 5) {
+        alert('자동차 이름을 5자 이하로 입력해 주세요.');
+        return;
+      }
+      carNames.push(name.trim());
     }
 
-    const getCarNames = () => {
-        const carNameInput = document.querySelector('.car-name').value;
-        carNameInput.split(',').forEach(name => {
-            carNames.push(name.trim());
-        });
-        console.log(carNames);
-        document.querySelector('.try-count-form').style.display = 'block';
-    }
+    document.querySelector('.try-count-form').style.display = 'block';
+  };
 
-    const addListeners = () => {
-        document.querySelector('.car-name-btn').addEventListener('click', getCarNames);
-    }
+  const addListeners = () => {
+    document
+      .querySelector('.car-name-btn')
+      .addEventListener('click', getCarNames);
+  };
 
-    const run = () => {
-        addListeners();
-        resetUI();
-    };
+  const run = () => {
+    addListeners();
+    resetUI();
+  };
 
-    run();
+  run();
 }
 
 new RacingCar();
