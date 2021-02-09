d94147d1e77d5de8b8c56f8994319eae92476f1a
bigsaigon333 <likepeppermint@gmail.com>
2021-02-09 18:54:57 +0900

    test: 시도할 횟수 입력하기 중 시도할 횟수가 1이상 20이하의 정수인지 테스트한다

--- cypress/integration/inputLapCount.spec.js
+++ cypress/integration/inputLapCount.spec.js
@@ -23,11 +23,21 @@ describe("시도할 횟수 입력하기", () => {
     );
   });
 
-      cy.get("@windowAlert").should(
-        "be.calledWith",
-        MESSAGE.LAP_COUNT.NOT_A_NUMBER
-      );
-      cy.get(SELECTOR.LAP_COUNT.INPUT).should("have.text", "");
-    });
+  it("시도할 횟수는 1이상이다.", () => {
+    ["-1", "0", "-5.6"].forEach((userInput) =>
+      testFailCase(userInput, MESSAGE.LAP_COUNT.OUT_OF_RANGE)
+    );
+  });
+
+  it("시도할 횟수는 20이하이다.", () => {
+    ["21", "369", "1e3"].forEach((userInput) =>
+      testFailCase(userInput, MESSAGE.LAP_COUNT.OUT_OF_RANGE)
+    );
+  });
+
+  it("시도할 횟수는 정수이다.", () => {
+    ["1.342", "-2.43", "0.111111"].forEach((userInput) =>
+      testFailCase(userInput, MESSAGE.LAP_COUNT.OUT_OF_RANGE)
+    );
   });
 });
