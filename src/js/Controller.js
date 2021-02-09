31d4a07cbb37e5a8919c42d02b652e050118de14
0imbean0 <tjsqls2003@gmail.com>
2021-02-09 15:49:12 +0900

    feat: 시도할 횟수를 받아오는 기능과 숫자가 아닌경우 검증 기능을 구현한다

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -27,10 +27,30 @@ export class Controller {
         return;
       }
 
+      // TODO: 동일한 이름이 있었을 때 alert하기
       this.carModels = splittedCarNames.map((carName) => new CarModel(carName));
 
+      // TODO: input하면 중복 생성되지 않게하기
       this.viewController.renderCarNameTag(this.carModels);
-      this.viewController.show(SELECTOR.LAP.CONTAINER);
+      this.viewController.show(SELECTOR.LAP_COUNT.CONTAINER);
+    });
+
+    const lapCountInput = document.querySelector(SELECTOR.LAP_COUNT.INPUT);
+    const lapCountButton = document.querySelector(SELECTOR.LAP_COUNT.BUTTON);
+
+    lapCountButton.addEventListener("click", () => {
+      const userInput = lapCountInput.value;
+
+      if (this.carModels.length === 0) {
+        alert(MESSAGE.COMMON.INVALID_ACCESS);
+        return;
+      }
+
+      if (userInput === "") {
+        alert(MESSAGE.LAP_COUNT.NOT_A_NUMBER);
+        lapCountInput.value = "";
+        return;
+      }
     });
   }
 }
