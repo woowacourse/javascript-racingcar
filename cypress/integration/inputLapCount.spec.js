fe4af57933b1274a97b8f3e6f0969425586fe687
bigsaigon333 <likepeppermint@gmail.com>
2021-02-09 18:54:02 +0900

    refactor: 시도할 횟수 입력하기 중 숫자가 아닌 경우에 대하여 리팩토링한다
    중복되는 부분에 대하여 별도의 함수로 분리한다

--- cypress/integration/inputLapCount.spec.js
+++ cypress/integration/inputLapCount.spec.js
@@ -8,11 +8,20 @@ describe("시도할 횟수 입력하기", () => {
     cy.get(SELECTOR.CAR_NAME.BUTTON).click();
   });
 
+  const testFailCase = (userInput, errorMessage) => {
+    cy.get(SELECTOR.LAP_COUNT.INPUT).type(userInput);
+    cy.get(SELECTOR.LAP_COUNT.BUTTON).click();
+
+    cy.get("@windowAlert").should("be.calledWith", errorMessage);
+    cy.get(SELECTOR.LAP_COUNT.INPUT).should("have.text", "");
+  };
+
   // TODO: "10+10"  사용자가 입력하는 것과 cypress type이 상이한 것으로 보여짐. 검토 필요
   it("시도할 횟수는 숫자이다.", () => {
-    [" ", "+-", "ㄱ", "10+10"].forEach((userInput) => {
-      cy.get(SELECTOR.LAP_COUNT.INPUT).type(userInput);
-      cy.get(SELECTOR.LAP_COUNT.BUTTON).click();
+    [" ", "+-", "ㄱ", "10+10"].forEach((userInput) =>
+      testFailCase(userInput, MESSAGE.LAP_COUNT.NOT_A_NUMBER)
+    );
+  });
 
       cy.get("@windowAlert").should(
         "be.calledWith",
