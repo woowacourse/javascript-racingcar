b75fd7a58b75db07be5aee22950107260ec8561c
2sooy <1401660@naver.com>
2021-02-09 22:54:11 +0900

    feat: 레이싱 결과 화면의 자동차 이름 렌더링

--- src/js/components/RacingResult.js
+++ src/js/components/RacingResult.js
@@ -7,9 +7,16 @@ export default class RacingResult {
   }
 
   init() {
-    const $container = document.createElement('div');
-    const containerClassList = ['d-flex', 'justify-center', 'mt-5', 'racing-result-container'];
-    containerClassList.forEach((className) => $container.classList.add(className));
+    const $container = document.createElement("div");
+    const containerClassList = [
+      "d-flex",
+      "justify-center",
+      "mt-5",
+      "racing-result-container",
+    ];
+    containerClassList.forEach((className) =>
+      $container.classList.add(className),
+    );
 
     this.$container = $container;
     this.$parent.appendChild(this.$container);
@@ -20,7 +27,25 @@ export default class RacingResult {
     this.render();
   }
 
+  createCarHTML(car) {
+    return `
+    <div>
+      <div class="car-player mr-2">${car.name}</div>
+    </div>
+    `;
+  }
+
+  createRacingResultHTML() {
+    return `        
+    <section class="mt-4">
+      <div class="d-flex">
+        ${this.cars.map(this.createCarHTML).join("")}
+      </div>
+    </section>`;
+  }
   render() {
-    this.$container.innerHTML = this.cars.length ? '결과' : '';
+    this.$container.innerHTML = this.cars.length
+      ? this.createRacingResultHTML()
+      : "";
   }
 }
