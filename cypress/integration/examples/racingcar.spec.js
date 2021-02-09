2bb1f028c5c554cd6b5a874e2a2e7a095a0f0b7d
ChayanRin <k852012@naver.com>
2021-02-09 14:10:24 +0900

    test : window:alert 리팩토링

--- cypress/integration/examples/racingcar.spec.js
+++ cypress/integration/examples/racingcar.spec.js
@@ -35,7 +35,6 @@ describe("ui-input-click-show", () => {
     cy.get("#process > .d-flex > div")
       .each(v => {
         if (v.find(".forward-icon").length > largestCount) {
-          console.log(v.find(".forward-icon").length);
           largestCount = v.find(".forward-icon").length;
         }
       })
@@ -62,67 +61,52 @@ describe("ui-input-click-show", () => {
 describe("ui-input-vaild-check", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5500/");
+    cy.window()
+      .then(win => cy.stub(win, "alert"))
+      .as("alertStub");
   });
 
   it("자동차 이름이 5글자 초과하면 alert 출력", () => {
-    const alertStub = cy.stub();
-    cy.on("window:alert", alertStub);
-
     cy.get("#car-input").type("overFive,a,b,c,d");
-    cy.get("#car-btn")
-      .click()
-      .then(() => {
-        expect(alertStub.getCall(0)).to.be.calledWith(
-          "자동차 이름의 길이는 최대 5글자 입니다."
-        );
-      });
+    cy.get("#car-btn").click();
+    cy.get("@alertStub").should(
+      "be.calledWith",
+      "자동차 이름의 길이는 최대 5글자 입니다."
+    );
   });
 
   it("자동차 이름에 공백 있으면 alert 출력", () => {
-    const alertStub = cy.stub();
-    cy.on("window:alert", alertStub);
-
     cy.get("#car-input").type("a,b,,c,d");
-    cy.get("#car-btn")
-      .click()
-      .then(() => {
-        expect(alertStub.getCall(0)).to.be.calledWith(
-          "자동차 이름은 공백이 될 수 없습니다."
-        );
-      });
+    cy.get("#car-btn").click();
+    cy.get("@alertStub").should(
+      "be.calledWith",
+      "자동차 이름은 공백이 될 수 없습니다."
+    );
   });
 
   it("입력한 시도 횟수가 숫자가 아니면 alert 출력", () => {
-    const alertStub = cy.stub();
-    cy.on("window:alert", alertStub);
-
     cy.get("#car-input").type("a,b,c,d");
     cy.get("#car-btn").click();
     cy.get("#count").should("have.css", "display", "block");
     cy.get("#count-input").type("string");
-    cy.get("#count-btn")
-      .click()
-      .then(() => {
-        expect(alertStub.getCall(0)).to.be.calledWith(
-          "시도 횟수는 숫자여야 합니다."
-        );
-      });
+    cy.get("#count-btn").click();
+    cy.get("@alertStub").should(
+      "be.calledWith",
+      "시도 횟수는 숫자여야 합니다."
+    );
   });
 
   it("입력한 시도 횟수가 0 이하면 alert 출력", () => {
-    const alertStub = cy.stub();
-    cy.on("window:alert", alertStub);
-
     cy.get("#car-input").type("a,b,c,d");
     cy.get("#car-btn").click();
     cy.get("#count").should("have.css", "display", "block");
     cy.get("#count-input").type(-1);
-    cy.get("#count-btn")
-      .click()
-      .then(() => {
-        expect(alertStub.getCall(0)).to.be.calledWith(
-          "시도 횟수는 자연수여야 합니다."
-        );
-      });
+    cy.get("#count-btn").click();
+    cy.get("@alertStub").should(
+      "be.calledWith",
+      "시도 횟수는 자연수여야 합니다."
+    );
   });
 });
+
+// TDD 방식으로 random 함수 테스트하기
