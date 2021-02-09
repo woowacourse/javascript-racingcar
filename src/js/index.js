700f0909e84ac4fe488d9197bfb2f870fdfa11ee
HyuuunjuKim <gus7wn@gmail.com>
2021-02-10 00:41:04 +0900

    :heavy_plus_sign: index.js에 주석 추가

--- src/js/index.js
+++ src/js/index.js
@@ -13,20 +13,17 @@ import {
   isTryNumNotNumber,
 } from "./validate-input.js";
 
+// section마다 고유한 id가 없어서, 각 section에 쉽게 접근하기 위해 sections Array 생성
 export const sections = document.getElementsByTagName("section");
+// 자동차 인스턴스 배열을 저장하는 state
 export const state = {
   cars: [],
 };
 
 export const parseHTML = (html) => {
   const parser = new DOMParser();
-  return parser.parseFromString(html, "text/html").body.firstElementChild;
-};
 
-export const setSectionDataID = () => {
-  for (let idx = 0; idx < sections.length; idx++) {
-    sections[idx].dataset.id = `${idx}`;
-  }
+  return parser.parseFromString(html, "text/html").body.firstElementChild;
 };
 
 const getCarInstance = (carNames) => {
@@ -37,6 +34,7 @@ const getCarInstance = (carNames) => {
 
 const onClickedCarNamesBtn = () => {
   const carNamesBtn = document.getElementsByTagName("button")[0];
+
   carNamesBtn.addEventListener("click", () => {
     const carNamesInput = document.getElementsByTagName("input")[0];
     const carNames = carNamesInput.value.split(",").map((carName) => {
@@ -57,11 +55,12 @@ const onClickedTryNumBtn = () => {
   const tryNumBtn = document.getElementsByTagName("button")[1];
   tryNumBtn.addEventListener("click", () => {
     const tryNum = document.getElementsByTagName("input")[1].value;
+
     if (isTryNumInvalid(tryNum) || isTryNumNotNumber(tryNum)) {
       return;
     }
     playGame();
-
+    // game이 잘 진행되었다면 game결과를 보여준다.
     setResultView();
     setWinnerView(getWinner());
     showElement(sections[3]);
@@ -71,7 +70,6 @@ const onClickedTryNumBtn = () => {
 };
 
 const init = () => {
-  setSectionDataID();
   resetView([2, 3, 4]);
   onClickedCarNamesBtn();
   onClickedTryNumBtn();
