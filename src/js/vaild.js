414b2d5e17a57092f0105d4f27cc5e50f6d04c5e
ChayanRin <k852012@naver.com>
2021-02-10 14:51:08 +0900

    feat : Valid class생성 및 isCarValid 메소드 구현

--- src/js/vaild.js
+++ src/js/vaild.js
@@ -1,1 +1,17 @@
-// 입력값 유효성 체크
+class Valid {
+  isCarValid(carNames) {
+    if (carNames.find(carName => carName.length > 5)) {
+      return alert("자동차 이름의 길이는 최대 5글자 입니다.");
+    }
+    if (carNames.find(carName => carName.length === 0)) {
+      return alert("자동차 이름은 공백이 될 수 없습니다.");
+    }
+    if (carNames.length !== new Set(carNames).size) {
+      return alert("자동차 이름은 중복이 될 수 없습니다.");
+    }
+
+    return true;
+  }
+
+  isCountValid() {}
+}
