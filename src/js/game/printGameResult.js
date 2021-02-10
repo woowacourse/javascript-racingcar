ec48997dcb3938a0cc8940c5ab4c084d567dc627
yujo <yujo@student.42seoul.kr>
2021-02-10 15:45:03 +0900

    feat: 경주가 끝난 후 2초 후에 축하의 alert메세지를 띄우는 기능

--- src/js/game/printGameResult.js
+++ src/js/game/printGameResult.js
@@ -1,10 +1,17 @@
 import { getWinners } from './getWinners.js';
 import { restartGame } from './restartGame.js';
 
+const alertGameResult = (winners) => {
+  alert(`🎉 축하드립니다! 우승자는 ${winners}입니다! 🎉`);
+};
+
 export const printGameResult = () => {
   const $gameResultText = document.querySelector('#game-result-text');
   const $gameRestartButton = document.querySelector('#game-restart-button');
+  const winners = getWinners();
 
-  $gameResultText.innerHTML = `🏆 최종 우승자: ${getWinners()} 🏆`;
+  $gameResultText.innerHTML = `🏆 최종 우승자: ${winners} 🏆`;
   $gameRestartButton.addEventListener('click', restartGame);
+
+  setTimeout(() => alertGameResult(winners), 2000);
 };
