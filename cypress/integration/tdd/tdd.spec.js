d08cfc7dd468d626a34b9f2bba50ffedfd46d684
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-10 15:24:59 +0900

    :hammer_and_wrench: refactoring

--- cypress/integration/tdd/tdd.spec.js
+++ cypress/integration/tdd/tdd.spec.js
@@ -8,11 +8,7 @@ context("tdd", () => {
 	});
 
 	it("이름 입력 칸에 ','로 구분해서 이름을 입력할 수 있다.", () => {
-		Model.makeCarNames("a,b,c");
-		const expectedValues = ["a", "b", "c"];
-
-		for (let i = 0; i < array.length; i++) {
-			expect(Model.cars[i]).to.equal(expectedValues[i]);
-		}
+		Model.setCarNames("a,b,c");
+		expect(Model.cars).to.deep.equal(["a", "b", "c"]);
 	});
 });
