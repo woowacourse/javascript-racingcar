a54b1d5b189cbbcc5c10aa0781af391a70ecc2e9
ChayanRin <k852012@naver.com>
2021-02-10 15:02:14 +0900

    feat: isCountVaild 구현

--- src/js/vaild.js
+++ src/js/vaild.js
@@ -13,5 +13,19 @@ class Valid {
     return true;
   }
 
-  isCountValid() {}
+  isCountValid(count) {
+    if (isNaN(count)) {
+      return alert("시도 횟수는 자연수여야 합니다.");
+    }
+
+    if (parseInt(count, 10) <= 0) {
+      return alert("시도 횟수는 0보다 작거나 같을 수 없습니다.");
+    }
+
+    if (parseInt(count, 10) % 1 !== 0) {
+      return alert("시도 횟수는 소수가 될 수 없습니다.");
+    }
+
+    return true;
+  }
 }
