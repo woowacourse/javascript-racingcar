7509a5a2f6935e30a111da9d81c70ef16aee5feb
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-10 22:15:14 +0900

    :racehorse: connect onCountSubmit to count-submit-button

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -4,18 +4,25 @@ import View from "./View.js";
 
 // 구현하지 못한 기능: 이름 입력창에 이모지 입력을 제한하는 기능
 class Controller {
+	onCountSubmit() {
+		const countInput = ElementManager.getCountInput();
+		countInput.value = "";
+		console.log(countInput);
+	}
+
 	onNameSubmit() {
 		const nameInput = ElementManager.getNameInput();
 		Model.setCarNames(nameInput.value);
 		View.clearInputValue(nameInput);
 		View.countSectionRender();
+		Model.addCountButtonEvent(this.onCountSubmit);
 	}
 
 	initializeEvents() {
 		const nameButton = ElementManager.getNameButton();
 		const nameInput = ElementManager.getNameInput();
 
-		nameButton.addEventListener("click", this.onNameSubmit);
+		nameButton.addEventListener("click", this.onNameSubmit.bind(this));
 		nameInput.addEventListener("input", (event) => {
 			const RegExp = /[ 0-9\{\}\[\]\/?.;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
 
--- src/js/Model.js
+++ src/js/Model.js
@@ -1,3 +1,4 @@
+import ElementManager from "./ElementManager.js";
 class Model {
 	constructor() {
 		this.cars = [];
@@ -11,6 +12,12 @@ class Model {
 		const carNames = this.generateCarNameArray(nameInputValue);
 		carNames.forEach((carName) => this.cars.push(carName));
 		console.log({ cars: this.cars });
+	}
+
+	addCountButtonEvent(callback) {
+		const countButton = ElementManager.getCountButton();
+		countButton.addEventListener("click", callback);
+		console.log({ callback });
 	}
 }
 
