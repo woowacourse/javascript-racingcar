e44a0420fc80e9ac0cde940ea841ab409812d593
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-10 21:16:11 +0900

    :ballot_box_with_check: resolve test case: 이름 입력 칸은 알파벳, 한글, ‘,’만 입력 가능하다.

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -2,6 +2,7 @@ import Model from "./Model.js";
 import ElementManager from "./ElementManager.js";
 import View from "./View.js";
 
+// 구현하지 못한 기능: 이름 입력창에 이모지 입력을 제한하는 기능
 class Controller {
 	onNameSubmit() {
 		const nameInput = ElementManager.getNameInput();
@@ -12,7 +13,16 @@ class Controller {
 
 	initializeEvents() {
 		const nameButton = ElementManager.getNameButton();
+		const nameInput = ElementManager.getNameInput();
+
 		nameButton.addEventListener("click", this.onNameSubmit);
+		nameInput.addEventListener("input", (event) => {
+			const RegExp = /[ 0-9\{\}\[\]\/?.;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
+
+			if (RegExp.test(event.target.value) === true) {
+				event.target.value = event.target.value.substring(0, event.target.value.length - 1);
+			}
+		});
 	}
 }
 
