a3756157a09ee61540c4027c5ae9df6f42627481
bigsaigon333 <likepeppermint@gmail.com>
2021-02-10 11:22:39 +0900

    fix: window.alert 테스트시 마지막 호출의 메세지가 적용되도록 수정한다

--- cypress/integration/inputLapCount.spec.js
+++ cypress/integration/inputLapCount.spec.js
@@ -8,36 +8,56 @@ describe("시도할 횟수 입력하기", () => {
     cy.get(SELECTOR.CAR_NAME.BUTTON).click();
   });
 
-  const testFailCase = (userInput, errorMessage) => {
+  const testFailCaseOne = (userInput, { errorMessage, callCount = 1 }) => {
     cy.get(SELECTOR.LAP_COUNT.INPUT).type(userInput);
     cy.get(SELECTOR.LAP_COUNT.BUTTON).click();
 
-    cy.get("@windowAlert").should("be.calledWith", errorMessage);
+    cy.get("@windowAlert")
+      .should("have.callCount", callCount)
+      .its("lastCall")
+      .should("be.calledWith", errorMessage);
+
     cy.get(SELECTOR.LAP_COUNT.INPUT).should("have.text", "");
+  };
+
+  const testFailCaseArrayWithSameErrorMessage = (
+    userInputArray,
+    errorMessage
+  ) => {
+    userInputArray.forEach((userInput, index) =>
+      testFailCaseOne(userInput, {
+        errorMessage,
+        callCount: index + 1,
+      })
+    );
   };
 
   // TODO: "10+10"  사용자가 입력하는 것과 cypress type이 상이한 것으로 보여짐. 검토 필요
   it("시도할 횟수는 숫자이다.", () => {
-    [" ", "+-", "ㄱ", "10+10"].forEach((userInput) =>
-      testFailCase(userInput, MESSAGE.LAP_COUNT.NOT_A_NUMBER)
+    testFailCaseArrayWithSameErrorMessage(
+      [" ", "+-", "ㄱ", "10+10"],
+      MESSAGE.LAP_COUNT.NOT_A_NUMBER
     );
   });
 
   it("시도할 횟수는 1이상이다.", () => {
-    ["-1", "0", "-5.6"].forEach((userInput) =>
-      testFailCase(userInput, MESSAGE.LAP_COUNT.OUT_OF_RANGE)
+    testFailCaseArrayWithSameErrorMessage(
+      ["-1", "0", "-5.6"],
+      MESSAGE.LAP_COUNT.OUT_OF_RANGE
     );
   });
 
   it("시도할 횟수는 20이하이다.", () => {
-    ["21", "369", "1e3"].forEach((userInput) =>
-      testFailCase(userInput, MESSAGE.LAP_COUNT.OUT_OF_RANGE)
+    testFailCaseArrayWithSameErrorMessage(
+      ["21", "369", "1e3"],
+      MESSAGE.LAP_COUNT.OUT_OF_RANGE
     );
   });
 
   it("시도할 횟수는 정수이다.", () => {
-    ["1.342", "-2.43", "0.111111"].forEach((userInput) =>
-      testFailCase(userInput, MESSAGE.LAP_COUNT.OUT_OF_RANGE)
+    testFailCaseArrayWithSameErrorMessage(
+      ["1.342", "-2.43", "0.111111"],
+      MESSAGE.LAP_COUNT.OUT_OF_RANGE
     );
   });
 });
