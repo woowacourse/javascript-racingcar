90c610b16708fd82259624c16a0eb35eb27f0c09
zereight <fullstackmachinelearning@gmail.com>
2021-02-11 00:54:03 +0900

    refactor : 변수명 리팩토링

--- src/js/controller/inputController.js
+++ src/js/controller/inputController.js
@@ -1,60 +1,62 @@
 import {
-  displayCountView,
-  toggleCarNameInputDisable,
-  toggleCountInputDisable,
-} from "../view/inputView.js";
-import { app } from "../index.js";
-import { alertMsg, selectors } from "../keys.js";
-import { displayRacingCars } from "../view/racingView.js";
-import { startRacingGame } from "./racingController.js";
-import { isNotDuplicatedArray, isAlphanumeric } from "./utils.js";
+	displayCountView,
+	toggleCarNameInputDisable,
+	toggleCountInputDisable,
+} from '../view/inputView.js';
+import { app } from '../index.js';
+import { alertMsg, selectors } from '../keys.js';
+import { displayRacingCars } from '../view/racingView.js';
+import { startRacingGame } from './racingController.js';
+import { isNotDuplicatedArray, isAlphanumeric } from './utils.js';
 
 const isValidCarNames = function (carNamesInput) {
-  return (
-    isNotDuplicatedArray(carNamesInput) &&
-    carNamesInput.every(
-      (carName) =>
-        1 <= carName.length && carName.length <= 5 && isAlphanumeric(carName)
-    )
-  );
+	const [lengthLowerBound, lengthUpperBound] = [1, 5];
+	return (
+		isNotDuplicatedArray(carNamesInput) &&
+		carNamesInput.every(
+			(carName) =>
+				lengthLowerBound <= carName.length &&
+				carName.length <= lengthUpperBound &&
+				isAlphanumeric(carName),
+		)
+	);
 };
 
-
 const getCarNamesInput = function () {
-  return document
-  .querySelector(selectors.carNamesInput)
-  .value.split(",")
-  .map((e) => e.trim());
+	return document
+		.querySelector(selectors.carNamesInput)
+		.value.split(',')
+		.map((e) => e.trim());
 };
 
 const isValidCount = function (value) {
-  return 1<=Number(value) && Number(value)<=100;
+	return 1 <= Number(value) && Number(value) <= 100;
 };
 
 export const handleCarNamesSubmit = function () {
-  const carNamesInput = getCarNamesInput();
-  if (!isValidCarNames(carNamesInput)) {
-    alert(alertMsg.isNotValidCarNames);
-    return;
-  }
-  if (10 < carNamesInput.length) {
-    alert(alertMsg.isNotValidCarNamesLength);
-    return;
-  }
-  toggleCarNameInputDisable();
-  displayCountView();
+	const carNamesInput = getCarNamesInput();
+	if (!isValidCarNames(carNamesInput)) {
+		alert(alertMsg.isNotValidCarNames);
+		return;
+	}
+	if (10 < carNamesInput.length) {
+		alert(alertMsg.isNotValidCarNamesLength);
+		return;
+	}
+	toggleCarNameInputDisable();
+	displayCountView();
 };
 
 export const handleCountSubmit = function () {
-  const carNamesInput = getCarNamesInput();
-  const countInput = document.querySelector(selectors.countInput).value;
+	const carNamesInput = getCarNamesInput();
+	const countInput = document.querySelector(selectors.countInput).value;
 
-  if (!isValidCount(countInput)) {
-    alert(alertMsg.isNotValidCount);
-    return;
-  }
-  app.addCars(carNamesInput);
-  toggleCountInputDisable();
-  displayRacingCars(app.cars);
-  startRacingGame(countInput);
+	if (!isValidCount(countInput)) {
+		alert(alertMsg.isNotValidCount);
+		return;
+	}
+	app.addCars(carNamesInput);
+	toggleCountInputDisable();
+	displayRacingCars(app.cars);
+	startRacingGame(countInput);
 };
