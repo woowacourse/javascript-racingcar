6462c1ef56e4eb2f9878af7b32310cb75ee0a789
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-10 13:37:58 +0900

    :racehorse: add makeCarNames

--- src/js/Model.js
+++ src/js/Model.js
@@ -1,3 +1,19 @@
-class Model {}
+class Model {
+	constructor() {
+		this.cars = [];
+	}
+
+	generateCarNameArray(nameInputValue) {
+		return nameInputValue.split(",");
+	}
+
+	makeCarNames(nameInputValue) {
+		const carNames = this.generateCarNameArray(nameInputValue);
+
+		for (let i = 0; i < carNames.length; i++) {
+			this.cars.push(carNames[i]);
+		}
+	}
+}
 
 export default new Model();
