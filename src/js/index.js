4fa8352532a9e86df98cf482c859e9eb7e7286a8
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-10 13:42:14 +0900

    :racehorse: connect onNameSubmit to name-submit-button

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import View from "./View.js";
+import Controller from "./Controller.js";
 
 const $app = document.getElementById("app");
 
 View.initialRender($app);
+
+document.getElementById("name-submit-button").addEventListener("click", Controller.onNameSubmit);
