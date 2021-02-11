30d2bb1b653ae7dfe7eb253f1cd938a36d8ed896
dawit jeong <wjdekdnlt1215@gmail.com>
2021-02-11 12:22:12 +0900

    :fire: remove beforeEach

--- cypress/integration/tdd/tdd.spec.js
+++ cypress/integration/tdd/tdd.spec.js
@@ -3,10 +3,6 @@ import Model from "../../../src/js/Model.js";
 /// <reference types="cypress" />
 
 context("tdd", () => {
-	beforeEach(() => {
-		cy.visit("http://localhost:5500/");
-	});
-
 	it("이름 입력 칸에 ','로 구분해서 이름을 입력할 수 있다.", () => {
 		Model.setCarNames("a,b,c");
 		expect(Model.cars).to.deep.equal(["a", "b", "c"]);
