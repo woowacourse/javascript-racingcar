0239fdd768b003950bda2bac88a43a2557f0d4bb
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-02-11 14:31:03 +0900

    Refactor : add comments about numbers

--- src/js/Utils/constants.js
+++ src/js/Utils/constants.js
@@ -11,8 +11,11 @@ export const TRY_COUNT_ERROR_MESSAGE = {
 };
 
 export const NUMBERS = {
+  // 게임의 전진하는 조건은 0에서 9 사이에서 random 값을 구한다.
   RANDOM_RANGE: 9,
+  // random 값이 4 이상일 경우 전진하고, 3 이하의 값이면 멈춘다.
   RUN_POINT: 4,
+  // 자동차 이름의 최대 글자는 5글자이다.
   MIN_NAME_LENGTH: 1,
   MAX_NAME_LENGTH: 5,
 };
