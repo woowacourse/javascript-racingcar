f598790c81b7f0607350aa5a4678339531b5c9d1
HyuuunjuKim <gus7wn@gmail.com>
2021-02-11 15:31:20 +0900

    :sparkles: section element 모음

---
+++ src/js/elements.js
@@ -0,0 +1,4 @@
+export const carNamesSection = document.getElementById("car-names");
+export const tryNumSection = document.getElementById("try-num");
+export const resultSection = document.getElementById("result");
+export const winnerSection = document.getElementById("winners");
