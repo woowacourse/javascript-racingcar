fba89120818c5ebe3464a3b2a4a035458b90e9ce
yujo <yujo@student.42seoul.kr>
2021-02-11 16:17:26 +0900

    refactor: Error Message 변수명 직관적이게 변경

--- src/js/utils/constant.js
+++ src/js/utils/constant.js
@@ -9,6 +9,6 @@ export const GAME = {
 
 export const ERR_MESSAGE = {
   NAME_TOO_LONG: `이름은 ${GAME.MAX_NAME_LENGTH}글자 이하로 입력해 주세요.`,
-  NAME_BLANK: '공백만으로는 이름을 구성할 수 없습니다.',
+  NAME_CANNOT_BE_BLANK: '공백만으로는 이름을 구성할 수 없습니다.',
   COUNT_TOO_SMALL: `${GAME.MIN_COUNT} 이상의 숫자를 입력해주세요.`,
 };
