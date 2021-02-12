b29d544d9b336cf2047bbf0ef85ba8f506fb6fc2
dawit jeong <wjdekdnlt1215@gmail.com>
2021-02-12 22:14:30 +0900

    :bug: add {force : true} to prevent detached element error at cy.type()

--- cypress/integration/bdd/bdd.spec.js
+++ cypress/integration/bdd/bdd.spec.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+/// <reference types="cypress" ,{force:true}/>
 
 context("bdd", () => {
 	beforeEach(() => {
@@ -17,43 +17,43 @@ context("bdd", () => {
 	});
 
 	it("이름 입력 칸에 이름을 입력하고 확인 버튼을 누르면 칸이 비워지고, 횟수 입력 칸이 나온다.", () => {
-		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH");
+		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH", { force: true });
 		cy.get("#name-submit-button").click();
 		cy.get("#count-input").should("exist");
 		cy.get("#count-submit-button").should("exist");
 	});
 
 	it("이름 입력 칸은 알파벳, 한글, ‘,’만 입력 가능하다.", () => {
-		cy.get("#name-input").type("EA45ST3?S가!능");
+		cy.get("#name-input").type("EA45ST3?S가!능", { force: true });
 		cy.get("#name-input").should("have.value", "EASTS가능");
 		cy.reload();
-		cy.get("#name-input").type("1234자동차");
+		cy.get("#name-input").type("1234자동차", { force: true });
 		cy.get("#name-input").should("have.value", "자동차");
 		cy.reload();
-		cy.get("#name-input").type("!@#1자동#$,123차");
+		cy.get("#name-input").type("!@#1자동#$,123차", { force: true });
 		cy.get("#name-input").should("have.value", "자동,차");
 	});
 
 	it("횟수 입력 칸에 placeholder('시도 횟수')가 있다.", () => {
-		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH");
+		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH", { force: true });
 		cy.get("#name-submit-button").click();
 		cy.get("#count-input").should("have.attr", "placeholder", "시도 횟수");
 	});
 
 	it("횟수 입력 칸에 횟수를 입력하고 확인 버튼을 누르면 칸이 비워진다.", () => {
-		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH");
+		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH", { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 		cy.get("#count-input").should("have.value", "");
 	});
 
 	it("횟수 입력 칸에 횟수를 입력하고 확인 버튼을 누르면 진행창/결과창/리셋 버튼이 나온다.", () => {
-		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH");
+		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH", { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get("#race-progress-container").should("exist");
@@ -62,7 +62,7 @@ context("bdd", () => {
 	});
 
 	it("이름 확인 버튼을 연달아 눌러도 횟수 입력 칸과 버튼은 한 번만 나온다.", () => {
-		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH");
+		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH", { force: true });
 		cy.get("#name-submit-button").click();
 
 		cy.get("#setting-container")
@@ -71,7 +71,7 @@ context("bdd", () => {
 				expect($children.length).to.eq(3);
 			});
 
-		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH");
+		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH", { force: true });
 		cy.get("#name-submit-button").click();
 
 		cy.get("#setting-container")
@@ -82,10 +82,10 @@ context("bdd", () => {
 	});
 
 	it("횟수 확인 버튼을 연달아 눌러도 진행창/결과창/리셋 버튼은 한 번만 나온다.", () => {
-		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH");
+		cy.get("#name-input").type("EAST, WEST, SOUTH, NORTH", { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get("#app")
@@ -94,11 +94,11 @@ context("bdd", () => {
 				expect($children.length).to.eq(3);
 			});
 
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get("#app")
@@ -106,13 +106,15 @@ context("bdd", () => {
 			.should(($children) => {
 				expect($children.length).to.eq(3);
 			});
+		cy.wait(10);
+		cy.reload();
 	});
 
 	it("입력한 이름이 ‘,’를 기준으로 진행창에 출력된다.", () => {
-		cy.get("#name-input").type("EAST,WEST,SOUTH");
+		cy.get("#name-input").type("EAST,WEST,SOUTH", { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get("#race-progress-screen>div>.car-player").eq(0).should("have.text", "EAST");
@@ -121,10 +123,10 @@ context("bdd", () => {
 	});
 
 	it("가장 많이 전진한 자동차가 우승인지 판별한다.", () => {
-		cy.get("#name-input").type("EAST,WEST,SOUTH");
+		cy.get("#name-input").type("EAST,WEST,SOUTH", { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("100");
+		cy.get("#count-input").type("100", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get(".car-player").then((cars) => {
@@ -145,10 +147,10 @@ context("bdd", () => {
 	});
 
 	it("각 자동차의 최대 전진 횟수가 입력한 횟수보다 적거나 같다.", () => {
-		cy.get("#name-input").type("EAST,WEST,SOUTH");
+		cy.get("#name-input").type("EAST,WEST,SOUTH", { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("10");
+		cy.get("#count-input").type("10", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get(".car-player").then((cars) => {
@@ -164,10 +166,10 @@ context("bdd", () => {
 
 	it("우승자의 수는 입력한 이름의 수보다 적거나 같다.", () => {
 		const carNames = ["EAST", "WEST", "SOUTH"];
-		cy.get("#name-input").type(carNames.join(","));
+		cy.get("#name-input").type(carNames.join(","), { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get("#result-container")
@@ -180,10 +182,10 @@ context("bdd", () => {
 	});
 
 	it("리셋 버튼을 누르면 초기 상태로 돌아간다.", () => {
-		cy.get("#name-input").type("EAST,WEST,SOUTH");
+		cy.get("#name-input").type("EAST,WEST,SOUTH", { force: true });
 		cy.get("#name-submit-button").click();
 
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get("#race-progress-screen>div>.car-player").eq(0).should("have.text", "EAST");
@@ -196,11 +198,11 @@ context("bdd", () => {
 		cy.get("#count-input").should("not.exist");
 		cy.get("#count-submit-button").should("not.exist");
 
-		cy.get("#name-input").type("안,녕");
+		cy.get("#name-input").type("안,녕", { force: true });
 		cy.get("#name-submit-button").click();
 		cy.get("#count-input").should("exist");
 		cy.get("#count-submit-button").should("exist");
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
 
 		cy.get("#race-progress-screen>div>.car-player").eq(0).should("have.text", "안");
@@ -227,7 +229,7 @@ context("bdd", () => {
 			});
 		cy.reload();
 
-		cy.get("#name-input").type("EAST,,SOUTH");
+		cy.get("#name-input").type("EAST,,SOUTH", { force: true });
 
 		cy.get("#name-submit-button")
 			.click()
@@ -241,7 +243,7 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("bob dylan");
+		cy.get("#name-input").type("bob dylan", { force: true });
 		cy.get("#name-submit-button")
 			.click()
 			.then(() => {
@@ -254,9 +256,9 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("sehee");
+		cy.get("#name-input").type("sehee", { force: true });
 		cy.get("#name-submit-button").click();
-		cy.get("#name-input").type("EAST,WEST,SOUTH");
+		cy.get("#name-input").type("EAST,WEST,SOUTH", { force: true });
 		cy.get("#name-submit-button")
 			.click()
 			.then(() => {
@@ -266,11 +268,11 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("dawit");
+		cy.get("#name-input").type("dawit", { force: true });
 		cy.get("#name-submit-button").click();
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
-		cy.get("#name-input").type("EAST,WEST,SOUTH");
+		cy.get("#name-input").type("EAST,WEST,SOUTH", { force: true });
 		cy.get("#name-submit-button")
 			.click()
 			.then(() => {
@@ -283,11 +285,11 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("sehee");
+		cy.get("#name-input").type("sehee", { force: true });
 		cy.get("#name-submit-button").click();
-		cy.get("#count-input").type("3");
+		cy.get("#count-input").type("3", { force: true });
 		cy.get("#count-submit-button").click();
-		cy.get("#count-input").type("909");
+		cy.get("#count-input").type("909", { force: true });
 		cy.get("#count-submit-button")
 			.click()
 			.then(() => {
@@ -300,9 +302,9 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("sehee");
+		cy.get("#name-input").type("sehee", { force: true });
 		cy.get("#name-submit-button").click();
-		cy.get("#count-input").type("-8");
+		cy.get("#count-input").type("-8", { force: true });
 		cy.get("#count-submit-button")
 			.click()
 			.then(() => {
@@ -312,9 +314,9 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("sehee");
+		cy.get("#name-input").type("sehee", { force: true });
 		cy.get("#name-submit-button").click();
-		cy.get("#count-input").type("e");
+		cy.get("#count-input").type("e", { force: true });
 		cy.get("#count-submit-button")
 			.click()
 			.then(() => {
@@ -324,9 +326,9 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("sehee");
+		cy.get("#name-input").type("sehee", { force: true });
 		cy.get("#name-submit-button").click();
-		cy.get("#count-input").type("4.4");
+		cy.get("#count-input").type("4.4", { force: true });
 		cy.get("#count-submit-button")
 			.click()
 			.then(() => {
@@ -336,7 +338,7 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("sehee");
+		cy.get("#name-input").type("sehee", { force: true });
 		cy.get("#name-submit-button").click();
 		cy.get("#count-submit-button")
 			.click()
@@ -350,7 +352,7 @@ context("bdd", () => {
 
 		cy.on("window:alert", stub);
 
-		cy.get("#name-input").type("sehee,sehee");
+		cy.get("#name-input").type("sehee,sehee", { force: true });
 		cy.get("#name-submit-button")
 			.click()
 			.then(() => {
