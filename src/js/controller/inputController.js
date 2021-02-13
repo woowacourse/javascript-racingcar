c757c22a36ef2e9ea1ff310c49fee888e216475a
jum0 <wnsah052@naver.com>
2021-02-13 21:37:40 +0900

    refactor : handCountSubmit 내에 중복되는 부분 삭제

--- src/js/controller/inputController.js
+++ src/js/controller/inputController.js
@@ -8,18 +8,18 @@ const isValidLength = (carName) => {
   return 1 <= carName.length && carName.length <= 5;
 };
 
-export const isAlphanumeric = function (input) {
+const isAlphanumeric = function (input) {
   return /^[a-zA-Z0-9]+$/.test(input);
 };
 
 const isValidCarName = (carName) => {
-  isValidLength(carName) && isAlphanumeric(carName);
+  return isValidLength(carName) && isAlphanumeric(carName);
 };
 
-export const isDuplicated = (array) => new Set(array).size !== array.length;
+const isDuplicated = (array) => new Set(array).size !== array.length;
 
 const isValidCarNames = (carNames) => {
-  carNames.every(isValidCarName) && !isDuplicated(carNames);
+  return carNames.every(isValidCarName) && !isDuplicated(carNames);
 };
 
 const isValidCount = function (value) {
@@ -34,11 +34,12 @@ export const handleCarNamesSubmit = function () {
     return;
   }
 
+  app.generateCars(carNames);
   displayCountView();
 };
 
 export const handleCountSubmit = function () {
-  const carNames = $("#car-names-input").value.split(",");
+  // const carNames = $("#car-names-input").value.split(",");
   const countInput = $("#count-input").value;
 
   if (!isValidCount(countInput)) {
@@ -46,7 +47,7 @@ export const handleCountSubmit = function () {
     return;
   }
 
-  app.generateCars(carNames);
+  // app.generateCars(carNames);
   displayRacingCars(app.cars);
   startRacingGame(countInput);
 };
