2dcf23853174e43d0fa55a5ef9c25aa2fae3fecd
dawit jeong <wjdekdnlt1215@gmail.com>
2021-02-13 13:59:52 +0900

    :hammer_and_wrench: replace Templates to TEMPLATES of constants

--- src/js/View.js
+++ src/js/View.js
@@ -1,13 +1,10 @@
 import Model from "./Model.js";
-import Templates from "./Templates.js";
+import { TEMPLATES } from "./constants.js";
 
 class View {
 	initialRender($parentElement) {
 		$parentElement.innerHTML =
-			Templates.openingSettingContainerTemplate +
-			Templates.titleSectionTemplate +
-			Templates.carNameSectionTemplate +
-			Templates.closingSettingContainerTemplate;
+			TEMPLATES.OPENING_SETTING_CONTAINER + TEMPLATES.TITLE_SECTION + TEMPLATES.CAR_NAME_SECTION + TEMPLATES.CLOSING_SETTING_CONTAINER;
 	}
 
 	clearInputValue(inputElement) {
@@ -15,18 +12,18 @@ class View {
 	}
 
 	countSectionRender($settingContainer) {
-		$settingContainer.insertAdjacentHTML("beforeend", Templates.countSectionTemplate);
+		$settingContainer.insertAdjacentHTML("beforeend", TEMPLATES.COUNT_SECTION);
 	}
 
 	progressContainerRender(template) {
 		const $app = document.getElementById("app");
 		$app.insertAdjacentHTML("beforeend", template);
-		$app.insertAdjacentHTML("beforeend", Templates.resultContainerTemplate);
+		$app.insertAdjacentHTML("beforeend", TEMPLATES.RESULT_CONTAINER);
 	}
 
 	arrowRender(boolsAboutMovement) {
 		const renderedCars = document.getElementById("race-progress-screen").children;
-		const arrowTemplate = Templates.arrowTemplate;
+		const arrowTemplate = TEMPLATES.ARROW;
 		boolsAboutMovement.forEach((isNeedToBeAdded, i) => isNeedToBeAdded && renderedCars[i].insertAdjacentHTML("beforeend", arrowTemplate));
 	}
 
