35a2802fb1a1a54bede2bd8de5c77815aa593b4a
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-14 19:29:04 +0900

    :hammer_and_wrench: refactoring - change else if to if

--- src/js/Model.js
+++ src/js/Model.js
@@ -87,28 +87,35 @@ class Model {
 
 		if (this.cars.length !== 0) {
 			return { validity: false, alertMessage: "이미 이름이 등록되었습니다." };
-		} else if (names.includes("")) {
+		}
+		if (names.includes("")) {
 			return { validity: false, alertMessage: "빈 문자인 이름은 등록할 수 없습니다." };
-		} else if (names.length > 9) {
+		}
+		if (names.length > 9) {
 			return { validity: false, alertMessage: "가로 스크롤 생성을 방지하기 위해 이름 등록은 9개 이하로 제한하고 있습니다." };
-		} else if (names.some((name) => name.length > 5)) {
+		}
+		if (names.some((name) => name.length > 5)) {
 			return { validity: false, alertMessage: "5자를 넘는 이름은 등록할 수 없습니다." };
-		} else if ([...new Set(names)].length !== names.length) {
+		}
+		if ([...new Set(names)].length !== names.length) {
 			return { validity: false, alertMessage: "중복된 이름은 등록할 수 없습니다." };
-		} else return { validity: true, alertMessage: null };
+		}
+
+		return { validity: true, alertMessage: null };
 	}
 
 	validateCount(submittedCount) {
 		if (this.count !== 0) {
 			return { validity: false, alertMessage: "이미 횟수를 설정하였습니다." };
-		} else if (submittedCount === NaN || submittedCount <= 0 || Number.isInteger(submittedCount) === false) {
+		}
+		if (submittedCount === NaN || submittedCount <= 0 || Number.isInteger(submittedCount) === false) {
 			return { validity: false, alertMessage: "자연수만 설정할 수 있습니다." };
-		} else if (submittedCount > 20000) {
-			return {
-				validity: false,
-				alertMessage: "원활한 게임을 위해 횟수는 20000 이하로 제한하고 있습니다.",
-			};
-		} else return { validity: true, alertMessage: null };
+		}
+		if (submittedCount > 20000) {
+			return { validity: false, alertMessage: "원활한 게임을 위해 횟수는 20000 이하로 제한하고 있습니다." };
+		}
+
+		return { validity: true, alertMessage: null };
 	}
 }
 
