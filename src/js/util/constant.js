89f90734881709712f4570ff4aba5a3dd5f1b44d
2sooy <1401660@naver.com>
2021-02-15 09:10:58 +0900

    feat: 2초 뒤에 축하 메시지를 alert하는 유틸함수 추가
    - 2초에 대해 상수값 적용

--- src/js/util/constant.js
+++ src/js/util/constant.js
@@ -2,3 +2,6 @@ export const CAR_NAME_MAX_LENGTH = 5;
 export const MIN_NUMBER = 0;
 export const MAX_NUMBER = 9;
 export const MOVE_BOUNDED_NUMBER = 4; // 4이상일 때 전진
+
+export const GAME_PROCESS_DELAY = 1000;
+export const WINNER_CONGURATULATION_DELAY = 2000;
--- src/js/util/game.js
+++ src/js/util/game.js
@@ -1,3 +1,6 @@
+import { WINNER_CONGURATULATION_DELAY } from './constant.js';
+import { GAME_MESSAGE } from './message.js';
+
 export const getRandomNumber = ({ max, min }) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -9,4 +12,9 @@ export const delay = ms => {
   return new Promise(res => {
     setTimeout(res, ms);
   });
+};
+
+export const alertConguratulationMessage = async () => {
+  await delay(WINNER_CONGURATULATION_DELAY);
+  alert(GAME_MESSAGE.CONGRATULATION);
 };
