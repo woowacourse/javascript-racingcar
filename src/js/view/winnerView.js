f10ce3389cb7fa09e579b6ef4bfd6d4992f3faba
zereight <fullstackmachinelearning@gmail.com>
2021-02-15 14:54:49 +0900

    refactor : 비동기 처리안된부분 수정

--- src/js/view/winnerView.js
+++ src/js/view/winnerView.js
@@ -1,5 +1,5 @@
 import { SELECTOR, GLOBAL_TEXT, GLOBAL_ATTR } from '../keys.js';
-import { $ } from '../utils.js';
+import { $, sleep } from '../utils.js';
 
 const toggleDisplayCountView = function () {
 	$(SELECTOR.WINNER_CONTAINER).classList.toggle(
@@ -7,11 +7,12 @@ const toggleDisplayCountView = function () {
 	);
 };
 
-export const displayWinnerView = function (winners) {
+export const displayWinnerView = async function (winners) {
 	const $winnerElement = $(SELECTOR.WINNER_TEXT_AREA);
 	$winnerElement.innerText = GLOBAL_TEXT.MAKE_WINNER_TEXT(winners);
 	toggleDisplayCountView();
-	setTimeout(() => alert(GLOBAL_TEXT.MAKE_WINNER_TEXT(winners)), 2000);
+	await sleep(2);
+	alert(GLOBAL_TEXT.MAKE_WINNER_TEXT(winners));
 };
 
 export const initializeWinnerView = function () {
