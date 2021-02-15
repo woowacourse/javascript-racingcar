d46b20b43053c27b8ad7c72701b1a859c4668c10
swon3210 <swon3210@gmail.com>
2021-02-15 23:45:18 +0900

    test: step2 테스트 코드 작성

--- cypress/integration/racing_car_game.spec.js
+++ cypress/integration/racing_car_game.spec.js
@@ -6,6 +6,9 @@ import {
   SHOULD_GREATER_THAN_ZERO_MESSAGE,
   SHOULD_REGISTER_CAR_FIRST_MESSAGE,
   CAR_MOVE_THRESHOLD_NUMBER,
+  ANIMATION_DURATION_SECOND,
+  CONGRATULATION_DURATION_SECOND,
+  getCongratulationsMessage,
 } from '../../src/scripts/constants.js';
 
 describe('step1', () => {
@@ -126,5 +129,49 @@ describe('step1', () => {
         }
       });
     }
+  });
+});
+
+describe('step1', () => {
+  it(`자동차 경주 게임의 턴이 진행 될 때마다 ${ANIMATION_DURATION_SECOND}초의 텀을 두고 진행한다`, () => {
+    cy.get('#car-name-input').type('chris, beuc');
+    cy.get('#car-name-submit').click();
+    cy.get('#try-count-input').type('100');
+    cy.get('#play-game-button').click();
+    // 가장 긴 track을 찾고
+    let maxTrackArrowCount = 0;
+    cy.get('.track').then((tracks) => {
+      Array.from(tracks).forEach((track) => {
+        const trackArrowCount = track.querySelectorAll('.forward-icon').length;
+        if (trackArrowCount > trackArrowCount) {
+          maxTrackArrowCount = trackArrowCount;
+        }
+      });
+    });
+    // display 값이 바뀐 개수가 있는지 확인해봐야겠네
+    let prevVisibleArrowCount = cy
+      .get('#result-area')
+      .find('.forward-icon[style*="display:flex"]')
+      .its('length');
+    for (let i = 0; i < maxTrackArrowCount; i += 1) {
+      cy.wait(ANIMATION_DURATION_SECOND * 1000);
+      const currentVisibleArrowCount = cy
+        .get('#result-area')
+        .find('.forward-icon')
+        .its('length');
+      currentVisibleArrowCount.should('not.eq', prevVisibleArrowCount);
+      prevVisibleArrowCount = currentVisibleArrowCount;
+    }
+  });
+  it(`정상적으로 게임의 턴이 다 동작된 후에는 결과를 보여주고, ${CONGRATULATION_DURATION_SECOND}초 후에 축하의 alert 메세지를 띄운다`, () => {
+    const tryCount = 10;
+    cy.get('#car-name-input').type('chris, beuc');
+    cy.get('#car-name-submit').click();
+    cy.get('#try-count-input').type(String(tryCount));
+    cy.get('#play-game-button').click();
+    cy.wait((CONGRATULATION_DURATION_SECOND + tryCount + 2) * 1000);
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains(getCongratulationsMessage([]));
+    });
   });
 });
--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -4,12 +4,17 @@ const WINNER_TEXT_SEPERATOR = ', ';
 const MAX_CAR_NAME_LENGTH_NUMBER = 5;
 const CAR_MOVE_THRESHOLD_RANGE_NUMBER = 10;
 const CAR_MOVE_THRESHOLD_NUMBER = 4;
+const CONGRATULATION_DURATION_SECOND = 2;
+const ANIMATION_DURATION_SECOND = 1;
 
 const MAX_CAR_NAME_EXCEEDED_MESSAGE = `차 이름은 ${MAX_CAR_NAME_LENGTH_NUMBER}자 이하로 작성해주십시오`;
 const CAR_NAME_EMPTY_MESSAGE = '등록하려는 차의 이름이 비어있습니다.';
 const SHOULD_NOT_DECIMAL_MESSAGE = '횟수에 소수점이 포함될 수 없습니다.';
 const SHOULD_GREATER_THAN_ZERO_MESSAGE = '1 이상의 횟수를 입력해주세요.';
 const SHOULD_REGISTER_CAR_FIRST_MESSAGE = '차를 먼저 등록해주세요.';
+
+const getCongratulationsMessage = (winners) =>
+  `${winners.join(' 와 ')} 의 우승입니다! 축하합니다!`;
 
 export {
   CAR_NAME_SEPERATOR,
@@ -22,4 +27,7 @@ export {
   SHOULD_NOT_DECIMAL_MESSAGE,
   SHOULD_GREATER_THAN_ZERO_MESSAGE,
   SHOULD_REGISTER_CAR_FIRST_MESSAGE,
+  CONGRATULATION_DURATION_SECOND,
+  ANIMATION_DURATION_SECOND,
+  getCongratulationsMessage,
 };
