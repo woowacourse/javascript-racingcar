d83913a8a10fd819dd027b5640a363091c94e38c
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-03-07 21:45:22 +0900

    refactor : rearrange methods and remove unnecessary method

--- src/js/CarGameManager.js
+++ src/js/CarGameManager.js
@@ -1,7 +1,11 @@
 import CarGameView from './CarGameView.js';
+
 import RacingCarGame from './Game/RacingCarGame.js';
+
 import { CELEBRATE_MESSAGE } from './Utils/constants.js';
 import { $, $$ } from './Utils/dom.js';
+import { alertMessage } from './Utils/utils.js';
+
 import RacingCarValidator from './Validators/RacingCarValidator.js';
 
 export default class CarGameManager {
@@ -18,20 +22,59 @@ export default class CarGameManager {
     this.errorMessage = '';
   }
 
-  alertMessage(message) {
-    alert(message);
+  bindEvents() {
+    this.bindInputCarNamesEvent();
+    this.bindInputTryCountEvent();
+    this.bindResetEvent();
+  }
+
+  bindInputCarNamesEvent() {
+    $('#input-names-btn').addEventListener('click', () => {
+      this.carNamesInputHandler();
+    });
+  }
+
+  carNamesInputHandler() {
+    this.carNames = $('#input-car-names')
+      .value.split(',')
+      .map((name) => name.trim());
+
+    const errorMessage = this.racingCarValidator.checkCarNamesValidation(this.carNames);
+    if (errorMessage) {
+      alertMessage(errorMessage);
+      this.initGame();
+      return;
+    }
+
+    this.carGameView.showView($('#input-count-wrapper'));
+  }
+
+  bindInputTryCountEvent() {
+    $('#input-count-btn').addEventListener('click', () => {
+      this.tryCountInputHandler();
+    });
+  }
+
+  tryCountInputHandler() {
+    const tryCount = Number($('#input-try-count').value);
+    const errorMessage = this.racingCarValidator.checkTryCountValidation(tryCount);
+
+    if (errorMessage) {
+      alertMessage(errorMessage);
+      this.carGameView.resetInput($('#input-try-count'));
+      return;
+    }
+
+    this.racingCarGame = new RacingCarGame(this.carNames, tryCount);
+    new Promise((resolve) => {
+      this.renderInitGameProgress();
+      this.racingProgress = setInterval(() => this.playGame(resolve), 1000);
+    }).then(() => this.renderResult());
   }
 
   renderInitGameProgress() {
     this.carGameView.showView($('#display-game-progress'));
     this.carGameView.renderGameProgress(this.racingCarGame.getCars());
-  }
-
-  renderResult() {
-    this.carGameView.renderWinners(this.racingCarGame.getWinners());
-    this.carGameView.showView($('#display-game-result'));
-
-    setTimeout(() => this.alertMessage(CELEBRATE_MESSAGE), 2000);
   }
 
   playGame(resolve) {
@@ -45,63 +88,16 @@ export default class CarGameManager {
     }
   }
 
-  bindEvents() {
-    this.bindInputCarNamesEvent();
-    this.bindInputTryCountEvent();
-    this.bindResetEvent();
-  }
+  renderResult() {
+    this.carGameView.renderWinners(this.racingCarGame.getWinners());
+    this.carGameView.showView($('#display-game-result'));
 
-  bindInputCarNamesEvent() {
-    $('#input-names-btn').addEventListener('click', () => {
-      this.carNamesInputHandler();
-    });
-  }
-
-  bindInputTryCountEvent() {
-    $('#input-count-btn').addEventListener('click', () => {
-      this.tryCountInputHandler();
-    });
+    setTimeout(() => alertMessage(CELEBRATE_MESSAGE), 2000);
   }
 
   bindResetEvent() {
     $('#reset-btn').addEventListener('click', () => {
-      this.resetHandler();
-    });
-  }
-
-  carNamesInputHandler() {
-    this.carNames = $('#input-car-names')
-      .value.split(',')
-      .map((name) => name.trim());
-
-    const errorMessage = this.racingCarValidator.checkCarNamesValidation(this.carNames);
-    if (errorMessage) {
-      this.alertMessage(errorMessage);
       this.initGame();
-      return;
-    }
-
-    this.carGameView.showView($('#input-count-wrapper'));
-  }
-
-  tryCountInputHandler() {
-    const tryCount = Number($('#input-try-count').value);
-    const errorMessage = this.racingCarValidator.checkTryCountValidation(tryCount);
-
-    if (errorMessage) {
-      this.alertMessage(errorMessage);
-      this.carGameView.resetInput($('#input-count-wrapper > div'));
-      return;
-    }
-
-    this.racingCarGame = new RacingCarGame(this.carNames, tryCount);
-    new Promise((resolve) => {
-      this.renderInitGameProgress();
-      this.racingProgress = setInterval(() => this.playGame(resolve), 1000);
-    }).then(() => this.renderResult());
-  }
-
-  resetHandler() {
-    this.initGame();
+    });
   }
 }
