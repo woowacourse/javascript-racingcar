990183f4e5270e1c73db7f568a257cacf61bdc54
JASUN LEE <liswktjs@gmail.com>
2022-02-10 21:02:25 +0900

    ⚡️ feat : render racing content 유저가 입력한 시도 횟수 만큼 각각의 자동차 인스턴스에서 랜덤한 값을 할당해 전진시킨다 해당결과를 render한다

--- src/class/racingCar.js
+++ src/class/racingCar.js
@@ -1,3 +1,4 @@
+import { $ } from "../dom/dom.js";
 import Car from "./car.js";
 export default class RacingCar {
     constructor(carNameArray, raceCount) {
@@ -8,6 +9,7 @@ export default class RacingCar {
 
     gameStart(){
         this.generateCars();
+        this.playGame();
         
     }
 
@@ -19,5 +21,41 @@ export default class RacingCar {
         this.carNameArray.forEach(car => {
             this.carArray.push(new Car(car));
         });
+    }
+    
+    playGame(){
+        for(let i=0; i < this.raceCount; i++){
+            this.updateCarSuccessCount();
+        }
+        this.renderRacingContent();
+        console.log(this.carArray);
+    }
+
+    updateCarSuccessCount() {
+        this.carArray.forEach((item) => {
+            if(this.generateRandomNumber() >= 4){
+                item.successCount++;
+            }
+        })
+    }
+
+    renderRacingContent() {
+        let racingResult = '';
+        this.carArray.forEach(car => {
+            racingResult += `<div class="car">
+                <label>${car.name}</label>
+                <div>${this.renderOneCarContent(car.successCount)}</div>
+                </div>
+            `;
+        })
+        $('.racing-content').innerHTML = racingResult;
+    }
+
+    renderOneCarContent(carCount) {
+        let arrowResult = '';
+        for(let i = 0; i < carCount; i++) {
+            arrowResult +='<p>⬇️</p>';
+        }
+        return arrowResult;
     }
 }
