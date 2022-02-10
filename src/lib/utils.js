cc86b9e79adf41737aa86bf02bee962a2575814c
uk960214 <uk960214@gmail.com>
2022-02-10 17:08:45 +0900

    refactor: airbnb 컨벤션 적용
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,5 @@
-export const splitString = (str, separator) => {
-  return str.split(separator);
-};
+export const splitString = (str, separator) => str.split(separator);
+
+export const isNumberBelowZero = (number) => number <= 0;
+
+export const checkStringLength = (str, standard) => str.length <= standard;
