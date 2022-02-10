6e22fbf5d4cab234ec5e8f9b5eaee79d2c2f9334
sangminsong <ssmin940606@gmail.com>
2022-02-10 23:45:52 +0900

    test: 뷰에 대한 테스트 코드 추가 - 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
    Co-authored-by: Daye Lee <508yeah@gmail.com>

--- cypress/integration/racingCar.spec.js
+++ cypress/integration/racingCar.spec.js
@@ -1,6 +1,5 @@
 // - [ ] 자동차에 이름을 부여할 수 있다.
 // - [ ] 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
-// - [ ] 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
 // - [ ] 사용자가 잘못된 입력 값을 작성한 경우 alert을 이용해 메시지를 보여주고, 다시 입력할 수 있게 한다.
 //    - [ ] 자동차 이름
 //    - [ ] 시도할 횟수
@@ -16,6 +15,8 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   const SELECTOR = {
     CAR_NAMES_INPUT: '#car-names-input',
     CAR_NAMES_SUBMIT_BUTTON: '#car-names-submit',
+    RACING_COUNT_INPUT: '#racing-count-input',
+    RACING_COUNT_SUBMIT: '#racing-count-submit',
   };
 
   it('자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.', () => {
@@ -33,5 +34,18 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
       .then(() => {
         expect(alertStub).to.be.called;
       });
+  });
+
+  it('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
+    cy.visit(baseUrl);
+
+    const validCarNamesInput = 'apple,banan,carro';
+
+    cy.get(SELECTOR.CAR_NAMES_INPUT).type(validCarNamesInput);
+
+    cy.get(SELECTOR.CAR_NAMES_SUBMIT_BUTTON).click();
+
+    cy.get(SELECTOR.RACING_COUNT_INPUT).should('exist');
+    cy.get(SELECTOR.RACING_COUNT_SUBMIT).should('exist');
   });
 });
