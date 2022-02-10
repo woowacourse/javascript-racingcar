60099a8b5ac378a1deb956896796fa0b616d6b2a
LAH1203 <lah1203@naver.com>
2022-02-10 19:39:30 +0900

    feat: make constants and elements file

---
+++ src/js/util/constants.js
@@ -0,0 +1,22 @@
+export const CAR_NAME_LENGTH = {
+  MIN: 1,
+  MAX: 5,
+};
+
+export const RACING_COUNT = {
+  MIN: 1,
+};
+
+export const RANDOM_RANGE = {
+  MIN: 0,
+  MAX: 9,
+};
+
+export const EXCEPTIONS = {
+  INCORRECT_CAR_NAME: "조건에 맞게 중복 없이 2대 이상을 입력해주세요.",
+  INCORRECT_RACING_COUNT: `${RACING_COUNT.MIN} 이상의 정수로 입력해주세요.`,
+};
+
+export const EVENT = {
+  CLICK: "click",
+};
---
+++ src/js/util/elements.js
@@ -0,0 +1,10 @@
+export const logo = document.getElementById("logo");
+export const carNamesInput = document.getElementById("car-names-input");
+export const carNamesSubmitButton = document.getElementById("car-names-submit");
+export const racingCountArea = document.getElementById("racing-count-area");
+export const racingCountInput = document.getElementById("racing-count-input");
+export const racingCountSubmitButton = document.getElementById("racing-count-submit");
+export const racingResultArea = document.getElementById("racing-result");
+export const racingWinnerArea = document.getElementById("winner-area");
+export const racingWinnerText = document.getElementById("racing-winner");
+export const restartButton = document.getElementById("restart-button");
