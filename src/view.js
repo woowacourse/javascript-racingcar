802a569cd8c0d4c90bc6251f4d9a6b001708bdb6
kamwoo <rkadndud06@naver.com>
2022-02-10 15:42:53 +0900

    feat: View DOM 프로퍼티 설정

--- src/view.js
+++ src/view.js
@@ -1,3 +1,17 @@
 import { $ } from './utils/common.js';
 
-export default class View {}
+export default class View {
+  constructor() {
+    this.configureDOM();
+  }
+
+  configureDOM() {
+    this.$nameInput = $('.input-section__name-input');
+    this.$countInput = $('.input-section__count-input');
+    this.$nameButton = $('.input-section__name-button');
+    this.$countButton = $('.input-section__count-button');
+    this.$resultSections = $('result-sections');
+    this.$winner = $('#winner');
+    this.$resetButton = $('#reset-button');
+  }
+}
