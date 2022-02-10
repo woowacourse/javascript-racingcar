16908b217e1bf2f2d1f79de8ea67555b18a851a7
sanaandmomo <qajjang46@naver.com>
2022-02-10 22:37:53 +0900

    refactor: 중복되는 테스트 코드 함수화

--- test/racingCar.spec.js
+++ test/racingCar.spec.js
@@ -3,42 +3,30 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
         cy.visit('index.html');
     });
 
-    it('자동차 이름에 공백이 포함된 경우, alert가 호출되어야 한다.', () => {
+    const formAlertTest = (selector, value) => () => {
         const alertStub = cy.stub();
         cy.on('window:alert', alertStub);
 
-        cy.get('#car-name-input').type('ab cd');
-
-        cy.get('#car-name-submit-button')
+        cy.get(`#${selector}-input`).type(value);
+        cy.get(`#${selector}-submit-button`)
             .click()
             .then(() => {
                 expect(alertStub).to.be.called;
             });
-    });
+    };
 
-    it('자동차 이름이 5자가 넘는 경우, alert가 호출되어야 한다.', () => {
-        const alertStub = cy.stub();
-        cy.on('window:alert', alertStub);
+    it(
+        '자동차 이름에 공백이 포함된 경우, alert가 호출되어야 한다.',
+        formAlertTest('car-name', 'ab cd'),
+    );
 
-        cy.get('#car-name-input').type('abcdefefsd');
+    it(
+        '자동차 이름이 5자가 넘는 경우, alert가 호출되어야 한다.',
+        formAlertTest('car-name', 'abcdfesf'),
+    );
 
-        cy.get('#car-name-submit-button')
-            .click()
-            .then(() => {
-                expect(alertStub).to.be.called;
-            });
-    });
-
-    it('전진 시도 횟수 입력이 소숫점 자리를 가진 경우, alert가 호출되어야 한다.', () => {
-        const alertStub = cy.stub();
-        cy.on('window:alert', alertStub);
-
-        cy.get('#try-count-input').type(123.456);
-
-        cy.get('#try-count-submit-button')
-            .click()
-            .then(() => {
-                expect(alertStub).to.be.called;
-            });
-    });
+    it(
+        '전진 시도 횟수 입력이 소숫점 자리를 가진 경우, alert가 호출되어야 한다.',
+        formAlertTest('try-count', 123.456),
+    );
 });
