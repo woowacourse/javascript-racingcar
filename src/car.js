a82aa01a6cdea210ca0432da10d465ce15f4aff4
kamwoo <rkadndud06@naver.com>
2022-02-10 16:23:18 +0900

    feat: car 객체 랜덤 전진 메소드 작성

--- src/car.js
+++ src/car.js
@@ -3,4 +3,15 @@ export default class Car {
     this.name = name;
     this.step = 0;
   }
+
+  generateRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+
+  randomMove() {
+    const number = this.generateRandomNumber();
+    if (number >= 4) {
+      this.step += 1;
+    }
+  }
 }
