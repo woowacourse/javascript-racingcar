ffb372abfd2b321ca6678959adf668554fde938e
JihyeokUm <75592315+Jihyeok-Um@users.noreply.github.com>
2022-02-10 17:14:02 +0900

    feat: 한 차례 시도가 끝날 때마다 결과를 출력 구현

--- src/view/renderResult.js
+++ src/view/renderResult.js
@@ -2,9 +2,15 @@ import { $ } from '../util/dom.js';
 
 export const renderResult = cars => {
   console.log('00');
-  $('#game-result').innerHTML = `${cars.map(car => {
-    return `<div>name:${car.name} score:${car.score}</div>`;
-  })}`;
+  $('#game-result').innerHTML += `${cars
+    .map(car => {
+      return `<div>name:${car.name} score:${'⬇️'.repeat(car.score)}</div>`;
+    })
+    .join('')}`;
 };
 
 const unitResult = cars => {};
+
+//const scoreEmoji = '⬇️'.repeat(score);
+
+// ⬇️️
