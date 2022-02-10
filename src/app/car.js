24df3da56d0521090f614c35697f63d5a74ac0e6
uk960214 <uk960214@gmail.com>
2022-02-10 15:01:49 +0900

    feat(checkNames): 자동차 이름 길이 검증 매서드 구현 및 오류 처리
    Co-authored-by: juunzzi juunzzi@users.noreply.github.com

--- src/app/car.js
+++ src/app/car.js
@@ -1,12 +1,20 @@
 class Car {
   constructor(name) {
     // this.id = generateId();
-    this.name = name;
-    this.progress = 0;
+    this.init(name);
   }
-
+  checkNameLength(name) {
+    return name.length <= 5;
+  }
   goForward() {
     this.progress += 1;
+  }
+  init(name) {
+    if (!this.checkNameLength(name)) {
+      throw Error("5자 이하의 자동차 이름을 입력해주세요");
+    }
+    this.name = name;
+    this.progress = 0;
   }
 }
 
