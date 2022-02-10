d3d5078374a1aca1c60e365cf91a52bcc0f773cc
sanaandmomo <qajjang46@naver.com>
2022-02-10 22:02:20 +0900

    test: 자동차 전진 시도 횟수 테스트(소수점)

--- test/racingCar.spec.js
+++ test/racingCar.spec.js
@@ -28,4 +28,17 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
                 expect(alertStub).to.be.called;
             });
     });
+
+    it('전진 시도 횟수 입력이 소숫점 자리를 가진 경우, alert가 호출되어야 한다.', () => {
+        const alertStub = cy.stub();
+        cy.on('window:alert', alertStub);
+
+        cy.get('#try-count-input').type(123.456);
+
+        cy.get('#try-count-submit-button')
+            .click()
+            .then(() => {
+                expect(alertStub).to.be.called;
+            });
+    });
 });
