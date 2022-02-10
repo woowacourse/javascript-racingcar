b0d7368ba3ed2cc894366ed8bd186b688b18f9d4
LAH1203 <lah1203@naver.com>
2022-02-10 19:44:24 +0900

    feat: add main module

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+import Cars from "./controller/carsName/Cars.js";
+import RacingCount from "./controller/racingCount/RacingCount.js";
+import { makeInitialView } from "./view/util/viewControl.js";
+import { EVENT } from "./util/constants.js";
+import { logo, restartButton } from "./util/elements.js";
+
+class RacingCar {
+  constructor() {
+    this.cars = new Cars();
+    this.racingCount = new RacingCount();
+    makeInitialView();
+    this.addLogoClickEvent();
+    this.addRestartEvent();
+  }
+
+  addLogoClickEvent() {
+    logo.addEventListener(EVENT.CLICK, () => {
+      makeInitialView();
+    });
+  }
+
+  addRestartEvent() {
+    restartButton.addEventListener(EVENT.CLICK, () => {
+      makeInitialView();
+      this.cars.init();
+      this.racingCount.init();
+    });
+  }
+}
+
+new RacingCar();
