df1781d7ff52807dde6d3296ab7a603d349c8fad
LAH1203 <lah1203@naver.com>
2022-02-10 19:41:16 +0900

    feat: add view control functions

---
+++ src/js/view/util/viewControl.js
@@ -0,0 +1,39 @@
+import {
+  carNamesInput,
+  carNamesSubmitButton,
+  racingCountArea,
+  racingCountInput,
+  racingCountSubmitButton,
+  racingResultArea,
+  racingWinnerArea,
+  racingWinnerText,
+  restartButton,
+} from "../../util/elements.js";
+
+export const makeInitialView = () => {
+  carNamesInput.value = "";
+  carNamesInput.readOnly = false;
+  carNamesSubmitButton.disabled = false;
+  racingCountArea.hidden = true;
+  racingCountInput.value = "";
+  racingCountInput.readOnly = false;
+  racingCountSubmitButton.disabled = false;
+  racingResultArea.innerHTML = "";
+  racingResultArea.hidden = true;
+  racingWinnerArea.hidden = true;
+  racingWinnerText.innerHTML = "";
+  restartButton.hidden = false;
+};
+
+export const showRacingCountArea = () => {
+  racingCountArea.hidden = false;
+};
+
+export const showRacingResultArea = () => {
+  racingResultArea.hidden = false;
+};
+
+export const showWinnerAndRestartButton = () => {
+  racingWinnerArea.hidden = false;
+  restartButton.hidden = false;
+};
