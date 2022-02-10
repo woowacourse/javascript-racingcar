77ef5d3f033c416a131900da0850cef531fbd502
uk960214 <uk960214@gmail.com>
2022-02-10 17:19:24 +0900

    test(game): 게임 실행 테스트 작성
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,43 +1,82 @@
-describe("구현 결과가 요구사항과 일치해야 한다.", () => {
-  const baseUrl = "../../index.html";
+describe('구현 결과가 요구사항과 일치해야 한다.', () => {
+  const baseUrl = '../../index.html';
 
   before(() => {
-    cy.visit(baseUrl);
+    Cypress.Commands.add('stubRandomReturn', (returnValues = []) => {
+      const randomStub = cy.stub();
+
+      returnValues.forEach((value, index) => {
+        randomStub.onCall(index).returns(value);
+      });
+
+      cy.visit(baseUrl, {
+        onBeforeLoad: (window) => {
+          window.MissionUtils = {
+            Random: {
+              pickNumberInRange: randomStub,
+            },
+          };
+        },
+      });
+    });
   });
 
-  it("잘못된 자동차 이름을 입력하면 alert가 호출되어야 한다.", () => {
+  beforeEach(() => {
+    cy.stubRandomReturn([5, 1]);
+  });
+
+  it('잘못된 자동차 이름을 입력하면 alert가 호출되어야 한다.', () => {
     //given
     const alertStub = cy.stub();
-    const invalidInput = "juunzzi";
+    const invalidInput = 'juunzzi';
 
-    cy.on("window:alert", alertStub);
+    cy.on('window:alert', alertStub);
 
     //when
-    cy.get("#car-name-input").type(invalidInput);
+    cy.get('#car-name-input').type(invalidInput);
 
     //then
-    cy.get("#car-name-btn")
+    cy.get('#car-name-btn')
       .click()
       .then(() => {
         expect(alertStub).to.be.called;
       });
   });
 
-  it("횟수 입력란에 1 미만의 값이 주어지면 alert가 호출되어야 한다.", () => {
+  it('횟수 입력란에 1 미만의 값이 주어지면 alert가 호출되어야 한다.', () => {
     //given
     const alertStub = cy.stub();
     const invalidInput = -1;
 
-    cy.on("window:alert", alertStub);
+    cy.on('window:alert', alertStub);
 
     //when
-    cy.get("#count-input").type(invalidInput);
+    cy.get('#count-input').type(invalidInput);
 
     //then
-    cy.get("#count-btn")
+    cy.get('#count-btn')
       .click()
       .then(() => {
         expect(alertStub).to.be.called;
+      });
+  });
+
+  it('잘못된 자동차 이름을 입력하면 alert가 호출되어야 한다.', () => {
+    //given
+    const nameInput = 'bling,juunz';
+    const countInput = 1;
+    const winner = 'bling';
+
+    //when
+    cy.get('#car-name-input').type(nameInput);
+    cy.get('#car-name-btn').click();
+    cy.get('#count-input').type(countInput);
+
+    //then
+    cy.get('#count-btn')
+      .click()
+      .then(() => {
+        cy.get('#winner-name').should('have.text', winner);
       });
   });
 });
