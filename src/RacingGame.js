5dac08e45080703aa481e92fcd88dd9064cb1597
sanaandmomo <qajjang46@naver.com>
2022-02-10 22:16:53 +0900

    feat: 자동차 전진 시도 횟수 유효성 검사(소수점)

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,17 +1,25 @@
-import { haveBlank, exceedMaxLength } from './validation.js';
+import { haveBlank, exceedMaxLength, havePoint } from './validation.js';
 
 const $ = (selector) => document.querySelector(selector);
 
 export default class RacingGame {
     constructor() {
         this.setEvent();
+        this.initTryCntEvent();
     }
 
     setEvent() {
         $('#car-name-submit-button').addEventListener('click', () => {
             const carNames = $('#car-name-input').value.split(',');
-            if (haveBlank(carNames)) alert('자동차 이름에 공백이 들어갈 수 없습니다.');
-            if (exceedMaxLength(carNames)) alert('자동차 이름은 5자 이하입니다.');
+            if (haveBlank(carNames)) return alert('자동차 이름에 공백이 들어갈 수 없습니다.');
+            if (exceedMaxLength(carNames)) return alert('자동차 이름은 5자 이하입니다.');
+        });
+    }
+
+    initTryCntEvent() {
+        $('#try-count-submit-button').addEventListener('click', () => {
+            const tryCnt = $('#try-count-input').value;
+            if (!Number.isInteger(tryCnt)) return alert('자연수를 입력해주세요.');
         });
     }
 }
