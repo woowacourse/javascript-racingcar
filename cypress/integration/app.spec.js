06b494caed7bf1ddc5e9655cf64cf1363e592616
uk960214 <uk960214@gmail.com>
2022-02-10 14:15:43 +0900

    test(input): 이름과 횟수 입력값 검증 테스트 작성
    - cypress 설정
    - 자동차 이름 입력 검증 테스트 작성
    - 횟수 입력 검증 테스트 작성
    
    Co-authored-by: juunzzi juunzzi@users.noreply.github.com

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,123 +1,43 @@
-import { DOM } from '../../src/lib/constants.js';
+describe("구현 결과가 요구사항과 일치해야 한다.", () => {
+  const baseUrl = "../../index.html";
 
-describe('구현 결과가 요구사항과 일치해야 한다.', () => {
-  const baseUrl = '../../index.html';
-
-  beforeEach(() => {
+  before(() => {
     cy.visit(baseUrl);
   });
 
-  const carNameInputProcess = (nameInput) => {
-    cy.get(`#${DOM.CAR_NAME_INPUT_ID}`).type(nameInput);
-    cy.get(`#${DOM.CAR_NAME_BTN_ID}`).click();
-  };
-
-  const countInputProcess = (countInput) => {
-    cy.get(`#${DOM.COUNT_INPUT_ID}`).type(countInput);
-    cy.get(`#${DOM.COUNT_BTN_ID}`).click();
-  };
-
-  const setAlertStub = () => {
+  it("잘못된 자동차 이름을 입력하면 alert가 호출되어야 한다.", () => {
+    //given
     const alertStub = cy.stub();
-    cy.on('window:alert', alertStub);
-    return alertStub;
-  };
+    const invalidInput = "juunzzi";
 
-  // then function
-  const doesShowAlert = ({ inputSelector, inputValue, btnSelector, alertStub }) => {
-    // when
-    cy.get(inputSelector).type(inputValue);
+    cy.on("window:alert", alertStub);
 
-    // then
-    cy.get(btnSelector)
+    //when
+    cy.get("#car-name-input").type(invalidInput);
+
+    //then
+    cy.get("#car-name-btn")
       .click()
       .then(() => {
         expect(alertStub).to.be.called;
       });
-  };
-
-  const isInitialStatus = () => {
-    cy.get(`#${DOM.CAR_NAME_INPUT_ID}`).should('not.have.text');
-    cy.get(`.${DOM.CAR_PROGRESS_CLASS}`).should('not.exist');
-    cy.get(`.${DOM.WINNER_CONTAINER_ID}`).should('not.exist');
-    cy.get(`#${DOM.COUNT_INPUT_ID}`).should('not.be.visible');
-  };
-
-  it('게임을 완료하고 우승자를 확인할 수 있어야 한다.', () => {
-    //given
-    const nameInput = 'bling,juunz';
-    const countInput = 1;
-
-    //when
-    carNameInputProcess(nameInput);
-    countInputProcess(countInput);
-
-    //then
-    cy.get(`#${DOM.WINNER_NAME_ID}`).should('be.visible');
   });
 
-  it('잘못된 자동차 이름을 입력하면 alert가 호출되어야 한다.', () => {
+  it("횟수 입력란에 음수 값이 주어지면 alert가 호출되어야 한다.", () => {
     //given
-    const alertStub = setAlertStub();
-    const invalidInput = 'juunzzi';
-
-    // when - then
-    doesShowAlert({
-      inputSelector: `#${DOM.CAR_NAME_INPUT_ID}`,
-      inputValue: invalidInput,
-      btnSelector: `#${DOM.CAR_NAME_BTN_ID}`,
-      alertStub,
-    });
-  });
-
-  it('횟수 입력란에 1 미만의 값이 주어지면 alert가 호출되어야 한다.', () => {
-    //given
-    const alertStub = setAlertStub();
-    const nameInput = 'bling,juunz';
+    const alertStub = cy.stub();
     const invalidInput = -1;
-    carNameInputProcess(nameInput);
 
-    // when - then
-    doesShowAlert({
-      inputSelector: `#${DOM.COUNT_INPUT_ID}`,
-      inputValue: invalidInput,
-      btnSelector: `#${DOM.COUNT_BTN_ID}`,
-      alertStub,
-    });
-  });
-
-  it('재시작 버튼을 누르면 처음 상태로 돌아가야 한다.', () => {
-    //given
-    const nameInput = 'bling,juunz';
-    const countInput = 2;
+    cy.on("window:alert", alertStub);
 
     //when
-    carNameInputProcess(nameInput);
-    countInputProcess(countInput);
-    cy.get(`#${DOM.RESTART_BTN_ID}`).click();
+    cy.get("#count-input").type(invalidInput);
 
     //then
-    isInitialStatus();
-  });
-
-  // 자동차를 입력하여 생성하는 부분을 리팩토링 하고 나니
-  // 자동차가 정상 입력된 후 카운트가 입력되면
-  // 입력된 자동차들이 제대로 UI로 표시되는 지 검증하는 코드가 필요하다고 느꼈음
-
-  // UI에 노출되는 텍스트를 테스트 하는 방법 말고, 제대로 객체가 생겼는지 검증하는 방법은 없을까 ?
-
-  it('자동차를 정상적으로 입력하고, 카운트를 정상적으로 입력하면 자동차 텍스트들을 확인할 수 있다.', () => {
-    //given
-    const nameInput = 'bling,jun,zzi';
-    const countInput = 2;
-
-    //when
-    carNameInputProcess(nameInput);
-    countInputProcess(countInput);
-
-    //then
-    const result = nameInput.split(',').reduce((prev, current) => `${prev}${current}`, '');
-
-    cy.get(`.${DOM.CAR_NAME_CLASS}`).should('have.text', result);
+    cy.get("#count-btn")
+      .click()
+      .then(() => {
+        expect(alertStub).to.be.called;
+      });
   });
 });
