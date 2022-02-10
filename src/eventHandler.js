d34af130510cf7f109ec8e4f39643b5bc5ce1bdf
KIM YEJI <rladpwl0512@gmail.com>
2022-02-10 17:15:22 +0900

    feat: 시도 횟수를 입력받는 기능 구현

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -1,4 +1,5 @@
 import Car from './game/Car.js';
+import playRace from './game/playRace.js';
 import { ERROR, LIMIT } from './util/constants.js';
 
 function isValidNames(names) {
@@ -14,6 +15,18 @@ function isValidNames(names) {
   return true;
 }
 
+function isValidCount(inputCount, cars) {
+  if (cars.length === 0) {
+    alert(ERROR.EMPTY_NAMES);
+    return false;
+  }
+  if (+inputCount < LIMIT.MIN_LENGTH) {
+    alert(ERROR.MIN_COUNT);
+    return false;
+  }
+  return true;
+}
+
 function generateCars(names) {
   const cars = [];
 
@@ -25,13 +38,24 @@ function generateCars(names) {
 }
 
 export default function eventHandler() {
+  let cars = [];
+  let count = 0;
+
   document.querySelector('.name-form').addEventListener('submit', event => {
     event.preventDefault();
     const names = document.querySelector('.name-input').value.split(',');
-    let cars = [];
     if (isValidNames(names)) {
       cars = generateCars(names);
     }
-    console.log(cars);
+  });
+
+  document.querySelector('.count-form').addEventListener('submit', event => {
+    event.preventDefault();
+    const inputCount = document.querySelector('.count-input').value;
+
+    if (isValidCount(inputCount, cars)) {
+      count = inputCount;
+      playRace(cars, count);
+    }
   });
 }
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,6 +1,8 @@
 export const ERROR = {
   LONGER_THAN_FIVE: '에러! 이름은 5글자 이하여야 합니다.',
   IS_BLANK: '에러! 이름은 공백이 아니어야 합니다.',
+  EMPTY_NAMES: '에러! 자동차 이름이 입력되지 않았습니다.',
+  MIN_COUNT: '에러! 시도 횟수는 1 이상이여야 합니다.',
 };
 
 export const LIMIT = {
