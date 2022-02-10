91be7c7e0cddc62eb2df81840f560eb14af42aea
uk960214 <uk960214@gmail.com>
2022-02-10 18:12:00 +0900

    test: 게임 실행테스트 코드 수정
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,7 +2,7 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   const baseUrl = '../../index.html';
 
   before(() => {
-    Cypress.Commands.add('stubRandomReturn', (returnValues = []) => {
+    Cypress.Commands.add('stubRandomReturns', (returnValues = []) => {
       const randomStub = cy.stub();
 
       returnValues.forEach((value, index) => {
@@ -22,7 +22,23 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   });
 
   beforeEach(() => {
-    cy.stubRandomReturn([5, 1]);
+    cy.stubRandomReturns([5, 1]);
+  });
+
+  it('게임을 완료하고 우승자를 확인할 수 있어야 한다.', () => {
+    //given
+    const nameInput = 'bling,juunz';
+    const countInput = 1;
+    const winner = 'bling';
+
+    //when
+    cy.get('#car-name-input').type(nameInput);
+    cy.get('#car-name-btn').click();
+    cy.get('#count-input').type(countInput);
+    cy.get('#count-btn').click();
+
+    //then
+    cy.get('#winner-name').should('have.text', winner);
   });
 
   it('잘못된 자동차 이름을 입력하면 alert가 호출되어야 한다.', () => {
@@ -58,25 +74,6 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
       .click()
       .then(() => {
         expect(alertStub).to.be.called;
-      });
-  });
-
-  it('잘못된 자동차 이름을 입력하면 alert가 호출되어야 한다.', () => {
-    //given
-    const nameInput = 'bling,juunz';
-    const countInput = 1;
-    const winner = 'bling';
-
-    //when
-    cy.get('#car-name-input').type(nameInput);
-    cy.get('#car-name-btn').click();
-    cy.get('#count-input').type(countInput);
-
-    //then
-    cy.get('#count-btn')
-      .click()
-      .then(() => {
-        cy.get('#winner-name').should('have.text', winner);
       });
   });
 });
