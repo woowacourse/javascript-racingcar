76a3a34ff01769a0dc5c21465e0dd9b35115e522
jswith <9geul@naver.com>
2022-02-10 13:55:45 +0900

    feat: 자동차 이름 콤마로 구분하여 입력 받는 기능

---
+++ js/controller/controller.js
@@ -0,0 +1,20 @@
+import { $, $$ } from '../utils/dom.js';
+import Model from '../model/model.js';
+import View from '../view/view.js';
+
+export default class Controller {
+  constructor() {
+    this.model = new Model();
+    this.view = new View();
+    this.getCarNames();
+  }
+
+  getCarNames() {
+    //  확인 버튼 이벤트 리스터
+    $('#car-names-button').addEventListener('click', (e) => {
+      const carNames = $('#car-names-input').value.split(',');
+      console.log(carNames);
+      //model에 데이터로 넣어주는 로직
+    });
+  }
+}
---
+++ js/model/model.js
@@ -0,0 +1,6 @@
+export default class Model {
+  constructor() {
+    this.carNames;
+    this.carPosition;
+  }
+}
