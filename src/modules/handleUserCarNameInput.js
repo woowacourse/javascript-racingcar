286fce7620a2cede4ee082286935c432dbfd6ad5
JASUN LEE <liswktjs@gmail.com>
2022-02-10 15:58:32 +0900

    ⚡️ feat : hadle carnameinput error 유저가 자동차 이름을 빈값으로 입력하거나 , 5개를 초과하거나, 5자를 초과하였을때 alert창을 띄우고 text를 초기화한다

--- src/modules/handleUserCarNameInput.js
+++ src/modules/handleUserCarNameInput.js
@@ -1,12 +1,27 @@
-import { EMPYT_INPUT_ERROR } from "../constants/error.js";
+import { EMPYT_INPUT_ERROR, OVER_CAR_COUNT_ERROR, OVER_CARNAME_LENGTH_ERROR } from "../constants/error.js";
 import { $ } from "../dom/dom.js";
 import showAlert from "./showAlert.js";
+import initInputText from "./initInputText.js";
 
 export default function handleUserCarNameInput(){
     $('#car-name-button').addEventListener('click', (e) => {
         e.preventDefault();
-        if($('#car-name-input').innerText === ''){
+        const carNameInput = $('#car-name-input').value;
+
+        if(carNameInput === ''){
             showAlert(EMPYT_INPUT_ERROR);
         }
+        const carNameArray = carNameInput.split(',').map(carName => carName.trim());
+        
+        if(carNameArray.length > 5) {
+            showAlert(OVER_CAR_COUNT_ERROR);
+            initInputText($('#car-name-input'));
+        }
+        carNameArray.forEach(carName => {
+            if(carName.length > 5) {
+                showAlert(OVER_CARNAME_LENGTH_ERROR);
+                initInputText($('#car-name-input'));
+            }
+        });
     })
 }
