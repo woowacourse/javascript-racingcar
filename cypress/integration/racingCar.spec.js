023e81895d33172540bdc140e7ca5ef7c99a2bbf
sangminsong <ssmin940606@gmail.com>
2022-02-11 00:33:47 +0900

    test: 예외 사항에 대한 테스트 코드 추가 - 제출된 시도할 횟수가 양의 정수가 아닌 경우 - 5자를 초과하는 자동차 이름이 제출된 경우
    Co-authored-by: Daye Lee <508yeah@gmail.com>

--- cypress/integration/racingCar.spec.js
+++ cypress/integration/racingCar.spec.js
@@ -14,12 +14,12 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
   const SELECTOR = {
     CAR_NAMES_INPUT: '#car-names-input',
-    CAR_NAMES_SUBMIT_BUTTON: '#car-names-submit',
+    CAR_NAMES_SUBMIT: '#car-names-submit',
     RACING_COUNT_INPUT: '#racing-count-input',
     RACING_COUNT_SUBMIT: '#racing-count-submit',
   };
 
-  it('자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.', () => {
+  it('5자를 초과하는 자동차 이름이 제출된 경우에 alert을 이용해 메시지를 보여준다.', () => {
     cy.visit(baseUrl);
 
     const alertStub = cy.stub();
@@ -29,11 +29,23 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
 
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT_BUTTON)
+    cy.get(SELECTOR.CAR_NAMES_SUBMIT)
       .click()
       .then(() => {
         expect(alertStub).to.be.called;
       });
+  });
+
+  it('5자를 초과하는 자동차 이름이 제출된 경우에 다시 입력할 수 있게 한다.', () => {
+    cy.visit(baseUrl);
+
+    const invalidInput = 'abcdef';
+
+    cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
+
+    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click().type('{enter}');
+
+    cy.get(SELECTOR.CAR_NAMES_INPUT).should('have.value', '');
   });
 
   it('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
@@ -43,9 +55,36 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(validCarNamesInput);
 
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT_BUTTON).click();
+    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
 
     cy.get(SELECTOR.RACING_COUNT_INPUT).should('exist');
     cy.get(SELECTOR.RACING_COUNT_SUBMIT).should('exist');
+  });
+
+  it('제출된 시도할 횟수가 양의 정수가 아닌 경우에 alert을 이용해 메시지를 보여준다.', () => {
+    const alertStub = cy.stub();
+    const invalidInput = 'e';
+
+    cy.on('window:alert', alertStub);
+
+    cy.get(SELECTOR.RACING_COUNT_INPUT).type(invalidInput);
+
+    cy.get(SELECTOR.RACING_COUNT_SUBMIT)
+      .click()
+      .then(() => {
+        expect(alertStub).to.be.called;
+      });
+  });
+
+  it('제출된 시도할 횟수가 양의 정수가 아닌 경우에 다시 입력할 수 있게 한다.', () => {
+    cy.visit(baseUrl);
+
+    const invalidInput = '-123';
+
+    cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
+
+    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click().type('{enter}');
+
+    cy.get(SELECTOR.CAR_NAMES_INPUT).should('have.value', '');
   });
 });
