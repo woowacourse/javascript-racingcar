81a0ff68a340aa4106397f24c81eff0b73c2739c
uk960214 <uk960214@gmail.com>
2022-02-10 18:11:39 +0900

    feat(render): 게임 결과 렌더링 구현
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- src/app/view.js
+++ src/app/view.js
@@ -1,93 +1,42 @@
-import { DOM, ID_PREFIX } from '../lib/constants.js';
-import { findElement } from '../lib/utils.js';
-
 class RacingCarGameView {
   constructor() {
-    this.#init();
+    this.initDOM();
   }
 
-  #init() {
-    this.inputField = findElement(ID_PREFIX, DOM.INPUT_FIELD);
-    this.resultField = findElement(ID_PREFIX, DOM.RESULT_FIELD_ID);
-    this.#initInputField();
-    this.#initResultField();
-    this.#initDOM();
+  initDOM() {
+    this.resultField = document.querySelector('#result-field');
+    this.gameProgress = document.querySelector('#game-progress');
+    this.winners = document.querySelector('#winners');
   }
 
-  #initInputField() {
-    this.inputField.innerHTML = `<form id="car-name-input-form">
-    <label for="car-name-input">5자 이하의 자동차 이름을 콤마로 구분하여 입력해주세요.</label>
-    <div id="car-name-input-field">
-      <input id="car-name-input" type="text" />
-      <button id="car-name-btn">확인</button>
-    </div>
-  </form>  <form id="count-input-form">
-    <label for="count-input">시도할 횟수를 입력해주세요.</label>
-    <div id="count-input-field">
-      <input id="count-input" type="number" />
-      <button id="count-btn">확인</button>
-    </div>
-  </form>`;
-  }
-
-  #initResultField() {
-    this.resultField.innerHTML = `<section id="game-progress">
-    </section>
-    <section id="winners">
-      <button id="${DOM.RESTART_BTN_ID}">다시 시작하기</button> 
-    </section>`;
-  }
-
-  #initDOM() {
-    this.countInputForm = findElement(ID_PREFIX, DOM.COUNT_INPUT_FORM_ID);
-    this.gameProgress = findElement(ID_PREFIX, DOM.GAME_PROGRESS_ID);
-    this.winners = findElement(ID_PREFIX, DOM.WINNERS_ID);
-    this.carNameBtn = findElement(ID_PREFIX, DOM.CAR_NAME_BTN_ID);
-    this.countBtn = findElement(ID_PREFIX, DOM.COUNT_BTN_ID);
-    this.restartBtn = findElement(ID_PREFIX, DOM.RESTART_BTN_ID);
-  }
-
-  renderResults(cars, winners) {
-    const progressTemplate = cars.reduce(
-      (acc, car) => `${acc}${RacingCarGameView.generateProgressTemplate(car)}`,
-      ''
-    );
+  render(cars, winners) {
+    const progressTemplate = cars
+      .map((car) => RacingCarGameView.generateProgressTemplate(car))
+      .join('');
     const winnersTemplate = RacingCarGameView.generateWinnersTemplate({
       winners,
     });
 
     this.gameProgress.innerHTML = progressTemplate;
-    this.winners.insertAdjacentHTML('beforebegin', winnersTemplate);
-    this.renderRestartButton();
-  }
-
-  renderCountInputForm() {
-    this.countInputForm.style.display = 'block';
-  }
-
-  renderRestartButton() {
-    this.restartBtn.style.display = 'block';
-  }
-
-  disableInputButtons() {
-    this.carNameBtn.disabled = true;
-    this.countBtn.disabled = true;
+    this.winners.innerHTML = winnersTemplate;
   }
 
   static generateProgressTemplate({ name, progress }) {
     return `
-    <div class="${DOM.CAR_PROGRESS_CLASS}">
-      <div class="${DOM.CAR_NAME_CLASS}">${name}</div>
-      ${`<div class="${DOM.STEP_CLASS}">⬇️️</div>`.repeat(progress)}
+    <div class="car-progress">
+      <div class="car-name">${name}</div>
+      ${'<div class="step">⬇️️</div>'.repeat(progress)}
     </div>
   `;
   }
 
   static generateWinnersTemplate({ winners }) {
-    return `<h2 id="${DOM.WINNER_CONTAINER_ID}">🏆최종 승리자:<span id="${
-      DOM.WINNER_NAME_ID
-    }">${winners.join(',')}</span>🏆</h2>
-      `;
+    return `<h2 id="winner-container">🏆최종 승리자:<span id="winner-name">${winners.join(
+      ','
+    )}</span>🏆</h2>
+      <button id="restart-btn">다시 시작하기</button> `;
   }
 }
 export default RacingCarGameView;
+
+// [...new Array(progress)].map(()=>`<div class="step">⬇️️</div>`).join('')
