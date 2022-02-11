fa72a7906d93e9e6870f7e0a8de0cda84eec882b
byhhh2 <rlaalsdl111@gmail.com>
2022-02-11 16:43:50 +0900

    refactor: separate constants

--- src/js/view/winnerViewControl.js
+++ src/js/view/winnerViewControl.js
@@ -1,5 +1,6 @@
 import { racingWinnerText } from "../util/elements.js";
+import { SEPARATOR } from "../util/constants.js";
 
 export const setWinnerText = winners => {
-  racingWinnerText.innerHTML = winners.join(", ");
+  racingWinnerText.innerHTML = winners.join(`${SEPARATOR} `);
 };
