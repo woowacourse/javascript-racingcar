f9a48f04f3f5591b7477297c8f06917dbe3bfdbd
Jeongmin Woo <greenblues1190@gmail.com>
2022-02-11 16:13:43 +0900

    refactor: 이벤트 바인딩 분리

--- src/js/index.js
+++ src/js/index.js
@@ -27,12 +27,12 @@ class RacingCarGame {
     this.winners = [];
   }
 
-  trimStringArray(array) {
-    return array.map((string) => string.trim());
-  }
-
   splitCarNames() {
     return this.$carNameInput.value.split(DELIMETER);
+  }
+
+  trimStringArray(array) {
+    return array.map((string) => string.trim());
   }
 
   initializeInput(clearElement, focusElement = clearElement) {
@@ -63,80 +63,98 @@ class RacingCarGame {
     });
   }
 
+  restart() {
+    this.$carNameInput.value = '';
+    this.$racingCountInput.value = '';
+    this.$carNameInput.focus();
+
+    this.carList = [];
+    this.winners = [];
+
+    this.renderRacingResult();
+    this.renderResult();
+  }
+
+  submitCarNames() {
+    const carNameList = this.trimStringArray(this.splitCarNames());
+
+    if (
+      !carNameList.every(
+        (name) =>
+          name.length >= CAR_NAME_LENGTH_RANGE.MIN &&
+          name.length <= CAR_NAME_LENGTH_RANGE.MAX
+      )
+    ) {
+      alert(ERROR_MESSAGE.OUT_OF_CAR_NAME_LENGTH_RANGE);
+      this.initializeInput(this.$carNameInput);
+
+      return;
+    }
+
+    if (carNameList.length !== new Set(carNameList).size) {
+      alert(ERROR_MESSAGE.DUPLICATED_CAR_NAME);
+      this.initializeInput(this.$carNameInput);
+
+      return;
+    }
+
+    this.carList = carNameList.map((name) => new Car(name));
+
+    this.renderRacingResult();
+  }
+
+  submitRacingCount() {
+    const racingCount = this.$racingCountInput.valueAsNumber;
+
+    if (
+      !Number.isInteger(racingCount) ||
+      racingCount <= RACING_COUNT_RANGE.MIN ||
+      racingCount > RACING_COUNT_RANGE.MAX
+    ) {
+      alert(ERROR_MESSAGE.OUT_OF_RACING_COUNT_RANGE);
+      this.initializeInput(this.$racingCountInput);
+
+      return;
+    }
+
+    if (!this.carList.length) {
+      alert(ERROR_MESSAGE.CAR_NAME_SHOULD_COME_FIRST);
+      this.initializeInput(this.$racingCountInput, this.$carNameInput);
+
+      return;
+    }
+
+    for (let i = 0; i < racingCount; i += 1) {
+      this.carList.forEach((car) => car.race());
+      this.renderRacingResult();
+    }
+
+    const maxDistance = Math.max(...this.carList.map((car) => car.distance));
+    this.winners = this.carList
+      .filter((car) => car.distance === maxDistance)
+      .map((winner) => winner.name);
+
+    this.renderResult();
+  }
+
   main() {
-    this.bindEventListener('click', SELECTOR.$RACING_COUNT_BUTTON, () => {
-      const racingCount = this.$racingCountInput.valueAsNumber;
+    this.bindEventListener(
+      'click',
+      SELECTOR.$CAR_NAME_BUTTON,
+      this.submitCarNames.bind(this)
+    );
 
-      if (
-        !Number.isInteger(racingCount) ||
-        racingCount <= RACING_COUNT_RANGE.MIN ||
-        racingCount > RACING_COUNT_RANGE.MAX
-      ) {
-        alert(ERROR_MESSAGE.OUT_OF_RACING_COUNT_RANGE);
-        this.initializeInput(this.$racingCountInput);
+    this.bindEventListener(
+      'click',
+      SELECTOR.$RACING_COUNT_BUTTON,
+      this.submitRacingCount.bind(this)
+    );
 
-        return;
-      }
-
-      if (!this.carList.length) {
-        alert(ERROR_MESSAGE.CAR_NAME_SHOULD_COME_FIRST);
-        this.initializeInput(this.$racingCountInput, this.$carNameInput);
-
-        return;
-      }
-
-      for (let i = 0; i < racingCount; i += 1) {
-        this.carList.forEach((car) => car.race());
-        this.renderRacingResult();
-      }
-
-      const maxDistance = Math.max(...this.carList.map((car) => car.distance));
-      this.winners = this.carList
-        .filter((car) => car.distance === maxDistance)
-        .map((winner) => winner.name);
-
-      this.renderResult();
-    });
-
-    this.bindEventListener('click', SELECTOR.$CAR_NAME_BUTTON, () => {
-      const carNameList = this.trimStringArray(this.splitCarNames());
-
-      if (
-        !carNameList.every(
-          (name) =>
-            name.length >= CAR_NAME_LENGTH_RANGE.MIN &&
-            name.length <= CAR_NAME_LENGTH_RANGE.MAX
-        )
-      ) {
-        alert(ERROR_MESSAGE.OUT_OF_CAR_NAME_LENGTH_RANGE);
-        this.initializeInput(this.$carNameInput);
-
-        return;
-      }
-
-      if (carNameList.length !== new Set(carNameList).size) {
-        alert(ERROR_MESSAGE.DUPLICATED_CAR_NAME);
-        this.initializeInput(this.$carNameInput);
-
-        return;
-      }
-
-      this.carList = carNameList.map((name) => new Car(name));
-
-      this.renderRacingResult();
-    });
-
-    this.bindEventListener('click', SELECTOR.$RESTART_BUTTON, () => {
-      this.$carNameInput.value = '';
-      this.$racingCountInput.value = '';
-      this.$carNameInput.focus();
-
-      this.carList = [];
-      this.winners = [];
-
-      this.renderRacingResult();
-      this.renderResult();
-    });
+    this.bindEventListener(
+      'click',
+      SELECTOR.$RESTART_BUTTON,
+      this.restart.bind(this)
+    );
   }
 }
 
