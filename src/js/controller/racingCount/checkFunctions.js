a15e1387bfa36f5b1fbcbf8ea880aadbd42b2475
byhhh2 <rlaalsdl111@gmail.com>
2022-02-11 16:23:53 +0900

    refactor: fix restricted globals

--- src/js/controller/racingCount/checkFunctions.js
+++ src/js/controller/racingCount/checkFunctions.js
@@ -1,5 +1,5 @@
 export const isInteger = input => {
-  if (isNaN(input) || Number(input) % 1 !== 0) {
+  if (Number.isNaN(input) || Number(input) % 1 !== 0) {
     return false;
   }
   return true;
