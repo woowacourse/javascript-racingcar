4e50b22ea47bd82bc51ccd01ab4d5e8126a32131
uk960214 <uk960214@gmail.com>
2022-02-11 15:37:07 +0900

    feat(generateCarId): 자동차 아이디 생성 함수 구현
    Co-authored-by: juunzzi <juunzzi@users.noreply.github.com>

--- src/app/car.js
+++ src/app/car.js
@@ -1,22 +1,25 @@
 import { CAR_NAME_LENGTH_LIMIT, ERROR_MESSAGE } from '../lib/constants.js';
-import { checkStringLength } from '../lib/utils.js';
+import { checkStringLength, generateId } from '../lib/utils.js';
 
 class Car {
+  static carIdObj = {};
+
   constructor(name) {
-    // this.id = generateId();
-    this.init(name);
+    this.id = generateId(Car.carIdObj);
+    this.init(this.id, name);
   }
 
   goForward() {
     this.progress += 1;
   }
 
-  init(name) {
+  init(id, name) {
     if (!checkStringLength(name, CAR_NAME_LENGTH_LIMIT)) {
       throw Error(ERROR_MESSAGE.CAR_NAME_LENGTH_OVER);
     }
     this.name = name;
     this.progress = 0;
+    Car.carIdObj = { ...Car.carIdObj, [`${id}`]: true };
   }
 }
 
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,4 +4,21 @@ export const isNumberBelowZero = (number) => number <= 0;
 
 export const checkStringLength = (str, standard) => str.length <= standard;
 
-export const pickNumberInRange = (min, max) => Math.random() * (max - min) + min;
+export const pickNumberInRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+// 기능 요구 사항
+// 1. 아이디는 중복될 수 없다.
+// 2. 아이디는 6개의 숫자로 구성된 문자열이다.
+
+/**
+ * props : idArr(아이디 객체, 로직의 복잡도가 커짐을 고려)
+ * return : 아이디 문자열
+ */
+export const generateId = (idObj) => {
+  while (true) {
+    const idStr = pickNumberInRange(0, 999999).toString().padStart(6, 0);
+    if (!idObj[idStr]) {
+      return idStr;
+    }
+  }
+};
