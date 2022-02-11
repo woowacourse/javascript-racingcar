b3d2d71ba130055ee940ff902ca80d7dafcf7b80
gytks4 <gytks4@naver.com>
2022-02-11 14:36:21 +0900

    test: 자동차 이름 중복될 경우 테스트 추가

--- cypress/integration/racing.spec.js
+++ cypress/integration/racing.spec.js
@@ -1,10 +1,10 @@
 describe('UI 조작 테스트', () => {
   beforeEach(() => {
     cy.visit('index.html');
-    const alertStub = cy.stub();
-    cy.on('window:alert', alertStub);
   });
   it('자동차 이름이 공백인 경우 alert를 띄운다', () => {
+    const alertStub = cy.stub();
+    cy.on('window:alert', alertStub);
     cy.get('.input-section__name-input')
       .type('{enter}')
       .then(() => {
@@ -13,6 +13,8 @@ describe('UI 조작 테스트', () => {
   });
 
   it('자동차 이름이 6자 이상인 경우 alert를 띄운다', () => {
+    const alertStub = cy.stub();
+    cy.on('window:alert', alertStub);
     cy.get('.input-section__name-input')
       .type('abcdefg,abc,ddd{enter}')
       .then(() => {
@@ -20,7 +22,19 @@ describe('UI 조작 테스트', () => {
       });
   });
 
+  it('자동차 이름이 중복될 경우 alert를 띄운다.', () => {
+    const alertStub = cy.stub();
+    cy.on('window:alert', alertStub);
+    cy.get('.input-section__name-input')
+      .type('a, a{enter}')
+      .then(() => {
+        expect(alertStub).to.be.called;
+      });
+  });
+
   it('횟수가 0 또는 음수인 경우 alert를 띄운다', () => {
+    const alertStub = cy.stub();
+    cy.on('window:alert', alertStub);
     cy.get('.input-section__name-input').type('east,west,south,north{enter}');
     cy.get('.input-section__count-input')
       .type('-3{enter}')
@@ -36,6 +50,9 @@ describe('UI 조작 테스트', () => {
   });
 
   it('횟수가 소수점인 경우 alert를 띄운다.', () => {
+    const alertStub = cy.stub();
+    cy.on('window:alert', alertStub);
+    cy.get('.input-section__name-input').type('east,west,south,north{enter}');
     cy.get('.input-section__count-input')
       .type('2.8{enter}')
       .then(() => {
