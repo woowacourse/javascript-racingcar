3a97226c8a74df20dd9e0dd40c24d94727874e12
KIM YEJI <rladpwl0512@gmail.com>
2022-02-11 15:22:52 +0900

    test: 예외처리 테스트 코드 작성

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -1,13 +1,73 @@
+import { ERROR } from '../../src/util/constants.js';
+
 describe('racingcar Test', () => {
-  it('입력을 받고 우승자를 출력할 수 있어야 한다', () => {
+  beforeEach(() => {
+    cy.visit('index.html');
+  });
+
+  it('레이싱 실행 이후 우승자를 출력할 수 있어야 한다', () => {
     const carNames = 'east, west, south, north, all';
     const count = 5;
 
-    cy.visit('index.html');
     cy.get('.name-input').type(carNames);
     cy.get('.name-button').click();
 
     cy.get('.count-input').type(count);
     cy.get('.count-button').click();
+
+    cy.get('.winners-name').should('be.visible');
+  });
+
+  it('다시 시작하기 버튼 클릭시 화면이 리셋 돼야 한다.', () => {
+    const carNames = 'east, west, south, north, all';
+    const count = 5;
+
+    cy.get('.name-input').type(carNames);
+    cy.get('.name-button').click();
+
+    cy.get('.count-input').type(count);
+    cy.get('.count-button').click();
+
+    cy.get('.restart').click();
+
+    cy.get('.count-form').should('not.be.visible');
+    cy.get('.game-result-container').should('not.be.visible');
+    cy.get('.restart-container').should('not.be.visible');
+  });
+
+  it('자동차 이름이 5자 초과면 에러 메세지가 뜬다.', () => {
+    const invalid = 'aaabbb,ff,gg,hh';
+
+    cy.get('.name-input').type(invalid);
+    cy.get('.name-button').click();
+
+    cy.on('window:alert', text => {
+      expect(text).to.equal(ERROR.LONGER_THAN_FIVE);
+    });
+  });
+
+  it('자동차 이름이 공백으로만 이루어지면 에러메세지가 뜬다.', () => {
+    const invalid = '   ,ff,gg,hh';
+
+    cy.get('.name-input').type(invalid);
+    cy.get('.name-button').click();
+
+    cy.on('window:alert', text => {
+      expect(text).to.equal(ERROR.IS_BLANK);
+    });
+  });
+
+  it('반복할 횟수가 1미만이면 에러메세지가 뜬다.', () => {
+    const carNames = 'east, west, south, north, all';
+    const invalid = '-3';
+
+    cy.get('.name-input').type(carNames);
+    cy.get('.name-button').click();
+    cy.get('.count-input').type(invalid);
+    cy.get('.count-button').click();
+
+    cy.on('window:alert', text => {
+      expect(text).to.equal(ERROR.MIN_COUNT);
+    });
   });
 });
