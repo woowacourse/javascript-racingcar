37dae3ba6db15c8c5156ed03f7fd82aae5596373
jswith <9geul@naver.com>
2022-02-12 22:32:51 +0900

    refactor: validator 구조 변경

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,7 +1,7 @@
 import { $, $$ } from '../utils/dom.js';
 import Model from '../model/model.js';
 import View from '../view/view.js';
-import Validator from '../validator/validator.js';
+import { Validator } from '../validator/validator.js';
 import { NUMBER, SELECTOR } from '../utils/constants.js';
 
 export default class Controller {
--- js/validator/validator.js
+++ js/validator/validator.js
@@ -1,63 +1,34 @@
 import { NUMBER, ERROR_MSG } from '../utils/constants.js';
 
-export default class Validator {
-  static isCarNameBlank(carNames) {
+export const Validator = {
+  isCarNameBlank(carNames) {
     const filtered = carNames.filter((name) => name === '');
     if (filtered.length > NUMBER.ZERO) {
       alert(ERROR_MSG.BLANK_NAME);
       return true;
     }
-  }
+  },
 
-  static isInvalidCarNameLength(carNames) {
+  isInvalidCarNameLength(carNames) {
     const filtered = carNames.filter((name) => name.length > NUMBER.MAX_LENGTH);
     if (filtered.length > NUMBER.ZERO) {
       alert(ERROR_MSG.INVALID_NAME_LENGTH);
       return true;
     }
-  }
+  },
 
-  static isInValidCarNamesInput(carNames) {
-    if (this.isCarNameBlank(carNames)) {
-      return true;
-    }
-    if (this.isInvalidCarNameLength(carNames)) {
-      return true;
-    }
-    return false;
-  }
+  isInValidCarNamesInput(carNames) {
+    return ['isCarNameBlank', 'isInvalidCarNameLength'].some((method) => this[method](carNames));
+  },
 
-  static isFloat(number) {
-    if (!Number.isInteger(Number(number))) {
-      alert(ERROR_MSG.INVALID_RACING_COUNT);
-      return true;
-    }
-  }
-
-  static isNotNaturalNumber(number) {
+  isNotNaturalNumber(number) {
     if (number <= NUMBER.ZERO) {
       alert(ERROR_MSG.INVALID_RACING_COUNT);
       return true;
     }
-  }
+  },
 
-  static isString(number) {
-    if (typeof Number(number) !== 'number') {
-      alert(ERROR_MSG.INVALID_RACING_COUNT);
-      return true;
-    }
-  }
-
-  static isInValidRacingCountInput(number) {
-    if (this.isFloat(number)) {
-      return true;
-    }
-    if (this.isNotNaturalNumber(number)) {
-      return true;
-    }
-    if (this.isString(number)) {
-      return true;
-    }
-    return false;
-  }
-}
+  isInValidRacingCountInput(number) {
+    return this.isNotNaturalNumber(number);
+  },
+};
