5cf09477dbd7f634b657ec2b8cd03ff90f39d7aa
InKyoJeong <viapolar@naver.com>
2022-02-12 19:16:52 +0900

    refactor: 구조분해할당 사용

--- src/controller/handleCountInput.js
+++ src/controller/handleCountInput.js
@@ -1,18 +1,19 @@
 import { ERROR, LIMIT } from '../util/constants.js';
 import playRace from './playRace.js';
 
-function isValidCount(inputCount) {
-  if (+inputCount < LIMIT.MIN_LENGTH) {
+function isValidCount(count) { 
+  if (+count < LIMIT.MIN_LENGTH) {
     alert(ERROR.MIN_COUNT);
     return false;
   }
+
   return true;
 }
 
 export default function handleCountInput() {
-  const inputCount = document.querySelector('.count-input').value;
+  const { value } = document.querySelector('.count-input');
 
-  if (isValidCount(inputCount)) {
-    playRace(inputCount);
+  if (isValidCount(value)) {
+    playRace(value);
   }
 }
