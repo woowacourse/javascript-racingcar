47c4570221572ab6d1a4908ac7ec3f4fc1d3a669
Ahn <jin7969@gmail.com>
2022-02-12 11:33:54 +0900

    refactor: 시도할 횟수를 입력받는 기능 리팩토링

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,56 @@
 import $ from './utils/dom.js';
-import { ERR_MESSAGE } from './utils/constants.js';
+import { ERR_MESSAGE, GAME } from './utils/constants.js';
 import { isValidLength, isBlank } from './utils/validation.js';
 import { showCountInput } from './views/setScreen.js';
 
 function App() {
-  this.car = [];
+  this.carName = [];
   this.count = 0;
 
-  const isValidCarNames = (carNames) => {
-    if (!carNames.every(isValidLength)) {
+  const isValidCarNames = (carName) => {
+    if (!carName.every(isValidLength)) {
       alert(ERR_MESSAGE.NAME_TOO_LONG);
       return false;
     }
-    if (!carNames.every(isBlank)) {
+    if (!carName.every(isBlank)) {
       alert(ERR_MESSAGE.NAME_CANNOT_BE_BLANK);
       return false;
     }
     return true;
   };
 
+  const isValidRacingCount = (count) => {
+    if (count < GAME.MIN_INPUT_COUNT) {
+      alert(ERR_MESSAGE.COUNT_TOO_SMALL);
+      return false;
+    }
+    return true;
+  };
+
   const handleCarNamesSubmit = () => {
-    const carNames = $('#car-names-input')
+    this.carName = $('#car-names-input')
       .value.split(',')
       .map((car) => car.trim());
-
-    if (!isValidCarNames(carNames)) {
+    if (!isValidCarNames(this.carName)) {
       $('#car-names-input').value = '';
       return;
     }
     showCountInput();
   };
 
+  const handleRacingCountSubmit = () => {
+    this.count = $('#racing-count-input').value;
+
+    if (!isValidRacingCount(this.count)) {
+      return;
+    }
+    console.log(this.count);
+    // startRacingGame(this.count);
+  };
+
   $('#car-names-button').addEventListener('click', handleCarNamesSubmit);
 
-  // $('#racing-count-button').addEventListener('click', handleRacingCountSubmit);
+  $('#racing-count-button').addEventListener('click', handleRacingCountSubmit);
 }
 
 // eslint-disable-next-line no-new
