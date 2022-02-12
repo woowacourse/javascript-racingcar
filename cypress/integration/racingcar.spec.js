2b8437a6ef5871edd82642a8560334f8ef53a58b
jswith <9geul@naver.com>
2022-02-12 21:04:43 +0900

    test: 테스트 코드 수정

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -5,20 +5,6 @@ describe('자동차 경주 게임', () => {
     cy.visit('http://localhost:5500/');
   });
 
-  it('우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.', () => {
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('a,b,c,d');
-    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(1);
-
-    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON)
-      .click()
-      .then(() => {
-        for (let i = 0; i < 200; i++) {
-          cy.get(SELECTOR.CAR_RACING_WINNER).contains(',');
-        }
-      });
-  });
-
   it('자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.', () => {
     const alertStub = cy.stub();
     cy.get(SELECTOR.CAR_NAMES_INPUT).type('easteee,west');
@@ -44,7 +30,7 @@ describe('자동차 경주 게임', () => {
 
     cy.get(SELECTOR.CAR_NAMES_INPUT).type('a,b,c');
     cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(-3);
+    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(1.3);
     cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON)
       .click()
       .then(() => {
@@ -66,21 +52,7 @@ describe('자동차 경주 게임', () => {
       });
   });
 
-  it('시도할 횟수가 문자열이면 alert을 띄운다.', () => {
-    const alertStub = cy.stub();
-    cy.on(SELECTOR.WINDOW_ALERT, alertStub);
-
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('a,b,c');
-    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type('a');
-    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON)
-      .click()
-      .then(() => {
-        expect(alertStub).to.be.called;
-      });
-  });
-
-  it('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
+  it('사용자가 몇 번의 이동을 할 것인지를 정상적으로 입력하면 다시 시작하기 버튼이 보여야 한다.', () => {
     cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west');
     cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
     cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(5);
