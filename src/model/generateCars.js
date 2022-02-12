1ff8f3fd7ede01407cd971a53752249526f4110e
InKyoJeong <viapolar@naver.com>
2022-02-12 19:04:24 +0900

    refactor: generateCar 함수 수정 forEach를 map으로 변경

--- src/model/generateCars.js
+++ src/model/generateCars.js
@@ -1,15 +1,10 @@
 import Car from './Car.js';
 
 export default function generateCars() {
-  const cars = [];
   const names = document
     .querySelector('.name-input')
     .value.split(',')
     .map(name => name.trim());
 
-  names.forEach(name => {
-    cars.push(new Car(name));
-  });
-
-  return cars;
+  return names.map(name => new Car(name));
 }
