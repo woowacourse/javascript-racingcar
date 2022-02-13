d41d351af160dfcb33c4080383a6c9e4cf26c751
LAH1203 <lah1203@naver.com>
2022-02-13 17:36:12 +0900

    refactor: combine cars name check functions

--- src/js/controller/carsName/checkFunctions.js
+++ src/js/controller/carsName/checkFunctions.js
@@ -1,40 +1,11 @@
 import { CAR_NAME_LENGTH } from "../../util/constants.js";
 
-const isLengthOK = carName => {
+export const isValidCarsName = carNameArr => {
   const { MIN, MAX } = CAR_NAME_LENGTH;
 
-  return carName.length >= MIN && carName.length <= MAX;
-};
-
-export const isValidLength = carNameArr => {
-  for (let i = 0; i < carNameArr.length; i++) {
-    carNameArr[i] = carNameArr[i].trim();
-    if (!carNameArr[i] || !isLengthOK(carNameArr[i])) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-export const isDuplicateName = carNameArr => {
-  const carNameSet = new Set(carNameArr);
-
-  if (carNameArr.length !== carNameSet.size) {
-    return true;
-  }
-
-  return false;
-};
-
-export const isValidCarsName = carNameArr => {
-  if (
-    carNameArr.length <= 1 ||
-    isDuplicateName(carNameArr) ||
-    !isValidLength(carNameArr)
-  ) {
-    return false;
-  }
-
-  return true;
+  return (
+    carNameArr.length >= 2 &&
+    new Set(carNameArr).size === carNameArr.length &&
+    carNameArr.every(carName => carName.length >= MIN && carName.length <= MAX)
+  );
 };
