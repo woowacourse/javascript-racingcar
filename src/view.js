1123dfb679bc00d448f605cf20e4df1e7d0dd3a3
kamwoo <rkadndud06@naver.com>
2022-02-14 01:39:32 +0900

    refactor: view에 element id 또는 class 이름 SELECTOR 상수로 변경

--- src/view.js
+++ src/view.js
@@ -1,4 +1,5 @@
 import { $, makeDOMHidden, makeDOMVisible } from './utils/common.js';
+import { SELECTOR } from './utils/constants.js';
 
 export default class View {
   constructor() {
@@ -8,14 +9,14 @@ export default class View {
   }
 
   configureDOM() {
-    this.$nameInput = $('.input-section__name-input');
-    this.$countInput = $('.input-section__count-input');
-    this.$nameButton = $('.input-section__name-button');
-    this.$countButton = $('.input-section__count-button');
-    this.$countSubmitContainer = $('#count-submit-container');
-    this.$stepSections = $('.step-sections');
-    this.$winner = $('#winner');
-    this.$resetButton = $('#reset-button');
+    this.$nameInput = $(SELECTOR.NAME_INPUT);
+    this.$countInput = $(SELECTOR.COUNT_INPUT);
+    this.$nameButton = $(SELECTOR.NAME_BUTTON);
+    this.$countButton = $(SELECTOR.COUNT_BUTTON);
+    this.$countSubmitContainer = $(SELECTOR.COUNT_SUBMIT_CONTAINER);
+    this.$stepSections = $(SELECTOR.STEP_SECTIONS);
+    this.$winner = $(SELECTOR.WINNER);
+    this.$resetButton = $(SELECTOR.RESET_BUTTON);
   }
 
   makeDOMReset() {
