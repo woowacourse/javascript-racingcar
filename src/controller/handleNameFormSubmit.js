62d8dfb4908569edbe51f25ebb900c0b30d3f3fb
KIM YEJI <rladpwl0512@gmail.com>
2022-02-13 17:10:09 +0900

    refactor: isValidNames 함수에서 alert하는 기능을 alertError 함수로 분리

--- src/controller/handleNameFormSubmit.js
+++ src/controller/handleNameFormSubmit.js
@@ -1,12 +1,13 @@
 import { ERROR, LIMIT } from '../util/constants.js';
+import alertError from '../view/alertError.js';
 
 function isValidNames(names) {
   if (names.some(name => name.length > LIMIT.MAX_LENGTH)) {
-    alert(ERROR.LONGER_THAN_FIVE);
+    alertError(ERROR.LONGER_THAN_FIVE);
     return false;
   }
   if (names.some(name => name.length < LIMIT.MIN_LENGTH)) {
-    alert(ERROR.IS_BLANK);
+    alertError(ERROR.IS_BLANK);
     return false;
   }
   return true;
