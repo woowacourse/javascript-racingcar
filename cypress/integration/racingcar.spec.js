af8d576f314d99029bfb74cf3adcb347a17a4def
KIM YEJI <rladpwl0512@gmail.com>
2022-02-13 16:51:56 +0900

    refactor: 중복되는 코드 beforeEach에서 처리

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -1,38 +1,34 @@
 import { ERROR } from '../../src/util/constants.js';
 
-describe('racingcar Test', () => {
+describe('자동차 경주 게임을 진행할 수 있다.', () => {
   beforeEach(() => {
-    cy.visit('index.html');
-  });
-
-  it('레이싱 실행 이후 우승자를 출력할 수 있어야 한다', () => {
     const carNames = 'east, west, south, north, all';
     const count = 5;
+
+    cy.visit('index.html');
 
     cy.get('.name-input').type(carNames);
     cy.get('.name-button').click();
 
     cy.get('.count-input').type(count);
     cy.get('.count-button').click();
+  });
 
+  it('레이싱 실행 이후 우승자를 출력할 수 있어야 한다', () => {
     cy.get('.winners-name').should('be.visible');
   });
 
   it('다시 시작하기 버튼 클릭시 화면이 리셋 돼야 한다.', () => {
-    const carNames = 'east, west, south, north, all';
-    const count = 5;
-
-    cy.get('.name-input').type(carNames);
-    cy.get('.name-button').click();
-
-    cy.get('.count-input').type(count);
-    cy.get('.count-button').click();
-
     cy.get('.restart').click();
-
     cy.get('.count-form').should('not.be.visible');
     cy.get('.game-result-container').should('not.be.visible');
     cy.get('.restart-container').should('not.be.visible');
+  });
+});
+
+describe('예외 상황에서 적절한 에러메시지를 출력한다.', () => {
+  beforeEach(() => {
+    cy.visit('index.html');
   });
 
   it('자동차 이름이 5자 초과면 에러 메세지가 뜬다.', () => {
