a34dd6032961cfee4bfa315fcebc6ee8f7c0366a
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-13 23:32:41 +0900

    style: 사용하지 않는 아이디 제거

--- src/js/constants/selector.js
+++ src/js/constants/selector.js
@@ -1,8 +1,6 @@
 const SELECTOR = Object.freeze({
-  CAR_NAME_FORM: '#car-name-form',
   CAR_NAME_INPUT: '#car-name-input',
   CAR_NAME_BUTTON: '#car-name-button',
-  RACE_TIME_FORM: '#race-time-form',
   RACE_TIME_INPUT: '#race-time-input',
   RACE_TIME_BUTTON: '#race-time-button',
   RACE_CONTAINER_DIV: '#racing-car-container',
