ce34444b40b340244910c03ff88585a3fb2b1cf1
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-14 00:15:33 +0900

    test: 테스트 코드 E2E 테스트 파트 완성

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,55 +1,76 @@
-/*
-  리팩터링 하기
-  - 선택자 상수 분리.
-*/
+import { SELECTOR } from '../src/js/constants/selector.js';
 
-import SELECTOR from '../src/js/constants/selector.js';
-
-describe('기본 사용 순서 체크', () => {
+describe('기본 사용 순서 체크 (E2E)', () => {
   before(() => {
     cy.visit('./');
   });
 
-  it('자동차의 이름을 입력 후 입력란과 버튼이 비활성화 되어야한다.', () => {
+  it('자동차의 이름을 입력할 수 있어야한다.', () => {
     // 입력 & 클릭
-    cy.get('#car-name-input').type('compy, usage');
-    cy.get('#car-name-button').click();
+    cy.get(SELECTOR.CAR_NAME_INPUT).type('compy, usage');
+    cy.get(SELECTOR.CAR_NAME_BUTTON).click();
 
     // 비활성화 체크
-    cy.get('#car-name-input')
+    cy.get(SELECTOR.CAR_NAME_INPUT)
       .invoke('attr', 'disabled')
       .should('eq', 'disabled');
-    cy.get('#car-name-button')
+    cy.get(SELECTOR.CAR_NAME_BUTTON)
       .invoke('attr', 'disabled')
       .should('eq', 'disabled');
   });
 
-  it('이동 횟수 입력 후 입력란과 버튼이 비활성화 되어야한다.', () => {
+  it('이동 횟수를 입력할 수 있어야한다.', () => {
     // 입력 & 클릭
-    cy.get('#race-time-input').type('5');
-    cy.get('#race-time-button').click();
+    cy.get(SELECTOR.RACE_TIME_INPUT).type('5');
+    cy.get(SELECTOR.RACE_TIME_BUTTON).click();
 
     // 비활성화 체크
-    cy.get('#race-time-input')
+    cy.get(SELECTOR.RACE_TIME_INPUT)
       .invoke('attr', 'disabled')
       .should('eq', 'disabled');
-    cy.get('#race-time-button')
+    cy.get(SELECTOR.RACE_TIME_BUTTON)
       .invoke('attr', 'disabled')
       .should('eq', 'disabled');
   });
 
-  it('자동차의 전진 기준은 0에서 9 사이의 무작위 값을 구한 후 값이 4 이상일 시 전진한다.', () => {
-    // 랜덤 함수 출력 테스트
-    // 전진 테스트
+  it('자동차 이름과 이동 횟수를 입력했다면 결과가 출력되어야 한다.', () => {
+    cy.get(SELECTOR.RACE_CONTAINER)
+      .invoke('attr', 'data-state')
+      .should('eq', 'on');
+
+    cy.get(SELECTOR.RESULT_CONTAINER)
+      .invoke('attr', 'data-state')
+      .should('eq', 'on');
+
+    cy.get(SELECTOR.RACE_ADVANCE).contains('⬇️️');
+    cy.get(SELECTOR.WINNERS).contains('최종 우승자');
   });
 
-  it('자동차의 전진 횟수에 따라 화살표를 표시한다.', () => {});
+  it('게임을 재시작할 수 있어야 한다.', () => {
+    cy.get(SELECTOR.RETRY_BUTTON).click();
 
-  it('전진 횟수가 가장 많은 자동차는 우승자가 된다.', () => {});
+    cy.get(SELECTOR.CAR_NAME_INPUT)
+      .invoke('attr', 'disabled')
+      .should('eq', undefined);
+    cy.get(SELECTOR.CAR_NAME_BUTTON)
+      .invoke('attr', 'disabled')
+      .should('eq', undefined);
+
+    cy.get(SELECTOR.CAR_NAME_INPUT).should('have.text', '');
+
+    cy.get(SELECTOR.RACE_TIME_INPUT)
+      .invoke('attr', 'disabled')
+      .should('eq', undefined);
+    cy.get(SELECTOR.RACE_TIME_BUTTON)
+      .invoke('attr', 'disabled')
+      .should('eq', undefined);
+
+    cy.get(SELECTOR.RACE_TIME_INPUT).should('have.text', '');
+  });
 });
 
 /*
-describe('입력 예외사항 체크', () => {
+describe('입력 예외사항 체크 (Unit)', () => {
   before(() => {
     cy.visit('./');
   });
