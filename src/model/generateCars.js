307c4472ee3bdeb505a462ae747a4917fda12f79
KIM YEJI <rladpwl0512@gmail.com>
2022-02-13 17:18:46 +0900

    refactor: generateCars에서 map 메소드 활용

--- src/model/generateCars.js
+++ src/model/generateCars.js
@@ -1,15 +1,12 @@
 import Car from './Car.js';
 
 export default function generateCars() {
-  const cars = [];
   const names = document
     .querySelector('.name-input')
     .value.split(',')
     .map(name => name.trim());
 
-  names.forEach(name => {
-    cars.push(new Car(name));
+  return names.map(name => {
+    return new Car(name);
   });
-
-  return cars;
 }
