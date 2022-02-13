29cc57061ca58175d90f69be0b53eb63b2e05b2e
kamwoo <rkadndud06@naver.com>
2022-02-14 01:45:31 +0900

    refactor: Car 클래스 내부에서만 사용하는 메소드 private 접근자 추가

--- src/car.js
+++ src/car.js
@@ -1,16 +1,17 @@
-import { NUMBER } from "./utils/constants.js"
+import { NUMBER } from './utils/constants.js';
+
 export default class Car {
   constructor(name) {
     this.name = name;
     this.step = NUMBER.INITIAL_STEP;
   }
 
-  generateRandomNumber() {
+  #generateRandomNumber() {
     return MissionUtils.Random.pickNumberInRange(NUMBER.RANGE_START, NUMBER.RANGE_END);
   }
 
   randomMove() {
-    const number = this.generateRandomNumber();
+    const number = this.#generateRandomNumber();
     if (number >= NUMBER.STEP_BASE_NUMBER) {
       this.step += NUMBER.STEP;
     }
