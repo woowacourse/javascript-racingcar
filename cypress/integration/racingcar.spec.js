67af1ee268a4a91901b32d4265022d0a6b55e59d
jhy979 <jhy979@gmail.com>
2022-02-13 14:08:55 +0900

    refactor: cypress 테스트 그룹화하기

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -1,15 +1,40 @@
 import { ERROR_MSG, SELECTOR } from '../../js/utils/constants.js';
 
-describe('자동차 경주 게임', () => {
+describe('자동차의 전진이 정상적으로 작동하는지 검증합니다.', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/');
+    cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west');
+    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
+    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(5);
+  });
+
+  it('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
+    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
+    cy.contains('다시 시작하기').should('be.visible');
+  });
+
+  it('자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
+    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
+    cy.get(SELECTOR.CAR_NAMES).should('have.text', 'eastwest');
+  });
+
+  it('주어진 횟수 동안 n 대의 자동차는 전진 또는 멈출 수 있다.', () => {
+    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(40);
+    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
+    cy.get(SELECTOR.CAR_PROGRESS).contains('⬇️️');
+  });
+});
+
+describe('최종 우승자를 검증합니다.', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5500/');
+    cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west,south,north');
+    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
+    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(1);
+    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
   });
 
   it('우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.', () => {
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('a,b,c,d');
-    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(1);
-
     cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON)
       .click()
       .then(() => {
@@ -19,11 +44,27 @@ describe('자동차 경주 게임', () => {
       });
   });
 
+  it('최종 우승자를 출력할 수 있다.', () => {
+    cy.get(SELECTOR.CAR_RACING_WINNER).contains('최종 우승자');
+  });
+});
+
+describe('사용자의 입력을 검증합니다.', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5500/');
+  });
+
   it('자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.', () => {
     const alertStub = cy.stub();
+    cy.on(SELECTOR.WINDOW_ALERT, alertStub);
+
     cy.get(SELECTOR.CAR_NAMES_INPUT).type('easteee,west');
     cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.on(SELECTOR.WINDOW_ALERT, alertStub);
+    cy.get(SELECTOR.CAR_NAMES_BUTTON)
+      .click()
+      .then(() => {
+        expect(alertStub).to.be.calledWith(ERROR_MSG.INVALID_CAR_NAME);
+      });
   });
 
   it('자동차 이름이 빈칸이면 alert을 띄운다.', () => {
@@ -78,37 +119,5 @@ describe('자동차 경주 게임', () => {
       .then(() => {
         expect(alertStub).to.be.calledWith(ERROR_MSG.INVALID_RACING_COUNT);
       });
-  });
-
-  it('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west');
-    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(5);
-    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
-    cy.contains('다시 시작하기').should('be.visible');
-  });
-
-  it('자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west');
-    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(5);
-    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
-    cy.get(SELECTOR.CAR_NAMES).should('have.text', 'eastwest');
-  });
-
-  it('주어진 횟수 동안 n 대의 자동차는 전진 또는 멈출 수 있다.', () => {
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west,south,north');
-    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(40);
-    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
-    cy.get(SELECTOR.CAR_PROGRESS).contains('⬇️️');
-  });
-
-  it('최종 우승자를 출력할 수 있다.', () => {
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west,south,north');
-    cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(5);
-    cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
-    cy.get(SELECTOR.CAR_RACING_WINNER).contains('최종 우승자');
   });
 });
