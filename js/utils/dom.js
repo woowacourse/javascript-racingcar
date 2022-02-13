269cd7a93358eae44560d5ddb0338f3953bc070d
jhy979 <jhy979@gmail.com>
2022-02-14 08:03:02 +0900

    refactor: 틁정 element부터 DOM 탐색하는 함수 추가 구현

--- js/utils/dom.js
+++ js/utils/dom.js
@@ -1,2 +1,3 @@
 export const $ = (selector) => document.querySelector(selector);
 export const $$ = (selector) => document.querySelectorAll(selector);
+export const $$$ = (selector, baseEl) => baseEl.querySelector(selector);
