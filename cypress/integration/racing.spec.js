7f0bbeb73d91e239605ae08dadf78cec3ae47f1e
kamwoo <rkadndud06@naver.com>
2022-02-14 01:44:14 +0900

    refactor: 테스트 코드 각 맥락에대한 설명 추가 및 element constant로 변경

--- cypress/integration/racing.spec.js
+++ cypress/integration/racing.spec.js
@@ -1,21 +1,22 @@
-describe('UI 조작 테스트', () => {
+import { SELECTOR } from '../../src/utils/constants.js';
+
+describe('입력된 자동차 이름과 시도할 횟수가 조건에 맞지 않을 때 alert가 나타나는지 테스트', () => {
   beforeEach(() => {
     cy.visit('index.html');
   });
-  it('자동차 이름이 공백인 경우 alert를 띄운다', () => {
+
+  it('자동차 이름이 공백이거나, 5자를 넘을 경우 alert를 띄운다', () => {
     const alertStub = cy.stub();
     cy.on('window:alert', alertStub);
-    cy.get('.input-section__name-input')
+    cy.get(SELECTOR.NAME_INPUT)
       .type('{enter}')
       .then(() => {
         expect(alertStub).to.be.called;
       });
-  });
 
-  it('자동차 이름이 6자 이상인 경우 alert를 띄운다', () => {
-    const alertStub = cy.stub();
-    cy.on('window:alert', alertStub);
-    cy.get('.input-section__name-input')
+    cy.get(SELECTOR.NAME_INPUT).clear();
+
+    cy.get(SELECTOR.NAME_INPUT)
       .type('abcdefg,abc,ddd{enter}')
       .then(() => {
         expect(alertStub).to.be.called;
@@ -25,23 +26,23 @@ describe('UI 조작 테스트', () => {
   it('자동차 이름이 중복될 경우 alert를 띄운다.', () => {
     const alertStub = cy.stub();
     cy.on('window:alert', alertStub);
-    cy.get('.input-section__name-input')
+    cy.get(SELECTOR.NAME_INPUT)
       .type('a, a{enter}')
       .then(() => {
         expect(alertStub).to.be.called;
       });
   });
 
-  it('횟수가 0 또는 음수인 경우 alert를 띄운다', () => {
+  it('시도할 횟수가 0 또는 음수인 경우 alert를 띄운다', () => {
     const alertStub = cy.stub();
     cy.on('window:alert', alertStub);
-    cy.get('.input-section__name-input').type('east,west,south,north{enter}');
-    cy.get('.input-section__count-input')
+    cy.get(SELECTOR.NAME_INPUT).type('east,west,south,north{enter}');
+    cy.get(SELECTOR.COUNT_INPUT)
       .type('-3{enter}')
       .then(() => {
         expect(alertStub).to.be.called;
       });
-    cy.get('.input-section__count-input')
+    cy.get(SELECTOR.COUNT_INPUT)
       .clear()
       .type('0{enter}')
       .then(() => {
@@ -49,11 +50,11 @@ describe('UI 조작 테스트', () => {
       });
   });
 
-  it('횟수가 소수점인 경우 alert를 띄운다.', () => {
+  it('시도할 횟수가 소수점인 경우 alert를 띄운다.', () => {
     const alertStub = cy.stub();
     cy.on('window:alert', alertStub);
-    cy.get('.input-section__name-input').type('east,west,south,north{enter}');
-    cy.get('.input-section__count-input')
+    cy.get(SELECTOR.NAME_INPUT).type('east,west,south,north{enter}');
+    cy.get(SELECTOR.COUNT_INPUT)
       .type('2.8{enter}')
       .then(() => {
         expect(alertStub).to.be.called;
@@ -61,45 +62,54 @@ describe('UI 조작 테스트', () => {
   });
 });
 
-context('화면표시 테스트', () => {
+context('입력이 완료된 후, 레이싱 결과와 우승자, 다시 시작 버튼이 올바르게 나타는지 테스트', () => {
   beforeEach(() => {
     cy.visit('index.html');
-    cy.get('.input-section__name-input').type('east,west,south,north{enter}');
-    cy.get('.input-section__count-input').type('2{enter}');
+    cy.get(SELECTOR.NAME_INPUT).type('east,west,south,north{enter}');
+    cy.get(SELECTOR.COUNT_INPUT).type('2{enter}');
   });
+
   it('자동차 이름과 횟수를 입력하면, 자동차 이름이 순서에 맞게 결과로 표시된다.', () => {
-    cy.get('.step-sections > .step-section').eq(0).children().eq(0).should('have.text', 'east');
-    cy.get('.step-sections > .step-section').eq(1).children().eq(0).should('have.text', 'west');
-    cy.get('.step-sections > .step-section').eq(2).children().eq(0).should('have.text', 'south');
-    cy.get('.step-sections > .step-section').eq(3).children().eq(0).should('have.text', 'north');
+    cy.get(`${SELECTOR.STEP_SECTIONS} > ${SELECTOR.STEP_SECTION}`).eq(0).children().eq(0).should('have.text', 'east');
+    cy.get(`${SELECTOR.STEP_SECTIONS} > ${SELECTOR.STEP_SECTION}`).eq(1).children().eq(0).should('have.text', 'west');
+    cy.get(`${SELECTOR.STEP_SECTIONS} > ${SELECTOR.STEP_SECTION}`).eq(2).children().eq(0).should('have.text', 'south');
+    cy.get(`${SELECTOR.STEP_SECTIONS} > ${SELECTOR.STEP_SECTION}`).eq(3).children().eq(0).should('have.text', 'north');
   });
 
   it('자동차 이름과 횟수를 입력하면, 레이싱 경기 우승자가 표시된다.', () => {
-    cy.get('#winner').should((p) => {
+    cy.get(SELECTOR.WINNER).should((p) => {
       expect(p).to.contain('최종 우승자');
     });
   });
 
   it('자동차 이름과 횟수를 입력하면, "다시 시작하기" 버튼이 표시된다.', () => {
-    cy.get('#reset-button').should('be.visible');
+    cy.get(SELECTOR.RESET_BUTTON).should('be.visible');
+  });
+});
+
+context('"다시 시작하기 버튼을 클릭했을 때 초기 화면의 상태로 리셋되는지 테스트', () => {
+  beforeEach(() => {
+    cy.visit('index.html');
+    cy.get(SELECTOR.NAME_INPUT).type('east,west,south,north{enter}');
+    cy.get(SELECTOR.COUNT_INPUT).type('2{enter}');
   });
 
   it('"다시 시작하기" 버튼을 클릭하면, 레이싱 결과와 다시 시작하기 버튼이 사라진다.', () => {
-    cy.get('#reset-button')
+    cy.get(SELECTOR.RESET_BUTTON)
       .click()
       .then(() => {
-        cy.get('.step-sections').should('not.be.visible');
-        cy.get('#winner').should('not.be.visible');
-        cy.get('#reset-button').should('not.be.visible');
+        cy.get(SELECTOR.STEP_SECTIONS).should('not.be.visible');
+        cy.get(SELECTOR.WINNER).should('not.be.visible');
+        cy.get(SELECTOR.RESET_BUTTON).should('not.be.visible');
       });
   });
 
   it('"다시 시작하기" 버튼을 클릭하면, input들을 초기화한다.', () => {
-    cy.get('#reset-button')
+    cy.get(SELECTOR.RESET_BUTTON)
       .click()
       .then(() => {
-        cy.get('.input-section__name-input').should('have.value', '');
-        cy.get('.input-section__count-input').should('have.value', '');
+        cy.get(SELECTOR.NAME_INPUT).should('have.value', '');
+        cy.get(SELECTOR.COUNT_INPUT).should('have.value', '');
       });
   });
 });
