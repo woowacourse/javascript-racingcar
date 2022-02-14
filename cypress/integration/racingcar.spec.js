496a2b6b2f4f3a0c457550219d82d71a92596c15
JihyeokUm <75592315+Jihyeok-Um@users.noreply.github.com>
2022-02-14 22:27:54 +0900

    feat: alert의 내용을 검증하는 부분 추가

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -1,13 +1,15 @@
+import { alertMessage } from '../../src/js/constants/string';
+
 const CAR_NAMES_BUTTON = '#car-names-button';
 const TRY_COUNT_BUTTON = '#try-count-button';
 
-Cypress.Commands.add('checkDisplayAlert', button => {
+Cypress.Commands.add('checkDisplayAlert', (button, alertMessage) => {
   const alertStub = cy.stub();
   cy.on('window:alert', alertStub);
   cy.get(button)
     .click()
     .then(() => {
-      expect(alertStub).to.be.called;
+      expect(alertStub).to.be.calledWith(alertMessage);
     });
 });
 
@@ -19,35 +21,21 @@ it('자동차의 이름이 5자 초과일 때 확인버튼을 누를 시, alert 
   const invalidInput = 'abcdef,ab';
 
   cy.get('#car-names-input').type(invalidInput);
-  cy.checkDisplayAlert(CAR_NAMES_BUTTON);
+  cy.checkDisplayAlert(CAR_NAMES_BUTTON, alertMessage.overMaxCarNameLength);
 });
 
 it('자동차 이름에 중복이 존재할 때 확인버튼을 누를 시, alert 띄우기', () => {
   const invalidInput = 'ab,ab';
 
   cy.get('#car-names-input').type(invalidInput);
-  cy.checkDisplayAlert(CAR_NAMES_BUTTON);
-});
-
-it('자동차가 1대 이하일 때 확인버튼을 누를 시, alert 띄우기', () => {
-  const invalidInput = 'a';
-
-  cy.get('#car-names-input').type(invalidInput);
-  cy.checkDisplayAlert(CAR_NAMES_BUTTON);
-});
-
-it('`,`뒤에 자동차 이름이 입력되지 않았을 때 확인버튼을 누를 시, alert 띄우기', () => {
-  const invalidInput = 'abcf,';
-
-  cy.get('#car-names-input').type(invalidInput);
-  cy.checkDisplayAlert(CAR_NAMES_BUTTON);
+  cy.checkDisplayAlert(CAR_NAMES_BUTTON, alertMessage.OverlapCarNames);
 });
 
 it('시도 횟수 입력된 숫자가 1이상의 수가 아닐 시, alert 띄우기', () => {
   const invalidInput = -1;
 
   cy.get('#try-count-input').type(invalidInput);
-  cy.checkDisplayAlert(TRY_COUNT_BUTTON);
+  cy.checkDisplayAlert(TRY_COUNT_BUTTON, alertMessage.TryCountPositiveNumber);
 });
 
 it('다시 게임을 시작하면 이전 결과를 지워준다.', () => {
