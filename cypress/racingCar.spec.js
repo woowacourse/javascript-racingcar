0693cfcee77500f60f021495defccdfa6c16e9ea
Ahn <jin7969@gmail.com>
2022-02-14 22:25:58 +0900

    test: 테스트 항목 추가 및 수정

--- cypress/racingCar.spec.js
+++ cypress/racingCar.spec.js
@@ -1,8 +1,21 @@
 /* eslint-disable */
 describe('구현 결과가 요구사항과 일치해야 한다.', () => {
+  const baseURL = 'index.html';
+
   beforeEach(() => {
-    cy.visit('index.html');
+    cy.visit(baseURL);
   });
+
+  const CAR_NAMES = '아반떼';
+  const TRY_COUNT = 5;
+
+  const playGameCorrectly = () => {
+    cy.get('#car-names-input').type(CAR_NAMES);
+    cy.get('#car-names-button').click();
+    cy.get('#racing-count-input').type(TRY_COUNT);
+    cy.get('#racing-count-button').click();
+    cy.get('#reset-btn').click();
+  };
 
   it('잘못된 자동차 이름을 입력하는 경우 alert이 호출되어야 한다.', () => {
     const alertStub = cy.stub();
@@ -33,5 +46,20 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
       .then(() => {
         expect(alertStub).to.be.called;
       });
+  });
+
+  it('게임을 완료하고 최종 우승자를 출력한다.', () => {
+    const winners = `🏆 최종 우승자: ${CAR_NAMES} 🏆`;
+    playGameCorrectly();
+    cy.get('#winner-name').should('have.text', winners);
+  });
+
+  it('다시 시도하기 버튼을 클릭하면, 게임이 초기화되어야 한다.', () => {
+    playGameCorrectly();
+    cy.get('#car-names-input').should('have.value', '');
+    cy.get('#racing-count-input').should('have.value', '');
+    cy.get('#racing-count-form').should('have.css', 'display', 'none');
+    cy.get('#result-screen').should('have.css', 'display', 'none');
+    cy.get('#final-winner').should('have.css', 'display', 'none');
   });
 });
