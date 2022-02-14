9d32e47021a94957252b8e4a971b64f62c788ae6
JASUN LEE <liswktjs@gmail.com>
2022-02-14 17:59:20 +0900

    🐛 fix: minimum race count를 1로 변경 1이상 20이하이여야 하므로 minimum race count를 1로 변경

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const RANDOM_NUMBER_LIMIT = 9;
 export const CARNAME_LENGTH_LIMIT = 5;
 
-export const MINMUM_RACE_COUNT = 0;
+export const MINMUM_RACE_COUNT = 1;
 export const MAXIMUM_RACE_COUNT = 20;
 
 export const FORWARD_MARK_NUMBER = 4;
