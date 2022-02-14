44edd4222ea14e0729aa7d7ef49c937cb4305186
moonheekim0118 <hellomooneekim@gmail.com>
2022-02-14 16:28:26 +0900

    refactor: 불필요한 상수 제거

--- src/constants.js
+++ src/constants.js
@@ -14,7 +14,6 @@ const ID = {
 const MESSAGE = {
   WRONG_NAME_LENGTH: '1~5자의 자동차 이름을 입력해 주세요.',
   DUPLICATE_NAME: '중복된 자동차 이름은 입력이 불가능합니다.',
-  REINPUT_COUNT: '레이싱 횟수 재입력이 불가능합니다.',
   NO_CAR: '자동차 이름이 입력되지 않았습니다.',
   WRONG_COUNT:
     '올바르지 않은 레이싱 횟수입니다. 레이싱횟수는 최소 1회, 최대 1000회까지 가능합니다.',
