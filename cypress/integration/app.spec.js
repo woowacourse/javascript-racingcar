1355e845e30e9183a513d743de8e9d78c8aa7a0a
junzzi <jjh01027717502@gmail.com>
2022-02-14 17:33:58 +0900

    test: 자동차를 정상적으로 입력하고 카운트를 정상적으로 입력하면, 자동차 이름들이 정상적으로 UI에 표시된다.
    1. TDD를 지향하였지만, 리팩토링 이후 테스트 코드가 추가적으로 필요함을 느껴 진행하게됨
    2. 궁금 사항: UI에 노출되는 텍스트를 검증하는 방법말고, 제대로 객체가 생성되었는지 검증하는 방법은 없을까? (UI를 테스트 하는 것이 아닌)

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -99,4 +99,25 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
     //then
     isInitialStatus();
   });
+
+  // 자동차를 입력하여 생성하는 부분을 리팩토링 하고 나니
+  // 자동차가 정상 입력된 후 카운트가 입력되면
+  // 입력된 자동차들이 제대로 UI로 표시되는 지 검증하는 코드가 필요하다고 느꼈음
+
+  // UI에 노출되는 텍스트를 테스트 하는 방법 말고, 제대로 객체가 생겼는지 검증하는 방법은 없을까 ?
+
+  it('자동차를 정상적으로 입력하고, 카운트를 정상적으로 입력하면 자동차 텍스트들을 확인할 수 있다.', () => {
+    //given
+    const nameInput = 'bling,jun,zzi';
+    const countInput = 2;
+
+    //when
+    carNameInputProcess(nameInput);
+    countInputProcess(countInput);
+
+    //then
+    const result = nameInput.split(',').reduce((prev, current) => `${prev}${current}`, '');
+
+    cy.get(`.${DOM.CAR_NAME_CLASS}`).should('have.text', result);
+  });
 });
