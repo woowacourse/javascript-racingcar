d90bd97ae0d765141eaed82b409a5f81455cc146
nan-noo <uni613@naver.com>
2022-02-14 15:31:10 +0900

    refactor: change selector constants to variables
    Use SELECTOR object in selector.js

--- src/modules/checkUserCarNameInput.js
+++ src/modules/checkUserCarNameInput.js
@@ -2,6 +2,7 @@ import { $ } from "./dom-selector.js";
 import { EMPTY_INPUT_ERROR, OVER_CARNAME_LENGTH_ERROR } from "../constants/error.js";
 import initInputText from "./initInputText.js";
 import { MAX_CAR_NAME_LENGTH } from '../constants/gameCondition.js';
+import { SELECTOR } from '../constants/selector.js';
 
 function isCorrectCarNameLength(carNameArray){
     return !carNameArray.filter(carName => carName.length > MAX_CAR_NAME_LENGTH).length;
@@ -17,7 +18,7 @@ export default function checkUserCarNameInput(carNameInput) {
         .filter(carName => !!carName.trim().length);
 
     if(!isCorrectCarNameLength(carNameArray)){
-        initInputText($('#car-name-input'));
+        initInputText($(SELECTOR.CAR_NAME_INPUT));
         throw new Error(OVER_CARNAME_LENGTH_ERROR);
     }
 
--- src/modules/checkUserRacingCountInput.js
+++ src/modules/checkUserRacingCountInput.js
@@ -2,6 +2,7 @@ import { $ } from "./dom-selector.js";
 import initInputText from "./initInputText.js";
 import { NOT_NUMBER_TYPE_ERROR, EMPTY_INPUT_ERROR, OVER_COUNT_RANGE_ERROR } from "../constants/error.js";
 import { MAX_RACE_COUNT, MIN_RACE_COUNT } from "../constants/gameCondition.js";
+import { SELECTOR } from '../constants/selector.js';
 
 function isCorrectRaceCountRange(raceCountInput) {
     return raceCountInput <= MAX_RACE_COUNT && raceCountInput >= MIN_RACE_COUNT && Number.isInteger(raceCountInput);
@@ -13,12 +14,12 @@ export default function checkUserRacingCountInput(raceCountInput){
     }
     
     if(!Number(raceCountInput)) {
-        initInputText($('#race-count-input'));
+        initInputText($(SELECTOR.RACE_COUNT_INPUT));
         throw new Error(NOT_NUMBER_TYPE_ERROR);
     }
     
     if(!isCorrectRaceCountRange(Number(raceCountInput))){  
-        initInputText($('#race-count-input'));
+        initInputText($(SELECTOR.RACE_COUNT_INPUT));
         throw new Error(OVER_COUNT_RANGE_ERROR);
     }
     return raceCountInput;
