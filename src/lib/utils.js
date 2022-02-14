8546cdbbc4eb33c36fa75719a83681eb074fdb86
junzzi <jjh01027717502@gmail.com>
2022-02-15 00:50:42 +0900

    refactor: jsdoc의 양식을 유지하도록 변경

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,10 +9,9 @@ export const pickNumberInRange = (min, max) => Math.floor(Math.random() * (max -
 // 기능 요구 사항
 // 1. 아이디는 중복될 수 없다.
 // 2. 아이디는 6개의 숫자로 구성된 문자열이다.
-
 /**
- * props : idArr(아이디 객체, 로직의 복잡도가 커짐을 고려)
- * return : 아이디 문자열
+ * @params {number[]} idArr 아이디 객체, 로직의 복잡도가 커짐을 고려
+ * @returns 아이디 문자열
  */
 export const generateId = (idObj) => {
   let isUnique = false;
