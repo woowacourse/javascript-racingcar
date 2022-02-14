a690c8239f62df9f76c3a5759cbb6aa36a623cf7
junzzi <jjh01027717502@gmail.com>
2022-02-15 01:42:36 +0900

    refactor: include types in jsdoc으로 수정

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,8 +8,8 @@ export const pickNumberInRange = (min, max) => Math.floor(Math.random() * (max -
 
 // include types in JSDoc 라면 String Set은 어떻게 표현할 수 있나요 ?
 /**
- * @params idSet - 이미 존재하는 유저 아이디 Set
- * @returns idStr - 유저 아이디(숫자 문자열)
+ * @params {Set<string>} idSet - 이미 존재하는 유저 아이디 Set
+ * @returns {string} idStr - 유저 아이디(숫자 문자열)
  */
 export const generateId = (idSet) => {
   let idStr = pickNumberInRange(0, 999999).toString().padStart(6, 0);
