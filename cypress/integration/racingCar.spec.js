497401ecf4c07e0c7a0d76cc21ba785d28faa989
Daye Lee <leeda1801@gmail.com>
2022-02-15 23:44:01 +0900

    refactor: 불필요한 공백 제거 및 코드 순서 변경

--- cypress/integration/racingCar.spec.js
+++ cypress/integration/racingCar.spec.js
@@ -2,17 +2,15 @@ import { SELECTOR } from '../../src/common/constants.js';
 
 describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   const baseUrl = '../index.html';
-
   beforeEach(() => {
     cy.visit(baseUrl);
   });
 
   it('5자를 초과하는 자동차 이름이 제출된 경우에 alert을 이용해 메시지를 보여준다.', () => {
     const alertStub = cy.stub();
-    const invalidInput = 'ab,cde,fghijk';
-
     cy.on('window:alert', alertStub);
 
+    const invalidInput = 'ab,cde,fghijk';
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT)
@@ -24,7 +22,6 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
   it('5자를 초과하는 자동차 이름이 제출된 경우에 다시 입력할 수 있게 한다.', () => {
     const invalidInput = 'abcdef';
-
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT).click().type('{enter}');
@@ -34,10 +31,9 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
   it('공백으로만 이루어진 자동차 이름이 제출된 경우에 alert를 이용해 메시지를 보여준다', () => {
     const alertStub = cy.stub();
-    const invalidInput = ' , , ';
-
     cy.on('window:alert', alertStub);
 
+    const invalidInput = ' , , ';
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT)
@@ -49,7 +45,6 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
   it('공백으로만 이루어진 자동차 이름이 제출된 경우에 다시 입력할 수 있게 한다.', () => {
     const invalidInput = ' , , ';
-
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT).click().type('{enter}');
@@ -59,10 +54,9 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
   it('중복된 자동차 이름이 제출된 경우에 alert를 이용해 메시지를 보여준다', () => {
     const alertStub = cy.stub();
-    const invalidInput = 'abc,abc,abcd';
-
     cy.on('window:alert', alertStub);
 
+    const invalidInput = 'abc,abc,abcd';
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT)
@@ -74,7 +68,6 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
   it('중복된 자동차 이름이 제출된 경우 다시 입력할 수 있게 한다.', () => {
     const invalidInput = 'abc,abc,abcd';
-
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT).click().type('{enter}');
@@ -84,26 +77,25 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
   it('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
     const validCarNamesInput = 'apple,banan,carro';
-
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(validCarNamesInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
 
     cy.get(SELECTOR.RACING_COUNT_INPUT).should('exist');
+
     cy.get(SELECTOR.RACING_COUNT_SUBMIT).should('exist');
   });
 
   it('제출된 시도할 횟수가 양의 정수가 아닌 경우에 alert을 이용해 메시지를 보여준다.', () => {
     const alertStub = cy.stub();
-    const invalidInput = 'e';
-    const validCarNamesInput = 'apple,banan,carro';
-
     cy.on('window:alert', alertStub);
 
+    const validCarNamesInput = 'apple,banan,carro';
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(validCarNamesInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
 
+    const invalidInput = 'e';
     cy.get(SELECTOR.RACING_COUNT_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.RACING_COUNT_SUBMIT)
@@ -114,14 +106,12 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   });
 
   it('제출된 시도할 횟수가 양의 정수가 아닌 경우에 다시 입력할 수 있게 한다.', () => {
-    const invalidInput = '-123';
-
     const validCarNamesInput = 'apple,banan,carro';
-
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(validCarNamesInput);
 
     cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
 
+    const invalidInput = '-123';
     cy.get(SELECTOR.RACING_COUNT_INPUT).type(invalidInput);
 
     cy.get(SELECTOR.RACING_COUNT_SUBMIT).click().type('{enter}');
