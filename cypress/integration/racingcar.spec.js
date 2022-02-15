7a342ee401ae7dd6cadc97d085c17d4d22bb88d7
Jeongmin Woo <greenblues1190@gmail.com>
2022-02-15 10:26:10 +0900

    test: 테스트코드 수정
    레이싱횟수 버튼 disabled 추가

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -9,6 +9,7 @@ function createAlertStub() {
 
 describe('기능 요구사항', () => {
   const testCarNames = ['우디', '꼬재'];
+  const inputString = '우디, 꼬재';
   const racingCount = 4;
 
   beforeEach(() => {
@@ -16,10 +17,10 @@ describe('기능 요구사항', () => {
   });
 
   it('자동차 이름을 입력하고 확인 버튼을 누르면 레이스를 출력할 때 쉼표로 구분된 자동차 이름들을 같이 출력한다.', () => {
-    cy.get(SELECTOR.$CAR_NAME_INPUT).type(testCarNames.join(',')); // '우디,꼬재'
+    cy.get(SELECTOR.$CAR_NAME_INPUT).type(inputString); // '우디,꼬재'
 
     cy.get(SELECTOR.$CAR_NAME_BUTTON).click();
-    cy.get(SELECTOR.$CAR_NAME).should('have.length', testCarNames.length);
+    cy.get(SELECTOR.$CAR_NAME).should('have.length', 2);
     cy.get(SELECTOR.$CAR_NAME).each((name, index) => {
       cy.wrap(name).should('have.text', testCarNames[index]);
     });
@@ -64,34 +65,46 @@ describe('예외 상황', () => {
   });
 
   it('양의 정수가 아닌 레이싱 횟수를 입력하면 alert를 보여준다.', () => {
+    const testCarNames = '우디, 꼬재';
     const racingCount = '-1';
     const alertStub = createAlertStub();
 
-    cy.get(SELECTOR.$RACING_COUNT_INPUT).type(racingCount);
-    cy.get(SELECTOR.$RACING_COUNT_BUTTON)
+    cy.get(SELECTOR.$CAR_NAME_INPUT).type(testCarNames);
+    cy.get(SELECTOR.$CAR_NAME_BUTTON)
       .click()
       .then(() => {
-        expect(alertStub).to.be.calledWith(
-          ERROR_MESSAGE.OUT_OF_RACING_COUNT_RANGE
-        );
-        cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.value', '');
-        cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.focus');
+        cy.get(SELECTOR.$RACING_COUNT_INPUT).type(racingCount);
+        cy.get(SELECTOR.$RACING_COUNT_BUTTON)
+          .click()
+          .then(() => {
+            expect(alertStub).to.be.calledWith(
+              ERROR_MESSAGE.OUT_OF_RACING_COUNT_RANGE
+            );
+            cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.value', '');
+            cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.focus');
+          });
       });
   });
 
-  it('100보다 큰 레이싱 횟수를 입력하면 alert를 보여준다.', () => {
-    const racingCount = '101';
+  it('10보다 큰 레이싱 횟수를 입력하면 alert를 보여준다.', () => {
+    const testCarNames = '우디, 꼬재';
+    const racingCount = '11';
     const alertStub = createAlertStub();
 
-    cy.get(SELECTOR.$RACING_COUNT_INPUT).type(racingCount);
-    cy.get(SELECTOR.$RACING_COUNT_BUTTON)
+    cy.get(SELECTOR.$CAR_NAME_INPUT).type(testCarNames);
+    cy.get(SELECTOR.$CAR_NAME_BUTTON)
       .click()
       .then(() => {
-        expect(alertStub).to.be.calledWith(
-          ERROR_MESSAGE.OUT_OF_RACING_COUNT_RANGE
-        );
-        cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.value', '');
-        cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.focus');
+        cy.get(SELECTOR.$RACING_COUNT_INPUT).type(racingCount);
+        cy.get(SELECTOR.$RACING_COUNT_BUTTON)
+          .click()
+          .then(() => {
+            expect(alertStub).to.be.calledWith(
+              ERROR_MESSAGE.OUT_OF_RACING_COUNT_RANGE
+            );
+            cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.value', '');
+            cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.focus');
+          });
       });
   });
 
@@ -109,19 +122,11 @@ describe('예외 상황', () => {
       });
   });
 
-  it('자동차 이름을 입력하지 않고 레이싱 횟수 버튼을 누르면 alert를 보여준다.', () => {
+  it('자동차 이름이 등록되어 있지 않을 때 레이싱 횟수 버튼이 disabled되어 있어야 한다.', () => {
     const inputString = '5';
     const alertStub = createAlertStub();
 
     cy.get(SELECTOR.$RACING_COUNT_INPUT).type(inputString);
-    cy.get(SELECTOR.$RACING_COUNT_BUTTON)
-      .click()
-      .then(() => {
-        expect(alertStub).to.be.calledWith(
-          ERROR_MESSAGE.CAR_NAME_SHOULD_COME_FIRST
-        );
-        cy.get(SELECTOR.$RACING_COUNT_INPUT).should('have.value', '');
-        cy.get(SELECTOR.$CAR_NAME_INPUT).should('have.focus');
-      });
+    cy.get(SELECTOR.$RACING_COUNT_BUTTON).should('be.disabled');
   });
 });
