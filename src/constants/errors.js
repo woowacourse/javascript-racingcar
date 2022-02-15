5168891dcaed1a4a21f49fd36c61f761c6690056
cks3066 <cks3066@naver.com>
2022-02-15 22:41:59 +0900

    refactor: 상수 네이밍 명확하게 변경

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,7 +1,7 @@
-export const ERR_HAS_DUPLICATE_NAME = "중복된 이름이 있습니다.";
-export const ERR_HAS_EMPTY_NAME = "공백으로된 이름이 있습니다.";
-export const ERR_HAS_LONG_NAME = "5글자 이하의 이름을 입력해주세요.";
-export const ERR_NUMBER_NOT_INTEGER = "소수점을 제외하고 입력해주세요.";
-export const ERR_NUMBER_UNDER_ZERO = "1이상의 수를 입력해주세요.";
-export const ERR_CAR_NAME_NOT_EXIST = "자동차 이름을 먼저 입력해주세요.";
-export const ERR_RACING_NUMBER_NOT_EXIST = "라운드 수를 입력해주세요.";
+export const ERROR_HAS_DUPLICATE_NAME = "중복된 이름이 있습니다.";
+export const ERROR_HAS_EMPTY_NAME = "공백으로된 이름이 있습니다.";
+export const ERROR_HAS_LONG_NAME = "5글자 이하의 이름을 입력해주세요.";
+export const ERROR_NUMBER_NOT_INTEGER = "소수점을 제외하고 입력해주세요.";
+export const ERROR_NUMBER_UNDER_ZERO = "1이상의 수를 입력해주세요.";
+export const ERROR_CAR_NAME_NOT_EXIST = "자동차 이름을 먼저 입력해주세요.";
+export const ERROR_RACING_NUMBER_NOT_EXIST = "라운드 수를 입력해주세요.";
