49a3a3f5918ac4d328400f037b39a13831ce2726
2019015414 <2019015414@hanyang.ac.kr>
2022-02-15 12:17:27 +0900

    refactor: cypress에 beforeEach 추가

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -7,47 +7,49 @@
 // [] 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
 // [V] 사용자가 잘못된 입력 값을 작성한 경우 alert을 이용해 메시지를 보여주고, 다시 입력할 수 있게 한다.
 
-it("자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.", () => {
-  cy.visit("index.html");
+describe("자동차 경주 게임 테스트", () => {
+  beforeEach(() => {
+    cy.visit("index.html");
+  });
 
-  const alertStub = cy.stub();
+  it("자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.", () => {
+    const alertStub = cy.stub();
 
-  cy.on("window:alert", alertStub);
-  cy.get(".car-name-input").type("abcdef");
-  cy.get(".car-name-button")
-    .click()
-    .then(() => {
-      expect(alertStub.getCall(0));
-    });
-});
-
-it("사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.", () => {
-  cy.visit("index.html");
-
-  const alertStub = cy.stub();
-
-  cy.on("window:alert", alertStub);
-  cy.get(".race-count-button")
-    .click()
-    .then(() => {
-      if (cy.get(".race-count-input").should("have.value", "")) {
+    cy.on("window:alert", alertStub);
+    cy.get(".car-name-input").type("abcdef");
+    cy.get(".car-name-button")
+      .click()
+      .then(() => {
         expect(alertStub.getCall(0));
-      }
-    });
-});
+      });
+  });
 
-it("자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.", () => {
-  cy.visit("index.html");
+  it("사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.", () => {
+    const alertStub = cy.stub();
 
-  cy.get(".car-name-input").type("car1,car2,car3");
-  cy.get(".car-name-button").click();
-  cy.get(".race-count-input").type("10");
-  cy.get(".race-count-button").click();
+    cy.get(".car-name-input").type("car1,car2,car3");
+    cy.get(".car-name-button").click();
+    cy.on("window:alert", alertStub);
+    cy.get(".race-count-button")
+      .click()
+      .then(() => {
+        if (cy.get(".race-count-input").should("have.value", "")) {
+          expect(alertStub.getCall(0));
+        }
+      });
+  });
 
-  // cy.get(".racing-cars").children().eq(0).should("have.text", "car1");
-  // cy.get(".racing-cars").children().eq(1).should("have.text", "car2");
-  // cy.get(".racing-cars").children().eq(2).should("have.text", "car3");
-  cy.get(".racing-cars").children().eq(0).contains("car1");
-  cy.get(".racing-cars").children().eq(1).contains("car2");
-  cy.get(".racing-cars").children().eq(2).contains("car3");
+  it("자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.", () => {
+    cy.get(".car-name-input").type("car1,car2,car3");
+    cy.get(".car-name-button").click();
+    cy.get(".race-count-input").type("10");
+    cy.get(".race-count-button").click();
+
+    // cy.get(".racing-cars").children().eq(0).should("have.text", "car1");
+    // cy.get(".racing-cars").children().eq(1).should("have.text", "car2");
+    // cy.get(".racing-cars").children().eq(2).should("have.text", "car3");
+    cy.get(".racing-cars").children().eq(0).contains("car1");
+    cy.get(".racing-cars").children().eq(1).contains("car2");
+    cy.get(".racing-cars").children().eq(2).contains("car3");
+  });
 });
