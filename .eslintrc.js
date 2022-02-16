89722161e7eb81610c236c58ae7d7dba5b42c1ce
airman5573 <rpf5573@kakao.com>
2022-02-17 01:41:08 +0900

    :truck: chore(eslint): class안에 static property를 사용하기 위해서 ecmaVersion을 최신으로 설정한다

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true, // browser에서 지원하는 기능들을 사용하겠다고 선언한다(에러를 안뿜는다)
     // es2021에서 지원하는 기능들을 사용하겠다고 선언한다(에러를 안뿜는다)
     // 자동으로 ecmaVersion parser option을 12으로 설정한다
-    es2021: true,
+    es2022: true,
   },
   // airbnb-base : airbnb에서 권장하는 rule을 가져온다
   // plugin:prettier/recommended : eslint 와 prettier를 같이 사용할때 필요한 설정들을 가져온다
@@ -11,6 +11,7 @@ module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     sourceType: 'module', // ECMAScript module을 사용한다(import/export)
+    ecmaVersion: 'latest',
   },
   rules: {
     'no-alert': 'off', // alert을 사용해도 된다
