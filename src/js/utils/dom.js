225f7297bcda88717d3d02aa484578cf87249f1a
airman5573 <rpf5573@kakao.com>
2022-02-17 02:22:11 +0900

    :recycle: refactor(util): 여러개의 node도 찝을 수 있도록 개선한다

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,2 +1,9 @@
-const $ = (selector, node = document) => node.querySelector(selector);
+const $ = (selector, node = document) => {
+  const nodeList = node.querySelectorAll(selector);
+  if (nodeList.length === 1) {
+    return nodeList[0];
+  }
+  return nodeList;
+};
+
 export default $;
