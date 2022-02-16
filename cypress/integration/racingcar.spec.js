622e3485ae00060843b8108f57c11dc3d9f54b8f
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-02-16 18:48:07 +0900

    refactor: clarify test code

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -88,22 +88,22 @@ describe("정상 시나리오에 대해 만족해야 한다.", () => {
     cy.reload();
   });
 
-  it("자동차 이름 입력이 제대로 되었을 경우, 입력창과 버튼이 모두 비활성화가 되어야 한다.", () => {
+  it("사용자가 자동차 이름을 쉼표로 구분하여 5자 이하로 알맞게 입력했을 경우, 성공적으로 입력창이 비활성화 상태가 된다.", () => {
     submitCarNames("movie, halee");
   });
 
-  it("레이싱 횟수 입력이 제대로 되었을 경우, 입력창과 버튼이 모두 비활성화가 되어야 한다.", () => {
+  it("사용자가 레이싱 횟수를 1이상 정수로 입력했을 경우, 성공적으로 입력창이 비활성화 상태가 된다.", () => {
     submitCarNames("movie, halee");
     submitRacingCount(10);
   });
 
-  it("게임을 종료하고 우승자를 확인할 수 있어야 한다.", () => {
+  it("게임을 종료되면 우승자를 확인할 수 있어야 한다.", () => {
     submitCarNames("movie, halee");
     submitRacingCount(10);
     countLocation();
   });
 
-  it("다시 시작 버튼을 누르면 화면 내의 모든 값이 초기화되어야 한다.", () => {
+  it("'다시 시작하기' 버튼을 누르면 화면 내의 모든 값이 초기화되어야 한다.", () => {
     const {
       CAR_NAMES_INPUT,
       RACING_COUNT_INPUT,
@@ -121,7 +121,7 @@ describe("정상 시나리오에 대해 만족해야 한다.", () => {
   });
 });
 
-describe("비정상 시나리오에 대해 사용자에게 alert를 띄운다.", () => {
+describe("비정상 시나리오에 대해서는 사용자에게 경고를 준다.", () => {
   beforeEach(() => {
     cy.visit(baseUrl);
   });
@@ -130,25 +130,25 @@ describe("비정상 시나리오에 대해 사용자에게 alert를 띄운다.",
     cy.reload();
   });
 
-  it("자동차 이름이 아무것도 입력되지 않았을 경우 사용자에게 alert를 띄운다.", () => {
+  it("자동차 이름이 아무것도 입력되지 않았을 경우 사용자에게 경고를 준다.", () => {
     submitCarNamesAlert();
   });
 
-  it("이름의 길이가 5자를 초과했을 경우 alert를 띄운다.", () => {
+  it("자동차 이름의 길이가 5자를 초과했을 경우 사용자에게 경고를 준다.", () => {
     const invalidInput = "loveracingcar";
 
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
     submitCarNamesAlert();
   });
 
-  it("중복된 이름의 자동차가 입력될 경우 alert를 띄운다.", () => {
+  it("중복된 이름의 자동차가 입력될 경우 사용자에게 경고를 준다.", () => {
     const invalidInput = "woowa,course,woowa";
 
     cy.get(SELECTOR.CAR_NAMES_INPUT).type(invalidInput);
     submitCarNamesAlert();
   });
 
-  it("레이싱 횟수가 정수가 아닌 경우 alert를 띄운다.", () => {
+  it("레이싱 횟수가 정수가 아닌 경우 사용자에게 경고를 준다.", () => {
     const invalidInput = 12.5;
 
     submitCarNames("movie, halee");
@@ -156,7 +156,7 @@ describe("비정상 시나리오에 대해 사용자에게 alert를 띄운다.",
     submitRacingCountAlert();
   });
 
-  it("1이상의 수가 아닌 경우 alert를 띄운다.", () => {
+  it("레이싱 횟수가 1이상의 수가 아닌 경우 사용자에게 경고를 준다.", () => {
     const invalidInput = 0;
 
     submitCarNames("movie, halee");
