83d30e989c20efc161602d5d6ef2adcbb6a299f9
intae92 <kangit2013@gamil.com>
2022-02-17 10:56:59 +0900

    chore: eslint cypress플러그인 설정, 환경설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
+  ],
   rules: {
     'react/jsx-filename-extension': [
       'error',
