e7762a7cdc49de867217129a46826f8aafe2429b
jhy979 <jhy979@gmail.com>
2022-02-17 09:56:12 +0900

    refactor: Validator 클래스에서 객체로 변경

--- js/validator/validator.js
+++ js/validator/validator.js
@@ -1,33 +1,35 @@
 import { NUMBER } from '../utils/constants.js';
 
-export default class Validator {
-  static isCarNameBlank(carNames) {
+const Validator = {
+  isCarNameBlank(carNames) {
     const filtered = carNames.filter((name) => name === '');
     return filtered.length > NUMBER.ZERO;
-  }
+  },
 
-  static isInvalidCarNameLength(carNames) {
+  isInvalidCarNameLength(carNames) {
     const filtered = carNames.filter((name) => name.length > NUMBER.MAX_LENGTH);
     return filtered.length > NUMBER.ZERO;
-  }
+  },
 
-  static isInValidCarNamesInput(carNames) {
+  isInValidCarNamesInput(carNames) {
     return this.isCarNameBlank(carNames) || this.isInvalidCarNameLength(carNames);
-  }
+  },
 
-  static isFloat(number) {
+  isFloat(number) {
     return !Number.isInteger(Number(number));
-  }
+  },
 
-  static isNotNaturalNumber(number) {
+  isNotNaturalNumber(number) {
     return number <= NUMBER.ZERO;
-  }
+  },
 
-  static isString(number) {
+  isString(number) {
     return typeof Number(number) !== 'number';
-  }
+  },
 
-  static isInValidRacingCountInput(number) {
+  isInValidRacingCountInput(number) {
     return this.isFloat(number) || this.isNotNaturalNumber(number) || this.isString(number);
-  }
-}
+  },
+};
+
+export default Validator;
