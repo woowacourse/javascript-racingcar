57e589c5d236df68122ac5287181e34cf14e28e5
airman5573 <rpf5573@kakao.com>
2022-02-17 23:02:16 +0900

    :white_check_mark: test(cypress): 유효한 자동차 이름이 입력된 경우를 테스트 한다

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,4 +1,5 @@
 import { ERROR_MESSAGES } from '../../src/js/constants.js';
+import testid from '../support/utils/test-id.js';
 
 describe('입력된 자동차 이름에 대한 유효성 검사가 실패하는 경우', () => {
   beforeEach(() => {
@@ -24,5 +25,13 @@ describe('입력된 자동차 이름에 대한 유효성 검사가 실패하는 
   it('입력된 자동차 이름들 중 중복된 이름이 있는 경우, alert 메세지를 띄운다.', () => {
     const invalidCarNames = 'aa,bb,aa';
     alertTestOnSubmitCarNames(invalidCarNames, ERROR_MESSAGES.DUPLICATED_CAR_NAME);
+  });
+});
+
+describe('유효한 자동차 이름을 입력한 경우', () => {
+  it('시도 횟수를 입력하는 fieldset을 보여준다', () => {
+    const validCarNames = 'aa,bb,cc,dd';
+    cy.formSubmit('car-names-input', 'car-names-submit-button', validCarNames);
+    cy.get(testid`racing-count-fieldset`).should('be.visible');
   });
 });
