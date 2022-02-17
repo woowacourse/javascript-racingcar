f9bd6d63ee9251c5086514d797836d76f49591e0
intae92 <kangit2013@gamil.com>
2022-02-17 22:47:15 +0900

    refactor: render부분, async/await 비동기 처리 수정
    - PR후 수정한 부분
    - renderResult() 메서드에서 async/await를 너무 Promise처럼 사용하는걸
    느껴 수정하였습니다. 좀 더 코드가 깔끔해진거 같아요

--- src/js/view/renderResult.js
+++ src/js/view/renderResult.js
@@ -25,22 +25,16 @@ const playTurn = (cars, currentTurnCount) => {
 };
 
 const playTurnResult = async ({ lastTurnCount, cars, currentTurnCount }) => {
-  await playTurn(cars, currentTurnCount)
-    .then(res => {
-      const { nextTurnCount } = res;
-      const shouldPlayNext = lastTurnCount >= nextTurnCount;
+  const { nextTurnCount } = await playTurn(cars, currentTurnCount);
+  const shouldPlayNext = lastTurnCount >= nextTurnCount;
 
-      if (shouldPlayNext) {
-        return playTurnResult({
-          lastTurnCount,
-          cars,
-          currentTurnCount: nextTurnCount,
-        });
-      }
-
-      return res;
-    })
-    .catch(err => console.log('err', err));
+  if (shouldPlayNext) {
+    await playTurnResult({
+      lastTurnCount,
+      cars,
+      currentTurnCount: nextTurnCount,
+    });
+  }
 };
 
 const renderWinners = winners => {
@@ -57,41 +51,46 @@ const removeLoadingAnimation = () => {
   });
 };
 
-const domInit = cars => {
-  const turnResult = document.querySelector('#turn-result');
+const domInit = async cars => {
+  return new Promise(resolve => {
+    const turnResult = document.querySelector('#turn-result');
 
-  cars.forEach(car => {
-    const { name } = car;
-    const carResult = document.createElement('div');
-    const carNameTitle = document.createElement('div');
-    const carScoreArrowsBox = document.createElement('div');
+    cars.forEach(car => {
+      const { name } = car;
+      const carResult = document.createElement('div');
+      const carNameTitle = document.createElement('div');
+      const carScoreArrowsBox = document.createElement('div');
 
-    carResult.setAttribute('class', 'car-result');
-    carNameTitle.setAttribute('class', 'car-Name-title');
-    carNameTitle.innerHTML = `${name}`;
-    carScoreArrowsBox.setAttribute('class', 'car-score-arrows');
+      carResult.setAttribute('class', 'car-result');
+      carNameTitle.setAttribute('class', 'car-Name-title');
+      carNameTitle.innerHTML = `${name}`;
+      carScoreArrowsBox.setAttribute('class', 'car-score-arrows');
 
-    carResult.append(carNameTitle, carScoreArrowsBox, loadingAnimation());
-    turnResult.appendChild(carResult);
+      carResult.append(carNameTitle, carScoreArrowsBox, loadingAnimation());
+      turnResult.appendChild(carResult);
+    });
+
+    resolve();
   });
 };
 
 export const renderResult = async ({ cars, lastTurnCount, winners }) => {
   const currentTurnCount = 1;
-  domInit(cars);
+
+  await domInit(cars);
 
   await playTurnResult({
     lastTurnCount,
     cars,
     currentTurnCount,
-  }).then(() => {
-    renderWinners(winners);
-    removeLoadingAnimation();
-
-    setTimeout(() => {
-      window.alert('축하합니다!');
-    }, 2000);
   });
+
+  renderWinners(winners);
+  removeLoadingAnimation();
+
+  setTimeout(() => {
+    window.alert('축하합니다!');
+  }, 2000);
 };
 
 export const removeBeforeResult = e => {
