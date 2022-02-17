2b83723d3f147f0640966764f560d39150a35edf
sanaandmomo <qajjang46@naver.com>
2022-02-17 12:28:54 +0900

    feat: 엔터키 누를 때 이벤트 추가

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import { onSubmitCarNames, onSubmitTryCount, onClickRestartButton } from './eventListener.js';
 import { $ } from './util.js';
+import { KEYCODE_ENTER } from './constant.js';
+
+const onSubmitClickedEnter = (onSubmit) => (e) => {
+    if (e.keyCode === KEYCODE_ENTER) {
+        onSubmit();
+    }
+};
 
 $('#car-name-submit-button').addEventListener('click', onSubmitCarNames);
+$('#car-name-input').addEventListener('keyup', onSubmitClickedEnter(onSubmitCarNames));
 
 $('#try-count-submit-button').addEventListener('click', onSubmitTryCount);
+$('#try-count-input').addEventListener('keyup', onSubmitClickedEnter(onSubmitTryCount));
 
 $('#restart-button').addEventListener('click', onClickRestartButton);
