c79d449d5d1a30e4f7a93c60bf0be8c4946e40ee
airman5573 <rpf5573@kakao.com>
2022-02-17 22:19:43 +0900

    :white_check_mark: test(car-names): 자동차 이름에 대한 유효성 검사가 잘 이뤄지고 있는지 확인한다

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,28 @@
+import { ERROR_MESSAGES } from '../../src/js/constants.js';
+
+describe('자동차 이름에 대한 유효성 검사가 잘 이뤄지고 있는지 확인한다', () => {
+  beforeEach(() => {
+    cy.visit('/index.html');
+  });
+
+  const alertTestOnSubmitCarNames = (invalidCarNames, errorMessage) => {
+    cy.formSubmit('car-names-input', 'car-names-submit-button', invalidCarNames, (txt) => {
+      expect(txt).to.contains(errorMessage);
+    });
+  };
+
+  it('자동차 이름이 비어있으면, alert메세지를 띄운다', () => {
+    const invalidCarNames = '';
+    alertTestOnSubmitCarNames(invalidCarNames, ERROR_MESSAGES.EMPTY_CAR_NAME);
+  });
+
+  it('입력된 자동차 이름이 5자를 초과하면, alert 메세지를 띄운다.', () => {
+    const invalidCarNames = 'aaaaaa,bbb';
+    alertTestOnSubmitCarNames(invalidCarNames, ERROR_MESSAGES.BEYOND_MAX_CAR_NAME_LENGTH);
+  });
+
+  it('입력된 자동차 이름들 중 중복된 이름이 있는 경우, alert 메세지를 띄운다.', () => {
+    const invalidCarNames = 'aa,bb,aa';
+    alertTestOnSubmitCarNames(invalidCarNames, ERROR_MESSAGES.DUPLICATED_CAR_NAME);
+  });
+});
