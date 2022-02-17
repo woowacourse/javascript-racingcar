5b2bf96d24ed7e8dcae1b90b89bb595a72727be7
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-02-17 23:37:11 +0900

    refactor: html 코드 상수화 (template.js)

---
+++ src/js/constants/template.js
@@ -0,0 +1,9 @@
+const TEMPLATE = Object.freeze({
+  ADVANCE_MARK: '<div class="car-advance">⬇️️</div>',
+  LOADING_MARK:
+    '<div class="round-loading"><img src="./src/asset/loading.png" class="spinner" alt="loading"></div>',
+  ADVANCE_MARK: '<div class="car-advance">⬇️️</div>',
+  ADVANCE_MARK: '<div class="car-advance">⬇️️</div>',
+});
+
+export default TEMPLATE;
