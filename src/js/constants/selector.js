a41217bd047eb1533e3d716e5039925ad0f65ae5
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-19 01:37:59 +0900

    chore: conflict 문제 해결 2

--- src/js/constants/selector.js
+++ src/js/constants/selector.js
@@ -1,4 +1,12 @@
+import { EMPTY_STRING } from './constants.js';
+
 const SELECTOR = Object.freeze({
+  // 범용 선택자
+  APP: '#app',
+  PROGRESS: '.progress',
+  PROGRESS_INNER: '.round',
+
+  // RacingGame 사용 선택자
   CAR_NAME_INPUT: '#car-name-input',
   CAR_NAME_BUTTON: '#car-name-button',
   RACE_TIME_INPUT: '#race-time-input',
@@ -12,15 +20,14 @@ const SELECTOR = Object.freeze({
   RETRY_BUTTON: '#retry-button',
 });
 
-function replaceSelectorToDomID(origin) {
-  const output = new Object();
+const replaceSelectorToDomID = (origin) => {
+  const output = {};
   Object.entries(origin).forEach(([key, value]) => {
-    output[key] = value.replace(/\#|\./g, '');
+    output[key] = value.replace(/#|\./g, EMPTY_STRING);
   });
 
   return output;
-}
+};
 
 const DOM_ID = Object.freeze(replaceSelectorToDomID(SELECTOR));
-
 export { SELECTOR, DOM_ID };
--- src/js/models/RacingGameModel.js
+++ src/js/models/RacingGameModel.js
@@ -1,53 +1,50 @@
 import RacingCarInstance from './RacingCarInstance.js';
-import isAdvance from '../utils/isAdvance.js';
+import { isAdvance } from '../utils/RacingGame/validator.js';
 
 export default class RacingGameModel {
+  #state;
+  #maxDistance;
+
   constructor() {
     this.init();
   }
 
   init() {
-    this._state = {
+    this.#state = {
       carList: [],
       round: 0,
       winners: [],
     };
 
-    this._maxDistance = Number.MIN_SAFE_INTEGER;
+    this.#maxDistance = Number.MIN_SAFE_INTEGER;
   }
 
   get carList() {
-    return this._state.carList;
+    return this.#state.carList;
   }
 
-  set carList(array) {
-    const { carList } = this._state;
-    carList.length = 0;
-
-    array.forEach((carName) => {
-      const newCar = new RacingCarInstance(carName);
-      carList.push(newCar);
-    });
+  set carList(nameList) {
+    this.#state.carList = nameList.map((name) => new RacingCarInstance(name));
   }
 
   set round(number) {
-    this._state.round = number;
+    this.#state.round = Number(number);
   }
 
   get round() {
-    return this._state.round;
+    return this.#state.round;
   }
 
-  _isWinner(carInstance) {
+  #isWinner(carInstance) {
     const { distance } = carInstance;
-    const { winners } = this._state;
+    const { winners } = this.#state;
 
-    if (distance < this._maxDistance) {
+    if (distance < this.#maxDistance) {
       return false;
     }
 
-    if (distance > this._maxDistance) {
-      this._maxDistance = distance;
+    if (distance > this.#maxDistance) {
+      this.#maxDistance = distance;
       winners.length = 0;
     }
 
@@ -55,9 +52,9 @@ export default class RacingGameModel {
   }
 
   get winners() {
-    const { winners, carList } = this._state;
+    const { winners, carList } = this.#state;
     carList.forEach((carInstance) => {
-      if (this._isWinner(carInstance) === false) {
+      if (this.#isWinner(carInstance) === false) {
         return;
       }
 
@@ -68,7 +65,7 @@ export default class RacingGameModel {
   }
 
   play() {
-    const { carList } = this._state;
+    const { carList } = this.#state;
 
     carList.forEach((carInstance) => {
       if (isAdvance() === false) {
