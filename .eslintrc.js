ab9799ccca813df6d6f2e73d5dd07a90956cee44
sanaandmomo <qajjang46@naver.com>
2022-02-18 13:10:12 +0900

    chore: eslint 재설정

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto', // prettier delete cr 설정 끄기
+            },
+        ],
+        'import/extensions': [
+            'off',
+            {
+                extensions: ['.js'], // import ... from ..js 가능하게 하기
+            },
+        ],
+    },
+};
