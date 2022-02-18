6455d9824abb69f684bd0726685b291fb9d62f2d
moonheekim0118 <hellomooneekim@gmail.com>
2022-02-18 17:25:29 +0900

    refactor: View 클래스 내 멤버변수 private 변수로 수정

--- src/class/View.js
+++ src/class/View.js
@@ -30,33 +30,42 @@ const generateWinnersView = winners =>
     .join(',')}🏆</h3>`;
 
 export default class View {
+  #carNamesInput;
+  #carNamesButton;
+  #racingCountInput;
+  #racingCountButton;
+  #racingCountContainer;
+  #racingStatusContainer;
+  #racingResultContainer;
+  #winnersContainer;
+  #racingProgressElements = [];
+
   constructor($app) {
-    this.$carNamesInput = $(SELECTOR.CAR_NAMES_INPUT, $app);
-    this.$carNamesButton = $(SELECTOR.CAR_NAMES_BUTTON, $app);
-    this.$racingCountInput = $(SELECTOR.RACING_COUNT_INPUT, $app);
-    this.$racingCountButton = $(SELECTOR.RACING_COUNT_BUTTON, $app);
-    this.$racingCountContainer = $(SELECTOR.RACING_COUNT_CONTAINER, $app);
-    this.$racingStatusContainer = $(SELECTOR.RACING_STATUS_CONTAINER, $app);
-    this.$racingResultContainer = $(SELECTOR.RACING_RESULT_CONTAINER, $app);
-    this.$winnersContainer = $(SELECTOR.WINNERS_CONTAINER, $app);
-    this.racingProgressElements = [];
+    this.#carNamesInput = $(SELECTOR.CAR_NAMES_INPUT, $app);
+    this.#carNamesButton = $(SELECTOR.CAR_NAMES_BUTTON, $app);
+    this.#racingCountInput = $(SELECTOR.RACING_COUNT_INPUT, $app);
+    this.#racingCountButton = $(SELECTOR.RACING_COUNT_BUTTON, $app);
+    this.#racingCountContainer = $(SELECTOR.RACING_COUNT_CONTAINER, $app);
+    this.#racingStatusContainer = $(SELECTOR.RACING_STATUS_CONTAINER, $app);
+    this.#racingResultContainer = $(SELECTOR.RACING_RESULT_CONTAINER, $app);
+    this.#winnersContainer = $(SELECTOR.WINNERS_CONTAINER, $app);
   }
 
   showRacingCountInput() {
-    disableElement(this.$carNamesInput);
-    disableElement(this.$carNamesButton);
-    removeClass(this.$racingCountContainer, CLASS_NAME.HIDDEN);
+    disableElement(this.#carNamesInput);
+    disableElement(this.#carNamesButton);
+    removeClass(this.#racingCountContainer, CLASS_NAME.HIDDEN);
   }
 
   renderRacingStatus(cars) {
-    disableElement(this.$racingCountInput);
-    disableElement(this.$racingCountButton);
-    removeClass(this.$racingStatusContainer, CLASS_NAME.HIDDEN);
-    setHTML(this.$racingStatusContainer, generateRacingStatusView(cars));
+    disableElement(this.#racingCountInput);
+    disableElement(this.#racingCountButton);
+    removeClass(this.#racingStatusContainer, CLASS_NAME.HIDDEN);
+    setHTML(this.#racingStatusContainer, generateRacingStatusView(cars));
   }
 
   removeSpinners() {
-    this.racingProgressElements.forEach(element => {
+    this.#racingProgressElements.forEach(element => {
       const { lastChild } = element;
       if (lastChild && lastChild.dataset.status === CAR_STATUS.STAY) {
         element.removeChild(lastChild);
@@ -65,12 +74,12 @@ export default class View {
   }
 
   renderSpinners() {
-    if (!this.racingProgressElements.length)
-      this.racingProgressElements = $$(
+    if (!this.#racingProgressElements.length)
+      this.#racingProgressElements = $$(
         SELECTOR.RACING_PROGRESS,
-        this.$racingStatusContainer,
+        this.#racingStatusContainer,
       );
-    this.racingProgressElements.forEach(element => {
+    this.#racingProgressElements.forEach(element => {
       const { lastChild } = element;
       if (!lastChild || lastChild.dataset.status === CAR_STATUS.MOVE) {
         appendHTML(element, spinnerView);
@@ -79,7 +88,7 @@ export default class View {
   }
 
   renderRacingProgress(carInformations) {
-    this.racingProgressElements.forEach(element => {
+    this.#racingProgressElements.forEach(element => {
       const isMoved = carInformations.get(element.parentNode.dataset.name);
       const { lastChild } = element;
       if (isMoved && lastChild.dataset.status === CAR_STATUS.STAY) {
@@ -90,27 +99,27 @@ export default class View {
   }
 
   renderWinners(winners) {
-    removeClass(this.$racingResultContainer, CLASS_NAME.HIDDEN);
-    setHTML(this.$winnersContainer, generateWinnersView(winners));
+    removeClass(this.#racingResultContainer, CLASS_NAME.HIDDEN);
+    setHTML(this.#winnersContainer, generateWinnersView(winners));
   }
 
   restartGame() {
-    enableElement(this.$carNamesInput);
-    enableElement(this.$carNamesButton);
-    resetInputValue(this.$carNamesInput);
+    enableElement(this.#carNamesInput);
+    enableElement(this.#carNamesButton);
+    resetInputValue(this.#carNamesInput);
 
-    enableElement(this.$racingCountInput);
-    enableElement(this.$racingCountButton);
-    resetInputValue(this.$racingCountInput);
+    enableElement(this.#racingCountInput);
+    enableElement(this.#racingCountButton);
+    resetInputValue(this.#racingCountInput);
 
-    clearHTML(this.$racingStatusContainer);
-    clearHTML(this.$winnersContainer);
+    clearHTML(this.#racingStatusContainer);
+    clearHTML(this.#winnersContainer);
 
-    addClass(this.$racingCountContainer, CLASS_NAME.HIDDEN);
-    addClass(this.$racingStatusContainer, CLASS_NAME.HIDDEN);
-    addClass(this.$racingResultContainer, CLASS_NAME.HIDDEN);
+    addClass(this.#racingCountContainer, CLASS_NAME.HIDDEN);
+    addClass(this.#racingStatusContainer, CLASS_NAME.HIDDEN);
+    addClass(this.#racingResultContainer, CLASS_NAME.HIDDEN);
 
-    this.racingProgressElements = [];
+    this.#racingProgressElements = [];
   }
 
   endGame(winners) {
