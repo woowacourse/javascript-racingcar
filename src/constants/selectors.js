9cea9aa2ff2c1a5d9b044f8910e509056478bc9a
Soyi Jeon <soyi.dev@gmail.com>
2022-02-18 16:41:34 +0900

    refactor: semantic mark-up, CSS 수정

--- src/constants/selectors.js
+++ src/constants/selectors.js
@@ -1,14 +1,14 @@
 const SELECTOR = {
-    CAR_NAME_SUBMIT_BUTTON: '#car-name-submit-button',
-    TRY_COUNT_SUBMIT_BUTTON: '#try-count-submit-button',
-    RESTART_BUTTON: '#restart-button',
+    APP: '#app',
     CAR_NAME_INPUT: '#car-name-input',
     TRY_COUNT_INPUT: '#try-count-input',
-    APP: '#app',
+    CAR_NAME_SUBMIT_BUTTON: '#car-name-submit-button',
+    TRY_COUNT_SUBMIT_BUTTON: '#try-count-submit-button',
     RESULT_AREA: '.result-area',
     TRACK_AREA: '#track-area',
     CAR_STEPS: '.car-steps',
     WINNERS: '#winners',
+    RESTART_BUTTON: '#restart-button',
 };
 
 export default SELECTOR;
