6abf28616f9f3d450c8828cfe119811335b95cce
nan-noo <uni613@naver.com>
2022-02-18 17:32:27 +0900

    refactor: edit test code title

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -22,21 +22,25 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   });
 
   /* 차 이름 */
-  it("자동차 이름의 최대 길이(5글자)를 초과한 값을 입력한 경우 alert로 사용자에게 피드백을 해야 한다.", () => {
+  it("자동차 이름의 최대 길이(5글자)를 초과한 값을 입력한 경우 경고 메세지 창이 떠야 한다.", () => {
     cy.showCarNameAlert("abcdef");
   });
 
+  it("자동차 이름의 공백 제외 최소 길이(1글자) 미만인 값을 입력한 경우 경고 메세지 창이 떠야 한다.", () => {
+    cy.showCarNameAlert("   ");
+  });
+
   /* 시도 횟수 */
-  it("시도 횟수 입력값이 범위를 벗어날 경우(1이상 20이하의 자연수) alert로 사용자에게 피드백을 해야 한다.", () => {
+  it("시도 횟수 입력값이 범위를 벗어날 경우(1이상 20이하의 자연수) 경고 메세지 창이 떠야 한다.", () => {
     cy.showRaceCountAlert('1,2,3,4,5', "-2");
   });
 
-  it("시도 횟수 입력값이 숫자가 아닌 경우 alert로 사용자에게 피드백을 해야 한다.", () => {
+  it("시도 횟수 입력값이 숫자가 아닌 경우 경고 메세지 창이 떠야 한다.", () => {
     cy.showRaceCountAlert('1,2,3,4,5','aae');
   });
 
   /* 다시 시작 */
-  it("다시 시작하기 버튼을 눌렀을 때에 race-count-input-container 요소가 display none이어야 한다", () => {
+  it("다시 시작하기 버튼을 눌렀을 때에 화면이 다시 처음 상태로 돌아가야 한다.", () => {
     cy.normalWorking("tt,sally", raceCount);
     cy.tick(raceCount * ONE_ROUND_TIME_INTERVAL).then(() => {
       cy.get(SELECTOR.RESTART_BUTTON).click();
