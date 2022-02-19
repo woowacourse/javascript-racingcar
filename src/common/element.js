0b0f5e8e3c2faf5869843c95420360b9d6d8d412
Daye Lee <leeda1801@gmail.com>
2022-02-19 18:33:27 +0900

    fix: 로딩 스피너 이미지 경로 수정

--- src/common/element.js
+++ src/common/element.js
@@ -1,6 +1,6 @@
 export default function loadingElement() {
   const loading = document.createElement('img');
-  loading.setAttribute('src', '../../public/loading.png');
+  loading.setAttribute('src', 'public/loading.png');
   loading.setAttribute('alt', 'loading');
   loading.setAttribute('class', 'loading');
 
