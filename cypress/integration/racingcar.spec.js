94ee6e123c029529f845df3b06a8f71eddd4b709
JASUN LEE <liswktjs@gmail.com>
2022-02-19 23:35:01 +0900

    ♻️ refactor: alert문구 검사하기 alert창에 있는 문구를 검사하기 위해서 is-ValidInput에서 error.message을 출력하는 것으로 교체 + 승자 확인하는 경우에는 랜덤으로 돌아가기 때문에 test를 위해서 하나의 이름만을 입력

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -1,21 +1,27 @@
-import {BASE_URL, POSITIVE_CAR_NAME_INPUT, POSITIVE_RACE_COUNT_INPUT} from '../support/constants.js';
-import {ARROW_RENDER_DELAY_TIME, RESULT_RENDER_DELAY_TIME} from '../../src/constants/constants.js';
-describe('구현 결과가 요구사항과 일치해야 한다.', () => {
-
+import {
+  BASE_URL,
+  POSITIVE_CAR_NAME_INPUT,
+  POSITIVE_RACE_COUNT_INPUT,
+} from "../support/constants.js";
+import {
+  ARROW_RENDER_DELAY_TIME,
+  RESULT_RENDER_DELAY_TIME,
+} from "../../src/constants/constants.js";
+describe("구현 결과가 요구사항과 일치해야 한다.", () => {
   beforeEach(() => {
     cy.visit(BASE_URL);
   });
 
   it("5글자 초과 자동차 이름을 입력한 경우 에러메세지를 확인할 수 있어야 한다.", () => {
-    cy.showCarNameAlert("abcdef");
+    cy.showCarNameLengthAlert("abcdef");
   });
 
   it("1이상 20이하의 자연수가 아닌 경우 에러 메세지를 확인 할 수 있어야 한다.", () => {
-    cy.showRaceCountAlert('1,2,3,4,5', "-2");
+    cy.showRaceCountOverRangeAlert("1,2,3,4,5", "-2");
   });
 
   it("숫자가 아닌 경우 에러 메세지를 확인 할 수 있어야 한다.", () => {
-    cy.showRaceCountAlert('1,2,3,4,5','aae');
+    cy.showRaceCountNotIntegerAlert("1,2,3,4,5", "aae");
   });
 
   it("게임을 완료하고 우승자를 알 수 있도록 한다", () => {
@@ -24,28 +30,33 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
     cy.clock();
     cy.tick(POSITIVE_RACE_COUNT_INPUT * ARROW_RENDER_DELAY_TIME).then(() => {
-      cy.get('.result-text').should((result) => {
+      cy.get(".result-text").should((result) => {
         const text = result.text();
-        expect(text).to.include('최종 우승자');
+        expect(text).to.include("최종 우승자");
       });
     });
   });
-  
+
   it("우승자가 화면에 표시 되고 된 난 후에 alert창으로 우승자를 알려 줄 수 있다.", () => {
     const winnerAlertStub = cy.stub();
     cy.on("window:alert", winnerAlertStub);
-    cy.carNamePositiveInputEvent(POSITIVE_CAR_NAME_INPUT);
+    cy.carNamePositiveInputEvent("sally");
     cy.raceCountPositiveInputEvent(POSITIVE_RACE_COUNT_INPUT);
 
     cy.clock();
-    cy.get('.result-text').should((result) => {
-      const text = result.text();
-      expect(text).to.include('최종 우승자')
+    cy.get(".result-text").should((result) => {
+      const resultText = result.text();
+      expect(resultText).to.include("최종 우승자");
     });
-    cy.tick(POSITIVE_RACE_COUNT_INPUT * ARROW_RENDER_DELAY_TIME + RESULT_RENDER_DELAY_TIME).then(() => {
-      expect(winnerAlertStub).to.be.called;
-    })
-  })
+    cy.tick(
+      POSITIVE_RACE_COUNT_INPUT * ARROW_RENDER_DELAY_TIME +
+        RESULT_RENDER_DELAY_TIME
+    ).then(() => {
+      expect(winnerAlertStub).to.be.calledWith(
+        "게임의 우승자인 sally 축하합니다"
+      );
+    });
+  });
 
   it("다시 시작하기 버튼을 눌렀을 때에 시도횟수 입력창이 보이지 않도록 한다.", () => {
     cy.carNamePositiveInputEvent(POSITIVE_CAR_NAME_INPUT);
@@ -53,8 +64,12 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
 
     cy.clock();
     cy.tick(POSITIVE_RACE_COUNT_INPUT * ARROW_RENDER_DELAY_TIME).then(() => {
-      cy.get('.restart-button').contains('다시 시작하기').click();
-      cy.get('.race-count-input-container').should('have.css', 'display', 'none');
-    })
+      cy.get(".restart-button").contains("다시 시작하기").click();
+      cy.get(".race-count-input-container").should(
+        "have.css",
+        "display",
+        "none"
+      );
+    });
   });
 });
--- src/modules/isCarNameInputValid.js
+++ src/modules/isCarNameInputValid.js
@@ -3,24 +3,27 @@ import { OVER_CARNAME_LENGTH_ERROR } from "../constants/error.js";
 import checkUserInputIsEmpty from "./checkUserInputIsEmpty.js";
 import { $ } from "../dom/dom.js";
 
-
-function checkCarNameLength(carNameArray){
-    if(carNameArray.filter(carName => carName.length > CARNAME_LENGTH_LIMIT).length){
-        throw new Error(OVER_CARNAME_LENGTH_ERROR);
-    };
-    return true;
+function checkCarNameLength(carNameArray) {
+  if (
+    carNameArray.filter((carName) => carName.length > CARNAME_LENGTH_LIMIT)
+      .length
+  ) {
+    throw new Error(OVER_CARNAME_LENGTH_ERROR);
+  }
+  return true;
 }
 
-export default function isCarNameInputValid(userCarNameInput){
-    try{
-        let isCarNameValid = false;
-        if(!checkUserInputIsEmpty(userCarNameInput)){
-            isCarNameValid = checkCarNameLength(userCarNameInput.split(',').map(carName => carName.trim()));
-        };
-        return isCarNameValid;
-    }catch(msg){
-        $('#car-name-input').value = '';
-        alert(msg);
+export default function isCarNameInputValid(userCarNameInput) {
+  try {
+    let isCarNameValid = false;
+    if (!checkUserInputIsEmpty(userCarNameInput)) {
+      isCarNameValid = checkCarNameLength(
+        userCarNameInput.split(",").map((carName) => carName.trim())
+      );
     }
+    return isCarNameValid;
+  } catch (err) {
+    $("#car-name-input").value = "";
+    alert(err.message);
+  }
 }
-
