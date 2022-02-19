e3204a64f1fddb17aa3424c8b9b6cfb28af78615
airman5573 <rpf5573@kakao.com>
2022-02-19 14:29:49 +0900

    :recycle: refactor(cleanup): 병합하면서 생긴 불필요한 파일들을 삭제한다

--- src/js/utils/htmlToElement.js
+++
@@ -1,7 +0,0 @@
-const htmlToElement = (html) => {
-  const template = document.createElement('template');
-  template.innerHTML = html.trim();
-  return template.content.firstChild;
-};
-
-export default htmlToElement;
--- src/js/utils/setAttribute.js
+++
@@ -1,7 +0,0 @@
-export function hideElement(element) {
-  return element.setAttribute('hidden', true);
-}
-
-export function showElement(element) {
-  return element.removeAttribute('hidden');
-}
