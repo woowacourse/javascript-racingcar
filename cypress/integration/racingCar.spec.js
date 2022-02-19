7324c6fa22fb18c6e51f0ca2da7e479ab4365dad
Daye Lee <leeda1801@gmail.com>
2022-02-19 15:49:45 +0900

    refactor: custom commands 생성 및 테스트 분리

--- cypress/integration/racingCar.spec.js
+++ cypress/integration/racingCar.spec.js
@@ -1,11 +1,134 @@
 import { ERROR_MESSAGE, SELECTOR } from '../../src/common/constants.js';
 
-describe('구현 결과가 요구사항과 일치해야 한다.', () => {
-  const baseUrl = '../index.html';
+describe('잘못된 자동차 이름이 제출되면 에러 메세지를 확인하고 자동차 이름을 다시 입력할 수 있어야 한다.', () => {
+  const message = ERROR_MESSAGE.CAR_NAMES;
 
+  before(() => {
+    cy.visit('index.html');
+  });
+
+  it('공백으로만 이루어진 자동차 이름이 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
+    const alertStub = cy.stub();
+    const carNames = ' ';
+
+    cy.on('window:alert', alertStub);
+
+    cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, carNames).then(() => {
+      expect(alertStub).to.be.calledWith(message);
+    });
+  });
+
+  it('공백으로만 이루어진 자동차 이름이 제출되면 자동차 이름을 다시 입력할 수 있어야 한다.', () => {
+    cy.initInput(SELECTOR.CAR_NAMES_INPUT);
+  });
+
+  it('5자를 초과하는 자동차 이름이 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
+    const alertStub = cy.stub();
+    const carNames = 'ab, cde, fghijk';
+
+    cy.on('window:alert', alertStub);
+
+    cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, carNames).then(() => {
+      expect(alertStub).to.be.calledWith(message);
+    });
+  });
+
+  it('5자를 초과하는 자동차 이름이 제출되면 자동차 이름을 다시 입력할 수 있어야 한다.', () => {
+    cy.initInput(SELECTOR.CAR_NAMES_INPUT);
+  });
+
+  it('하나의 자동차 이름이 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
+    const alertStub = cy.stub();
+    const carNames = 'king';
+
+    cy.on('window:alert', alertStub);
+
+    cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, carNames).then(() => {
+      expect(alertStub).to.be.calledWith(message);
+    });
+  });
+
+  it('하나의 자동차 자동차 이름이 제출되면 자동차 이름을 다시 입력할 수 있어야 한다.', () => {
+    cy.initInput(SELECTOR.CAR_NAMES_INPUT);
+  });
+
+  it('중복된 자동차 이름이 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
+    const alertStub = cy.stub();
+    const carNames = 'king, tiger, tiger';
+
+    cy.on('window:alert', alertStub);
+
+    cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, carNames).then(() => {
+      expect(alertStub).to.be.calledWith(message);
+    });
+  });
+
+  it('중복된 자동차 이름이 제출되면 자동차 이름을 다시 입력할 수 있어야 한다.', () => {
+    cy.initInput(SELECTOR.CAR_NAMES_INPUT);
+  });
+});
+
+describe('잘못된 시도 횟수가 제출되면 에러 메세지를 확인하고 시도 횟수를 다시 입력할 수 있어야 한다.', () => {
+  const message = ERROR_MESSAGE.RACING_COUNT;
+
+  it('숫자가 아닌 시도 횟수가 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
+    const alertStub = cy.stub();
+    const carNames = 'king, white, tiger';
+    const racingCount = 'e';
+
+    cy.on('window:alert', alertStub);
+
+    cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, carNames);
+    cy.submitForm(SELECTOR.RACING_COUNT_INPUT, SELECTOR.RACING_COUNT_SUBMIT, racingCount).then(
+      () => {
+        expect(alertStub).to.be.calledWith(message);
+      }
+    );
+  });
+
+  it('숫자가 아닌 시도 횟수가 제출되면 시도 횟수를 다시 입력할 수 있어야 한다.', () => {
+    cy.initInput(SELECTOR.RACING_COUNT_INPUT);
+  });
+
+  it('1 미만의 시도 횟수가 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
+    const alertStub = cy.stub();
+    const racingCount = 0;
+
+    cy.on('window:alert', alertStub);
+
+    cy.submitForm(SELECTOR.RACING_COUNT_INPUT, SELECTOR.RACING_COUNT_SUBMIT, racingCount).then(
+      () => {
+        expect(alertStub).to.be.calledWith(message);
+      }
+    );
+  });
+
+  it('1 미만의 시도 횟수가 제출되면 시도 횟수를 다시 입력할 수 있어야 한다.', () => {
+    cy.initInput(SELECTOR.RACING_COUNT_INPUT);
+  });
+
+  it('정수가 아닌 시도 횟수가 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
+    const alertStub = cy.stub();
+    const racingCount = 1.5;
+
+    cy.on('window:alert', alertStub);
+
+    cy.submitForm(SELECTOR.RACING_COUNT_INPUT, SELECTOR.RACING_COUNT_SUBMIT, racingCount).then(
+      () => {
+        expect(alertStub).to.be.calledWith(message);
+      }
+    );
+  });
+
+  it('정수가 아닌 시도 횟수가 제출되면 시도 횟수를 다시 입력할 수 있어야 한다.', () => {
+    cy.initInput(SELECTOR.RACING_COUNT_INPUT);
+  });
+});
+
+describe('자동차 경주 게임을 진행할 수 있어야 한다.', () => {
   beforeEach(() => {
     cy.clock();
-    cy.visit(baseUrl);
+    cy.visit('index.html');
   });
 
   afterEach(() => {
@@ -14,36 +137,10 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
     });
   });
 
-  it('잘못된 자동차 이름이 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
-    const alertStub = cy.stub();
-    const carNames = 'ab,cde,fghijk';
-
-    cy.on('window:alert', alertStub);
-
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type(carNames);
-
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT)
-      .click()
-      .then(() => {
-        expect(alertStub).to.be.calledWith(ERROR_MESSAGE.CAR_NAMES);
-      });
-  });
-
-  it('잘못된 자동차 이름이 제출되면 자동차 이름을 다시 입력할 수 있어야 한다.', () => {
-    const carNames = 'ab,cde,fghijk';
-
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type(carNames);
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
-
-    cy.get(SELECTOR.CAR_NAMES_INPUT).should('have.value', '');
-    cy.get(SELECTOR.CAR_NAMES_INPUT).should('have.focus');
-  });
-
   it('올바른 자동차 이름이 제출되면 자동차 이름을 확인할 수 있어야 한다.', () => {
     const carNames = 'king, white, tiger';
 
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type(carNames);
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
+    cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, carNames);
 
     cy.get('#white').should('be.visible');
     cy.get('#king').should('be.visible');
@@ -53,123 +150,42 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   it('올바른 자동차 이름이 제출되면 시도 횟수를 입력할 수 있어야 한다.', () => {
     const carNames = 'king, white, tiger';
 
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type(carNames);
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
+    cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, carNames);
 
     cy.get(SELECTOR.RACING_COUNT_INPUT).should('be.visible');
     cy.get(SELECTOR.RACING_COUNT_SUBMIT).should('be.visible');
   });
 
-  it('잘못된 시도 횟수가 제출되면 에러 메세지를 확인할 수 있어야 한다.', () => {
-    const alertStub = cy.stub();
-    const racingCountInput = 'e';
-
-    cy.on('window:alert', alertStub);
-
-    // when
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('king, white, tiger');
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
-
-    cy.get(SELECTOR.RACING_COUNT_INPUT).type(racingCountInput);
-    cy.get(SELECTOR.RACING_COUNT_SUBMIT)
-      .click()
-      .then(() => {
-        // then
-        expect(alertStub).to.be.calledWith(ERROR_MESSAGE.RACING_COUNT);
-      });
-  });
-
-  it('잘못된 시도 횟수가 제출되면 시도 횟수를 다시 입력할 수 있어야 한다.', () => {
-    const racingCountInput = 'e';
-
-    // when
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('king, white, tiger');
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
-
-    cy.get(SELECTOR.RACING_COUNT_INPUT).type(racingCountInput);
-    cy.get(SELECTOR.RACING_COUNT_SUBMIT).click();
-
-    // then
-    cy.get(SELECTOR.RACING_COUNT_INPUT).should('have.value', '');
-    cy.get(SELECTOR.RACING_COUNT_INPUT).should('have.focus');
-  });
-
   it('게임이 끝나면 최종 우승자를 확인할 수 있어야 한다.', () => {
-    const racingCount = 5;
-    const delayPerRace = 1000;
-    const totalDelay = delayPerRace * racingCount;
-
-    // when
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('king, white, tiger');
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
-
-    cy.get(SELECTOR.RACING_COUNT_INPUT).type(racingCount);
-    cy.get(SELECTOR.RACING_COUNT_SUBMIT).click();
-
-    cy.tick(totalDelay).then(() => {
-      // then
+    cy.race(0).then(() => {
       cy.get('#winners').should('be.visible');
     });
   });
 
   it('게임이 끝나면 게임을 다시 시작할 수 있어야 한다.', () => {
-    const racingCount = 5;
-    const delayPerRace = 1000;
-    const totalDelay = delayPerRace * racingCount;
-
-    // when
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('king, white, tiger');
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
-
-    cy.get(SELECTOR.RACING_COUNT_INPUT).type(racingCount);
-    cy.get(SELECTOR.RACING_COUNT_SUBMIT).click();
-
-    cy.tick(totalDelay).then(() => {
-      // then
+    cy.race(0).then(() => {
       cy.get(SELECTOR.RESTART).should('be.visible');
     });
   });
 
   it('게임이 끝나고 2초 후에 최종 우승자 축하 메세지를 확인할 수 있어야 한다.', () => {
     const alertStub = cy.stub();
-    const racingCount = 5;
-    const delayPerRace = 1000;
     const delayAfterEnd = 2000;
-    const totalDelay = delayPerRace * racingCount + delayAfterEnd;
 
     cy.on('window:alert', alertStub);
 
-    // when
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('king, white, tiger');
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
-
-    cy.get(SELECTOR.RACING_COUNT_INPUT).type(racingCount);
-    cy.get(SELECTOR.RACING_COUNT_SUBMIT).click();
-
-    cy.tick(totalDelay).then(() => {
-      // then
+    cy.race(delayAfterEnd).then(() => {
       expect(alertStub).to.be.called;
     });
   });
 
   it('게임을 다시 시작하면 자동차 이름 입력만 할 수 있어야 한다.', () => {
     const racingCount = 5;
-    const delayPerRace = 1000;
-    const delayAfterEnd = 2000;
-    const totalDelay = delayPerRace * racingCount + delayAfterEnd;
 
-    // when
-    cy.get(SELECTOR.CAR_NAMES_INPUT).type('king, white, tiger');
-    cy.get(SELECTOR.CAR_NAMES_SUBMIT).click();
-
-    cy.get(SELECTOR.RACING_COUNT_INPUT).type(racingCount);
-    cy.get(SELECTOR.RACING_COUNT_SUBMIT).click();
-
-    cy.tick(totalDelay).then(() => {
+    cy.race(0).then(() => {
       cy.get(SELECTOR.RESTART).click();
     });
 
-    // then
     cy.get(SELECTOR.CAR_NAMES_INPUT).should('be.visible');
     cy.get(SELECTOR.CAR_NAMES_SUBMIT).should('be.visible');
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { SELECTOR } from '../../src/common/constants.js';
+
+Cypress.Commands.add('submitForm', ($input, $submit, carNames) => {
+  cy.get($input).type(carNames);
+  cy.get($submit).click();
+});
+
+Cypress.Commands.add('initInput', ($input) => {
+  cy.get($input).should('have.value', '');
+  cy.get($input).should('have.focus');
+});
+
+Cypress.Commands.add('race', (delayAfterEnd) => {
+  const racingCount = 5;
+  const delayPerRace = 1000;
+  const totalDelay = delayPerRace * racingCount + delayAfterEnd;
+
+  cy.submitForm(SELECTOR.CAR_NAMES_INPUT, SELECTOR.CAR_NAMES_SUBMIT, 'king, white, tiger');
+  cy.submitForm(SELECTOR.RACING_COUNT_INPUT, SELECTOR.RACING_COUNT_SUBMIT, racingCount);
+  cy.tick(totalDelay);
+});
