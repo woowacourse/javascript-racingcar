a39a9c562957c3602b2e877946ae26135f3b2599
kamwoo <rkadndud06@naver.com>
2022-02-20 14:22:31 +0900

    feat: constant 변경 불가 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const NUMBER = {
+export const NUMBER = Object.freeze({
   MAX_LENGTH: 5,
   MIN_NUMBER: 1,
   INITIAL_STEP: 0,
@@ -8,17 +8,17 @@ export const NUMBER = {
   STEP: 1,
   ARROW_INTERVAL_TIME: 1000,
   WINNER_ALERT_TIME: 2000,
-};
+});
 
-export const MESSAGE = {
+export const MESSAGE = Object.freeze({
   EMPTY_INPUT: '1글자 이상의 자동차 이름을 입력해주세요.',
   LONG_LENGTH: `각 자동차의 이름을 ${NUMBER.MAX_LENGTH}자 이하로 입력해주세요.`,
   UNDER_MIN_NUMBER: `${NUMBER.MIN_NUMBER} 이상의 수를 입력해주세요.`,
   DECIMAL: '자연수를 입력해주세요.',
   DUPLICATE_NAME: '이름을 중복되지 않게 입력해주세요.',
-};
+});
 
-export const SELECTOR = {
+export const SELECTOR = Object.freeze({
   NAME_INPUT: '.input-section__name-input',
   NAME_BUTTON: '.input-section__name-button',
   COUNT_INPUT: '.input-section__count-input',
@@ -29,9 +29,9 @@ export const SELECTOR = {
   WINNER: '#winner',
   RESET_BUTTON: '#reset-button',
   STEP_SECTION_ARROWS: '.step-section__arrows',
-};
+});
 
-export const CLASS_NAME = {
+export const CLASS_NAME = Object.freeze({
   DISPLAY_NONE: 'display-none',
   SPINNING_BACKGROUND: 'spinning-bg',
-};
+});
