fd646cd062ec2f94069d90d148c3e22de0f05199
airman5573 <rpf5573@kakao.com>
2022-02-21 02:15:06 +0900

    :bug: fix(car-names-input): '<' 혹은 '>'와 같은 특수문자가 자동차 이름으로 입력되었을때 htlm tag로 인식되는 오류를 해결한다

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,2 +1,7 @@
 export const querySelector = (selector, context = document) => context.querySelector(selector);
 export const querySelectorAll = (selector, context = document) => context.querySelectorAll(selector);
+export const htmlToElement = (htmlString) => {
+  const template = document.createElement('div');
+  template.innerHTML = htmlString;
+  return template.firstElementChild;
+};
--- src/js/views/racing-screen.view.js
+++ src/js/views/racing-screen.view.js
@@ -1,5 +1,5 @@
 import { SELECTORS } from '../constants.js';
-import { querySelector, querySelectorAll } from '../utils/dom.js';
+import { htmlToElement, querySelector, querySelectorAll } from '../utils/dom.js';
 import { hideElement, showElement } from '../utils/visibility.js';
 
 class RacingScreen {
@@ -8,14 +8,13 @@ class RacingScreen {
     this.init();
   }
 
-  static laneTemplate(name) {
-    return `<div class="car-lane" data-testid="car-lane">
-        <label class="car-name" data-testid="car-name">${name}</label>
-        <div class="distance" data-current-distance="0" data-testid="distance">
-        </div>
-        <div class="spinner-container" data-testid="spinner" hidden><div class="spinner" data-testid="spinner"></div></div>
-      </div>`;
-  }
+  static laneTemplate = `
+    <div class="car-lane" data-testid="car-lane">
+      <label class="car-name" data-testid="car-name"></label>
+      <div class="distance" data-current-distance="0" data-testid="distance">
+      </div>
+      <div class="spinner-container" data-testid="spinner" hidden><div class="spinner" data-testid="spinner"></div></div>
+    </div>`;
 
   static forwardIcon = `<div class="forward-icon" data-testid="forward-icon">️⬇️️</div>`;
 
@@ -53,8 +52,9 @@ class RacingScreen {
   renderLanes(cars) {
     this.clear();
     cars.forEach(({ name }) => {
-      const template = RacingScreen.laneTemplate(name);
-      this.$screen.insertAdjacentHTML('beforeend', template);
+      const $lane = htmlToElement(RacingScreen.laneTemplate);
+      this.insertCarNameInLane($lane, name);
+      this.$screen.appendChild($lane);
     });
   }
 
@@ -63,6 +63,12 @@ class RacingScreen {
     const currentDistance = parseInt($distance.getAttribute('data-current-distance'), 10);
     $distance.insertAdjacentHTML('beforeend', RacingScreen.forwardIcon.repeat(distance - currentDistance));
     $distance.setAttribute('data-current-distance', distance);
+  }
+
+  insertCarNameInLane($lane, carName) {
+    const $carName = querySelector(SELECTORS.CAR_NAME, $lane);
+    const $textNode = document.createTextNode(carName);
+    $carName.appendChild($textNode);
   }
 
   clear() {
