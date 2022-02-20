e25c5a9c20f77abd52e91a326600db18f3310003
jhy979 <jhy979@gmail.com>
2022-02-21 00:18:08 +0900

    test: 로컬 서버 대신 index.html을 방문하도록 수정

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -2,7 +2,7 @@ import { ERROR_MESSAGE, RESULT, SELECTOR } from '../../js/utils/constants.js';
 
 describe('자동차의 전진이 정상적으로 작동하는지 검증합니다.', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5500/');
+    cy.visit('index.html');
     cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west');
     cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
   });
@@ -20,7 +20,6 @@ describe('자동차의 전진이 정상적으로 작동하는지 검증합니다
   });
 
   it('주어진 횟수 동안 n 대의 자동차는 전진 또는 멈출 수 있다.', () => {
-    cy.clock();
     cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(5);
     cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
     cy.wait(5000).then(() => {
@@ -31,9 +30,7 @@ describe('자동차의 전진이 정상적으로 작동하는지 검증합니다
 
 describe('최종 우승자를 검증합니다.', () => {
   beforeEach(() => {
-    cy.clock();
-    cy.visit('http://localhost:5500/');
-
+    cy.visit('index.html');
     cy.get(SELECTOR.CAR_NAMES_INPUT).type('east,west,south,north');
     cy.get(SELECTOR.CAR_NAMES_BUTTON).click();
     cy.get(SELECTOR.CAR_RACING_COUNT_INPUT).type(1);
@@ -42,7 +39,6 @@ describe('최종 우승자를 검증합니다.', () => {
   it('자동차 경주 게임을 정상적으로 완료하고 2초 뒤에 축하 메시지를 확인할 수 있다.', () => {
     const alertStub = cy.stub();
     cy.on(SELECTOR.WINDOW_ALERT, alertStub);
-
     cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
     cy.wait(3000).then(() => {
       expect(alertStub).to.be.calledWith(RESULT.RACE_RESULT);
@@ -51,8 +47,7 @@ describe('최종 우승자를 검증합니다.', () => {
 
   it('최종 우승자를 출력할 수 있다.', () => {
     cy.get(SELECTOR.CAR_RACING_COUNT_BUTTON).click();
-
-    cy.tick(3000).then(() => {
+    cy.wait(3000).then(() => {
       cy.get(SELECTOR.CAR_RACING_WINNER).contains('최종 우승자');
     });
   });
@@ -61,7 +56,7 @@ describe('최종 우승자를 검증합니다.', () => {
 describe('사용자의 입력을 검증합니다.', () => {
   let alertStub;
   beforeEach(() => {
-    cy.visit('http://localhost:5500/');
+    cy.visit('index.html');
     alertStub = cy.stub();
     cy.on(SELECTOR.WINDOW_ALERT, alertStub);
   });
