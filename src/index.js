0952747795f584bdb99d4aa7198ff28b1128db3f
junzzi <jjh01027717502@gmail.com>
2022-02-21 16:11:59 +0900

    refactor: 게임 구동 로직을 수정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 /* eslint-disable no-new */
 import RacingCarGame from './app/index.js';
 
-const racingCarGame = new RacingCarGame();
-
-racingCarGame.start();
-
-// racingCarGame이 가리키고 있는 동적 객체를 해제할 필요가 있나요 ??
+export default new RacingCarGame().start();
