9fed92b0e7c4ac848f924bd9ef8ae11e19fee53b
sh981013s <sh981013s@gmail.com>
2023-02-07 14:40:53 +0900

    feat(InputView): 초기 경주할 자동차 이름 입력 기능 구현

---
+++ src/view/InputView.js
@@ -0,0 +1,12 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const InputView = {
+  readCarNames(callback) {
+    Console.readLine(
+      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
+      callback,
+    );
+  },
+};
+
+module.exports = InputView;
