b34c46c0e16a1a97143b5ec668726fe10627be15
김태은 <taeeun@gimtaeeun-ui-MacBookAir.local>
2023-02-07 15:53:00 +0900

    feat: 자동차 이름 중복 여부 유효성 검사 구현

--- src/Validation.js
+++ src/Validation.js
@@ -3,7 +3,9 @@ const { Console } = require('@woowacourse/mission-utils');
 const Validation = {
   validateCarName(carNames) {
     this.validateCarNameLength(carNames);
+    this.validateCarNameDuplicated(carNames);
   },
+
   validateCarNameLength(carNames) {
     carNames.forEach((carName) => {
       if (carName.length <= 0 || carName.length > 5) {
@@ -11,6 +13,14 @@ const Validation = {
         throw new Error();
       }
     });
+  },
+
+  validateCarNameDuplicated(carNames) {
+    const carSet = new Set(carNames);
+    if (carNames.length !== carSet.size) {
+      Console.print('자동차 이름은 중복되지 않아야 합니다.');
+      throw new Error();
+    }
   },
 };
 
