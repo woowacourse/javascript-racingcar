d20847d057c5752c2fa01033661ddebd1b7b60ff
Leejin-Yang <ejin1996@gmail.com>
2023-02-07 15:21:46 +0900

    feat: InputView 구현
    Co-authored-by: nangkyeonglim <nangkyeonglim@gmail.com>

---
+++ src/views/InputView.js
@@ -0,0 +1,16 @@
+import Console from '../utils/console';
+
+const InputView = {
+  readCarName(callback) {
+    Console.readLine(
+      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
+      callback
+    );
+  },
+
+  readMovingCount(callback) {
+    Console.readLine('시도할 회수는 몇회인가요?\n', callback);
+  },
+};
+
+module.exports = InputView;
