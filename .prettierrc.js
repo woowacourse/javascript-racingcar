d7af1cc8bfcc829ca843027af8ffb0d1cd7cb7e0
Gilpop8663 <wolfye@naver.com>
2023-02-07 15:05:08 +0900

    chore: eslint, prettier 세팅 설정

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/**
+ * Prettier 옵션
+ * - https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  printWidth: 80, // 한 줄 최대 문자 수
+  tabWidth: 2, // 들여쓰기 시, 탭 너비
+  useTabs: false, // 스페이스 대신 탭 사용
+  semi: true, // 문장 끝 세미콜론 사용
+  singleQuote: true, // 작은 따옴표 사용
+  trailingComma: "es5", // es5 설정 (기본값)
+  bracketSpacing: true, // 중괄호 내에 공백 사용
+  arrowParens: "always", // https://github.com/airbnb/javascript#arrows--one-arg-parens
+  proseWrap: "never", // 마크다운 포매팅 제외
+  endOfLine: "auto", // 개행문자 유지 (혼합일 경우, 첫 줄 개행문자로 통일)
+  quoteProps: "as-needed",
+};
