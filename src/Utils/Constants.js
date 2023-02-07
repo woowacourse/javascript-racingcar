38187b686cc7e75b384a54839faa966c358af058
hozzijeong <ghwls0905@naver.com>
2023-02-07 15:21:22 +0900

    feat: Create Constants.js

---
+++ src/Utils/Constants.js
@@ -0,0 +1,9 @@
+const GAME_MESSAGE = Object.freeze({
+  INPUT_CAR_NAME:
+    "경주할 자동치 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).",
+  INPUT_TRY_COUNT: "시도할 횟수는 몇회인가요?",
+  RUN_RESULT_MESSAGE: "실행 결과",
+  MOVEMENT_UNIT: "-",
+  COLON: ":",
+  GAME_RESULT: "(이)가 최종 우승했습니다.",
+});
