313c1c320dac982d61bc1d53b424042be4540ae9
hae-on <solbi2004@naver.com>
2023-02-07 15:59:49 +0900

    feat: ERROR 상수 추가

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,6 @@
 const MESSAGE = Object.freeze({
   CAR_NAME_INPUT:
-    '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
+    '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
   TRY_COUNT_INPUT: '시도할 회수는 몇회인가요?',
   RESULT_OUTPUT: '실행 결과',
   WINNER_OUTPUT: '가 최종 우승했습니다.',
@@ -11,7 +11,13 @@ const SYMBOL = Object.freeze({
   MOVE_SYMBOL: '-',
 });
 
+const ERROR_MESSAGE = Object.freeze({
+  CAR_NAME_LENGTH_ERROR: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+  TRY_COUNT_TYPE_ERROR: '[ERROR] 시도 횟수는 숫자로 입력해주세요.',
+});
+
 module.exports = {
   MESSAGE,
   SYMBOL,
+  ERROR_MESSAGE,
 };
