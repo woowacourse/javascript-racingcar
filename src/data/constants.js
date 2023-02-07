98a0e2f744f5f8725ffaafb864f4543b0e9b24ff
hae-on <solbi2004@naver.com>
2023-02-07 14:52:23 +0900

    feat: 상수 목록 작성

---
+++ src/data/constants.js
@@ -0,0 +1,17 @@
+const MESSAGE = Object.freeze({
+  CAR_NAME_INPUT:
+    '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
+  TRY_COUNT_INPUT: '시도할 회수는 몇회인가요?',
+  RESULT_OUTPUT: '실행 결과',
+  WINNER_OUTPUT: '가 최종 우승했습니다.',
+});
+
+const SYMBOL = Object.freeze({
+  ASSIGNMENT: ':',
+  MOVE_SYMBOL: '-',
+});
+
+module.exports = {
+  MESSAGE,
+  SYMBOL,
+};
