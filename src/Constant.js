3d1bf0204a9d1ebb70d964e037855757e2a95fc7
NaveOWO <pianowell96@gmail.com>
2023-02-07 15:16:34 +0900

    [ Feat ] 상수 작성

---
+++ src/Constant.js
@@ -0,0 +1,15 @@
+
+const CONSTANT = {
+    INPUT_CAR_MASSEGE: "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n",
+    INPUT_ROUND_MASSEGE: "시도할 회수는 몇회인가요?\n",
+    
+    RANDOM_MIN: 0,
+    RANDOM_MAX: 9,
+    GO_POINT: 4,
+
+    RESULT_TITLE: "실행 결과\n",
+    RESULT_MASSEGE: "가 최종 우승했습니다.",
+}
+
+
+module.exports = CONSTANT;
