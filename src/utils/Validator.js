1dd7212f45e071c6ce961ad34b063838c7762d64
bassyu <yrkdgus@gmail.com>
2023-02-08 18:05:13 +0900

    feat(Validator): 입력값 예외처리 구현

---
+++ src/utils/Validator.js
@@ -0,0 +1,22 @@
+const Validator = {
+  checkName(input) {
+    if (input[input.length - 1] === ',') {
+      throw new Error('입력값 마지막에 쉼표가 포함되어있습니다.');
+    }
+  },
+  checkDuplicate(input) {
+    const names = input.split(',');
+    if (new Set(names).size !== names.length) {
+      throw new Error('입력값에 중복되는 이름이 존재합니다.');
+    }
+  },
+  checkIntegerNumber(input) {
+    input.split('').forEach((char) => {
+      if (!'0123456789'.includes(char)) {
+        throw new Error('입력값이 정수가 아닙니다.');
+      }
+    });
+  },
+};
+
+export default Validator;
