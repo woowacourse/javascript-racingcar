f7b4d3ad3d2acce46f092084b8adedb6d8bcbef0
sh981013s <sh981013s@gmail.com>
2023-02-08 18:12:34 +0900

    test(CarManager): 자동차 각각을 전진시키는 기능 테스트 작성

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -1,5 +1,14 @@
 const CarManager = require('../src/CarManager');
 
+const RandomNumberGenerator = require('../src/utils/RandomNumberGenerator');
+
+const mockRandoms = (numbers) => {
+  RandomNumberGenerator.generate = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, RandomNumberGenerator.generate);
+};
+
 describe('자동차를 관리하는 클래스 테스트', () => {
   let carManager;
 
@@ -9,5 +18,15 @@ describe('자동차를 관리하는 클래스 테스트', () => {
 
   test('자동차 관리자 인스턴스를 생성한다.', () => {
     expect(carManager.getCars().length).toEqual(3);
+  });
+
+  test('자동차 각각을 전진시킨다', () => {
+    mockRandoms([5, 2, 1]);
+
+    carManager.progress();
+
+    expect(carManager.getCars().map((singleCar) => singleCar.getProgressCount())).toEqual(
+      [1, 0, 0],
+    );
   });
 });
