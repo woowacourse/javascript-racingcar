ea6e1392cdd2169d5f60796f1d4f05a553bb0ea7
Jungwoo <evencoding@gmail.com>
2023-02-08 16:58:44 +0900

    feat: 입력된 시도 횟수에 대한 예외 처리 구현

--- src/Controller.js
+++ src/Controller.js
@@ -11,17 +11,31 @@ class Controller {
   askCarName() {
     View.input(
       "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n",
-      this.handleCarName
+      this.handleCarName.bind(this)
     );
   }
 
   handleCarName(names) {
     try {
       Validator.validateName(names);
-      this.#service = new Service(names.split(","));
+      //this.#service = new Service(names.split(","));
     } catch ({ message }) {
       View.output("ERROR: " + message);
       return this.askCarName();
+    }
+    this.askTryCnt();
+  }
+
+  askTryCnt() {
+    View.input("시도할 회수는 몇회인가요?\n", this.handleTryCnt.bind(this));
+  }
+
+  handleTryCnt(cnt) {
+    try {
+      Validator.validateTryCnt(cnt);
+    } catch ({ message }) {
+      View.output("ERROR: " + message);
+      return this.askTryCnt();
     }
   }
 }
--- src/Validator.js
+++ src/Validator.js
@@ -6,6 +6,12 @@ const Validator = {
       throw new Error("5자 이하 영어,한글,숫자의 조합을 입력하세요");
     }
   },
+  validateTryCnt: (inputValue) => {
+    const regex = /^[1-9][0-9]*$/;
+    if (!regex.test(inputValue)) {
+      throw new Error("1이상의 양의 정수를 입력하세요");
+    }
+  },
 };
 
 module.exports = { Validator };
