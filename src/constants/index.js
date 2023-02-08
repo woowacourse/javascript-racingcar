9915ecdfc19e7b7c4b578e84cd0704e3a5fe5368
inyeong-kang <tomatorkd@naver.com>
2023-02-08 17:37:42 +0900

    fix(constants): 함수에서 문자열로 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,13 +13,9 @@ const GAME_MESSAGE = Object.freeze({
   askCarName:
     '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
   askAttempts: '시도할 회수는 몇회인가요?',
-  getResult: (result) => {
-    // eslint-disable-next-line quotes
-    console.log(result);
-    ('실행 결과');
-  },
+  progressResult: '실행 결과',
   getWinners: (names) => {
-    `${names}가 최종 우승했습니다.`;
+    return `${names}가 최종 우승했습니다.`;
   },
 });
 
