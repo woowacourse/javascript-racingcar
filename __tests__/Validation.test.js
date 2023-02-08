e4806a1b05cedcfbc2d94e7559be2e5fd5190616
2yunseong <dbsdltjd3701@naver.com>
2023-02-08 16:19:20 +0900

    test: 시도 횟수를 입력이 자연수인지 판별하는 기능 테스트

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,4 +1,4 @@
-const { isValidCarNames } = require('../src/utils/Validation');
+const { isValidCarNames, isValidTryCount } = require('../src/utils/Validation');
 
 describe('Validation Test', () => {
   test('이름이 1~5자 인가?', () => {
@@ -7,5 +7,17 @@ describe('Validation Test', () => {
     //when
     expect(isValidCarNames(names)).toBeFalsy();
     //then
+  });
+
+  test('시도 횟수가 자연수인가?', () => {
+    const tryCount = '-1';
+    const tryCount2 = '0';
+    const tryCount3 = 'dfgh34';
+    const tryCount4 = '5';
+
+    expect(isValidTryCount(tryCount)).toBeFalsy();
+    expect(isValidTryCount(tryCount2)).toBeFalsy();
+    expect(isValidTryCount(tryCount3)).toBeFalsy();
+    expect(isValidTryCount(tryCount4)).toBeTruthy();
   });
 });
