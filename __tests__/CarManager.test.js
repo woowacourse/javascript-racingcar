436237f20f70d7589d0d26b038a29a4e1407cd0c
sh981013s <sh981013s@gmail.com>
2023-02-08 17:51:53 +0900

    refactor(CarManagerTest): carManager를 각 테스트 실행 이전에 초기화 하도록 setup

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -1,9 +1,13 @@
 const CarManager = require('../src/CarManager');
 
 describe('자동차를 관리하는 클래스 테스트', () => {
-  test('자동차 관리자 인스턴스를 생성한다.', () => {
-    const carManager = new CarManager(['pobi', 'crong', 'honux']);
+  let carManager;
 
+  beforeEach(() => {
+    carManager = new CarManager(['pobi', 'crong', 'honux']);
+  });
+
+  test('자동차 관리자 인스턴스를 생성한다.', () => {
     expect(carManager.getCars().length).toEqual(3);
   });
 });
