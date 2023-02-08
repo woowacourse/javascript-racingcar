bab84d4ebbf80c4892fd20206bf44380d7b0b885
yeopto <gunyeop530@gmail.com>
2023-02-08 16:08:29 +0900

    feat: InputView 객체를 생성하다

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,5 +1,25 @@
-const InputView = {
+const Console = require('../Utils/Console');
+const RaceMessage = require('../Constant/RaceMessage');
 
+const InputView = {
+  getUserInput(question, callback, redirect) {
+    Console.readLine(question, input => {
+      try {
+        callback(input);
+      } catch (err) {
+        Console.print(err);
+        redirect(callback);
+      }
+    });
+  },
+
+  readCarName(callback) {
+    this.getUserInput(RaceMessage.INPUT_CAR_NAME, callback, this.readCarName);
+  },
+
+  readTryCount(callback) {
+    this.getUserInput(RaceMessage.INPUT_TRY_COUNT, callback, this.readTryCount);
+  },
 };
 
 module.exports = InputView;
