084ab45b51de8618c19f1f471598cec4a4d6cca0
turtle601 <turtle601@naver.com>
2023-02-08 16:02:01 +0900

    ✨ feat(Car): 이름이 5자 이하이고, 문자인지 검사한다.

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -4,6 +4,21 @@ class Car {
   #name;
 
   #position;
+
+  constructor(name) {
+    this.#name = this.#validateName(name);
+    this.#position = 0;
+  }
+
+  #validateName(name) {
+    if (name.length > 5) throw new Error('[ERROR] 이름은 5자 이하여야 합니다.');
+
+    if (!REGEX.korean.test(name) || !REGEX.english.test(name)) {
+      throw new Error('[ERROR] 문자만 입력해야 합니다.');
+    }
+
+    return this;
+  }
 }
 
 module.exports = Car;
