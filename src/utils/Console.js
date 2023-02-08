629d08096ba7a3f35242252f8ebbf7a4a83f41f4
bassyu <yrkdgus@gmail.com>
2023-02-08 17:58:50 +0900

    refactor(Console): read함수 인자값 삭제

--- src/utils/Console.js
+++ src/utils/Console.js
@@ -1,11 +1,11 @@
-import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
 const Console = {
-  async read(outputLog) {
-    return await rl.question(outputLog);
+  async read() {
+    return await rl.question('');
   },
 
   print(outputLog) {
