2539980c5fdde43e3ce6a963789ecc0f6e92dac0
yeopto <gunyeop530@gmail.com>
2023-02-08 16:35:16 +0900

    feat: Car Class를 생성하다.

--- src/Models/Car.js
+++ src/Models/Car.js
@@ -1,5 +1,25 @@
 class Car {
-  
+  #name;
+  #position;
+
+  constructor (name) {
+    this.#name = name;
+    this.#position = 0;   
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getPosition() {
+    return this.#position;
+  }
+
+  move(number) {
+    if (number >= 4) {
+      this.#position += 1;
+    }
+  }
 };
 
 module.exports = Car;
