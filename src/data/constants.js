d17ba693f306e6b7921ee9f9553f4037d8ed6e42
hae-on <solbi2004@naver.com>
2023-02-08 15:59:22 +0900

    feat: 필요한 숫자 상수처리

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
 const MESSAGE = Object.freeze({
   CAR_NAME_INPUT:
     '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
-  TRY_COUNT_INPUT: '시도할 회수는 몇회인가요?',
+  TRY_COUNT_INPUT: '시도할 회수는 몇회인가요?\n',
   RESULT_OUTPUT: '실행 결과',
   WINNER_OUTPUT: '가 최종 우승했습니다.',
 });
@@ -16,8 +16,14 @@ const ERROR_MESSAGE = Object.freeze({
   TRY_COUNT_TYPE_ERROR: '[ERROR] 시도 횟수는 숫자로 입력해주세요.',
 });
 
+const UTIL_NUMBER = Object.freeze({
+  CAR_MOVE_MINIMUM_NUMBER: 4,
+  CAR_RANDOM_MAXIMUM_NUMBER: 9,
+});
+
 module.exports = {
   MESSAGE,
   SYMBOL,
   ERROR_MESSAGE,
+  UTIL_NUMBER,
 };
