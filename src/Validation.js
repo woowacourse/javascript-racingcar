23cd34bab1c04f2c7fd06676248c996aaaad0734
DOBOB_LAPTOP <dlehgus1110@naver.com>
2023-02-08 14:37:37 +0900

    refactor: 에러 문구 추가

--- src/Validation.js
+++ src/Validation.js
@@ -9,12 +9,11 @@ class Validation {
     return names.length >= 2;
   }
 }
-
 const ERROR_MESSAGE = {
-  invalidCarNumber: '2개 이상의 차이름을 입력하세요.',
-  invalidCarNameLength: '1자 이상, 5자 이하의 이름을 입력하세요.',
-  duplicateCarName: '중복되는 차 이름이 존재합니다.',
-  invalidGameRound: '1 이상의 숫자를 입력하세요.',
+  invalidCarNumber: `[ERROR] 2개 이상의 차이름을 입력하세요.`,
+  invalidCarNameLength: `[ERROR] 1자 이상, 5자 이하의 이름을 입력하세요.`,
+  duplicateCarName: `[ERROR] 중복되는 차 이름이 존재합니다.`,
+  invalidGameRound: `[ERROR] 1 이상의 숫자를 입력하세요.`,
 };
 
 module.exports = Validation;
