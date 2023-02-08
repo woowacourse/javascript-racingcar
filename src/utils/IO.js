e0f5346ab27b943cc4bdbba93efe36d15359b357
hae-on <solbi2004@naver.com>
2023-02-08 13:55:04 +0900

    feat: readline implement

---
+++ src/utils/IO.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+class IO {
+  static read(message, callback) {
+    rl.on(message, callback);
+  }
+
+  static print(message) {
+    console.log(message);
+  }
+
+  static close() {
+    rl.close();
+  }
+}
+
+module.exports = IO;
