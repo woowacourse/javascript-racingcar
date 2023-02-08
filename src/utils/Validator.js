b3c1732dc42a4338f38ce135f0ac9f832e8d7793
dladncks1217 <dlaxodud1217@gmail.com>
2023-02-08 17:42:24 +0900

    feat(Validator): 차 이름 알파벳 유효성 검사 메서드 추가

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -5,9 +5,16 @@ class Validator{
         })
     }
 
-    nameOverLapped(names){
+    validateOverLap(names){
         const set = new Set(names);
         if(names.length!==set.size) throw new Error();
+    }
+
+    validateInvalidInput(names) {
+        const ALPHA_REGEXP = /^[a-z|A-Z]+$/;
+        names.forEach((name)=>{
+            if(!ALPHA_REGEXP.test(name)) throw new Error();
+        })
     }
 
     
