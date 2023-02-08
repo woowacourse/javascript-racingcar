73a5e67139fa8b4c75eeb98955840d0991be883b
shackstack <kms1061@naver.com>
2023-02-08 15:14:22 +0900

    feat: 입력 받은 자동차 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 const InputView = require("./view/InputView.js");
 const Validator = require("./model/Validator.js");
+const Car = require("./model/Car.js");
 
 class App {
+  #cars = [];
   play() {
     this.readCarName();
   }
@@ -10,8 +12,12 @@ class App {
     InputView.readCarName((carName) => this.readCarNameCallback(carName));
   }
 
-  readCarNameCallback(carName) {
-    Validator.carName(carName);
+  readCarNameCallback(carNames) {
+    Validator.carName(carNames);
+
+    carNames.split(",").forEach((carName) => {
+      this.#cars.push(new Car(carName));
+    });
   }
 }
 
---
+++ src/model/Car.js
@@ -0,0 +1,10 @@
+class Car {
+  #name;
+  #moving = [];
+
+  constructor(carName) {
+    this.#name = carName;
+  }
+}
+
+module.exports = Car;
