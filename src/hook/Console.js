a1b168d3ab3085470af274589802b081f783d9e4
shackstack <kms1061@naver.com>
2023-02-08 13:55:18 +0900

    feat: readLine 구현

--- src/hook/Console.js
+++ src/hook/Console.js
@@ -1,1 +1,16 @@
+const readline = require("readline");
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+class Console {
+  static readLine(string, callback) {
+    rl.question(string, callback);
+  }
+
+  static print() {}
+}
+
+module.exports = Console;
