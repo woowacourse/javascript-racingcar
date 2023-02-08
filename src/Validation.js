a48bd4f794a3c1b184c08540eb02c59d11926264
DOBOB_LAPTOP <dlehgus1110@naver.com>
2023-02-08 15:04:51 +0900

    refactor: 메서드, 변수명 의미 개선
    carNumber -> carQuantity 변경으로 갯수의 의미를 가지도록 함

--- src/Validation.js
+++ src/Validation.js
@@ -7,8 +7,8 @@ const {
 
 class Validation {
   static validateCarNames(names) {
-    if (!Validation.#isCarNumberValid(names)) {
-      throw new Error(ERROR_MESSAGE.invalidCarNumber);
+    if (!Validation.#isCarQuantityValid(names)) {
+      throw new Error(ERROR_MESSAGE.invalidCarQuantity);
     }
 
     if (!Validation.#isCarNameLengthValid(names)) {
@@ -16,7 +16,7 @@ class Validation {
     }
   }
 
-  static #isCarNumberValid(names) {
+  static #isCarQuantityValid(names) {
     return names.length >= minimumCarQuantity;
   }
 
@@ -27,7 +27,7 @@ class Validation {
   }
 }
 const ERROR_MESSAGE = {
-  invalidCarNumber: `[ERROR] 2개 이상의 차이름을 입력하세요.`,
+  invalidCarQuantity: `[ERROR] 2개 이상의 차이름을 입력하세요.`,
   invalidCarNameLength: `[ERROR] 1자 이상, 5자 이하의 이름을 입력하세요.`,
   duplicateCarName: `[ERROR] 중복되는 차 이름이 존재합니다.`,
   invalidGameRound: `[ERROR] 1 이상의 숫자를 입력하세요.`,
