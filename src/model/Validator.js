e50a8f583576bbb87d07abe140767345d8f8320b
shackstack <kms1061@naver.com>
2023-02-08 14:43:06 +0900

    feat: 자동차 이름이 영소문자가 아닐 경우 예외처리

--- src/model/Validator.js
+++ src/model/Validator.js
@@ -4,16 +4,22 @@ const Validator = {
 
     if (!this.isCarNameLessThenFive(names))
       throw new Error("[ERROR] 자동차 이름은 5자 이하이어야 합니다.");
+
+    if (!this.isCarNameLowerCase(names))
+      throw new Error("[ERROR] 자동차 이름은 영소문자로 이루어져야 합니다.");
   },
 
   isCarNameLessThenFive(names) {
     return names.every((name) => {
-      if (!(name.length <= 5)) return false;
-      return true;
+      return name.length <= 5;
+    });
+  },
+
+  isCarNameLowerCase(names) {
+    return names.every((name) => {
+      return name.search(/[^a-z]/g) === -1;
     });
   },
 };
 
 module.exports = Validator;
-
-// Validator.carName("aaa, bbb, ccc");
