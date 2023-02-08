136b1e16415f43769767dd7aa25ee2709ef6b995
DOBOB_LAPTOP <dlehgus1110@naver.com>
2023-02-08 14:15:08 +0900

    feat: 입력 에러시 재입력 요청 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const InputView = require('./view/InputView');
 const Validation = require('./Validation');
+const inputErrorHandler = require('./utils/inputErrorHandler');
 
 class App {
   play() {
@@ -7,9 +8,14 @@ class App {
   }
 
   #requestCarNames() {
-    InputView.readCarNames((carNames) => {
-      const cars = carNames.split(',').map((carName) => carName.trim());
-      Validation.validateCarNames(cars);
+    InputView.readCarNames((carNamesInput) => {
+      const carNames = carNamesInput.split(',').map((carName) => carName.trim());
+      const isValidInput = inputErrorHandler(Validation.validateCarNames, carNames);
+
+      if (!isValidInput) {
+        this.#requestCarNames();
+        return;
+      }
     });
   }
 }
