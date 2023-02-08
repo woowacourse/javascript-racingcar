bbdfdad1a6994595122b15af7cb0ef0bc24f9758
gyeongza <gyeongza@naver.com>
2023-02-08 15:25:06 +0900

    feat: 콘솔 입출력 기능 구현

---
+++ src/UI/Console.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+class Console {
+  constructor() {}
+
+  static readLine(query, callback) {
+    rl.question(query, callback);
+  }
+
+  static close() {
+    rl.close();
+  }
+
+  static print(message) {
+    console.log(message);
+  }
+}
+
+module.exports = Console;
