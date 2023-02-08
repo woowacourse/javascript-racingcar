97bf6456cc6b633f17380b707430eca93e0d353e
sh981013s <sh981013s@gmail.com>
2023-02-08 18:08:43 +0900

    Revert "test(CarManager): 자동차 각각을 전진시키는 기능 테스트 작성"
    This reverts commit d80a07aa381b3d7420dfbb96527ec5862c785134.

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -1,14 +1,5 @@
 const CarManager = require('../src/CarManager');
 
-const RandomNumberGenerator = require('../src/utils/RandomNumberGenerator');
-
-const mockRandoms = (numbers) => {
-  RandomNumberGenerator.generate = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, RandomNumberGenerator.generate);
-};
-
 describe('자동차를 관리하는 클래스 테스트', () => {
   let carManager;
 
@@ -18,13 +9,5 @@ describe('자동차를 관리하는 클래스 테스트', () => {
 
   test('자동차 관리자 인스턴스를 생성한다.', () => {
     expect(carManager.getCars().length).toEqual(3);
-  });
-
-  test('자동차 각각을 전진시킨다.', () => {
-    mockRandoms([2, 2, 5]);
-
-    carManager.progress();
-
-    console.log(carManager.getWinners());
   });
 });
