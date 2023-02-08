0dc894b7e85cca9244cc6c2e52d94d06aae3bfd6
shackstack <kms1061@naver.com>
2023-02-08 14:55:30 +0900

    feat: 자동차 이름에 공백이 포함되면 예외처리

--- src/model/Validator.js
+++ src/model/Validator.js
@@ -1,12 +1,19 @@
 const Validator = {
-  carName(names) {
-    names = names.split(",");
+  carName(string) {
+    const names = string.split(",");
+
+    if (this.isCarNameHasBlank)
+      throw new Error("[ERROR] 자동차 이름에 공백이 포함되어서는 안됩니다.");
 
     if (!this.isCarNameLessThenFive(names))
       throw new Error("[ERROR] 자동차 이름은 5자 이하이어야 합니다.");
 
     if (!this.isCarNameLowerCase(names))
       throw new Error("[ERROR] 자동차 이름은 영소문자로 이루어져야 합니다.");
+  },
+
+  isCarNameHasBlank(string) {
+    return string.search(/\s/g) !== -1;
   },
 
   isCarNameLessThenFive(names) {
