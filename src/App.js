27a57e29ae77128b1e18844c4662c27ae0b17f7f
DOBOB_LAPTOP <dlehgus1110@naver.com>
2023-02-08 13:59:20 +0900

    refactor: 변수명 변경 및 메서드 병합
    메서드 bind(this) 형식 -> callback 형식으로 변경

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ const Validation = require('./Validation');
 
 class App {
   play() {
-    this.getCarNames();
+    this.#requestCarNames();
   }
 
-  getCarNames() {
-    InputView.readCarNames(this.actionWithCarNames.bind(this));
-  }
-
-  actionWithCarNames(carNames) {
-    const cars = carNames.split(',').map((car) => car.trim());
-    Validation.validateCarNames(cars);
+  #requestCarNames() {
+    InputView.readCarNames((carNames) => {
+      const cars = carNames.split(',').map((carName) => carName.trim());
+      Validation.validateCarNames(cars);
+    });
   }
 }
 
