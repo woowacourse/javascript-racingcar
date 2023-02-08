b7b3270ba248578dc35b9c55c604fb00f076bf82
inyeong-kang <tomatorkd@naver.com>
2023-02-08 16:15:59 +0900

    refactor(Controller): 메서드 분리

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,14 +1,33 @@
+const Attempts = require('../model/Attempts');
+const Car = require('../model/Car');
 const InputView = require('../view/InputView');
 
 class Controller {
-  // eslint-disable-next-line no-useless-constructor
-  constructor() {
-    
-  }
+  #cars = [];
+
+  #attempt;
+
+  constructor() {}
 
   play() {
-    InputView.readAttempts((msg) => {
-      console.log(msg);
+    this.askCarName();
+  }
+
+  askCarName() {
+    InputView.readCarName((nameInput) => {
+      const carNames = nameInput.split(',');
+      console.log(carNames);
+      carNames.forEach((name) => {
+        const carModel = new Car(name);
+        this.#cars.push(carModel);
+      });
+      this.askAttempts();
+    });
+  }
+
+  askAttempts() {
+    InputView.readAttempts((attemptInput) => {
+      this.#attempt = new Attempts(attemptInput);
     });
   }
 }
