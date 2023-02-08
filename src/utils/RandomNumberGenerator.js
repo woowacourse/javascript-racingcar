a4d58a49f354e81ad906823fe82ebcab382a07be
sh981013s <sh981013s@gmail.com>
2023-02-08 13:22:46 +0900

    refactor(RandomNumberGenerator): missionutils package 의존성 제거 후 난수 생성 기능 구현

--- src/utils/RandomNumberGenerator.js
+++ src/utils/RandomNumberGenerator.js
@@ -1,13 +1,12 @@
-const { Random } = require('@woowacourse/mission-utils');
-
 const RandomNumberGenerator = {
   RANDOM_LOWER_INCLUSIVE: 1,
   RANDOM_UPPER_INCLUSIVE: 8,
 
   generate() {
-    return Random.pickNumberInRange(
-      this.RANDOM_LOWER_INCLUSIVE,
-      this.RANDOM_UPPER_INCLUSIVE,
+    return (
+      Math.floor(
+        Math.random() * (this.RANDOM_UPPER_INCLUSIVE - this.RANDOM_LOWER_INCLUSIVE),
+      ) + this.RANDOM_LOWER_INCLUSIVE
     );
   },
 };
