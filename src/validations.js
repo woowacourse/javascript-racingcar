d1bacea1a20a7b6e6bff6188a17ff089f8465931
NaveOWO <pianowell96@gmail.com>
2023-02-08 19:51:15 +0900

    [ Feat ] 예외처리 함수 생성

---
+++ src/validations.js
@@ -0,0 +1,29 @@
+const CONSTANT = require("./Constant");
+
+const validations = {
+
+    validateCarNameLength(carNames) {
+        carNames.forEach(name => {
+            if (name.length > CONSTANT.CAR_NAME_MAX) {
+                throw new Error(CONSTANT.ERROR_CAR_LENGTH)
+            }
+        });
+    },
+
+    validateIsCarName(carNames) {
+        carNames.forEach(name => {
+            if (name.length <= CONSTANT.CAR_NAME_MIN) {
+                throw new Error(CONSTANT.ERROR_CAR_NONAME)
+            }
+        });
+    },
+
+    validateRound(num) {
+        if (!Number.isInteger(num) || num <= 0) {
+            throw new Error(CONSTANT.ERROR_ROUND);
+        }
+    }
+}
+
+
+module.exports = validations;
