6a6b62efe6427aae915d89f3d4f6cd5e1316a38b
gyeongza <gyeongza@naver.com>
2023-02-09 16:27:57 +0900

    refector: 불필요한 테스트 함수 삭제

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,38 +1,4 @@
-const Console = require('../src/UI/Console');
-const Car = require('../src/Car');
 const Validator = require('../src/Validator');
-
-const mockQuestions = (answers) => {
-  Console.readLine = jest.fn();
-  answers.reduce(
-    (acc, input) => acc.mockImplementationOnce((_, callback) => { callback(input); }),
-    Console.readLine,
-  );
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
-};
-
-const getOutput = (logSpy) => [...logSpy.mock.calls].join('');
-
-const runException = (inputs) => {
-  mockQuestions(inputs);
-  const logSpy = getLogSpy();
-  const app = new App();
-
-  app.play();
-
-  expectLogContains(getOutput(logSpy), ['[ERROR]']);
-};
-
-const expectLogContains = (received, logs) => {
-  logs.forEach((log) => {
-    expect(received).toEqual(expect.stringContaining(log));
-  });
-};
 
 describe('자동차 이름 검사', () => {
     test.each([[['참새','에이든']], [['참새']]])('정상', (carNames) => {
