d378730c815f384b16a8ea97de377a97b79eef99
bassyu <yrkdgus@gmail.com>
2023-02-09 16:20:10 +0900

    feat(test): Validator 테스트 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -19,12 +19,20 @@ describe('Model 테스트', () => {
 
 describe('utils 테스트', () => {
   describe('Validator 테스트', () => {
+    test('입력값에 공백이 있으면 에러가 발생한다.', () => {
+      expect(() => Validator.checkBlank('a, b, c')).toThrow();
+    });
+
     test('이름들을 입력할 때 마지막이 쉼표면 에러가 발생한다.', () => {
-      expect(() => Validator.checkName('a, b, c,')).toThrow();
+      expect(() => Validator.checkLastComma('a,b,c,')).toThrow();
+    });
+
+    test('이름의 길이가 0이면 에러가 발생한다.', () => {
+      expect(() => Validator.checkNameLength('a,b,c,')).toThrow();
     });
 
     test('중복된 이름이 있으면 에러가 발생한다.', () => {
-      expect(() => Validator.checkDuplicate('a, a')).toThrow();
+      expect(() => Validator.checkDuplicate('a,a')).toThrow();
     });
 
     test('정수가 아니면 에러가 발생한다.', () => {
