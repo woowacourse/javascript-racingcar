17b7492c04188d9122772d19e0190beb70195cff
shackstack <kms1061@naver.com>
2023-02-09 15:21:12 +0900

    test: 자동차 이름 길이 유효성 검사

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -6,4 +6,10 @@ describe("자동차 이름 입력값에 대한 유효성 검사", () => {
       Validator.carName("aaa,b b,ccc");
     }).toThrow("[ERROR] 자동차 이름에 공백이 포함되어서는 안됩니다.");
   });
+
+  test("자동차 이름 길이가 잘못된 경우 예외를 발생시킨다.", () => {
+    expect(() => {
+      Validator.carName("aaa,bbbbbb,ccc");
+    }).toThrow("[ERROR] 자동차 이름은 5자 이하이어야 합니다.");
+  });
 });
