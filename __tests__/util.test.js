41a8f9d3f55e6ab281d07f74ced7edcc02c2e73b
semnil5202 <semnil5202@gmail.com>
2023-02-09 15:04:57 +0900

    test: 유틸 함수 deepFreeze 테스트 추가

---
+++ __tests__/util.test.js
@@ -0,0 +1,23 @@
+const { deepFreeze, toInt, randomGenerator } = require('../src/utils/common');
+
+describe('유틸 함수 테스트', () => {
+  it('deepFreeze(object): 객체 object를 변경할 수 없게 동결한다.', () => {
+    const deepObject = {
+      prop1: 'value1',
+      prop2: {
+        prop3: 'value2',
+      },
+    };
+
+    const freezeObject = () => {
+      const frozenDeepObject = deepFreeze(deepObject);
+      frozenDeepObject.prop2.prop3 = 'change';
+    };
+
+    expect(() => freezeObject()).toThrowError(TypeError);
+  });
+
+  it('toInt', () => {});
+
+  it('randomGenerator', () => {});
+});
