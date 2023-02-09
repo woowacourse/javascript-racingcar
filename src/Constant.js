1835430f76d010f12c37cf4f7efe75c0e8aa86b6
NaveOWO <pianowell96@gmail.com>
2023-02-09 14:22:15 +0900

    [ Style ] 상수 메세지 수정

--- src/Constant.js
+++ src/Constant.js
@@ -1,7 +1,7 @@
 
 const CONSTANT = {
-    INPUT_CAR_MASSEGE: "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).",
-    INPUT_ROUND_MASSEGE: "시도할 회수는 몇회인가요?",
+    INPUT_CAR_MASSEGE: "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n",
+    INPUT_ROUND_MASSEGE: "시도할 회수는 몇회인가요?\n",
     
     RANDOM_MIN: 0,
     RANDOM_MAX: 9,
