85b6d40b6889b561ae6a889efb6252a9c1a5a510
turtle601 <turtle601@naver.com>
2023-02-09 10:53:37 +0900

    ✨ feat(Car): 마지막 위치 결과를 반환한다.

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,29 +1,23 @@
-const { REGEX } = require('../constant/regex');
-
 class Car {
   #name;
 
   #position;
 
   constructor(name) {
-    this.#name = this.#validateName(name);
+    this.#name = name;
     this.#position = [1];
   }
 
-  #validateName(name) {
-    if (name.length > 5) throw new Error('[ERROR] 이름은 5자 이하여야 합니다.');
-
-    if (!REGEX.korean.test(name) || !REGEX.english.test(name)) {
-      throw new Error('[ERROR] 문자만 입력해야 합니다.');
-    }
-
-    return this;
+  getName() {
+    return this.#name;
   }
 
-  move(randomNumber) {
-    if (randomNumber > 3) {
-      this.#position.push(this.#position[this.#position.length - 1] + 1);
-    }
+  getPosition() {
+    return this.#position;
+  }
+
+  getLast() {
+    return this.#position[this.#position.length - 1];
   }
 }
 
