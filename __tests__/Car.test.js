9968083c61517aaf272eef432a212e3b8d463b84
woo-jk <201702042@o.cnu.ac.kr>
2023-02-09 13:42:30 +0900

    test: Car 클래스 테스트 코드 작성

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,5 +1,18 @@
-describe("테스트", () => {
-  test("테스트1", () => {
-    expect(true);
+const Car = require("../src/domain/Car");
+
+describe("Car 클래스 테스트", () => {
+  test("이름이 '룩소'인 자동차 생성, 이동 거리의 초기값은 0", () => {
+    const car = new Car("룩소");
+
+    expect(car.getName()).toBe("룩소");
+    expect(car.getCurrentDistance()).toBe(0);
+  });
+
+  test("무작위 값이 4 이상이면 1칸 이동", () => {
+    const car = new Car("룩소");
+
+    car.move(4);
+
+    expect(car.getCurrentDistance()).toBe(1);
   });
 });
