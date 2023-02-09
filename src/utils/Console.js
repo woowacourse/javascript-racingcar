5c76d0cd7dabb0c9772cf442b7ef183a4300e7f8
solo5star <solo_5@naver.com>
2023-02-09 16:09:03 +0900

    feat: Console 클래스 추가
    * readline, console.log 를 래핑하여 구현
    
    Co-authored-by: cruelladevil <dev.timetravel@gmail.com>

---
+++ src/utils/Console.js
@@ -0,0 +1,21 @@
+import { stdin as input, stdout as output } from 'node:process';
+import * as readline from 'node:readline/promises';
+
+const rl = readline.createInterface({ input, output });
+
+class Console {
+  static readline(query) {
+    this.print(query);
+    return rl.question('');
+  }
+
+  static close() {
+    rl.close();
+  }
+
+  static print(message) {
+    console.log(message);
+  }
+}
+
+export default Console;
