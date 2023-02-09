a208fcbdf80a2297043bcafc51c73f142ed10d6e
sh981013s <sh981013s@gmail.com>
2023-02-09 11:09:14 +0900

    refactor(Validator): 테스트 이름이 목적을 더 잘나타내도록 수정

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,6 +1,6 @@
 const Validator = require('../src/utils/Validator.js');
 
-describe('입력값의 유효성을 검사하는 객체 테스트', () => {
+describe('자동차 이름을 위한 입력값의 유효성 검사', () => {
   test.each([['!@#'], ['pobi.crong.honux'], ['포비,크롱,호눅스!']])(
     '영어, 한글, 쉼표(,) 이외의 문자가 포함된 경우 예외 처리',
     (input) => {
