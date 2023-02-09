5c69f208c7124f416daaa4da8ec2d37893b89d53
shackstack <kms1061@naver.com>
2023-02-09 17:06:19 +0900

    refactor: 테스트코드 리팩터링
    자동차 이름 유효성 검사 성공 및 실패 후 메서드 호출에 대한 테스트코드 리팩터링

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -5,47 +5,29 @@ const OutputView = require("../src/view/OutputView.js");
 describe("readCarNameCallback 메서드에 대한 테스트 코드", () => {
   // given
   const app = new App();
-  test("자동차 객체 생성 메서드 호출 테스트", () => {
+  test("자동차 이름 유효성 검사 성공 후 메서드 호출 테스트", () => {
     // given
     const createCarObjectSpy = jest.spyOn(app, "createCarObject");
-
-    // when
-    app.readCarNameCallback("pobi,jun,noah");
-
-    // then
-    expect(createCarObjectSpy).toHaveBeenCalledTimes(1);
-  });
-
-  test("자동차 객체 생성 이후 readTryCount 메서드 호출 테스트", () => {
-    // given
     const readTryCountSpy = jest.spyOn(app, "readTryCount");
 
     // when
     app.readCarNameCallback("pobi,jun,noah");
 
     // then
+    expect(createCarObjectSpy).toHaveBeenCalledTimes(1);
     expect(readTryCountSpy).toHaveBeenCalledTimes(1);
   });
 
-  test("잘못된 값 입력시 OutputView.printErrorMessage 메서드 호출 테스트", () => {
+  test("자동차 이름 유효성 검사 실패 후 메서드 호출 테스트", () => {
     // given
     const printErrorMessageSpy = jest.spyOn(OutputView, "printErrorMessage");
-
-    // when
-    app.readCarNameCallback("pobi,jun,noahhhh");
-
-    // then
-    expect(printErrorMessageSpy).toHaveBeenCalledTimes(1);
-  });
-
-  test("test: 잘못된 값을 입력시 readCarName 메서드 호출 테스트", () => {
-    // given
     const readCarNameSpy = jest.spyOn(app, "readCarName");
 
     // when
     app.readCarNameCallback("pobi,jun,noahhhh");
 
     // then
+    expect(printErrorMessageSpy).toHaveBeenCalledTimes(1);
     expect(readCarNameSpy).toHaveBeenCalledTimes(1);
   });
 });
@@ -64,26 +46,17 @@ describe("readTryCountCallback 메서드 전체 호출 테스트", () => {
     expect(moveCarSpy).toHaveBeenCalledTimes(1);
   });
 
-  test("잘못된 값 입력시 OutputView.printErrorMessage 메서드 호출 테스트", () => {
+  test("잘못된 값 입력시 메서드 호출 테스트", () => {
     // given
     const printErrorMessageSpy = jest.spyOn(OutputView, "printErrorMessage");
     printErrorMessageSpy.mockClear();
-
-    // when
-    app.readTryCountCallback("1a");
-
-    // then
-    expect(printErrorMessageSpy).toHaveBeenCalledTimes(1);
-  });
-
-  test("잘못된 값 입력시 readTryCount 메서드 호출 테스트", () => {
-    // given
     const readTryCountSpy = jest.spyOn(app, "readTryCount");
 
     // when
     app.readTryCountCallback("1a");
 
     // then
+    expect(printErrorMessageSpy).toHaveBeenCalledTimes(1);
     expect(readTryCountSpy).toHaveBeenCalledTimes(1);
   });
 });
