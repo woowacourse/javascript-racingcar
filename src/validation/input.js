3c4f1ffd38d3b809dc9a51e9415a6a873722a2a9
semnil5202 <semnil5202@gmail.com>
2023-02-09 13:22:13 +0900

    fix: 예외 처리 발생 후 재입력을 받지 못하던 오류 수정
    예외가 발생했을 때 각 조건 검증 함수에서 반환값이 없어 컨트롤러에서 재입력을 받을 수 없는 오류를 수정한다.

--- src/validation/input.js
+++ src/validation/input.js
@@ -14,43 +14,47 @@ const checkCarNameLength = (carNames) => {
   if (!carNames.every((carName) => carName.length >= 1 && carName.length <= 5)) {
     return handleError('자동차 이름은 1~5글자 사이여야 합니다.');
   }
+  return true;
 };
 
 const checkDuplicatedCarName = (carNames) => {
   if (carNames.length !== new Set(carNames).size) {
     return handleError('자동차 이름은 중복될 수 없습니다.');
   }
+  return true;
 };
 
 const checkBlankInCarName = (carNames) => {
   if (carNames.some((carName) => carName.includes(' '))) {
     return handleError('자동차 이름은 공백이 포함될 수 없습니다.');
   }
+  return true;
 };
 
 const validateCarNames = (carNames) => {
-  checkCarNameLength(carNames);
-  checkDuplicatedCarName(carNames);
-  checkBlankInCarName(carNames);
-  return true;
+  return (
+    checkCarNameLength(carNames) &&
+    checkDuplicatedCarName(carNames) &&
+    checkBlankInCarName(carNames)
+  );
 };
 
 const checkIsBetweenValidRange = (winningDistance) => {
   if (!(GAME.DISTANCE.min <= winningDistance && winningDistance < GAME.DISTANCE.max)) {
     return handleError('시도 횟수는 3 이상 10 미만이여야 합니다.');
   }
+  return true;
 };
 
 const checkIsInt = (winningDistance) => {
   if (Number.isNaN(winningDistance)) {
     return handleError('시도 횟수는 숫자로 입력해야 합니다.');
   }
+  return true;
 };
 
 const validateWinningDistance = (winningDistance) => {
-  checkIsInt(winningDistance);
-  checkIsBetweenValidRange(winningDistance);
-  return true;
+  return checkIsInt(winningDistance) && checkIsBetweenValidRange(winningDistance);
 };
 
 module.exports = { validateCarNames, validateWinningDistance };
