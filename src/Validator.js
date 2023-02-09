da85801a137df08245b392cfcafac9df04794669
Jungwoo <evencoding@gmail.com>
2023-02-09 13:50:14 +0900

    refactor(Validator): 하드코딩 상수화

--- src/Validator.js
+++ src/Validator.js
@@ -1,20 +1,29 @@
+const REGEX = {
+  CAR_NAME: /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9]{1,5}$/,
+  TRY_CNT: /^[1-9][0-9]*$/,
+};
+
+const ERROR_MESSAGE = {
+  INVALID_NAME: '5자 이하 영어,한글,숫자의 조합을 입력하세요',
+  DUPLICATE_NAME: '중복된 이름의 자동차가 있습니다',
+  INVALID_NUMBER: '1이상의 양의 정수를 입력하세요',
+};
+
 const Validator = {
   validateName: (names) => {
-    const regex = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9]{1,5}$/;
     names.forEach((name) => {
-      if (!regex.test(name)) {
-        throw new Error('5자 이하 영어,한글,숫자의 조합을 입력하세요');
+      if (!REGEX.CAR_NAME.test(name)) {
+        throw new Error(ERROR_MESSAGE.INVALID_NAME);
       }
     });
     if (names.length !== [...new Set(names)].length) {
-      throw new Error('중복된 이름의 자동차가 있습니다');
+      throw new Error(ERROR_MESSAGE.DUPLICATE_NAME);
     }
   },
 
   validateTryCnt: (inputValue) => {
-    const regex = /^[1-9][0-9]*$/;
-    if (!regex.test(inputValue)) {
-      throw new Error('1이상의 양의 정수를 입력하세요');
+    if (!REGEX.TRY_CNT.test(inputValue)) {
+      throw new Error(ERROR_MESSAGE.INVALID_NUMBER);
     }
   },
 };
