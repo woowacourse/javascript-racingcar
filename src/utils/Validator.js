a7f05a74a7cf709bf2aa3d8f3b3a11d82eae2f37
bassyu <yrkdgus@gmail.com>
2023-02-09 11:32:22 +0900

    refactor(Validator): Validator 상수화

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,22 +1,24 @@
+import { ERROR_MESSAGE, VALIDATOR } from '../constants';
+
 const Validator = {
   checkName(input) {
     if (input.includes(' ')) {
-      throw new Error('입력값에 공백이 포함되어 있습니다.');
+      throw new Error(ERROR_MESSAGE.blank);
     }
     if (input[input.length - 1] === ',') {
-      throw new Error('입력값 마지막에 쉼표가 포함되어 있습니다.');
+      throw new Error(ERROR_MESSAGE.lastComma);
     }
   },
   checkDuplicate(input) {
-    const names = input.split(',');
-    if (new Set(names).size !== names.length) {
-      throw new Error('입력값에 중복되는 이름이 존재합니다.');
+    const value = input.split(',');
+    if (new Set(value).size !== value.length) {
+      throw new Error(ERROR_MESSAGE.duplicate);
     }
   },
   checkIntegerNumber(input) {
     input.split('').forEach((char) => {
-      if (!'0123456789'.includes(char)) {
-        throw new Error('입력값이 정수가 아닙니다.');
+      if (!VALIDATOR.integerString.includes(char)) {
+        throw new Error(ERROR_MESSAGE.notInteger);
       }
     });
   },
