97337535bc89bf0db985265149a5081590734536
Dain Lee <feb.dain@gmail.com>
2023-02-09 13:56:29 +0900

    feat: Add exceptions and make input validator methods

--- src/Utils/Validator.js
+++ src/Utils/Validator.js
@@ -1,10 +1,16 @@
-const { ERROR_MESSAGE } = require("../Utils/Constants");
 const {
-  NAME_LENGTH_LIMIT,
-  MINIMUM_CAR_COUNT,
+  ERROR_MESSAGE,
   MAXIMUM_CAR_NAME_LENGTH,
   MINIMUM_TRY_COUNT,
   MINIMUM_NUMBER_OF_CARS,
+} = require("../Utils/Constants");
+const {
+  NAME_LENGTH_LIMIT,
+  NOT_BLANK,
+  INVALID_NAME,
+  NAME_DUPLICATE,
+  MINIMUM_CAR_COUNT,
+  NOT_POSITIVE_NUMBER,
 } = ERROR_MESSAGE;
 
 class Validator {
@@ -17,34 +23,54 @@ class Validator {
     }
   }
 
+  checkDuplicate(names) {
+    if (names.length !== new Set(names).size) {
+      throw new Error(NAME_DUPLICATE);
+    }
+  }
+
   hasBlank(string) {
-    if (!/ /g.test(string)) {
+    if (/ /g.test(string)) {
       throw new Error(NOT_BLANK);
     }
   }
 
   isNumber(number) {
-    if (number.includes("e")) {
-      throw new Error(NOT_POSITIVE_NUMBER);
-    }
-    if (isNaN(number)) {
+    if (isNaN(number) || number.includes("e")) {
       throw new Error(NOT_POSITIVE_NUMBER);
     }
   }
 
   numberOfCars(number) {
-    this.isNumber(number);
-
     if (number < MINIMUM_NUMBER_OF_CARS) {
       throw new Error(MINIMUM_CAR_COUNT);
     }
   }
 
   tryCount(count) {
-    this.isNumber(number);
+    this.isNumber(count);
 
     if (count < MINIMUM_TRY_COUNT) {
       throw new Error(NOT_POSITIVE_NUMBER);
     }
   }
 }
+
+const validator = new Validator();
+
+const inputCarNameValidator = (cars) => {
+  validator.numberOfCars(cars.length);
+  validator.checkDuplicate(cars);
+
+  cars.forEach((car) => {
+    validator.carName(car);
+    validator.hasBlank(car);
+  });
+};
+
+const tryCountValidator = (count) => {
+  validator.hasBlank(count);
+  validator.tryCount(count);
+};
+
+module.exports = { inputCarNameValidator, tryCountValidator };
