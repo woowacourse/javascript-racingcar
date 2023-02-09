5e1af33788db333bfe5e71c10282c5825ceffbc5
shackstack <kms1061@naver.com>
2023-02-09 15:23:38 +0900

    test: 자동차 이름이 영소문자로만 이루어지지 않은 경우

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -12,4 +12,10 @@ describe("자동차 이름 입력값에 대한 유효성 검사", () => {
       Validator.carName("aaa,bbbbbb,ccc");
     }).toThrow("[ERROR] 자동차 이름은 5자 이하이어야 합니다.");
   });
+
+  test("자동차 이름이 영소문자로만 이루어지지 않은 경우 예외를 발생시킨다.", () => {
+    expect(() => {
+      Validator.carName("aaa,bbb,Ccc");
+    }).toThrow("[ERROR] 자동차 이름은 영소문자로 이루어져야 합니다.");
+  });
 });
