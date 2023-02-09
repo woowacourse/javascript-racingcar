6fd2e70a881fd5bdb8f51a49097aab9ec1e70d3b
hafnium1923 <hafnium1923@gmail.com>
2023-02-09 11:42:57 +0900

    feat: InputView promise 적용

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,12 +1,23 @@
 const rl = require("../library/ReadLine");
 
 const InputView = {
-  readCarNames(messages, callback) {
-    rl.question(messages, callback);
+  readCarNames(messages) {
+    return new Promise(function(resolve, reject) {
+      rl.question(messages, (carNames) => {
+        if (!/^([^,]{1,5},)*[^,]{1,5}$/.test(carNames)) reject(new Error());
+        let cars = carNames.split(",");
+        resolve(cars);
+      });
+    });
   },
 
-  readRepeatNumber(messages, callback) {
-    rl.question(messages, callback);
+  readRepeatNumber(messages) {
+    return new Promise(function(resolve, reject) {
+      rl.question(messages, (repeatNumber) => {
+        if (!/^[1-9]\d*$/.test(repeatNumber)) reject(new Error());
+        resolve(repeatNumber);
+      });
+    });
   },
 };
 
