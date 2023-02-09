f95f64caca607d38f0dd114dca7415813a129326
Dain Lee <feb.dain@gmail.com>
2023-02-09 16:26:12 +0900

    test: Make test code about car game

---
+++ __tests__/CarGame.test.js
@@ -0,0 +1,27 @@
+const CarGame = require("../src/CarGame");
+
+describe("자동차 게임 테스트", () => {
+  const game = new CarGame();
+  test("자동차 상태 초기화 테스트", () => {
+    const carNames = ["야미", "클린", "레고"];
+    game.initializeCarStatus(carNames);
+
+    expect(game.getCarStatus()).toEqual({
+      야미: 0,
+      클린: 0,
+      레고: 0,
+    });
+  });
+
+  test("우승자 반환 테스트", () => {
+    const carStatus = {
+      야미: 2,
+      클린: 3,
+      레고: 3,
+    };
+
+    const winner = game.findWinner(carStatus);
+
+    expect(winner).toEqual("클린,레고");
+  });
+});
