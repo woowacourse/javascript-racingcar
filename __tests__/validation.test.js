75725f4e2e17c6cc0e6117d64830f6412abb489e
semnil5202 <semnil5202@gmail.com>
2023-02-09 17:03:17 +0900

    feat: 시도 횟수 입력 유효성 테스트 추가
    추가로 이전에 작성한 불린값을 통한 유효성 테스트는 의도한 예외 처리가 발생하는지 확신할 수 없다.
    따라서 콘솔 에러문을 직접 비교할 수 있도록 log spy 기능을 사용한다.

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,32 +1,74 @@
 const { validateCarNames, validateWinningDistance } = require('../src/validation/input');
-const { GAME } = require('../src/utils/constants');
+const { GAME, ERROR } = require('../src/utils/constants');
+const OutputView = require('../src/view/OutputView');
+const { toInt } = require('../src/utils/common');
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(OutputView, 'print');
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join('');
+};
 
 describe('입력 validation 테스트', () => {
+  const logSpy = getLogSpy();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('자동차 이름 입력 테스트 (validateCarNames)', () => {
     it.each([
-      [['a'.repeat(GAME.CAR_NAME.min)], true],
-      [['a'.repeat(GAME.CAR_NAME.max)], true],
-      [['a'.repeat(GAME.CAR_NAME.max + 1)], false],
+      [['a'.repeat(GAME.CAR_NAME.min)], ''],
+      [['a'.repeat(GAME.CAR_NAME.max)], ''],
+      [['a'.repeat(GAME.CAR_NAME.max + 1)], ERROR.carNameLength],
     ])(
       `자동차 이름의 길이는 ${GAME.CAR_NAME.min}, ${GAME.CAR_NAME.max} 사이여야 한다.`,
-      (carNames, result) => {
-        expect(validateCarNames(carNames)).toEqual(result);
+      (carNames, errorMessage) => {
+        validateCarNames(carNames);
+        const log = getOutput(logSpy);
+
+        expect(log).toEqual(errorMessage);
       },
     );
 
     it('중복된 자동차 이름이 들어오면 안된다.', () => {
       const carNames = ['pobi', 'pobi'];
-      expect(validateCarNames(carNames)).toEqual(false);
+
+      validateCarNames(carNames);
+      const log = getOutput(logSpy);
+
+      expect(log).toEqual(ERROR.duplicatedCarName);
     });
 
     it('자동차 이름에 공백이 있으면 안된다.', () => {
       const carNames = ['a b', 'cr on', ' ', ' hey', 'hey '];
-      expect(validateCarNames(carNames)).toEqual(false);
+
+      validateCarNames(carNames);
+      const log = getOutput(logSpy);
+
+      expect(log).toEqual(ERROR.blankInCarName);
     });
   });
 
   describe('시도 횟수 입력 테스트 (validateWinningDistance)', () => {
-    it('', () => {});
-    it('', () => {});
+    it.each(['a', 'ㄱ', '!', '~', '@', '#', '$', '%', '^', '&', '*', '(', ' '])(
+      '숫자가 아닌 값을 입력하면 안된다.',
+      (input) => {
+        validateWinningDistance(toInt(input));
+        const log = getOutput(logSpy);
+
+        expect(log).toEqual(ERROR.invalidWinningDistanceType);
+      },
+    );
+
+    it.each([2, 10])('입력값의 범위가 3미만 10이상이면 안된다.', (input) => {
+      validateWinningDistance(input);
+      const log = getOutput(logSpy);
+
+      expect(log).toEqual(ERROR.invalidWinningDistanceRange);
+    });
   });
 });
