9021ce67a320114e17f24b72d41d6db5383867e5
turtle601 <turtle601@naver.com>
2023-02-09 11:31:52 +0900

    ✨ feat(OutputView): 시도한 횟수에 따라 자동차 경주 전체 진행 결과를 출력한다.

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -7,6 +7,14 @@ const OutputView = {
       Console.print(`${carInstance.getName()}: ${trace}`);
     });
   },
+
+  printRaceResult(raceInstance) {
+    Console.print('\n실행결과');
+    for (let i = 0; i <= raceInstance.getTrial(); i += 1) {
+      this.printTrace(raceInstance, i);
+      Console.print('');
+    }
+  },
 };
 
 module.exports = OutputView;
