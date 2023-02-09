3ea8cd929f04f0deab01b5fca7aa9a61a9f0143d
2yunseong <dbsdltjd3701@naver.com>
2023-02-09 16:09:15 +0900

    test: 전진할지 안할지 판단하는 기능 테스트

--- __tests__/GameManager.test.js
+++ __tests__/GameManager.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-lines-per-function */
 /* eslint-disable no-undef */
 // const Car = require('../src/Car');
-// const GameManager = require('../src/GameManager');
+const GameManager = require('../src/GameManager');
 const RandomGenerator = require('../src/utils/RandomGenerator');
 
 const mockRandom = (number) => {
@@ -13,5 +13,16 @@ describe('GameManager Test', () => {
   test('Random mock Test', () => {
     mockRandom(10);
     expect(RandomGenerator.pickRandomNumber()).toBe(10);
+  });
+
+  test.each([
+    [9, true],
+    [4, true],
+    [3, false],
+    [0, false],
+  ])('isFoward Test Random value : %i', (number, expected) => {
+    mockRandom(number);
+    const gameManager = new GameManager();
+    expect(gameManager.isForward()).toEqual(expected);
   });
 });
