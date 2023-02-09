8838ee15b71810cef230a6957daa1c7e559a0d3f
turtle601 <turtle601@naver.com>
2023-02-09 11:17:47 +0900

    ✨ feat(errorCheckFor): 사용자가 잘못된 입력을 했을 때, 에러문을 출력하고 입력을 다시 받는다.

---
+++ src/utils/errorCheckFor.js
@@ -0,0 +1,14 @@
+const Console = require('./Console');
+
+const errorCheckFor = (tryFn, catchFn) => {
+  try {
+    tryFn();
+  } catch (error) {
+    Console.print(error.message);
+    catchFn();
+  }
+};
+
+module.exports = {
+  errorCheckFor,
+};
