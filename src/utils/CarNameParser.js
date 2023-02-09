75d30ecf712c5b128ec8ac6879cf19abe877f81f
dladncks1217 <dlaxodud1217@gmail.com>
2023-02-09 14:23:40 +0900

    refactor: 메서드 분리, 변수명 수정, 콘솔 출력 방식 수정

--- src/utils/CarNameParser.js
+++
@@ -1,9 +0,0 @@
-function CarNameParser(str) {
-  let carNames = str.split(',');
-  carNames = carNames.map((name) => {
-    return name.trim();
-  });
-  return carNames.slice();
-}
-
-module.exports = CarNameParser;
