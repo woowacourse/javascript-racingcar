1bd6ac17553a9b9cb3b22efb6dbe34d504013e84
Jungwoo <evencoding@gmail.com>
2023-02-09 13:56:01 +0900

    refactor(randomGenerator): 하드코딩 상수화

--- src/randomGenerator.js
+++ src/randomGenerator.js
@@ -1,4 +1,7 @@
+const MAX_RANGE = 9;
+
 const randomGenerator = () => {
-  return Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * MAX_RANGE);
 };
+
 module.exports = { randomGenerator };
