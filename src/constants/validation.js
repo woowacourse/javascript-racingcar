1c8ab92fea0da9b753762b393d86358d0849f142
sh981013s <sh981013s@gmail.com>
2023-02-09 14:48:26 +0900

    feat(Validator): 자동차 이름 길이 유효성 검사 로직 구현

--- src/constants/validation.js
+++ src/constants/validation.js
@@ -1,10 +1,12 @@
 const VALID_CHARACTER_REGEX = /^[a-z|A-Z|가-힣|,]+$/g;
 const VALID_INTEGER_REGEX = /^[1-9]\d*$/;
 
-const MAX_CARNAME_LENGTH = 6;
+const MAX_CARNAME_LENGTH = 5;
 
 const ERROR_MESSAGE = {
   INVALID_CHARACTER: '[ERROR] 올바르지 않은 문자가 입력되었습니다. 다시 입력해 주세요.',
+  INVALID_NAME_LENGTH:
+    '[ERROR] 자동차 이름은 1자 이상, 5자 이하여야 합니다. 다시 입력해 주세요.',
   INVALID_TRY_COUNT: '[ERROR] 양의 정수가 아닌 값이 입력되었습니다. 다시 입력해 주세요.',
 };
 
