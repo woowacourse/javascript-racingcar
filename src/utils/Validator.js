983b525a95e6dc339c7729a8675c8243c9844fcf
sh981013s <sh981013s@gmail.com>
2023-02-09 14:48:26 +0900

    feat(Validator): 자동차 이름 길이 유효성 검사 로직 구현

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -2,6 +2,7 @@ const {
   VALID_CHARACTER_REGEX,
   VALID_INTEGER_REGEX,
   ERROR_MESSAGE,
+  MAX_CARNAME_LENGTH,
 } = require('../constants/validation');
 
 const Validator = {
@@ -9,10 +10,24 @@ const Validator = {
     if (!this.hasValidCharacter(nameInput)) {
       throw new Error(ERROR_MESSAGE.INVALID_CHARACTER);
     }
+
+    if (!this.isValidCarNameLength(nameInput)) {
+      throw new Error(ERROR_MESSAGE.INVALID_NAME_LENGTH);
+    }
   },
 
   hasValidCharacter(nameInput) {
     return VALID_CHARACTER_REGEX.test(nameInput);
+  },
+
+  isValidCarNameLength(nameInput) {
+    const splittedNames = nameInput.split(',');
+
+    for (let singleName of splittedNames) {
+      if (singleName.length > MAX_CARNAME_LENGTH || singleName.length <= 0) return false;
+    }
+
+    return true;
   },
 
   validateTryCountsInput(tryCountsInput) {
