9b11b1eb5de028f1b6183ea154f7f14a17e08c19
sh981013s <sh981013s@gmail.com>
2023-02-09 11:07:35 +0900

    fix(Validator): 자동차 이름 입력값에 대한 유효성 검사 로직 수정

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,15 +1,16 @@
 const Validator = {
-  INVALID_CHARACTER_REGEX: /[^a-z|^A-Z|^가-힣|,]/g,
+  VALID_CHARACTER_REGEX: /^[a-z|A-Z|가-힣|,]+$/g,
 
   validateNameInput(nameInput) {
     // 영어랑 한글 콤마
-    if (this.hasInvalidCharacter(nameInput)) {
+    if (!this.hasValidCharacter(nameInput)) {
       throw new Error('invalid character');
     }
   },
 
-  hasInvalidCharacter(nameInput) {
-    return this.INVALID_CHARACTER_REGEX.test(nameInput);
+  hasValidCharacter(nameInput) {
+    return this.VALID_CHARACTER_REGEX.test(nameInput);
+  },
   },
 };
 
