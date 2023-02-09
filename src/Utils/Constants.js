33e890ba52bc5a053001efb88f292dae23902ec3
Dain Lee <feb.dain@gmail.com>
2023-02-09 13:57:47 +0900

    feat: Add error message constants

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,17 +1,19 @@
+const COMMA = ",";
+const LINE_BREAK = "\n";
+
 const GAME_MESSAGE = Object.freeze({
-  INPUT_CAR_NAME:
-    "경주할 자동치 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).",
-  INPUT_TRY_COUNT: "시도할 횟수는 몇회인가요?",
-  RUN_RESULT_MESSAGE: "실행 결과",
+  INPUT_CAR_NAME: `경주할 자동치 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).${LINE_BREAK}`,
+  INPUT_TRY_COUNT: `시도할 횟수는 몇회인가요?${LINE_BREAK}`,
+  RUN_RESULT_MESSAGE: `${LINE_BREAK}실행 결과`,
   MOVEMENT_UNIT: "-",
   COLON: ":",
   GAME_RESULT: "(이)가 최종 우승했습니다.",
 });
 
-const LINE_BREAK = "\n";
 const MAXIMUM_CAR_NAME_LENGTH = 5;
 const MINIMUM_TRY_COUNT = 1;
 const MINIMUM_NUMBER_OF_CARS = 2;
+const MOVEMENT_STANDARD_NUMBER = 4;
 
 const ERROR_MESSAGE = Object.freeze({
   NAME_LENGTH_LIMIT: "5글자 이하로 입력해 주세요.",
@@ -19,6 +21,7 @@ const ERROR_MESSAGE = Object.freeze({
   NOT_POSITIVE_NUMBER: "양수를 입력해 주세요.",
   INVALID_NAME: "유효한 이름을 입력해 주세요.",
   NOT_BLANK: "공백없이 입력해 주세요.",
+  NAME_DUPLICATE: "중복된 이름은 사용할 수 없습니다.",
 });
 
 module.exports = {
@@ -28,4 +31,6 @@ module.exports = {
   MAXIMUM_CAR_NAME_LENGTH,
   MINIMUM_TRY_COUNT,
   MINIMUM_NUMBER_OF_CARS,
+  COMMA,
+  MOVEMENT_STANDARD_NUMBER,
 };
