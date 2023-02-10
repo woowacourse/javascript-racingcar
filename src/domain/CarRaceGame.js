9e571e59c4baf7b88f8d2b7306e172a8000a82e9
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-10 20:45:16 +0900

    refactor: 우승자 명단 반환 함수의 리턴 데이터타입 변경, 함수명 수정
    - judgeWinners -> getWinners
    - string -> array

--- src/domain/CarRaceGame.js
+++ src/domain/CarRaceGame.js
@@ -4,13 +4,14 @@ const CarRaceGame = {
   updateRace(carDistances) {
     const newDistances = carDistances;
     const roundRaceResult = CarRaceResultRandomGenerator.generate(carDistances.length);
+
     for (let i = 0; i < carDistances.length; i += 1) {
       newDistances[i] += roundRaceResult[i];
     }
     return newDistances;
   },
 
-  judgeWinners(carNames, carDistances) {
+  getWinners(carNames, carDistances) {
     const winners = [];
     const maxDistance = Math.max(...carDistances);
     for (let i = 0; i < carDistances.length; i += 1) {
@@ -18,7 +19,7 @@ const CarRaceGame = {
         winners.push(carNames[i]);
       }
     }
-    return winners.join(", ");
+    return winners;
   },
 };
 
