e04be0fd3dc61d8f7a960e72855ae87b2c85c4a6
ashleysyheo <ashleysyheo@gmail.com>
2023-02-10 16:48:30 +0900

    refactor: 숫자가 아닌 input 값 검증하는 로직 변경

--- src/utils/Exception.js
+++ src/utils/Exception.js
@@ -22,7 +22,7 @@ const Exception = {
   },
 
   checkMoveCountInput(input) {
-    if (isNaN(input) || input <= 0 || Utils.haskWhiteSpace(input)) {
+    if (isNaN(input) || input <= 0) {
       throw new Error(ErrorMessage.MOVE_INPUT);
     }
   },
