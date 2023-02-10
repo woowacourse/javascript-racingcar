0da0f5f87e2b6916e34a04fa5d2b5e64e7fdb707
hozzijeong <ghwls0905@naver.com>
2023-02-10 17:38:55 +0900

    test: Use test.each in test case

--- __tests__/MovementIndicator.test.js
+++ __tests__/MovementIndicator.test.js
@@ -1,14 +1,8 @@
 const { isMoving } = require("../src/MovementIndicator");
 
-test("생성된 난수가 4이상일 경우 true 반환 ", () => {
-  expect([1, 3, 5, 2, 6, 7, 8, 9].map((number) => isMoving(number))).toEqual([
-    false,
-    false,
-    true,
-    false,
-    true,
-    true,
-    true,
-    true,
-  ]);
-});
+test.each([5, 4, 7, 6, 8, 9])(
+  "생성된 난수가 4이상일 경우 true 반환 ",
+  (number) => {
+    expect(() => isMoving(number)).toBeTruthy();
+  },
+);
--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,31 +1,27 @@
 const Validator = require("../src/Validator/Validator");
 const { inputCarNameValidator, tryCountValidator } = Validator;
 
-const checkException = (testList, checkFunction) => {
-  testList.forEach((test) => {
-    expect(() => {
-      checkFunction(test);
-    }).toThrow(Error);
-  });
-};
-
 describe("예외 테스트", () => {
-  test("자동차 이름을 2개 이상 입력하지 않거나 올바른 자동차 이름을 입력하지 않으면 예외가 발생한다.", () => {
-    const carNames = [
-      ["가나"],
-      ["가나다라마바사", "가나"],
-      ["가나", "가나"],
-      ["^^", "??"],
-      ["ㄱ", "ㄴ"],
-      ["0.3", "0.2"],
-      [0.3, 0.2],
-      [" ", "가나"],
-    ];
-    checkException(carNames, inputCarNameValidator);
-  });
+  test.each([
+    ["가나"],
+    ["가나다라마바사", "가나"],
+    ["가나", "가나"],
+    ["^^", "??"],
+    ["ㄱ", "ㄴ"],
+    ["0.3", "0.2"],
+    [0.3, 0.2],
+    [" ", "가나"],
+  ])(
+    "자동차 이름을 2개 이상 입력하지 않거나 올바른 자동차 이름을 입력하지 않으면 예외가 발생한다.",
+    (carName) => {
+      expect(() => inputCarNameValidator(carName)).toThrow(Error);
+    },
+  );
 
-  test("시도 횟수에 양수가 아닌 값을 입력하지 않으면 예외가 발생한다.", () => {
-    const tryCount = ["-1", "백 번", "0", "DD", "0.1", "..", " ", "2e3"];
-    checkException(tryCount, tryCountValidator);
-  });
+  test.each(["-1", "백 번", "0", "DD", "0.1", "..", " ", "2e3"])(
+    "시도 횟수에 양수가 아닌 값을 입력하지 않으면 예외가 발생한다.",
+    (tryCount) => {
+      expect(() => tryCountValidator(tryCount)).toThrow(Error);
+    },
+  );
 });
