3a9452fbf94bdccd32ddd13254568df769408e46
jeongwusi <jeongwusi@gmail.com>
2023-02-11 01:05:41 +0900

    refactor: validateCarNames 함수의 변수명 수정

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -12,7 +12,7 @@ class InputView {
         '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
         input => {
           const carNames = input.split(',');
-          if (!this.validateCarNames(carNames)) {
+          if (!this.isValidateCarNames(carNames)) {
             reject('다섯글자 이하의 자동차를 중복없이 입력해주세요.');
           }
           const carMap = new Map();
@@ -26,7 +26,7 @@ class InputView {
     });
   }
 
-  validateCarNames(carNames) {
+  isValidateCarNames(carNames) {
     if ([...new Set(carNames)].length !== carNames.length) return false;
 
     for (const car of carNames) {
