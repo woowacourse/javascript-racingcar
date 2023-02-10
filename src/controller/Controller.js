a26a61dcda80b88a5457deb534f44d84f8d6c22f
jeongwusi <jeongwusi@gmail.com>
2023-02-11 01:13:07 +0900

    feat: 최종 우승 출력 후 노드 종료 기능 추가

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -14,6 +14,7 @@ class Controller {
     const trialCount = await this.inputView.readCount();
     const result = this.racingCarGame.getResult(carNames, trialCount);
     this.outputView.printResult(result);
+    process.exit();
   }
 }
 
