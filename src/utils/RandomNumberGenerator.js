1162102bb2163f6517e3f38a19a52cee94d70102
Suyoung <jsy8492@gmail.com>
2023-02-10 15:50:51 +0900

    refactor(RandomNumberGenerator): 난수 생성 기능 객체가 도메인 종속적이지 않도록 로직 수정

--- src/utils/RandomNumberGenerator.js
+++ src/utils/RandomNumberGenerator.js
@@ -1,13 +1,6 @@
 const RandomNumberGenerator = {
-  RANDOM_LOWER_INCLUSIVE: 1,
-  RANDOM_UPPER_INCLUSIVE: 8,
-
-  generate() {
-    return (
-      Math.floor(
-        Math.random() * (this.RANDOM_UPPER_INCLUSIVE - this.RANDOM_LOWER_INCLUSIVE),
-      ) + this.RANDOM_LOWER_INCLUSIVE
-    );
+  generate(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   },
 };
 
