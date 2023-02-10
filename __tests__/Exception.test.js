50256d94ae4a190dca7a3ab1ecb66a3ed9201001
ashleysyheo <ashleysyheo@gmail.com>
2023-02-10 16:49:07 +0900

    refactor: 시도 횟수 입력 값 테스트 수정

--- __tests__/Exception.test.js
+++ __tests__/Exception.test.js
@@ -40,21 +40,11 @@ describe('Exception 객체 테스트', () => {
     });
 
     test('숫자가 아닌 문자 입력시 에러를 발생시키는지 확인', () => {
-      const MOVE_COUNT_INPUTS = ['a', '-b', '+c', undefined, true, false];
+      const MOVE_COUNT_INPUTS = ['a', '-b', '+c', '', ' '];
 
       MOVE_COUNT_INPUTS.forEach((moveCount) => {
         expect(() => {
           Exception.checkMoveCountInput(parseInt(moveCount, 10));
-        }).toThrow();
-      });
-    });
-
-    test('공백 또는 빈 문자열이 포함되어 있으면 에러를 발생시키는지 확인', () => {
-      const MOVE_COUNT_INPUTS = ['', ' ', ' 2', '3 '];
-
-      MOVE_COUNT_INPUTS.forEach((moveCount) => {
-        expect(() => {
-          Exception.checkMoveCountInput(moveCount);
         }).toThrow();
       });
     });
