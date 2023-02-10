9ed3111442a0e0ac9870d9d7ccfb5aa571f2cdd4
정우시 <122500517+jeongwusi@users.noreply.github.com>
2023-02-10 22:22:52 +0900

    [1단계 - 자동차 경주 구현] 고니(정우시) 미션 제출합니다. (#185)
    * docs: 기능 구현 목록 작성
    
    * chore: 노드 설치 및 미션 유틸 설치
    
    * feat: 자동차 이름 입력 받는 기능 추가
    
    * feat: 자동차 이름 유효성 검사
    
    * feat: 자동차 이름 입력 예외처리
    
    * feat: 숫자 입력 받는 기능 추가
    
    * fix: eslint와 prettier 환경 설정
    
    * refactor: 자동차 입력값 프로미스로 변경
    
    * feat: 컨트롤러 구성
    
    * feat: 자동차 게임 모델 구현
    
    * feat: 최종 우승자 선별하는 기능 구현
    
    * feat: 기능 구현 완성
    
    * test: RacingCarGame 메소스 우승자 확인

---
+++ src/controller/Controller.js
@@ -0,0 +1,23 @@
+const InputView = require('../view/InputView');
+const RacingCarGame = require('../model/RacingCarGame');
+const OutputView = require('../view/OutputView');
+
+class Controller {
+  constructor() {
+    this.inputView = new InputView();
+    this.racingCarGame = new RacingCarGame();
+    this.outputView = new OutputView();
+  }
+
+  async init() {
+    const carNames = await this.inputView.readCarNames();
+    const trialCount = await this.inputView.readCount();
+    const result = this.racingCarGame.getResult(carNames, trialCount);
+    this.outputView.printResult(result);
+  }
+}
+
+const controller = new Controller();
+controller.init();
+
+module.exports = Controller;
