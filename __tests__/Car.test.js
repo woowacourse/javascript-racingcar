dc979c613f2d67e887f404b125ce704060fd5d5e
yeopto <gunyeop530@gmail.com>
2023-02-10 17:42:24 +0900

    chore: Car Class 테스트 파일을 수정하다

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,6 +1,6 @@
 const Car = require('../src/Models/Car');
 
-test('Car Class 테스트', () => {
+test('Car Class move 메서드 테스트', () => {
   // given
   const car = new Car('garam');
   const numbers = [1, 3, 4, 5, 6, 2, 0];
