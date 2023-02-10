5cefdf30252865e873dd7373fd107b5ba8554bc7
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-10 20:48:16 +0900

    feat(randomnumbersgenerator): 여러 개의 랜덤 값을 추출하는 기능 구현
    - getRandomNumers(numberCount, maxValue)
    - 추출한 랜덤 값들을 배열의 형태로 리턴

--- src/util/randomNumbersGenerator.js
+++ src/util/randomNumbersGenerator.js
@@ -1,4 +1,11 @@
 const randomNumbersGenerator = {
+  getRandomNumbers(numberCount, maxValue) {
+    const resultNumbers = Array(numberCount)
+      .fill("")
+      .map(() => this.getSingleRandomNumber(maxValue));
+    return resultNumbers;
+  },
+
   getSingleRandomNumber(maxValue) {
     return Math.floor(Math.random() * maxValue);
   },
