458ca935784c3117c09d3ba9b9d40a012ef32f85
Suyoung <jsy8492@gmail.com>
2023-02-10 23:44:19 +0900

    refactor(Validator): 기존의 유효성 검사 객체 하나를 별도의 유효성 검사 객체들로 분리

---
+++ src/validators/CarNamesInputValidator.js
@@ -0,0 +1,32 @@
+const CarNamesInputValidator = {
+  VALID_CHARACTER_REGEX: /^[a-zA-Z가-힣,]+$/g,
+  MAX_CARNAME_LENGTH: 5,
+
+  validate(carNamesInput) {
+    if (!this.hasOnlyValidCharacters(carNamesInput)) {
+      throw Error(
+        '[ERROR] 영어, 한글, 쉼표(,)가 아닌 문자가 입력되었습니다. 다시 입력해 주세요.',
+      );
+    }
+
+    if (!this.hasOnlyValidCarNames(carNamesInput.split(','))) {
+      throw Error(
+        '[ERROR] 자동차 이름은 1자 이상, 5자 이하여야 합니다. 다시 입력해 주세요.',
+      );
+    }
+  },
+
+  hasOnlyValidCharacters(carNamesInput) {
+    return this.VALID_CHARACTER_REGEX.test(carNamesInput);
+  },
+
+  hasOnlyValidCarNames(carNames) {
+    return carNames.every((carName) => this.isValidCarNameLength(carName.length));
+  },
+
+  isValidCarNameLength(carNameLength) {
+    return 0 < carNameLength && carNameLength <= this.MAX_CARNAME_LENGTH;
+  },
+};
+
+module.exports = CarNamesInputValidator;
