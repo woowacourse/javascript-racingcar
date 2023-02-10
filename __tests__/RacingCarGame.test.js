63c1fcdf10e055b8d759fd7c2cea81709c7dd38d
ukkodeveloper <ukkodeveloper@gmail.com>
2023-02-10 20:43:51 +0900

    fix: correct test message to fit act

--- __tests__/RacingCarGame.test.js
+++ __tests__/RacingCarGame.test.js
@@ -2,36 +2,30 @@ const RacingCarGame = require('../src/model/RacingCarGame');
 
 describe('getWinnerCar 테스트', () => {
   const racingCarGame = new RacingCarGame();
-
-  //TODO: 제목 변경
-  test('pobi 3, ukko 5, gonni 7 -> gonni 우승', () => {
+  test('pobi 1칸 전진, ukko 2칸 전진, gonni 3칸 전진 -> gonni 우승', () => {
     const carNamesMap = ['pobi', 'ukko', 'gonni'].reduce((acc, cur, index) => {
-      return acc.set(cur, index + 1);
+      return acc.set(cur, index + 2);
     }, new Map());
 
     expect(racingCarGame.getWinnerCar(carNamesMap)).toBe(
       'gonni이(가) 최종 우승했습니다.'
     );
   });
-
-  //TODO: set함수 인자 변경
-  test('pobi 1, ukko 2, gonni 3 -> gonni 우승', () => {
+  test('pobi 2칸 전진, ukko 5칸 전진, gonni 5칸 전진 -> ukko, gonni 우승', () => {
     const carNamesMap = new Map();
     carNamesMap.set('pobi', 3);
-    carNamesMap.set('ukko', 5);
-    carNamesMap.set('gonni', 5);
+    carNamesMap.set('ukko', 6);
+    carNamesMap.set('gonni', 6);
 
     expect(racingCarGame.getWinnerCar(carNamesMap)).toBe(
       'ukko, gonni이(가) 최종 우승했습니다.'
     );
   });
-
-  //TODO: set함수 인자 변경
-  test('pobi 1, ukko 1, gonni 1 -> pobi, ukko, gonni 우승', () => {
+  test('pobi 1칸 전진, ukko 1칸 전진, gonni 1칸 전진 -> pobi, ukko, gonni 우승', () => {
     const carNamesMap = new Map();
-    carNamesMap.set('pobi', 5);
-    carNamesMap.set('ukko', 5);
-    carNamesMap.set('gonni', 5);
+    carNamesMap.set('pobi', 2);
+    carNamesMap.set('ukko', 2);
+    carNamesMap.set('gonni', 2);
     
     expect(racingCarGame.getWinnerCar(carNamesMap)).toBe(
       'pobi, ukko, gonni이(가) 최종 우승했습니다.'
