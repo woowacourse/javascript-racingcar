db966226567d583daa6c3b849dc698be0b242814
ashleysyheo <ashleysyheo@gmail.com>
2023-02-10 14:59:20 +0900

    fix: Exception 객체 테스트 케이스 수정

--- __tests__/Exception.test.js
+++ __tests__/Exception.test.js
@@ -2,7 +2,7 @@ const Exception = require('../src/utils/Exception');
 
 describe('Exception 객체 테스트', () => {
   test('공백 또는 빈 문자열이 포함되어 있으면 에러를 발생시키는지 확인', () => {
-    const NAME_INPUTS = ['', ' ', '우형, ,,배민'];
+    const NAME_INPUTS = ['', ' ', '우형 배민', ' 짜장면'];
 
     expect(() => {
       Exception.checkCarInput(NAME_INPUTS);
@@ -10,7 +10,7 @@ describe('Exception 객체 테스트', () => {
   });
 
   test('이름이 5글자 이상인 경우 에러를 발생시키는지 확인', () => {
-    const NAME_INPUTS = ['안녕하세요', '배달의민족', '우형, 배민'];
+    const NAME_INPUTS = ['안녕하세요', '배달의민족입니다', '우형', '배민입니다요'];
 
     expect(() => {
       Exception.checkCarInput(NAME_INPUTS);
@@ -18,7 +18,7 @@ describe('Exception 객체 테스트', () => {
   });
 
   test('중복된 이름 입력시 에러를 발생시키는지 확인', () => {
-    const NAME_INPUTS = ['안녕하세요', '배달의민족', '우형, 배달의민족'];
+    const NAME_INPUTS = ['안녕하세요', '배달의민족', '우형', '배달의민족'];
 
     expect(() => {
       Exception.checkCarInput(NAME_INPUTS);
