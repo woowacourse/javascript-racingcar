f14db6252b5eefc5834b99fee07b1a174f4e7d92
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-10 20:54:00 +0900

    refactor(carracegame): 거리를 업데이트하는 함수를 for문 -> forEach문으로 변경
    - i값을 잘못 정할 수 있는 for문을 굳이 사용하지 않아도 구현이 가능 -> for문 지양

--- src/domain/CarRaceGame.js
+++ src/domain/CarRaceGame.js
@@ -5,20 +5,23 @@ const CarRaceGame = {
     const newDistances = carDistances;
     const roundRaceResult = CarRaceResultRandomGenerator.generate(carDistances.length);
 
-    for (let i = 0; i < carDistances.length; i += 1) {
-      newDistances[i] += roundRaceResult[i];
-    }
+    roundRaceResult.forEach((increaseDistance, index) => {
+      newDistances[index] += increaseDistance;
+    });
+
     return newDistances;
   },
 
   getWinners(carNames, carDistances) {
     const winners = [];
     const maxDistance = Math.max(...carDistances);
+
     for (let i = 0; i < carDistances.length; i += 1) {
       if (carDistances[i] === maxDistance) {
         winners.push(carNames[i]);
       }
     }
+
     return winners;
   },
 };
