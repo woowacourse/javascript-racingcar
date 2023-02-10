16abb1b72e3efbdbae01f189a37587af5084591e
WaiNaat <qoli0703@gmail.com>
2023-02-11 04:24:20 +0900

    feat(Car): field를 private로 바꾸고 getter추가

--- src/Car.js
+++ src/Car.js
@@ -1,15 +1,27 @@
 const { Settings } = require('./Config');
 
 class Car {
+  #name;
+
+  #position;
+
   constructor(carName) {
-    this.name = carName;
-    this.position = 1;
+    this.#name = carName;
+    this.#position = 1;
   }
 
   move(power) {
     if (power >= Settings.MOVING_POWER) {
-      this.position += 1;
+      this.#position += 1;
     }
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getPosition() {
+    return this.#position;
   }
 }
 
