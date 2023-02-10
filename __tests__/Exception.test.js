f4cce20730ba4006279889ec957e80fa3bb3c1f0
ashleysyheo <ashleysyheo@gmail.com>
2023-02-10 16:16:01 +0900

    refactor: 숫자가 아닌 문자 입력 에러 테스트 수정

--- __tests__/Exception.test.js
+++ __tests__/Exception.test.js
@@ -40,11 +40,11 @@ describe('Exception 객체 테스트', () => {
     });
 
     test('숫자가 아닌 문자 입력시 에러를 발생시키는지 확인', () => {
-      const MOVE_COUNT_INPUTS = ['a', '-b', '+c'];
+      const MOVE_COUNT_INPUTS = ['a', '-b', '+c', undefined, true, false];
 
       MOVE_COUNT_INPUTS.forEach((moveCount) => {
         expect(() => {
-          Exception.checkMoveCountInput(moveCount);
+          Exception.checkMoveCountInput(parseInt(moveCount, 10));
         }).toThrow();
       });
     });
