acbb6c6762550f6d4ac50e8612f0677dd5d9885e
afds4567 <afds4567@gmail.com>
2023-02-11 02:05:34 +0900

    refactor: Validator test 구체화

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,18 +1,35 @@
-const { Validator } = require('../src/Validator');
+const { Validator } = require("../src/Validator");
 
-test('자동차 이름 입력에 대한 예외 테스트', () => {
-  const inputValues = [
-    ['abcdef', 'avcd', 'abc'],
-    ['a', 'a', 'c', 'd'],
-  ];
-  inputValues.forEach((inputValue) => {
-    expect(() => Validator.validateName(inputValue)).toThrow();
+describe("자동차 이름 유효성 검사", () => {
+  test("자동차 이름이 5글자 이하인 경우", () => {
+    expect(() => Validator.validateName(["a", "b", "c"]).toThrow());
+  });
+
+  test("자동차 이름이 5글자 초과인 경우", () => {
+    expect(() => Validator.validateName(["a", "b", "cccccc"])).toThrow();
+  });
+
+  test("자동차 이름이 5글자 이하 영어,한글,숫자 조합인 경우", () => {
+    expect(() =>
+      Validator.validateName(["aㄱ1", "22", "bbbb", "ㄷㄷㄷ"]).not.toThrow()
+    );
+  });
+
+  test("자동차 이름에 영어,한글,숫자 이외의 문자가 포함된 경우", () => {
+    expect(() => Validator.validateName("a1,22,bbbb,ㄷㄷㄷ,!")).toThrow();
   });
 });
 
-test('시도 횟수에 대한 예외 테스트', () => {
-  const inputValues = ['', '-4', '0', 'not', '1.34'];
-  inputValues.forEach((inputValue) => {
-    expect(() => Validator.validateName(inputValue)).toThrow();
+describe("시도 횟수 유효성 검사", () => {
+  test("시도 횟수가 1 이상인 양의 정수인 경우", () => {
+    expect(() => Validator.validateTryCnt(10)).not.toThrow();
+  });
+
+  test("시도 횟수가 0 미만인 경우", () => {
+    expect(() => Validator.validateTryCnt(-1)).toThrow();
+  });
+
+  test("시도 횟수가 정수가 아닌 경우", () => {
+    expect(() => Validator.validateTryCnt(1.23)).toThrow();
   });
 });
