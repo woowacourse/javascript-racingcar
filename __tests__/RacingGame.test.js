4b1b01c1fbc916b519d4e8c120ece9d9b19774c4
Suyoung <jsy8492@gmail.com>
2023-02-10 16:31:54 +0900

    refactor(RacingGameTest): 우승자를 도출하는 기능에 대한 테스트의 의미가 더 명확하도록 수정

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -26,17 +26,17 @@ describe('자동차를 관리하는 클래스 테스트', () => {
     );
   });
 
-  describe('경주 우승자를 도출한다.', () => {
-    test('우승자가 한 명인 경우', () => {
+  describe('경주 우승자를 도출하는 기능 테스트', () => {
+    test('우승자가 한 명인 경우 우승자 한 명의 이름이 담긴 배열을 반환한다.', () => {
       mockRandoms([5, 2, 1]);
 
       racingGame.progress();
 
       const winners = racingGame.getWinners();
-      expect(winners.length === 1 && winners[0] === 'pobi').toBe(true);
+      expect(winners).toEqual(['pobi']);
     });
 
-    test('우승자가 두 명 이상인 경우', () => {
+    test('우승자가 두 명 이상인 경우 모든 우승자의 이름이 담긴 배열을 반환한다.', () => {
       mockRandoms([6, 7, 2]);
 
       racingGame.progress();
