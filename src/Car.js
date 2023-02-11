3e5bd5c18c2cde98d2a8d352e1ab9aadc59f43f4
hozzijeong <ghwls0905@naver.com>
2023-02-11 15:19:43 +0900

    feat: Create Car.js

---
+++ src/Car.js
@@ -0,0 +1,21 @@
+class Car {
+  #name;
+  #position;
+  constructor(name) {
+    this.#name = name;
+    this.#position = 0;
+  }
+
+  move(condition) {
+    this.#position += condition;
+  }
+
+  getCarStatus() {
+    return {
+      name: this.#name,
+      position: this.#position,
+    };
+  }
+}
+
+module.exports = Car;
