52e7e4912a7189e12b17cd7a2651497f88ad0dc8
Dain Lee <feb.dain@gmail.com>
2023-02-12 02:04:06 +0900

    test: create test code for SplitAndTrimString.js

---
+++ __tests__/SplitAndTrimString.test.js
@@ -0,0 +1,8 @@
+const splitAndTrimString = require("../src/Utils/SplitAndTrimString");
+
+test("입력받은 자동차 이름 콤마(,)로 분리 후 앞뒤 공백 제거 테스트", () => {
+  const names = "야미,클린 ,레고, 타미";
+  const splitAndTrimmedString = ["야미", "클린", "레고", "타미"];
+
+  expect(splitAndTrimString(names)).toEqual(splitAndTrimmedString);
+});
