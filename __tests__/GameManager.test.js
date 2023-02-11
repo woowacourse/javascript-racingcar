991d7fe6c8433f27b10cadd8612ea9ec07341586
2yunseong <dbsdltjd3701@naver.com>
2023-02-11 18:01:46 +0900

    test: 시도 횟수 입력이 유효하지 않으면 에러를 던지는 지 테스트

--- __tests__/GameManager.test.js
+++ __tests__/GameManager.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 /* eslint-disable no-undef */
 
 import GameManager from '../src/domain/GameManager';
@@ -9,4 +10,12 @@ describe('GameManager Test', () => {
 
     expect(() => gameMananger.checkCarNames(badNamesInput)).toThrow();
   });
+
+  test.each([0, -1, NaN])(
+    '시도 횟수 입력이 유효하지 않으면 에러를 던지는 지 테스트',
+    (input) => {
+      const gameMananger = new GameManager();
+      expect(() => gameMananger.checkTryCount(input)).toThrow();
+    }
+  );
 });
