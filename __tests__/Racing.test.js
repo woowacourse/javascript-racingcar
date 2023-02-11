f1011fdc754a19cf78936ab06f9dc21636fb7f63
NaveOWO <pianowell96@gmail.com>
2023-02-11 19:53:00 +0900

    [ Test ] 원시값을 반환하는 함수에 대한 matcher함수 수정

--- __tests__/Racing.test.js
+++ __tests__/Racing.test.js
@@ -1,6 +1,5 @@
 const Racing = require("../src/Racing");
 const Car = require("../src/Car");
-const Random = require("../src/Random");
 
 describe("Racing 객체에 대한 단위테스트", () => {
     const car = new Car("nave");
@@ -8,24 +7,24 @@ describe("Racing 객체에 대한 단위테스트", () => {
 
     test("랜덤값이 4 이상이면 true를 반환하는 함수 테스트", () => {
         const randomNumber = 4;
-        expect(racing.checkRandomNumber(randomNumber)).toEqual(true);
+        expect(racing.checkRandomNumber(randomNumber)).toBe(true);
     })
 
     test("랜덤값이 4 이하면 false를 반환하는 함수 테스트", () => {
         const randomNumber = 3;
-        expect(racing.checkRandomNumber(randomNumber)).toEqual(false);
+        expect(racing.checkRandomNumber(randomNumber)).toBe(false);
     })
 
     test("랜덤값이 4 이상이면 각각의 car객체를 전진시키는 함수 테스트", () => {
         const randomNumber = 4;
         racing.raceEachCar(randomNumber);
-        expect(car.getScore()).toEqual(1);
+        expect(car.getScore()).toBe(1);
     })
  
     test("랜덤값이 4 이하면 각각의 car객체를 정지시키는 함수 테스트", () => {
         const randomNumber = 3;
         racing.raceEachCar(randomNumber);
-        expect(car.getScore()).toEqual(1);
+        expect(car.getScore()).toBe(1);
     })
 
 })
