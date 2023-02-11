e129500a4d16f0300dbf063c5dbe424cf8a95be5
Jungwoo <evencoding@gmail.com>
2023-02-11 21:57:11 +0900

    test(Validator): test.each 사용으로 중복문 최소화

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -3,23 +3,12 @@ const { Validator } = require('../src/Validator');
 describe('입력된 자동차 이름에 대한 테스트', () => {
   test.each(['!,b,c', 'a,a,b', 'abcdef,b,c'])('예외 발생 테스트', (names) => {
     console.log(names);
-    expect(Validator.validateName(names.split(','))).toThrow();
+    expect(() => Validator.validateName(names.split(','))).toThrow();
   });
-
-  // test.each()(
-  //   '유효한 값이 들어왔을 때 테스트',
-  //   (name) => {
-  //     expect(Validator.validateName(name));
-  //   }
-  // );
 });
 
 describe('입력된 시도 횟수에 대한 테스트', () => {
   test.each(['0', '-1', '', 's', '1.34'])('예외 발생 테스트', (tryCount) => {
-    expect(Validator.validateTryCnt(tryCount)).toThrow();
+    expect(() => Validator.validateTryCnt(tryCount)).toThrow();
   });
-
-  // test.each(['1', '3'])('유효한 값이 들어왔을 때 테스트', (tryCount) => {
-  //   expect(Validator.validateName(tryCount));
-  // });
 });
