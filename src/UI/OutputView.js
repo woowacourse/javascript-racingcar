85d1d9a91a2a040b163aeef7d6e10412f14e4e08
Dain Lee <feb.dain@gmail.com>
2023-02-12 01:54:21 +0900

    refactor: use 'new Map' instead of object

--- src/UI/OutputView.js
+++ src/UI/OutputView.js
@@ -1,5 +1,5 @@
 const { print } = require("../Utils/Utils");
-const { GAME_MESSAGE, LINE_BREAK } = require("../Utils/Constants");
+const { GAME_MESSAGE, LINE_BREAK } = require("../Constants/Constants");
 const { RUN_RESULT_MESSAGE, GAME_RESULT, COLON, MOVEMENT_UNIT } = GAME_MESSAGE;
 
 const OutputView = {
@@ -8,9 +8,10 @@ const OutputView = {
   },
 
   printCarMovement(carStatus) {
-    for (const [name, count] of Object.entries(carStatus)) {
-      print(`${name} ${COLON} ${MOVEMENT_UNIT.repeat(count)}`);
-    }
+    [...carStatus.entries()].forEach(([name, count]) =>
+      print(`${name} ${COLON} ${MOVEMENT_UNIT.repeat(count)}`)
+    );
+
     print(LINE_BREAK);
   },
 
