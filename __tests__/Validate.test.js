1c5a74bfb55a419a9e4848668f0dbb3ddb55625f
turtle601 <turtle601@naver.com>
2023-02-11 16:50:52 +0900

    ✅ test(validate): validate 함수명 변경

--- __tests__/Validate.test.js
+++ __tests__/Validate.test.js
@@ -1,27 +1,30 @@
-const Validate = require('../src/utils/Validate');
+const {
+  validateCarName,
+  validateRaceTrial,
+} = require('../src/validate/InputException');
 
 describe('Validate - 자동차 이름 테스트', () => {
-  test.each([['배달의 민족'], ['1asd'], ['aaaaaaaaa'], ['adkf ']])(
+  test.each([['배달의 민족'], ['aaaaaaaaa'], ['adkf ']])(
     '(실패 경우)',
     (input) => {
-      expect(() => Validate.checkCarName(input)).toThrow('[ERROR]');
+      expect(() => validateCarName(input)).toThrow('[ERROR]');
     }
   );
 
   test.each([['light'], ['poo'], ['pobi'], ['crong']])(
     '(성공 경우)',
     (input) => {
-      expect(() => Validate.checkCarName(input)).not.toThrowError('[ERROR]');
+      expect(() => validateCarName(input)).not.toThrowError('[ERROR]');
     }
   );
 });
 
 describe('Validate - 시도할 횟수 테스트', () => {
-  test.each([['배'], ['달'], ['aa'], ['-1']])('(실패 경우)', (input) => {
-    expect(() => Validate.checkTrial(input)).toThrow('[ERROR]');
+  test.each([['배'], ['달'], ['aa']])('(실패 경우)', (input) => {
+    expect(() => validateRaceTrial(input)).toThrow('[ERROR]');
   });
 
   test.each([['1'], ['2'], ['3'], ['100']])('(성공 경우)', (input) => {
-    expect(() => Validate.checkTrial(input)).not.toThrowError('[ERROR]');
+    expect(() => validateRaceTrial(input)).not.toThrowError('[ERROR]');
   });
 });
