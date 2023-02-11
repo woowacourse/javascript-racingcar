0071ea7a1de100b319a4b2642545572b32e743eb
NaveOWO <pianowell96@gmail.com>
2023-02-11 20:16:03 +0900

    [ Feat ] input 유효성 검사

--- src/App.js
+++ src/App.js
@@ -5,62 +5,83 @@ const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 const CONSTANT = require("./Constants");
 const Random = require("./Random");
+const Validations = require("./validations");
 
 class App {
 
     play() {
-        this.startRace()
+        this.startRace();
     }
     
     startRace() {
         InputView.readUserInput((input) => {
-            const carNames = input.split(",");
-            const cars = new Cars(this.createCars(carNames))
-            this.getRound(cars)
-        }, CONSTANT.INPUT_CAR_MASSEGE)
+            try {
+                this.validateCarInput(this.getSplitedCarName(input));
+                const cars = new Cars(this.createCars(this.getSplitedCarName(input)));
+                this.getRound(cars);
+            } catch (e) {
+                console.log(e);
+                this.startRace();
+            }
+        }, CONSTANT.INPUT_CAR_MASSEGE);
+    }
+
+    getSplitedCarName(carNames) {
+        return carNames.split(",");
+    }
+
+    validateCarInput(carNames) {
+        Validations.checkCarMaxName(carNames);
+        Validations.checkCarMinName(carNames);
     }
 
     createCars(carNames) {
         const cars = carNames.map(carName => {
-            return new Car(carName)
+            return new Car(carName);
         });
-        return cars
+        return cars;
     }
     
     getRound(cars) {
         InputView.readUserInput((round => {
-            this.getTotalResult(round,cars);
-        }),CONSTANT.INPUT_ROUND_MASSEGE)
+            try {
+                Validations.checkRound(Number(round));
+                this.getTotalResult(Number(round), cars);
+            } catch (e) {
+                console.log(e);
+                this.getRound(cars);
+            }
+        }), CONSTANT.INPUT_ROUND_MASSEGE);
     }
     
     getTotalResult(round,cars) {
         OutputView.outputResultTitle();
-        this.getRoundResult(round,cars)
+        this.getRoundResult(round, cars);
         this.getFinalResult(cars);
     }
 
     getRoundResult(round,cars) {
         for (let i = 0; i < round; i++) {
-            this.moveCar(cars)
-            OutputView.outputRoundResult(cars.getCars())
-        }
+            this.moveCar(cars);
+            OutputView.outputRoundResult(cars.getCars());
+        };
     }
 
     moveCar(cars) {
         cars.getCars().forEach(car => {
-            const racing = new Racing(car)
-            racing.raceEachCar(Random.generateRandomNumber())
-        })    
+            const racing = new Racing(car);
+            racing.raceEachCar(Random.generateRandomNumber());
+        });
     }
     
     getFinalResult(cars) {
-        OutputView.outputRoundResult(cars.getCars())
-        cars.decideWinnerCar()
-        OutputView.outputWinner(cars.getWinnerCar())
+        OutputView.outputRoundResult(cars.getCars());
+        cars.decideWinnerCar();
+        OutputView.outputWinner(cars.getWinnerCar());
     }
 
 }
 
-module.exports = App
-const app = new App()
-app.play()
+module.exports = App;
+const app = new App();
+app.play();
