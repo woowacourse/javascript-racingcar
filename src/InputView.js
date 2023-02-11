4c0202b23defcedf11abcb62fe12f0004de695bc
NaveOWO <pianowell96@gmail.com>
2023-02-11 15:33:22 +0900

    [ Refactor ] 입력을 받는 함수를 한개로 축소 & 재사용

--- src/InputView.js
+++ src/InputView.js
@@ -1,44 +1,18 @@
 const readline = require("readline");
-const validations = require("./validations");
-const CONSTANT = require("./Constant");
 
-const inputView = {
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-    inputCarName(callback) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
+const InputView = {
+
+    readUserInput(callback, inputMessage) {
+        rl.question(inputMessage, (input) => {
+            callback(input)
         })
-        rl.question(CONSTANT.INPUT_CAR_MASSEGE, (line) => {
-            rl.close()
-            callback(line)
-        }) ;
-    },
-
-    inputRound(callback) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        })
-        rl.question(CONSTANT.INPUT_ROUND_MASSEGE, (round) => {
-            this.tryCatchRound(rl, round,callback)
-        })
-    },
-
-    tryCatchRound(rl, round, callback) {
-        try {
-            rl.close();
-            validations.validateRound(+round);
-            callback(+round)
-        } catch (e) {
-            console.log(e.message);
-            this.inputRound(callback);
-        }
-    },
+    }
 
 }
 
-
-
-
-module.exports = inputView
+module.exports = InputView
