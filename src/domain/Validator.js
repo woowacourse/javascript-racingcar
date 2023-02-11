996bc2038c59171663213c2ab3e93ea6587e2a07
shackstack <kms1061@naver.com>
2023-02-11 11:21:26 +0900

    refactor(Validator): 유효성 검사 메서드 객체화

--- src/domain/Validator.js
+++ src/domain/Validator.js
@@ -4,47 +4,53 @@ const Validator = {
   carName(string) {
     const names = string.split(",");
 
-    if (this.isCarNameHasBlank(string)) throw new Error(MESSAGE.error.blank);
+    if (this.isCarName.hasBlank(string)) throw new Error(MESSAGE.error.blank);
 
-    if (!this.isCarNameLessThenFive(names))
+    if (!this.isCarName.lessThenFive(names))
       throw new Error(MESSAGE.error.nameLength);
 
-    if (!this.isCarNameNull(names)) throw new Error(MESSAGE.error.null);
+    if (!this.isCarName.null(names)) throw new Error(MESSAGE.error.null);
 
-    if (!this.isCarNameLowerCase(names))
+    if (!this.isCarName.lowerCase(names))
       throw new Error(MESSAGE.error.lowerCase);
 
-    if (this.isDuplicated(names)) throw new Error(MESSAGE.error.duplicatedName);
+    if (this.isCarName.duplicated(names))
+      throw new Error(MESSAGE.error.duplicatedName);
   },
 
   tryCount(number) {
-    if (!this.isNumeric(number)) throw new Error(MESSAGE.error.numeric);
+    if (!this.isTryCount.numeric(number))
+      throw new Error(MESSAGE.error.numeric);
   },
 
-  isCarNameHasBlank(string) {
-    return string.search(REGEXR.noBlank) !== -1;
+  isCarName: {
+    hasBlank(string) {
+      return string.search(REGEXR.noBlank) !== -1;
+    },
+
+    lessThenFive(names) {
+      return names.every((name) => name.length <= 5);
+    },
+
+    null(names) {
+      return names.every((name) => name !== "");
+    },
+
+    lowerCase(names) {
+      return names.every((name) => name.search(REGEXR.lowerCase) === -1);
+    },
+
+    duplicated(names) {
+      if (names.length !== new Set(names).size) return true;
+    },
   },
 
-  isCarNameLessThenFive(names) {
-    return names.every((name) => name.length <= 5);
-  },
+  isTryCount: {
+    numeric(number) {
+      if (number === "0") return false;
 
-  isCarNameNull(names) {
-    return names.every((name) => name !== "");
-  },
-
-  isCarNameLowerCase(names) {
-    return names.every((name) => name.search(REGEXR.lowerCase) === -1);
-  },
-
-  isDuplicated(names) {
-    if (names.length !== new Set(names).size) return true;
-  },
-
-  isNumeric(number) {
-    if (number === "0") return false;
-
-    return number.search(REGEXR.numeric) === -1;
+      return number.search(REGEXR.numeric) === -1;
+    },
   },
 };
 
