b3157ed0fa841b0f3fae18583de43e868a12f85d
nlom0218 <nlom0218@naver.com>
2023-02-11 09:50:11 +0900

    refactor: 호출에 대한 테스트 코드 제거
    유저가 프로그램을 사용하는 가정하에 필요한 동작 위주로 테스트 코드를 작성한다.

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,22 +1,22 @@
-const { MESSAGE } = require("../src/lib/Constant.js");
-const Validator = require("../src/model/Validator.js");
+const { MESSAGE } = require('../src/lib/Constant.js');
+const Validator = require('../src/model/Validator.js');
 
-describe("자동차 이름 입력값에 대한 유효성 검사", () => {
+describe('사용자가 올바른 자동차 이름을 입력하지 않을 경우, 에러 메시지를 호출한다.', () => {
   test.each([
-    ["aaa,b b,ccc", MESSAGE.error.blank],
-    ["aaa,bbbbbb,ccc", MESSAGE.error.nameLength],
-    ["aaa,bbb,Ccc", MESSAGE.error.lowerCase],
-    ["aaa,aaa,ccc", MESSAGE.error.duplicatedName],
-  ])("자동차 이름에 대한 유효성 검사 테스트", (carNames, errorMessage) => {
+    ['aaa,b b,ccc', MESSAGE.error.blank],
+    ['aaa,bbbbbb,ccc', MESSAGE.error.nameLength],
+    ['aaa,bbb,Ccc', MESSAGE.error.lowerCase],
+    ['aaa,aaa,ccc', MESSAGE.error.duplicatedName],
+  ])('자동차 이름에 대한 유효성 검사 테스트', (carNames, errorMessage) => {
     expect(() => {
       Validator.carName(carNames);
     }).toThrow(errorMessage);
   });
 });
 
-describe("시도 횟수에 대한 유효성 검사", () => {
-  test.each([["-2"], ["3.142592"], ["0"]])(
-    "%s가 입력되면 예외를 발생시킨다.",
+describe('사용자가 올바른 시도 횟수를 입력하지 않을 경우, 에러 메시지를 호출한다.', () => {
+  test.each([['-2'], ['3.142592'], ['0']])(
+    '%s가 입력되면 예외를 발생시킨다.',
     (number) => {
       expect(() => {
         Validator.tryCount(number);
