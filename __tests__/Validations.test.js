7484fdeeba81bb5af1862b8e2e0726b787ddc1b3
NaveOWO <pianowell96@gmail.com>
2023-02-11 17:52:29 +0900

    [ Test ] input유효성 검사하는 테스트코드 수정

--- __tests__/Validations.test.js
+++ __tests__/Validations.test.js
@@ -1,31 +1,26 @@
-const CONSTANT = require("../src/Constant");
+const Constants = require("../src/Constants");
 const Validations = require("../src/Validations");
 
-describe("validation_test", () => {
-    // const validations = new Validations()
-    test("차 이름 길이 유효성 검사_validateCarNameLength()", () => {
+describe("input의 유효성검사에 대한 단위 테스트", () => {
+    
+    test("차 이름의 길이가 5이상이면 에러를 던지는 함수 테스트", () => {
         const carNames = ['naveowo', 'nave']
-
-        expect(() => Validations.validateCarNameLength(carNames)).toThrow(CONSTANT.ERROR_CAR_LENGTH)
+        expect(() => Validations.checkCarMaxName(carNames)).toThrow(Constants.ERROR_CAR_LENGTH)
     });
 
-    test("차 이름 여부 유효성 검사_validateIsCarName()", () => {
+    test("차 이름이 1자 미만이면 에러를 던지는 함수 테스트", () => {
         const carNames = ['naveowo', 'nave', '']
-
-        expect(() => Validations.validateIsCarName(carNames)).toThrow(CONSTANT.ERROR_CAR_NONAME)
+        expect(() => Validations.checkCarMinName(carNames)).toThrow(Constants.ERROR_CAR_NONAME)
     });
 
-    test("경기 라운드 유효성 검사_validateRound()", () => {
+    test("라운드가 0이면 에러를 던지는 함수 테스트", () => {
         const roundNum = 0
-
-        expect(() => Validations.validateRound(roundNum)).toThrow(CONSTANT.ERROR_ROUND)
+        expect(() => Validations.checkRound(roundNum)).toThrow(Constants.ERROR_ROUND)
     });
 
-    test("경기 라운드 유효성 검사_validateRound()", () => {
+    test("라운드가 음수이면 에러를 던지는 함수 테스트", () => {
         const roundNum = -1
-
-        expect(() => Validations.validateRound(roundNum)).toThrow(CONSTANT.ERROR_ROUND)
+        expect(() => Validations.checkRound(roundNum)).toThrow(Constants.ERROR_ROUND)
     });
  
-
 })
