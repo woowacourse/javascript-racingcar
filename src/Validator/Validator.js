0041d1a1fd9df70c236f4ddb9eca94abc27a2742
Dain Lee <feb.dain@gmail.com>
2023-02-12 00:28:58 +0900

    refactor: edit and add condition, make code more readable
    used regex as constant and rename method, parameter

--- src/Validator/Validator.js
+++ src/Validator/Validator.js
@@ -1,53 +1,60 @@
 const {
-  ERROR_MESSAGE,
-  MAXIMUM_CAR_NAME_LENGTH,
-  MINIMUM_TRY_COUNT,
   MINIMUM_NUMBER_OF_CARS,
-} = require("../Utils/Constants");
+  ERROR_MESSAGE,
+  CAR_NAME_LENGTH,
+  REGEX,
+  MINIMUM_TRY_COUNT,
+} = require("../Constants/Constants");
 const {
-  NAME_LENGTH_LIMIT,
-  NOT_BLANK,
-  INVALID_NAME,
-  NAME_DUPLICATE,
   MINIMUM_CAR_COUNT,
+  NAME_DUPLICATE,
+  NAME_LENGTH_LIMIT,
+  INVALID_NAME,
+  NOT_BLANK,
   NOT_POSITIVE_NUMBER,
 } = ERROR_MESSAGE;
+const { MINIMUM_CAR_NAME_LENGTH, MAXIMUM_CAR_NAME_LENGTH } = CAR_NAME_LENGTH;
+const { ALPHABET_VALID_KOREAN_NUMBER, BLANK, ONLY_NUMBER } = REGEX;
 
 class Validator {
-  carName(name) {
-    if (name.length > MAXIMUM_CAR_NAME_LENGTH) {
-      throw new Error(NAME_LENGTH_LIMIT);
-    }
-    if (/[^a-z|A-Z|0-9|가-힣]/g.test(name)) {
-      throw new Error(INVALID_NAME);
-    }
-  }
-
-  checkDuplicate(names) {
-    if (names.length !== new Set(names).size) {
-      throw new Error(NAME_DUPLICATE);
-    }
-  }
-
-  hasBlank(string) {
-    if (/ /g.test(string)) {
-      throw new Error(NOT_BLANK);
-    }
-  }
-
-  isNumber(number) {
-    if (isNaN(number) || number.includes("e")) {
-      throw new Error(NOT_POSITIVE_NUMBER);
-    }
-  }
-
-  numberOfCars(number) {
+  isEnoughNumberOfCars(number) {
     if (number < MINIMUM_NUMBER_OF_CARS) {
       throw new Error(MINIMUM_CAR_COUNT);
     }
   }
 
-  tryCount(count) {
+  hasDuplicate(names) {
+    if (names.length !== new Set(names).size) {
+      throw new Error(NAME_DUPLICATE);
+    }
+  }
+
+  isValidCarName(name) {
+    if (
+      name.length < MINIMUM_CAR_NAME_LENGTH ||
+      MAXIMUM_CAR_NAME_LENGTH < name.length
+    ) {
+      throw new Error(NAME_LENGTH_LIMIT);
+    }
+
+    if (ALPHABET_VALID_KOREAN_NUMBER.test(name)) {
+      throw new Error(INVALID_NAME);
+    }
+  }
+
+  hasBlank(name) {
+    if (BLANK.test(name)) {
+      throw new Error(NOT_BLANK);
+    }
+  }
+
+  isNumber(number) {
+    if (ONLY_NUMBER.test(number)) {
+      throw new Error(NOT_POSITIVE_NUMBER);
+    }
+  }
+
+  isValidTryCount(count) {
     this.isNumber(count);
 
     if (count < MINIMUM_TRY_COUNT) {
@@ -58,19 +65,19 @@ class Validator {
 
 const validator = new Validator();
 
-const inputCarNameValidator = (cars) => {
-  validator.numberOfCars(cars.length);
-  validator.checkDuplicate(cars);
+const inputCarNameValidator = (carNames) => {
+  validator.isEnoughNumberOfCars(carNames.length);
+  validator.hasDuplicate(carNames);
 
-  cars.forEach((car) => {
-    validator.carName(car);
-    validator.hasBlank(car);
+  carNames.forEach((name) => {
+    validator.hasBlank(name);
+    validator.isValidCarName(name);
   });
 };
 
 const tryCountValidator = (count) => {
   validator.hasBlank(count);
-  validator.tryCount(count);
+  validator.isValidTryCount(count);
 };
 
 module.exports = { inputCarNameValidator, tryCountValidator };
