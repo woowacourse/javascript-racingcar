a4d01ed209e62363afd79490dff14ea330c670c2
hae-on <solbi2004@naver.com>
2023-02-12 01:06:48 +0900

    refactor: mocking 이용한 test 삭제 & blank validation test 추가

--- __tests__/Input.test.js
+++ __tests__/Input.test.js
@@ -1,40 +1,32 @@
 /* eslint-disable no-undef */
-const App = require('../src/index.js');
-const IO = require('../src/utils/IO.js');
 const {
   validateCarNames,
   validateTryCount,
 } = require('../src/utils/InputValidator.js');
 const { ERROR_MESSAGE } = require('../src/data/constants.js');
 
-const mockQuestions = (answers) => {
-  IO.read = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((_, callback) => {
-      callback(input);
-    });
-  }, IO.read);
-};
-
 describe('Input Test', () => {
-  const app = new App();
-
-  test('Input test', () => {
-    expect(() => {
-      app.init();
-      mockQuestions(['pobi', '5']);
-    }).not.toThrow();
-  });
-
   test('Car name length validation test', () => {
     expect(() => {
       validateCarNames(['pobisss', 'crongsss', 'honuxss']);
     }).toThrow(ERROR_MESSAGE.CAR_NAME_LENGTH_ERROR);
   });
 
+  test('Car name blank validation test', () => {
+    expect(() => {
+      validateCarNames(['']);
+    }).toThrow(ERROR_MESSAGE.BLANK_ERROR);
+  });
+
   test('Try count validation test', () => {
     expect(() => {
       validateTryCount('가');
     }).toThrow(ERROR_MESSAGE.TRY_COUNT_TYPE_ERROR);
+  });
+
+  test('Try count blank validation test', () => {
+    expect(() => {
+      validateTryCount('');
+    }).toThrow(ERROR_MESSAGE.BLANK_ERROR);
   });
 });
