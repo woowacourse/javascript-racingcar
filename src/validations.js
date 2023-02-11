4fec3f151119ada97ae2fa947708aae58b024417
NaveOWO <pianowell96@gmail.com>
2023-02-11 17:54:55 +0900

    [ Style ] constants 선언명 변경

--- src/validations.js
+++ src/validations.js
@@ -1,26 +1,26 @@
-const CONSTANT = require("./Constant");
+const Constants = require("./Constants");
 
 const Validations = {
 
     checkCarMaxName(carNames) {
         carNames.forEach(name => {
-            if (name.length > CONSTANT.CAR_NAME_MAX) {
-                throw new Error(CONSTANT.ERROR_CAR_LENGTH)
+            if (name.length > Constants.CAR_NAME_MAX) {
+                throw new Error(Constants.ERROR_CAR_LENGTH)
             } 
         });
     },
 
     checkCarMinName(carNames) {
         carNames.forEach(name => {
-            if (name.length <= CONSTANT.CAR_NAME_MIN) {
-                throw new Error(CONSTANT.ERROR_CAR_NONAME)
+            if (name.length <= Constants.CAR_NAME_MIN) {
+                throw new Error(Constants.ERROR_CAR_NONAME)
             }
         });
     },
 
     checkRound(num) {
         if (!Number.isInteger(num) || num <= 0) {
-            throw new Error(CONSTANT.ERROR_ROUND);
+            throw new Error(Constants.ERROR_ROUND);
         }
     }
 }
