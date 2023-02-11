b59a418c3da0870adbb974514be1c48ff281e896
guridaek <iue16@naver.com>
2023-02-11 19:00:22 +0900

    test: Validator 테스트 수정

--- __tests__/CarRaceValidator.test.js
+++ __tests__/CarRaceValidator.test.js
@@ -1,12 +1,12 @@
 const { RACE_ERROR_MESSAGE } = require('../src/Constant/ErrorMessage');
-const Validator = require('../src/Utils/Validator');
+const CarRaceValidator = require('../src/Models/CarRaceValidator');
 
-describe('Validator 테스트', () => {
+describe('CarRaceValidator 테스트', () => {
   test.each([[['garam'], [''], ['garam', 'garam']]])(
     '자동차 이름이 한개 이하거나 중복될 경우 예외처리한다.',
     input => {
       expect(() => {
-        Validator.validateNamesOfCars(input);
+        CarRaceValidator.validateNamesOfCars(input);
       }).toThrow(RACE_ERROR_MESSAGE.numberOfNames);
     },
   );
@@ -15,31 +15,22 @@ describe('Validator 테스트', () => {
     '이름이 5자를 초과하는 경우 예외처리한다.',
     input => {
       expect(() => {
-        Validator.validateCarName(input);
+        CarRaceValidator.validateCarName(input);
       }).toThrow(RACE_ERROR_MESSAGE.lengthOfName);
-    },
-  );
-
-  test.each([[' '], ['!'], [' '], ['123'], ['한123']])(
-    '자동차 이름이 한글이나 영문이 아닌 경우 예외처리한다.',
-    input => {
-      expect(() => {
-        Validator.validateCarName(input);
-      }).toThrow(RACE_ERROR_MESSAGE.invalidInput);
     },
   );
 
   test.each([[0], [-1]])('시도 횟수가 1이상이 아니면 예외처리한다', input => {
     expect(() => {
-      Validator.validateTryCount(input);
+      CarRaceValidator.validateTryCount(input);
     }).toThrow(RACE_ERROR_MESSAGE.rangeOfTryCount);
   });
 
   test.each([['aa'], [' '], ['한']])(
-    '시도 횟수가 1이상이 아니면 예외처리한다',
+    '시도 횟수가 숫자가 아니면 예외처리한다',
     input => {
       expect(() => {
-        Validator.validateTryCount(input);
+        CarRaceValidator.validateTryCount(input);
       }).toThrow(RACE_ERROR_MESSAGE.rangeOfTryCount);
     },
   );
