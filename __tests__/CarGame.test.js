5df5fa10b51b55100eaa59972430758b81912152
hozzijeong <ghwls0905@naver.com>
2023-02-11 16:21:34 +0900

    test: Refactor winner test and remove initialize car game status test

--- __tests__/CarGame.test.js
+++ __tests__/CarGame.test.js
@@ -1,26 +1,22 @@
+const Car = require("../src/Car");
 const CarGame = require("../src/CarGame");
 
 describe("자동차 게임 기능 단위 테스트", () => {
-  const game = new CarGame();
-  test("자동차 상태 초기화 테스트", () => {
-    const carNames = ["야미", "클린", "레고"];
-    game.initializeCarStatus(carNames);
-
-    expect(game.getCarStatus()).toEqual({
-      야미: 0,
-      클린: 0,
-      레고: 0,
-    });
-  });
-
   test("우승자 반환 테스트", () => {
-    const carStatus = {
-      야미: 2,
-      클린: 3,
-      레고: 3,
-    };
+    const game = new CarGame();
+    const carNames = [
+      ["야미", 2],
+      ["클린", 3],
+      ["레고", 3],
+    ];
+    const testStatus = new Map();
+    for (const [name, move] of carNames) {
+      const car = new Car(name);
+      car.move(move);
+      testStatus.set(name, car);
+    }
 
-    const winner = game.findWinner(carStatus);
+    const winner = game.findWinner([...testStatus.values()]);
 
     expect(winner).toEqual("클린,레고");
   });
