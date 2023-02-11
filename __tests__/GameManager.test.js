bd80c5f4b6150bdd3903339894e9c633797b8a96
2yunseong <dbsdltjd3701@naver.com>
2023-02-11 17:54:08 +0900

    test: 차 이름 입력이 유효하지 않으면 에러를 던지는 지 테스트

--- __tests__/GameManager.test.js
+++ __tests__/GameManager.test.js
@@ -1,3 +1,12 @@
 /* eslint-disable no-undef */
 
-describe('GameManager Test', () => {});
+import GameManager from '../src/domain/GameManager';
+
+describe('GameManager Test', () => {
+  test('차 이름 입력이 유효하지 않으면 에러를 던지는 지 테스트', () => {
+    const gameMananger = new GameManager();
+    const badNamesInput = 'abcdef,bad guy,nononono';
+
+    expect(() => gameMananger.checkCarNames(badNamesInput)).toThrow();
+  });
+});
