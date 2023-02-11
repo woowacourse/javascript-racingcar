0bb5911c7b635b8f97587fe9ba3e0ad447efb908
Dain Lee <feb.dain@gmail.com>
2023-02-11 23:08:54 +0900

    refactor: make code more readable

--- src/UI/InputView.js
+++ src/UI/InputView.js
@@ -1,11 +1,12 @@
-const { GAME_MESSAGE } = require("../Utils/Constants");
 const Utils = require("../Utils/Utils");
+const { GAME_MESSAGE } = require("../Constants");
 const { INPUT_CAR_NAME, INPUT_TRY_COUNT } = GAME_MESSAGE;
 
 const InputView = {
   readCarName(callback) {
     Utils.readLine(INPUT_CAR_NAME, callback);
   },
+
   readTryCount(callback) {
     Utils.readLine(INPUT_TRY_COUNT, callback);
   },
