d27854c03b6c61f1bc01bcb1ee012981a136a2a4
gyeongza <gyeongza@naver.com>
2023-02-11 17:07:53 +0900

    fix: 입력값 에러시 재시도를 하지 않고 다음 코드로 넘어가는 버그 수정

--- src/UI/InputView.js
+++ src/UI/InputView.js
@@ -7,7 +7,7 @@ const InputView = {
     let input;
     Console.readLine(Messages.INPUT_CAR_NAMES, (answer) => {
       input = answer.split(Settings.SEPARATOR).map((carName) => carName.trim());
-      if (Validator.isValidCarNames(input)) InputView.getCarNames(racingGame);
+      if (Validator.isValidCarNames(input)) return InputView.getCarNames(racingGame);
       racingGame.setCarList(input);
       InputView.getAttempts(racingGame);
     });
@@ -17,7 +17,7 @@ const InputView = {
     let attempts;
     Console.readLine(Messages.INPUT_ATTEMPTS, (answer) => {
       attempts = answer.trim();
-      if (Validator.isValidAttempts(attempts)) InputView.getAttempts(racingGame);
+      if (Validator.isValidAttempts(attempts)) return InputView.getAttempts(racingGame);
       racingGame.setAttempts(attempts);
       racingGame.playGame();
       Console.close();
