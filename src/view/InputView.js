e7896c310c6a65ff59b29c4077a75ce0a621aee7
sh981013s <sh981013s@gmail.com>
2023-02-11 19:21:01 +0900

    refactor(InputView): 사용자 편의를 위해 입력 시 대사 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,7 +3,7 @@ const Console = require('../utils/Console');
 const InputView = {
   readCarNames(callback) {
     Console.readLine(
-      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
+      '경주할 1~5 글자로 이루어진 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
       callback,
     );
   },
