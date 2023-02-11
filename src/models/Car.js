55509f5abf60629db8e7cfef7163881cd7905b59
cruelladevil <dev.timetravel@gmail.com>
2023-02-11 21:06:48 +0900

    feat: Car 객체 validation 추가

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,11 +1,27 @@
-class Car {
-  #name;
+import { ErrorMessages } from '../constants/Messages';
 
+class Car {
+  static MIN_CAR_NAME_LENGTH = 1;
+  static MAX_CAR_NAME_LENGTH = 5;
+
+  #name;
   #position;
 
   constructor(name) {
+    Car.#validateCarName(name);
+
     this.#name = name;
     this.#position = 0;
+  }
+
+  static #validateCarName(name) {
+    if (!Car.#isValidLength(name)) {
+      throw new Error(ErrorMessages.CAR_NAME_LENGTH_LIMIT);
+    }
+  }
+
+  static #isValidLength(name) {
+    return name.length >= Car.MIN_CAR_NAME_LENGTH && name.length <= Car.MAX_CAR_NAME_LENGTH;
   }
 
   getRaceState() {
