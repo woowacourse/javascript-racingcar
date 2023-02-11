d25f9a8d67e6cfc4ee0087cad7de53a2b5362f09
ukkodeveloper <ukkodeveloper@gmail.com>
2023-02-11 17:19:01 +0900

    fix: pass error object when reject, not just message string

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,6 +1,12 @@
+
+
 class OutputView {
   printResult(result) {
     console.log(result);
+  }
+
+  printError(error) {
+    console.log(error.message)
   }
 }
 
--- src/view/inputView.js
+++ src/view/inputView.js
@@ -13,7 +13,7 @@ class InputView {
         input => {
           const carNames = input.split(',');
           if (!this.validateCarNames(carNames)) {
-            reject('다섯글자 이하의 자동차를 중복없이 입력해주세요.');
+            reject(new Error('[ERROR] 다섯글자 이하의 자동차를 중복없이 입력해주세요.\n'));
           }
 
           const carMap = carNames.reduce((acc, cur) => {
@@ -41,7 +41,7 @@ class InputView {
       rl.question('시도할 회수는 몇회인가요?\n', input => {
         const trialCount = Number(input);
         if (isNaN(trialCount)) {
-          reject(new Error('숫자만 입력 가능합니다.'));
+          reject(new Error('[ERROR] 숫자만 입력 가능합니다.'));
         }
         resolve(trialCount);
       });
