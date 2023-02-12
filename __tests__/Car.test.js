0a851af1186cffa8667b4f33194b4944e4fd830d
dladncks1217 <dlaxodud1217@gmail.com>
2023-02-12 21:05:46 +0900

    test(Car, Utils): Car객체 테스트 추가, 유틸함수들(getObjectsName, inputDataParseAndSplit) 테스트 코드 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,9 +1,20 @@
-const Car = require('../src/models/Car');
+const Car = require('../src/domain/Car');
 
 describe('Car 테스트', () => {
   test('4이상이면 자동차가 이동한다.', () => {
     const car = new Car('pobi');
     expect(car.move(3)).toBe(0);
     expect(car.move(4)).toBe(1);
+  });
+
+  test('4이상이면 자동차가 이동한다.', () => {
+    const car = new Car('pobi');
+    car.move(5);
+    expect(car.getState()).toBe(1);
+  });
+
+  test('', () => {
+    const car = new Car('pobi');
+    expect(car.getName()).toBe('pobi');
   });
 });
