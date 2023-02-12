bc962be45f7b7eb89a5426fb0b1623b03eba17a4
yeopto <gunyeop530@gmail.com>
2023-02-13 00:45:11 +0900

    refactor: printWinners()를 수정하고 printErrorMessage()를 추가하다

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,26 +1,24 @@
 const RACE_MESSAGE = require('../Constant/RaceMessage');
 const Console = require('../Utils/Console');
-const DataParseUtils = require('../Utils/DataParseUtils');
 
 const OutputView = {
   printResultMessage() {
     Console.print(RACE_MESSAGE.raceResult);
   },
 
+  printErrorMessage(message) {
+    Console.print(message);
+  },
+
   printRaceResult(result) {
     result.forEach((value, key) => {
-      Console.print(
-        `${key} : ${'-'.repeat(value)}`,
-      );
+      Console.print(`${key} : ${'-'.repeat(value)}`);
     });
     Console.print('');
   },
 
-  printWinners(result) {
-    const winners = DataParseUtils.parseWinner(result);
-    Console.print(
-      `${winners.join(', ')}${RACE_MESSAGE.winnerResult}`,
-    );
+  printWinners(winners) {
+    Console.print(`${winners.join(', ')}${RACE_MESSAGE.winnerResult}`);
     Console.close();
   },
 };
