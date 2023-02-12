262879da3756c46f2576c402a03eba5004542645
chex <chex417@naver.com>
2023-02-13 02:22:33 +0900

    style(constants): 맞춤법 수정

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ const CAR = Object.freeze({
 
 const INPUT_MESSAGE = Object.freeze({
   ENTER_CAR_NAMES: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
-  ENTER_TRY_COUNT: '시도할 회수는 몇회인가요?\n',
+  ENTER_TRY_COUNT: '시도할 횟수는 몇회인가요?\n',
 });
 
 const OUTPUT_MESSAGE = Object.freeze({
