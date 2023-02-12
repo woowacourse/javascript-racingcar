0d50eb04d08dd11638f17d421ba76ad66df8df32
chsua <csj1919@naver.com>
2023-02-12 17:10:02 +0900

    [ Style ] Constant 변수명 변경에 따른 수정

--- __tests__/Run.test.js
+++ __tests__/Run.test.js
@@ -3,11 +3,11 @@ const Car = require("../src/Car");
 
 describe("전진하는 함수 테스트", () => {
     test("인수(랜덤값)가 전진 기준값 이상인지 확인하여 논리갑 return_이상인 경우", () => {
-        expect(Run.goStop(4)).toEqual(true);
+        expect(Run.isGoStop(4)).toEqual(true);
     });
 
     test("인수(랜덤값)가 전진 기준값 이상인지 확인하여 논리갑 return_이하인 경우", () => {
-        expect(Run.goStop(3)).toEqual(false);
+        expect(Run.isGoStop(3)).toEqual(false);
     });
 
     test("전진 기준값 이상이면 자동차 객체의 score+1 처리", () => {
@@ -16,7 +16,7 @@ describe("전진하는 함수 테스트", () => {
             //valOne = 0, valTwo = 9
             return valTwo;
         };
-        Run.isCarGo(car, fakeRandomFunction);
+        Run.randomNumMakeCarGo(car, fakeRandomFunction);
         const score = car.exportNameScore()[1];
 
         expect(score).toEqual(1);
@@ -28,7 +28,7 @@ describe("전진하는 함수 테스트", () => {
             //valOne = 0, valTwo = 9
             return valOne;
         };
-        Run.isCarGo(car, fakeRandomFunction);
+        Run.randomNumMakeCarGo(car, fakeRandomFunction);
         const score = car.exportNameScore()[1];
 
         expect(score).toEqual(0);
