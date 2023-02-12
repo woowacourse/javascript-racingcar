1d2a930ac34012d8a8212f475c0f97677fbbcd38
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-02-12 15:26:35 +0900

    refactor(Input.test): mockQuestion 함수 제거

--- __tests__/Input.test.js
+++ __tests__/Input.test.js
@@ -7,24 +7,8 @@ const {
 } = require('../src/utils/InputValidator.js');
 const { ERROR_MESSAGE } = require('../src/data/constants.js');
 
-const mockQuestions = (answers) => {
-  IO.read = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((_, callback) => {
-      callback(input);
-    });
-  }, IO.read);
-};
-
 describe('Input Test', () => {
   const app = new App();
-
-  test('Input test', () => {
-    expect(() => {
-      app.init();
-      mockQuestions(['pobi', '5']);
-    }).not.toThrow();
-  });
 
   test('Car name length validation test', () => {
     expect(() => {
