f3cc9f9ad8c9f7d7ecef50e6590bd8ddf368ef80
hozzijeong <ghwls0905@naver.com>
2023-02-12 19:35:13 +0900

    test: Add try count validate test case that NaN, undefiend, null and decimalNumber

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,6 +1,7 @@
-import Validator from "../src/Validator/Validator.js";
-
-const { inputCarNameValidator, tryCountValidator } = Validator;
+import {
+  inputCarNameValidator,
+  tryCountValidator,
+} from "../src/Validator/Validator.js";
 
 describe("예외 테스트", () => {
   test.each([
@@ -20,7 +21,20 @@ describe("예외 테스트", () => {
     },
   );
 
-  test.each(["-1", "백 번", "0", "DD", "0.1", "..", " ", "2e3"])(
+  test.each([
+    "-1",
+    "백 번",
+    "0",
+    "DD",
+    "0.1",
+    "..",
+    " ",
+    "2e3",
+    "2.3",
+    NaN,
+    undefined,
+    null,
+  ])(
     "시도 횟수에 양수가 아닌 값을 입력하지 않으면 예외가 발생한다.",
     (tryCount) => {
       expect(() => tryCountValidator(tryCount)).toThrow(Error);
