c65de86d204716768512a1c8ba52fd5057ce8e4f
ashleysyheo <ashleysyheo@gmail.com>
2023-02-12 21:06:36 +0900

    refactor: 변동 가능성 있는 값 상수 처리

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,3 +1,4 @@
+const { StaticValue } = require('../src/constants/constants');
 const Car = require('../src/model/Car');
 
 describe('Car 클래스 테스트', () => {
@@ -10,7 +11,7 @@ describe('Car 클래스 테스트', () => {
     });
   });
 
-  test('4이상의 숫자를 입력받았을 때 자동차 인스턴스의 distance 필드가 하나 증가는지 확인', () => {
+  test(`${StaticValue.MOVE_CONDITION}이상의 숫자를 입력받았을 때 자동차 인스턴스의 distance 필드가 하나 증가는지 확인`, () => {
     const CAR = new Car('우형합격');
     const RANDOM_NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     const EXPECTED_DISTANCES = [0, 0, 0, 0, 1, 2, 3, 4, 5, 6];
