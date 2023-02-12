b595163b1b5967e4bfef2b1cf37098fcb6fb6cb7
Suyoung <jsy8492@gmail.com>
2023-02-12 17:14:02 +0900

    refactor(RacingGameTest): 임시변수 제거 및 변수명 수정

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -9,25 +9,23 @@ describe('자동차 경주 게임을 진행하는 클래스 테스트', () => {
 
   describe('경주 우승자를 도출하는 기능 테스트', () => {
     test('우승자가 한 명인 경우 우승자 한 명의 이름이 담긴 배열을 반환한다.', () => {
-      const randomNumbers = [5, 2, 1];
+      const conditionNumbers = [5, 2, 1];
 
       racingGame.getCars().forEach((car, idx) => {
-        car.tryProgress(randomNumbers[idx]);
+        car.tryProgress(conditionNumbers[idx]);
       });
 
-      const winners = racingGame.getWinners();
-      expect(winners).toEqual(['pobi']);
+      expect(racingGame.getWinners()).toEqual(['pobi']);
     });
 
     test('우승자가 두 명 이상인 경우 모든 우승자의 이름이 담긴 배열을 반환한다.', () => {
-      const randomNumbers = [6, 7, 2];
+      const conditionNumbers = [6, 7, 2];
 
       racingGame.getCars().forEach((car, idx) => {
-        car.tryProgress(randomNumbers[idx]);
+        car.tryProgress(conditionNumbers[idx]);
       });
 
-      const winners = racingGame.getWinners();
-      expect(winners).toEqual(['pobi', 'crong']);
+      expect(racingGame.getWinners()).toEqual(['pobi', 'crong']);
     });
   });
 });
