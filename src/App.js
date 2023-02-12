df5cde651cdf6a5ae807a83132f909fb8f89b14a
semnil5202 <semnil5202@gmail.com>
2023-02-13 02:11:41 +0900

    refactor: view를 외부에서 주입하여 의존성 분리로 보다 유연한 유지보수 가능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 const RacingGameController = require('./controller/RacingGameController');
 const RacingGameService = require('./service/RacingGameService');
+const InputView = require('./view/InputView');
+const OutputView = require('./view/OutputView');
 
 class App {
   start() {
-    new RacingGameController(new RacingGameService()).playGame();
+    const view = { input: InputView, output: OutputView };
+    new RacingGameController(new RacingGameService(), view).playGame();
   }
 }
 
--- src/controller/RacingGameController.js
+++ src/controller/RacingGameController.js
@@ -1,19 +1,21 @@
-const InputView = require('../view/InputView');
-const OutputView = require('../view/OutputView');
 const { GAME, INPUT, OUTPUT, OUTPUT_RESULT, OUTPUT_WINNER } = require('../constant/constants');
 const { validateCarNames, validateWinningDistance } = require('../validation/input.js');
 const common = require('../utils/common');
 
 class RacingGameController {
   #service;
+  #input;
+  #output;
   #winningDistance;
 
-  constructor(service) {
+  constructor(service, view) {
     this.#service = service;
+    this.#input = view.input;
+    this.#output = view.output;
   }
 
   async playGame() {
-    OutputView.print(OUTPUT.startGame);
+    this.#output.print(OUTPUT.startGame);
     await this.setCars();
     await this.setWinningDistance();
     this.setCars();
@@ -22,14 +24,14 @@ class RacingGameController {
   }
 
   async setCars() {
-    const input = await InputView.readline(INPUT.carName);
+    const input = await this.#input.readline(INPUT.carName);
     const carNames = input.split(GAME.nameDivider);
     if (!validateCarNames(carNames)) return this.setCars();
     this.#service.makeCars(carNames);
   }
 
   async setWinningDistance() {
-    this.#winningDistance = common.toInt(await InputView.readline(INPUT.winningDistance));
+    this.#winningDistance = common.toInt(await this.#input.readline(INPUT.winningDistance));
     if (!validateWinningDistance(this.#winningDistance)) return this.setWinningDistance();
   }
 
@@ -51,7 +53,7 @@ class RacingGameController {
   }
 
   #showResult() {
-    OutputView.print(OUTPUT.resultMent);
+    this.#output.print(OUTPUT.resultMent);
     this.#service.getCarsMoveRecord().forEach((history) => {
       this.showCarsMoveHistory(history);
     });
@@ -60,14 +62,14 @@ class RacingGameController {
 
   showCarsMoveHistory(history) {
     history.forEach((car) => {
-      OutputView.print(OUTPUT_RESULT(car));
+      this.#output.print(OUTPUT_RESULT(car));
     });
-    OutputView.print(GAME.newLine);
+    this.#output.print(GAME.newLine);
   }
 
   showWinners() {
     const winners = this.#service.getCars().filter((car) => this.pickOutWinner(car));
-    OutputView.print(OUTPUT_WINNER(winners));
+    this.#output.print(OUTPUT_WINNER(winners));
   }
 }
 module.exports = RacingGameController;
