5bc767ef3175cc29066c89e04152d64e5866ec61
hozzijeong <ghwls0905@naver.com>
2023-02-12 19:51:53 +0900

    test: Add car maxposition test case

--- __tests__/CarGame.test.js
+++ __tests__/CarGame.test.js
@@ -16,8 +16,27 @@ describe("자동차 게임 기능 단위 테스트", () => {
       testStatus.set(name, car);
     }
 
-    const winner = game.findWinner([...testStatus.values()]);
+    const winner = game.getWinnerNames([...testStatus.values()], 3);
 
     expect(winner).toEqual("클린,레고");
+  });
+
+  test("제일 멀리 간 자동차 위치 확인 테스트", () => {
+    const game = new CarGame();
+    const carNames = [
+      ["야미", 2],
+      ["클린", 3],
+      ["레고", 3],
+    ];
+    const testStatus = new Map();
+    for (const [name, move] of carNames) {
+      const car = new Car(name);
+      car.move(move);
+      testStatus.set(name, car);
+    }
+
+    const maxPosition = game.getMaxPosition([...testStatus.values()]);
+
+    expect(maxPosition).toBe(3);
   });
 });
