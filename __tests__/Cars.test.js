54edde78307efa1cdd1f8a5e0ff838abf3d0280f
yeopto <gunyeop530@gmail.com>
2023-02-12 21:54:31 +0900

    test: Race 테스트 코드를 Car 테스트 코드로 바꾸다

--- __tests__/Cars.test.js
+++ __tests__/Cars.test.js
@@ -1,22 +1,19 @@
 const Car = require('../src/Models/Car');
-const Race = require('../src/Models/Race');
+const Cars = require('../src/Models/Cars');
 const RandomNumGenerator = require('../src/Utils/RandomNumGenerator');
 
 const mockRandoms = numbers => {
   RandomNumGenerator.generateNumber = jest.fn();
-  numbers.reduce(
-    (acc, number) => acc.mockReturnValueOnce(number),
-    RandomNumGenerator.generateNumber,
-  );
+  numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), RandomNumGenerator.generateNumber);
 };
 
 test('Race Class 테스트', () => {
   // given
-  const cars = [];
+  const tempCars = [];
   ['garam', 'yeop'].forEach(name => {
-    cars.push(new Car(name));
+    tempCars.push(new Car(name));
   });
-  const race = new Race(cars);
+  const cars = new Cars(tempCars);
   const tryCount = 3;
   mockRandoms([1, 2, 3, 4, 5, 6]);
   const result = new Map([
@@ -26,9 +23,9 @@ test('Race Class 테스트', () => {
 
   // when
   for (let i = 0; i < tryCount; i += 1) {
-    race.go();
+    cars.race();
   }
 
   // then
-  expect(race.getResult()).toEqual(result);
+  expect(cars.getRaceResult()).toEqual(result);
 });
