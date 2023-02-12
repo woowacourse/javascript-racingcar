a3b8a7f27a62db173e528087a83235926655ec13
chex <chex417@naver.com>
2023-02-13 02:01:39 +0900

    test(CarGame): '테스트 코드에서 jest.fn()을 사용하지 않는다'는 리팩터링 요구사항에 따라 해당 코드 삭제

--- __tests__/CarGame.test.js
+++ __tests__/CarGame.test.js
@@ -6,22 +6,4 @@ describe('CarGame 테스트', () => {
     carGame.initCarList(['pobi', 'crong', 'jun']);
     expect(carGame.getCarNames()).toEqual(['pobi', 'crong', 'jun']);
   });
-
-  test('우승자 리스트를 반환한다.', () => {
-    const carGame = new CarGame();
-
-    const getRandomNumber = jest.fn();
-
-    getRandomNumber.mockReturnValueOnce(1);
-    getRandomNumber.mockReturnValueOnce(6);
-    getRandomNumber.mockReturnValueOnce(7);
-    getRandomNumber.mockReturnValueOnce(8);
-    getRandomNumber.mockReturnValueOnce(3);
-    getRandomNumber.mockReturnValueOnce(5);
-
-    carGame.initCarList(['pobi', 'crong', 'jun']);
-    carGame.moveCars(2, getRandomNumber);
-
-    expect(carGame.getWinners()).toEqual(['jun']);
-  });
 });
