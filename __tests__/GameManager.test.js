696e4c17100381b8e733fd170415a740b5e371a2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-13 01:10:49 +0900

    fix: 로직 변경으로 인한 테스트 실패 해결

--- __tests__/GameManager.test.js
+++ __tests__/GameManager.test.js
@@ -1,27 +1,32 @@
 /* eslint-disable max-lines-per-function */
 /* eslint-disable no-undef */
-const GameManager = require('../src/GameManager');
+const GameManager = require('../src/controller/GameManager');
 
 describe('GameManager Test', () => {
 
   test.each([
-    [['yun', 'park', 'kim'], [9, 0, 1, 8, 1, 2, 7, 5, 6], ['yun']],
+    [
+      ['yun', 'park', 'kim'], 
+      [[9, 0, 1], [8, 1, 2], [7, 5, 6]], 
+      ['yun']
+    ],
     [
       ['choi', 'ann', 'lee', 'gabi'],
-      [0, 0, 5, 5, 0, 0, 9, 9],
+      [[0, 0, 5, 5], [0, 0, 9, 9]],
       ['lee', 'gabi'],
     ],
     [
       ['aa', 'bb'],
-      [0, 0, 5, 5, 0, 0, 9, 9],
+      [[0, 0], [5, 5], [0, 0], [9, 9]],
       ['aa', 'bb'],
     ],
   ])('승자를 판별하는 테스트 (%#)', (carNames, moves, winners) => {
     const gameManager = new GameManager();
     const cars = gameManager.generateCars(carNames);
-    // mockRandoms(moves);
     gameManager.setCars(cars);
-    gameManager.raceCars(moves.length/cars.length);
+    moves.forEach((numbers)=>{
+      gameManager.moveCars(numbers);
+    })
     expect(gameManager.judgeWinners()).toEqual(winners);
   });
 });
