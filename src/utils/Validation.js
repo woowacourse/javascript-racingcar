e58bb91af1bdf71b1dce16c9d8e9fbf25da6cde8
2yunseong <dbsdltjd3701@naver.com>
2023-02-12 17:30:29 +0900

    fix: 서로게이트 쌍 처리를 위한 유효성 검증 로직 수정

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -2,15 +2,25 @@ import constants from './constants.js';
 
 const Validation = {
   isValidCarNames(carNames) {
-    return carNames.every(
-      (name) =>
-        name.length >= constants.MIN_CAR_NAME_LENGTH &&
-        name.length <= constants.MAX_CAR_NAME_LENGTH
-    );
+    return carNames.every((carName) => {
+      let count = 0;
+      // eslint-disable-next-line no-unused-vars
+      for (const char of carName) {
+        count += 1;
+      }
+      return this.isValidCarNameLength(count);
+    });
   },
 
   isValidTryCount(tryCount) {
     return Number.isInteger(tryCount) && tryCount >= constants.MIN_TRY_COUNT;
+  },
+
+  isValidCarNameLength(carNameLength) {
+    return (
+      carNameLength >= constants.MIN_CAR_NAME_LENGTH &&
+      carNameLength <= constants.MAX_CAR_NAME_LENGTH
+    );
   },
 };
 export default Validation;
