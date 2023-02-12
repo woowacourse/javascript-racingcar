54b95b0b257accd663fed17c77dba9f999b37947
cruelladevil <dev.timetravel@gmail.com>
2023-02-12 17:32:05 +0900

    feat: Race 객체에 raceStep 필드 추가

--- src/domain/Race.js
+++ src/domain/Race.js
@@ -1,26 +1,45 @@
-import Random from '../utils/Random';
 import Car from './Car';
+import { randomNumberBetween } from '../util/Random';
 
 class Race {
   static RANDOM_NUMBER_MIN = 0;
   static RANDOM_NUMBER_MAX = 9;
   static MOVE_CONDITION = 4;
 
+  static STEP_IS_NOT_INTEGER = '시도할 횟수는 정수여야합니다.';
+  static STEP_IS_NOT_POSITIVE = '시도할 횟수는 양의 정수를 입력해주세요.';
+
   #cars;
+  #raceStep;
 
   constructor(carNames) {
     this.#cars = carNames.map((name) => new Car(name));
   }
 
+  setRaceStep(raceStep) {
+    Race.#validateRaceStep(raceStep);
+
+    this.#raceStep = raceStep;
+  }
+
+  static #validateRaceStep(raceStep) {
+    if (!Number.isInteger(raceStep)) {
+      throw new Error(Race.STEP_IS_NOT_INTEGER);
+    }
+
+    if (Number(raceStep) < 0) {
+      throw new Error(Race.STEP_IS_NOT_POSITIVE);
+    }
+  }
+
   moveOnce() {
     this.#cars.forEach((car) => {
-      const randomNumber = Random.randomNumberBetween(
-        Race.RANDOM_NUMBER_MIN,
-        Race.RANDOM_NUMBER_MAX,
-      );
+      const randomNumber = randomNumberBetween(Race.RANDOM_NUMBER_MIN, Race.RANDOM_NUMBER_MAX);
 
       if (randomNumber >= Race.MOVE_CONDITION) car.move();
     });
+
+    this.#raceStep -= 1;
   }
 
   getCars() {
