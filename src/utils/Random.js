a54a934c377c8ea46c2fd443df42cf71497a6fc4
regularPark <srk99279@naver.com>
2023-02-12 19:44:43 +0900

    refactor: generateRandomNumber를 객체에서 함수로 변경

--- src/utils/Random.js
+++ src/utils/Random.js
@@ -1,14 +1,19 @@
 const MakeRandomNumber = require("./MakeRandomNumber");
+const { MIN_RANDOM, MAX_RANDOM } = require("../constants");
 
 const Random = {
   makeRandomNumbers(countOfTrial) {
     const race = [];
     while (countOfTrial) {
-      const randomNumber = MakeRandomNumber.getRandomNUmber();
+      const randomNumber = this.generateRandomNumber();
       race.push(randomNumber);
       countOfTrial -= 1;
     }
     return race;
+  },
+
+  generateRandomNumber() {
+    return Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
   },
 };
 
