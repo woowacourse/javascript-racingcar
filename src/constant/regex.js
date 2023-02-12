c676e0e6eee4205eed8b82a5bcb34427f1a3357e
turtle601 <turtle601@naver.com>
2023-02-13 01:41:48 +0900

    ♻️ refactor(regex): 사용하지 않는 정규식 제거

--- src/constant/regex.js
+++ src/constant/regex.js
@@ -1,9 +1,6 @@
 const REGEX = Object.freeze({
   carName: /^[1-9|a-z|A-Z|가-힣]+$/,
   onlyNumbers: /^[0-9]+$/,
-  korean: /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+$/,
-  english: /^[a-zA-Z]+$/,
-  number: /^[0-9]+$/,
 });
 
 module.exports = {
