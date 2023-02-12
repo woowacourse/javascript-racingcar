27fc070debd6b625bc59fad91408dac0f6d4ff07
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-12 22:00:02 +0900

    🪛 fix(InputValidator): validation 버그 수정

--- src/constant/magicNumber.js
+++ src/constant/magicNumber.js
@@ -1,10 +1,9 @@
-const MAGIC_NUMBER = Object.freeze({
-    RANGE_START: 0,
-    RANGE_END: 9,
-    VALID_LENGTH_OF_NAME: 5,
-  });
-  
-  module.exports = {
-    MAGIC_NUMBER,
-  };
-  
+const RANGE_START = 0;
+const RANGE_END = 9;
+const VALID_LENGTH_OF_NAME = 5;
+
+module.exports = {
+  RANGE_START,
+  RANGE_END,
+  VALID_LENGTH_OF_NAME,
+};
