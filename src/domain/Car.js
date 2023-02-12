ecc19ddeaa55d1b5102a5c25cdd6ad604abcf5dc
cruelladevil <dev.timetravel@gmail.com>
2023-02-12 16:53:26 +0900

    style: 필드명, 메서드명 변경

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,8 +1,8 @@
 class Car {
-  static MIN_CAR_NAME_LENGTH = 1;
-  static MAX_CAR_NAME_LENGTH = 5;
+  static MIN_NAME_LENGTH = 1;
+  static MAX_NAME_LENGTH = 5;
 
-  static VALID_NAME_LENGTH = `자동차의 이름은 최소${Car.MIN_CAR_NAME_LENGTH}자, 최대${Car.MAX_CAR_NAME_LENGTH}자입니다.`;
+  static VALID_NAME_LENGTH = `자동차의 이름은 최소${Car.MIN_NAME_LENGTH}자, 최대${Car.MAX_NAME_LENGTH}자입니다.`;
 
   #name;
   #position;
@@ -15,13 +15,13 @@ class Car {
   }
 
   static #validateCarName(name) {
-    if (!Car.#isValidLength(name)) {
+    if (!Car.#isValidNameLength(name)) {
       throw new Error(Car.VALID_NAME_LENGTH);
     }
   }
 
-  static #isValidLength(name) {
-    return name.length >= Car.MIN_CAR_NAME_LENGTH && name.length <= Car.MAX_CAR_NAME_LENGTH;
+  static #isValidNameLength(name) {
+    return name.length >= Car.MIN_NAME_LENGTH && name.length <= Car.MAX_NAME_LENGTH;
   }
 
   getRaceState() {
