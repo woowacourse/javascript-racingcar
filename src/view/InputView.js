0174a6567cd6390957ddafb8a8aa366771a7e747
cruelladevil <dev.timetravel@gmail.com>
2023-02-12 17:08:33 +0900

    refactor: InputView와 관련된 상수를 InputView의 프로퍼티로 옮김

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,16 +1,18 @@
-import { Messages } from '../constants/Messages';
 import Console from '../utils/Console';
 
 const InputView = {
+  CAR_NAMES_QUESTION: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
+  RACE_STEP_QUESTION: '시도할 회수는 몇회인가요?',
+
   async readCarNames() {
-    const carnamesString = await Console.readline(Messages.READ_CAR_NAMES);
+    const carnamesString = await Console.readline(InputView.CAR_NAMES_QUESTION);
     const carNames = carnamesString.split(',');
 
     return carNames;
   },
 
   async readRaceStep() {
-    const raceStepString = await Console.readline(Messages.READ_RACE_STEP);
+    const raceStepString = await Console.readline(InputView.RACE_STEP_QUESTION);
     const raceStep = Number(raceStepString);
 
     return raceStep;
