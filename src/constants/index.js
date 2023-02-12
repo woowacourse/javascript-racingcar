058df97318d73d7565e6d00eefe72b6c661dfc99
inyeong-kang <tomatorkd@naver.com>
2023-02-13 03:36:57 +0900

    refactor(constants): 최종 우승자에 대한 게임 메시지를 보여주는 함수에서 return 제거

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,9 +27,7 @@ const GAME_MESSAGE = Object.freeze({
     '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
   askAttempts: '시도할 회수는 몇회인가요?\n',
   progressResult: '\n실행 결과',
-  getWinners: (names) => {
-    return `${names}가 최종 우승했습니다.`;
-  },
+  getWinners: (names) => `${names}가 최종 우승했습니다.`,
 });
 
 const ERROR_MESSAGE = Object.freeze({
