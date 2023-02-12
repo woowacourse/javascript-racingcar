0aa3c1418f551735ae2f1309649be3bd95b8ea13
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-12 20:10:44 +0900

    ✅ test(Car): test 경우의 명칭이 조건과 결과가 잘 드러나도록 수정

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,36 +1,21 @@
 const Random = require('../src/utils/Random');
 const Car = require('../src/domain/Car');
 
-const mockMathRandoms = (number) => {
-  Math.random = jest.fn();
-
-  return Math.random.mockReturnValueOnce(number);
-};
-
 describe('Car - move() 기능 테스트', () => {
-  test('(전진 경우)', () => {
-    jest.useFakeTimers();
+  test('move()의 인자가 4 이상의 숫자이면 전진한다.', () => {
     const name = 'test';
     const car = new Car(name);
 
-    mockMathRandoms(0.523);
-
-    const result = Random.pickNumberInRange(0, 9);
-
-    car.move(result);
+    car.move(4);
 
     expect(car.getPosition()).toEqual(2);
   });
 
-  test('(전진하지 않는 경우)', () => {
-    jest.useFakeTimers();
+  test('move()의 인자가 3 이하의 숫자이면 전진하지 않는다.', () => {
     const name = 'test';
     const car = new Car(name);
 
-    mockMathRandoms(0.123);
-
-    const result = Random.pickNumberInRange(0, 9);
-    car.move(result);
+    car.move(3);
 
     expect(car.getPosition()).toEqual(1);
   });
