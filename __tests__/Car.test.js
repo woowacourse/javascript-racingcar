a600370a0f6b293a32c122649f3f36d9d3c20a2f
WaiNaat <qoli0703@gmail.com>
2023-02-12 16:24:14 +0900

    style: 개행 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -4,12 +4,14 @@ describe('자동차 이동 테스트', () => {
   test.each([3, 2])('이동 불가', (power) => {
     const car = new Car('A');
     car.move(power);
+
     expect(car.getPosition()).toEqual(1);
   });
 
   test.each([4, 9])('한칸 이동', (power) => {
     const car = new Car('A');
     car.move(power);
+
     expect(car.getPosition()).toEqual(2);
   });
 });
