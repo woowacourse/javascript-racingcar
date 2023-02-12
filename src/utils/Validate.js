88cc0efe3fc33624e83cab57951c0231abc6d94f
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-12 17:00:18 +0900

    ♻️ refactor: 상수인 객체의 key값 대문자 처리

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -6,13 +6,13 @@ class Validate {
       throw new Error('[ERROR] 이름은 5자 이하여야 합니다.');
     }
 
-    if (!(REGEX.korean.test(carName) || REGEX.english.test(carName))) {
+    if (!(REGEX.KOREAN.test(carName) || REGEX.ENGLISH.test(carName))) {
       throw new Error('[ERROR] 문자만 입력해야 합니다.');
     }
   }
 
   static checkTrial(trial) {
-    if (!REGEX.number.test(trial)) {
+    if (!REGEX.NUMBER.test(trial)) {
       throw new Error('[ERROR] 숫자만 입력해야 합니다.');
     }
   }
