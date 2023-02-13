cd3aba14a1be56995374048d4de9dff4941dcf26
nlom0218 <nlom0218@naver.com>
2023-02-13 17:28:11 +0900

    test: 자동차 이름 유효성 검사 테스트 코드 추가하기

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,8 +1,9 @@
-const { MESSAGE } = require('../src/lib/Constant.js');
-const Validator = require('../src/model/Validator.js');
+import Validator from '../src/domain/Validator.js';
+import { MESSAGE } from '../src/util/Constant.js';
 
 describe('사용자가 올바른 자동차 이름을 입력하지 않을 경우, 에러 메시지를 호출한다.', () => {
   test.each([
+    ['aa,bb,', MESSAGE.error.exist],
     ['aaa,b b,ccc', MESSAGE.error.blank],
     ['aaa,bbbbbb,ccc', MESSAGE.error.nameLength],
     ['aaa,bbb,Ccc', MESSAGE.error.lowerCase],
