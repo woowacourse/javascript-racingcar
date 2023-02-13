b7c8e282c01ecf6d85672d63379935957cedeca9
guridaek <iue16@naver.com>
2023-02-13 15:11:11 +0900

    test: 테스트 수정

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,16 +1,28 @@
+const { RACE_ERROR_MESSAGE } = require('../src/Constant/ErrorMessage');
 const Car = require('../src/Models/Car');
 
-test('Car Class 테스트', () => {
-  // given
-  const car = new Car('garam');
-  const numbers = [1, 3, 4, 5, 6, 2, 0];
-  const resultPosition = 3;
+describe('Car Class 테스트', () => {
+  test('move 테스트', () => {
+    // given
+    const car = new Car('garam');
+    const numbers = [1, 3, 4, 5, 6, 2, 0];
+    const resultPosition = 3;
 
-  // when
-  numbers.forEach(number => {
-    car.move(number);
+    // when
+    numbers.forEach(number => {
+      car.move(number);
+    });
+
+    // then
+    expect(car.position).toEqual(resultPosition);
   });
 
-  // then
-  expect(car.position).toEqual(resultPosition);
+  test.each([['yeopto'], ['garame']])(
+    '이름이 5자를 초과하는 경우 예외처리한다.',
+    input => {
+      expect(() => {
+        new Car(input);
+      }).toThrow(RACE_ERROR_MESSAGE.lengthOfName);
+    },
+  );
 });
--- __tests__/CarRace.test.js
+++ __tests__/CarRace.test.js
@@ -1,3 +1,4 @@
+const { RACE_ERROR_MESSAGE } = require('../src/Constant/ErrorMessage');
 const Car = require('../src/Models/Car');
 const CarRace = require('../src/Models/CarRace');
 const RandomNumGenerator = require('../src/Utils/RandomNumGenerator');
@@ -10,21 +11,47 @@ const mockRandoms = numbers => {
   );
 };
 
-test('CarRace Class 테스트', () => {
-  const cars = ['garam', 'yeop'].map(name => new Car(name));
-  const carRace = new CarRace(cars);
-  const tryCount = 3;
-  mockRandoms([1, 2, 3, 4, 5, 6]);
-  const result = new Map([
-    ['garam', 1],
-    ['yeop', 2],
-  ]);
+describe('CarRace Class 테스트', () => {
+  test('동작 테스트', () => {
+    const cars = ['garam', 'yeop'].map(name => new Car(name));
+    const carRace = new CarRace(cars);
+    const tryCount = 3;
+    mockRandoms([1, 2, 3, 4, 5, 6]);
+    const result = new Map([
+      ['garam', 1],
+      ['yeop', 2],
+    ]);
 
-  // when
-  for (let i = 0; i < tryCount; i += 1) {
-    carRace.runOnce();
-  }
+    // when
+    for (let i = 0; i < tryCount; i += 1) {
+      carRace.runOnce();
+    }
 
-  // then
-  expect(carRace.getResult()).toEqual(result);
+    // then
+    expect(carRace.getResult()).toEqual(result);
+  });
+
+  test.each([[['garam'], [''], ['garam', 'garam']]])(
+    '자동차 이름이 한개 이하거나 중복될 경우 예외처리한다.',
+    input => {
+      expect(() => {
+        CarRace.validateCars(input);
+      }).toThrow(RACE_ERROR_MESSAGE.numberOfNames);
+    },
+  );
+
+  test.each([[0], [-1]])('시도 횟수가 1이상이 아니면 예외처리한다', input => {
+    expect(() => {
+      CarRace.validateTryCount(input);
+    }).toThrow(RACE_ERROR_MESSAGE.rangeOfTryCount);
+  });
+
+  test.each([['aa'], [' '], ['한']])(
+    '시도 횟수가 숫자가 아니면 예외처리한다',
+    input => {
+      expect(() => {
+        CarRace.validateTryCount(input);
+      }).toThrow(RACE_ERROR_MESSAGE.rangeOfTryCount);
+    },
+  );
 });
