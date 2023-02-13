e936a40284667d3d9c50c5e90538ad4d2f30a83b
yogjin <youngjin988@gmail.com>
2023-02-13 17:02:29 +0900

    test: validation test에서 mock함수(loySpy)제거, throw여부를 테스트 하는 것으로 수정
    1. `%s`, `%i`을 사용하여 각 테스트 요소를 테스트 메시지에 드러냄
    2. `logspy` 목함수를 지우고 각 `check`메서드의 throw 여부를 테스트 하는 것으로 수정

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -3,72 +3,56 @@ const { GAME, ERROR } = require('../src/utils/constants');
 const OutputView = require('../src/view/OutputView');
 const { toInt } = require('../src/utils/common');
 
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(OutputView, 'print');
-  return logSpy;
-};
-
-const getOutput = (logSpy) => {
-  return [...logSpy.mock.calls].join('');
-};
-
 describe('입력 validation 테스트', () => {
-  const logSpy = getLogSpy();
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   describe('자동차 이름 입력 테스트 (terminalInputValidator.validateCarNames)', () => {
     test.each([
       [['a'.repeat(GAME.CAR_NAME.min)], ''],
       [['a'.repeat(GAME.CAR_NAME.max)], ''],
-      [['a'.repeat(GAME.CAR_NAME.max + 1)], ERROR.carNameLength],
     ])(
-      `자동차 이름의 길이는 ${GAME.CAR_NAME.min}, ${GAME.CAR_NAME.max} 사이여야 한다.`,
-      (carNames, errorMessage) => {
-        terminalInputValidator.validateCarNames(carNames);
-        const log = getOutput(logSpy);
-
-        expect(log).toEqual(errorMessage);
+      `자동차 이름의 길이가 ${GAME.CAR_NAME.min}이상, ${GAME.CAR_NAME.max}이하 라면 통과한다. - 이름: %s`,
+      (carNames) => {
+        expect(() => terminalInputValidator.checkCarNameLength(carNames)).not.toThrow();
       },
     );
 
-    test('중복된 자동차 이름이 들어오면 안된다.', () => {
+    test(`자동차 이름의 길이가 ${GAME.CAR_NAME.min}미만, ${GAME.CAR_NAME.max}초과라면 에러를 던진다. - 이름: %s`, () => {
+      expect(() =>
+        terminalInputValidator.checkCarNameLength(['a'.repeat(GAME.CAR_NAME.max + 1)]),
+      ).toThrow();
+    });
+
+    test('중복된 자동차 이름이 들어오면 에러를 던진다.', () => {
       const carNames = ['pobi', 'pobi'];
 
       terminalInputValidator.validateCarNames(carNames);
-      const log = getOutput(logSpy);
 
-      expect(log).toEqual(ERROR.duplicatedCarName);
+      expect(() => terminalInputValidator.checkDuplicatedCarName(carNames)).toThrow();
     });
 
-    test('자동차 이름에 공백이 있으면 안된다.', () => {
-      const carNames = ['a b', 'cr on', ' ', ' hey', 'hey '];
+    test.each([['a b'], ['cr on'], [' '], [' hey'], ['hey ']])(
+      `자동차 이름에 공백이 있으면 에러를 던진다. - "%s"`,
+      (carNames) => {
+        terminalInputValidator.validateCarNames(carNames);
 
-      terminalInputValidator.validateCarNames(carNames);
-      const log = getOutput(logSpy);
-
-      expect(log).toEqual(ERROR.blankInCarName);
-    });
+        expect(() => terminalInputValidator.checkBlankInCarName(carNames)).toThrow();
+      },
+    );
   });
 
   describe('시도 횟수 입력 테스트 (terminalInputValidator.validateWinningDistance)', () => {
     test.each(['a', 'ㄱ', '!', '~', '@', '#', '$', '%', '^', '&', '*', '(', ' '])(
-      '숫자가 아닌 값을 입력하면 안된다.',
+      `숫자가 아닌 값을 입력하면 에러를 던진다. - "%s"`,
       (input) => {
         terminalInputValidator.validateWinningDistance(toInt(input));
-        const log = getOutput(logSpy);
 
-        expect(log).toEqual(ERROR.invalidWinningDistanceType);
+        expect(() => terminalInputValidator.checkIsInt(carNames)).toThrow();
       },
     );
 
-    test.each([2, 10])('입력값의 범위가 3미만 10이상이면 안된다.', (input) => {
+    test.each([2, 10])('입력값의 범위가 3미만 10이상이면 에러를 던진다. - 입력값: %i', (input) => {
       terminalInputValidator.validateWinningDistance(input);
-      const log = getOutput(logSpy);
 
-      expect(log).toEqual(ERROR.invalidWinningDistanceRange);
+      expect(() => terminalInputValidator.checkIsBetweenValidRange(carNames)).toThrow();
     });
   });
 });
