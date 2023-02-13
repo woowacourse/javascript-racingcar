dd2a93b02a1ebc996970db4b318c95470c544727
NaveOWO <pianowell96@gmail.com>
2023-02-13 12:39:20 +0900

    [ Style ] 테스트 이름 수정

--- __tests__/Validations.test.js
+++ __tests__/Validations.test.js
@@ -2,22 +2,22 @@ const Validations = require("../src/utils/Validations");
 
 describe("input의 유효성검사에 대한 단위 테스트", () => {
     
-    test("차 이름의 길이가 5이상이면 에러를 던지는 함수 테스트", () => {
+    test("차 이름의 길이가 5이상이면 false를 반환하는 함수 테스트", () => {
         const carNames = ['nave','naveowo'];
         expect(Validations.isCarNameUnderMax(carNames)).toBe(false);
     });
 
-    test("차 이름이 1자 미만이면 에러를 던지는 함수 테스트", () => {
+    test("차 이름이 1자 미만이면 false를 반환하는 함수 테스트", () => {
         const carNames = ['naveowo', 'nave', ''];
         expect(Validations.isCarNameOverMin(carNames)).toBe(false);
     });
 
-    test("라운드가 0이면 에러를 던지는 함수 테스트", () => {
+    test("라운드가 0이면 false를 반환하는 함수 테스트", () => {
         const roundNum = 0;
         expect(Validations.isCorrectRoundNumber(roundNum)).toBe(false);
     });
 
-    test("라운드가 음수이면 에러를 던지는 함수 테스트", () => {
+    test("라운드가 음수이면 false를 반환하는 함수 테스트", () => {
         const roundNum = -1;
         expect(Validations.isCorrectRoundNumber(roundNum)).toBe(false);
     });
