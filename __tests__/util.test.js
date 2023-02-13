a626c0136510db8dd7af75ab44552e82eb19d24d
yogjin <youngjin988@gmail.com>
2023-02-13 16:57:33 +0900

    test: jest.fn() 사용한 mockRandoms 삭제
    mock함수를 이용하지 않으면 randomGenerator 테스트 의미가 없다고 생각하여 삭제

--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -1,13 +1,5 @@
 const { deepFreeze, randomGenerator } = require('../src/utils/common');
 
-const mockRandoms = (numbers) => {
-  Math.random = jest.fn();
-
-  numbers.reduce((acc, cur) => {
-    return acc.mockReturnValueOnce(cur);
-  }, Math.random);
-};
-
 describe('유틸 함수 테스트', () => {
   test('deepFreeze(object): 객체 object를 변경할 수 없게 동결한다.', () => {
     const deepObject = {
@@ -23,23 +15,5 @@ describe('유틸 함수 테스트', () => {
     };
 
     expect(() => freezeObject()).toThrowError(TypeError);
-  });
-
-  describe('randomGenerator', () => {
-    test.each([
-      [3, 10],
-      [-1, 10],
-      [0, 1],
-      [-30, -2],
-      [11, 23],
-    ])('getBetween($min, $max): $min 이상 $max 미만인 랜덤 숫자를 반환한다.', (min, max) => {
-      mockRandoms([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]);
-
-      for (let i = 0; i < 10; i++) {
-        const randomNumber = randomGenerator.getBetween(min, max);
-        const answer = randomNumber >= min && randomNumber < max;
-        expect(answer).toEqual(true);
-      }
-    });
   });
 });
