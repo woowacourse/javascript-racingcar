f24991886fbc1935d4914b70f722750846c7d3ec
TaeeunKim <xodms0309@naver.com>
2023-02-13 14:05:30 +0900

    refactor: 테스트 케이스에 차 움직임 여부 상수화

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,17 +1,18 @@
 const Car = require('../src/domain/Car');
+const { NOT_MOVED, MOVE_FORWARD } = require('../src/constants');
 describe('자동차 움직임 테스트', () => {
   const car = new Car('타미');
   test.each([0, 3])(
     '0~3 사이의 값이 들어오면 자동차는 움직이지 않는다. %#',
     (testCase) => {
-      expect(car.move(testCase)).toBe('');
+      expect(car.move(testCase)).toBe(NOT_MOVED);
     }
   );
 
   test.each([4, 9])(
     '4~9 사이의 값이 들어오면 자동차가 전진한다. %#',
     (testCase) => {
-      expect(car.move(testCase)).toBe('-');
+      expect(car.move(testCase)).toBe(MOVE_FORWARD);
     }
   );
 });
