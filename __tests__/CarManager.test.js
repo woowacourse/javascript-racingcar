372ee921ed11fbbda4b77a4fad54af20657f6e6e
sh981013s <sh981013s@gmail.com>
2023-02-14 02:10:52 +0900

    refactor(CarMagagerTest): 테스트 시 app (최상단 컨트롤러) 사용을 carManager 사용으로 스코프 단순화

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -1,44 +1,44 @@
-const App = require('../src/App');
+import OverallRacingGameManager from '../src/domain/CarManager';
 
 describe('자동차를 관리하는 클래스 테스트', () => {
-  const app = new App();
+  let carManager;
+  let cars;
+
+  beforeEach(() => {
+    carManager = new OverallRacingGameManager(['pobi', 'crong', 'honux']);
+    carManager.createInitialCars(['pobi', 'crong', 'honux']);
+    cars = carManager.getCars();
+  });
 
   const initializeBasicTestSettingWithRandomNumbers = (randomNumbers) => {
-    const cars = app.carManager.getCars();
-
     for (let i = 0; i < cars.length; i++) {
       cars[i].tryProgress(randomNumbers[i]);
     }
   };
 
-  beforeEach(() => {
-    app.carManager.createInitialCars(['pobi', 'crong', 'honux']);
-  });
-
   test('자동차 관리자 인스턴스를 생성한다.', () => {
-    expect(app.carManager.getCars().length).toEqual(3);
+    expect(cars.length).toEqual(3);
   });
 
   test('자동차 각각을 전진시킨다', () => {
     initializeBasicTestSettingWithRandomNumbers([5, 2, 1]);
 
-    expect(app.carManager.getCars().map((singleCar) => singleCar.progressCount)).toEqual([
-      1, 0, 0,
-    ]);
+    expect(cars.map((car) => car.progressCount)).toEqual([1, 0, 0]);
   });
 
   describe('경주 우승자를 도출한다.', () => {
     test('우승자가 한 명인 경우', () => {
       initializeBasicTestSettingWithRandomNumbers([5, 2, 1]);
 
-      const winners = app.carManager.getWinners();
+      const winners = carManager.getWinners();
+      console.log(carManager.accumulatedProgress());
       expect(winners.length === 1 && winners[0] === 'pobi').toBe(true);
     });
 
     test('우승자가 두 명 이상인 경우', () => {
       initializeBasicTestSettingWithRandomNumbers([6, 7, 2]);
 
-      const winners = app.carManager.getWinners();
+      const winners = carManager.getWinners();
       expect(winners).toEqual(['pobi', 'crong']);
     });
   });
