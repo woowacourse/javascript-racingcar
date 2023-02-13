81387aa557322bf84896a7dd168cb63551ea48f3
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-13 11:52:00 +0900

    refactor(constant): Object.freeze 추가
    - 상수에 있는 값들은 바뀔 일이 없으므로 Object.freeze 추가 (다른 개발자에게도 알리기 위함)

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -1,30 +1,30 @@
-const MESSAGES = {
+const MESSAGES = Object.freeze({
   carText: "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n",
   repeatNumber: "시도할 회수는 몇회인가요?\n",
   resultTitle: "\n실행 결과",
   resultEnd: "가 최종 우승했습니다,\n",
   carTextError: "[ERROR] 자동차 이름은 5자 이하로 입력해주세요.\n",
   repeatRangeError: "[ERROR] 횟수는 자연수로 입력해주세요.\n",
-};
+});
 
-const RANDOMS = {
+const RANDOMS = Object.freeze({
   maximumNumber: 10,
   minimumRunNumber: 4,
-};
+});
 
-const RESULT = {
+const RESULT = Object.freeze({
   run: 1,
   stay: 0,
-};
+});
 
-const CONDITION = {
+const CONDITION = Object.freeze({
   carNames: /^([^,]{1,5},)*[^,]{1,5}$/,
   repeatNumber: /^[1-9]\d*$/,
-};
+});
 
-module.exports = {
+module.exports = Object.freeze({
   MESSAGES,
   RANDOMS,
   RESULT,
   CONDITION,
-};
+});
