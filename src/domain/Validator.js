4d91612407bf65682ec5c86c0553f97f0746bb8e
nlom0218 <nlom0218@naver.com>
2023-02-13 13:17:31 +0900

    refactor: 유효성 검사 메서드 이름 수정하기
    이름을 통해 해당 메서드가 하는 역할을 알 수 있도록 한다.

--- src/domain/Validator.js
+++ src/domain/Validator.js
@@ -4,47 +4,50 @@ const Validator = {
   carName(string) {
     const names = string.split(',');
 
-    if (!this.isVaildCarNameExsit(names))
-      throw new Error('[ERROR] 자동차 이름은 공백일 수 없습니다.');
+    if (!this.isVaildCarNameExist(names))
+      throw new Error('[ERROR] 자동차 이름은 존재하지 않습니다.');
 
-    if (this.isCarNameHasBlank(string)) throw new Error(MESSAGE.error.blank);
+    if (!this.isValidCarNameSpacing(string))
+      throw new Error(MESSAGE.error.blank);
 
-    if (this.isCarNameGreaterThanFive(names))
+    if (!this.isValidCarNameLength(names))
       throw new Error(MESSAGE.error.nameLength);
 
-    if (!this.isCarNameLowerCase(names))
+    if (!this.isValidCarNameCharacter(names))
       throw new Error(MESSAGE.error.lowerCase);
 
-    if (this.isDuplicated(names)) throw new Error(MESSAGE.error.duplicatedName);
+    if (!this.isValidCarNameDuplicated(names))
+      throw new Error(MESSAGE.error.duplicatedName);
   },
 
   tryCount(number) {
-    if (!this.isNumeric(number)) throw new Error(MESSAGE.error.numeric);
+    if (!this.isValidTryCountNumeric(number))
+      throw new Error(MESSAGE.error.numeric);
   },
 
-  isVaildCarNameExsit(names) {
+  isVaildCarNameExist(names) {
     return names.every((name) => name !== '');
   },
 
-  isCarNameHasBlank(string) {
-    return string.search(/\s/g) !== -1;
+  isValidCarNameSpacing(string) {
+    return string.search(/\s/g) === -1;
   },
 
-  isCarNameGreaterThanFive(names) {
-    return names.some((name) => name.length > 5);
+  isValidCarNameLength(names) {
+    return names.every((name) => name.length <= 5);
   },
 
-  isCarNameLowerCase(names) {
+  isValidCarNameCharacter(names) {
     return names.every((name) => {
       return name.search(/[^a-z]/g) === -1;
     });
   },
 
-  isDuplicated(names) {
-    if (names.length !== new Set(names).size) return true;
+  isValidCarNameDuplicated(names) {
+    if (names.length === new Set(names).size) return true;
   },
 
-  isNumeric(number) {
+  isValidTryCountNumeric(number) {
     if (number === '0') return false;
 
     return number.search(/[^0-9]/g) === -1;
