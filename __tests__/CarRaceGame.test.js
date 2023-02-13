9143f51b586c0fd2905d656ce3c4ced74da3a003
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-13 17:50:45 +0900

    test(racegame): 테스트 코드 작성

--- __tests__/CarRaceGame.test.js
+++ __tests__/CarRaceGame.test.js
@@ -1,37 +1,160 @@
 /* eslint-disable */
 
 const CarRaceGame = require("../src/domain/CarRaceGame");
-const CarRaceResultRandomGenerator = require("../src/domain/CarRaceResultRandomGenerator");
-const mockedFunction = CarRaceResultRandomGenerator.generate;
-jest.mock("../src/domain/CarRaceResultRandomGenerator");
+const { RESULT } = require("../src/constant/Constant");
 
-describe("[CarRaceGame - updateRace] 현재 자동차의 이동 거리가 주어지면, 시뮬레이션 이후 올바른 이동 거리를 반환하여야 한다.", () => {
-  test("라운드 진행 시 거리 업데이트 테스트", () => {
-    mockedFunction.mockReturnValueOnce([1, 1, 1, 0, 0]);
-    expect(CarRaceGame.updateRace([3, 4, 5, 6, 7])).toEqual([4, 5, 6, 6, 7]);
+describe("[CarRaceGame] 자동차 개수 확인 테스트", () => {
+  test("자동차가 a, b, c일 경우, 자동차가 몇 대인지를 불러올 때 3을 가져와야 한다.", () => {
+    const game = new CarRaceGame();
+    game.createRaceUsingCarNames(["a", "b", "c"]);
+    expect(game.getCarCount()).toBe(3);
   });
 
-  test("라운드 첫 시작 시 거리 업데이트 테스트", () => {
-    mockedFunction.mockReturnValueOnce([1, 0, 1, 0, 1, 0, 1]);
-    expect(CarRaceGame.updateRace([0, 0, 0, 0, 0, 0, 0])).toEqual([1, 0, 1, 0, 1, 0, 1]);
+  test("자동차가 10대일 경우, 자동차가 몇 대인지를 불러올 때 10을 가져와야 한다.", () => {
+    const game = new CarRaceGame();
+    game.createRaceUsingCarNames([
+      "sZ0Xv",
+      "vPmWI",
+      "vJm",
+      "u",
+      "okQZ",
+      "vNM",
+      "wEq8",
+      "3M",
+      "-0V;",
+      "zK",
+    ]);
+    expect(game.getCarCount()).toBe(10);
   });
 
-  test("큰 숫자의 라운드 진행 시 거리 업데이트 테스트", () => {
-    mockedFunction.mockReturnValueOnce([1]);
-    expect(CarRaceGame.updateRace([1234567])).toEqual([1234568]);
+  test("자동차가 단 한 대일 경우, 1을 가져와야 한다.", () => {
+    const game = new CarRaceGame();
+    game.createRaceUsingCarNames(["Solo"]);
+    expect(game.getCarCount()).toBe(1);
   });
 });
 
-describe("[CarRaceGame - judgeWinners] 자동차의 이름과 거리에 대한 정보가 주어지면, 우승자를 올바르게 반환하여야 한다.", () => {
-  test("경기 종료 후 우승자 반환 테스트", () => {
-    const carNames = ["carA", "carB", "carC"];
-    const carDistances = [4, 5, 3];
-    expect(CarRaceGame.judgeWinners(carNames, carDistances)).toEqual("carB");
+describe("[CarRaceGame] 자동차 이동 거리 및 자동차 이름 테스트", () => {
+  test("자동차가 골고루 이동하는 케이스 - 이동 후 자동차 목록과 거리를 가져와야 한다.", () => {
+    const game = new CarRaceGame();
+    game.createRaceUsingCarNames(["carA", "carB", "carC"]);
+
+    game.performRaceOnce([RESULT.run, RESULT.run, RESULT.stay]);
+    game.performRaceOnce([RESULT.stay, RESULT.run, RESULT.stay]);
+    game.performRaceOnce([RESULT.run, RESULT.stay, RESULT.stay]);
+    game.performRaceOnce([RESULT.stay, RESULT.stay, RESULT.stay]);
+    game.performRaceOnce([RESULT.stay, RESULT.run, RESULT.stay]);
+
+    expect(game.getRaceResult()).toEqual({
+      carNames: ["carA", "carB", "carC"],
+      carDistances: [2, 3, 0],
+    });
   });
 
-  test("공동 우승자 반환 테스트", () => {
-    const carNames = ["carA", "carB", "carC"];
-    const carDistances = [5, 3, 5];
-    expect(CarRaceGame.judgeWinners(carNames, carDistances)).toEqual("carA, carC");
+  test("많은 자동차가 이동하는 케이스 - 모든 자동차가 한 번씩 이동하므로 결과는 모두 1이어야 한다.", () => {
+    const game = new CarRaceGame();
+    game.createRaceUsingCarNames([
+      "sZ0Xv",
+      "vPmWI",
+      "vJm",
+      "u",
+      "okQZ",
+      "vNM",
+      "wEq8",
+      "3M",
+      "-0V;",
+      "zK",
+    ]);
+
+    game.performRaceOnce([
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.stay,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.run,
+    ]);
+    game.performRaceOnce([
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.run,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.stay,
+    ]);
+
+    expect(game.getRaceResult()).toEqual({
+      carNames: ["sZ0Xv", "vPmWI", "vJm", "u", "okQZ", "vNM", "wEq8", "3M", "-0V;", "zK"],
+      carDistances: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    });
+
+    game.performRaceOnce([
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.run,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.stay,
+    ]);
+
+    expect(game.getRaceResult()).toEqual({
+      carNames: ["sZ0Xv", "vPmWI", "vJm", "u", "okQZ", "vNM", "wEq8", "3M", "-0V;", "zK"],
+      carDistances: [1, 2, 1, 2, 2, 2, 1, 2, 1, 1],
+    });
+  });
+
+  test("하나의 자동차만 이동하는 케이스 - 이동 거리는 15이어야 한다.", () => {
+    const game = new CarRaceGame();
+    game.createRaceUsingCarNames(["Solo"]);
+    const operations = [
+      RESULT.run,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.run,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.run,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.stay,
+      RESULT.stay,
+      RESULT.run,
+      RESULT.run,
+      RESULT.run,
+      RESULT.run,
+      RESULT.stay,
+    ];
+
+    console.log(operations[1]);
+
+    operations.forEach((currentOperation) => {
+      game.performRaceOnce([currentOperation]);
+    });
+
+    expect(game.getRaceResult()).toEqual({
+      carNames: ["Solo"],
+      carDistances: [15],
+    });
   });
 });
