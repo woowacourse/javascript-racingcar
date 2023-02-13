972c151007e854f67d0d82ee753f10097dce950f
afds4567 <afds4567@gmail.com>
2023-02-13 09:46:38 +0900

    refactor: 각Car  테스트 코드설명 구체화

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,22 +1,15 @@
 const { Car } = require("../src/Car");
 const { randomGenerator } = require("../src/randomGenerator");
 
-const mockRandoms = (numbers) => {
-  randomGenerator.generateNumber = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, randomGenerator.generateNumber);
-};
-
 describe("Car 객체 기능 검사", () => {
-  test("Car 객체 생성 테스트", () => {
+  test("입력 받은 자동차 이름 배열을 순회하면서 Car 객체를 생성한다.", () => {
     const car = new Car("testCar");
     const carInfo = car.getCarInfo();
     expect(carInfo.name).toEqual("testCar");
     expect(carInfo.moveCnt).toBe(0);
   });
 
-  test("Car 객체 move 테스트", () => {
+  test("car가 move()한 경우 car의 moveCnt가 1 증가한다.", () => {
     const car = new Car("testCar");
     car.move();
     const carInfo = car.getCarInfo();
