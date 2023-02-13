f5e44984821be36fb0269a740494d0c3cc0d6016
solo5star <solo_5@naver.com>
2023-02-13 16:34:15 +0900

    refactor: `Random`을 object literal 대신 `export function` 으로 변경

--- src/utils/Random.js
+++ src/utils/Random.js
@@ -1,7 +1,8 @@
-const Random = {
-  randomNumberBetween(inclusiveStart, exclusiveEnd) {
-    return inclusiveStart + Math.floor(Math.random() * exclusiveEnd);
-  },
-};
-
-export default Random;
+/**
+ * @param {number} inclusiveStart 생성할 랜덤 숫자 범위의 시작
+ * @param {number} exclusiveEnd 생성할 랜덤 숫자 범위의 끝. 끝은 포함하지 않는다
+ * @returns {number} 주어진 범위에서 생성된 랜덤 숫자
+ */
+export function randomNumberBetween(inclusiveStart, exclusiveEnd) {
+  return inclusiveStart + Math.floor(Math.random() * exclusiveEnd);
+}
