e0f141d9569b148fd677334ec7a65aed2c7f2461
Gilpop8663 <wolfye@naver.com>
2023-02-13 11:59:25 +0900

    fix(Car.test,Utils.test): 테스트 파일 이름과 어울리지 않던 코드 이동 및 수정

--- __tests__/Utils.test.js
+++ __tests__/Utils.test.js
@@ -1,16 +1,6 @@
-const { isMove } = require('../src/domain/model/Car');
 const { Random } = require('../src/utils');
 
 describe('utils 테스트', () => {
-  test.each([
-    [4, true],
-    [3, false],
-    [1.7, false],
-    [4.1, true],
-  ])('isMove 함수 테스트: %s일 때 %s 반환', (number, expected) => {
-    expect(isMove(number)).toBe(expected);
-  });
-
   test('Random.getCarGameNumber 함수 테스트', () => {
     Array.from({ length: 10 }).forEach(() => {
       const randomNumber = Random.getCarGameNumber();
