400e0d8284779d1086f73777b710f36be5f827f6
shackstack <kms1061@naver.com>
2023-02-14 20:05:00 +0900

    test: 자동차 이름 유효성 검사 테스트 추가

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -2,23 +2,52 @@ const { MESSAGE } = require("../src/util/Constant.js");
 const Validator = require("../src/domain/Validator.js");
 
 describe("자동차 이름 입력값에 대한 유효성 검사", () => {
-  test.each([
-    [" ", MESSAGE.error.blank],
-    ["aaa,b b,ccc", MESSAGE.error.blank],
-    ["aaa, ,ccc", MESSAGE.error.blank],
-    ["aaa,bbbbbb,ccc", MESSAGE.error.nameLength],
-    ["aaa,bbb,Ccc", MESSAGE.error.lowerCase],
-    ["aaa,aaa,ccc", MESSAGE.error.duplicatedName],
-    ["", MESSAGE.error.null],
-    ["aaa,,ccc", MESSAGE.error.null],
-  ])("자동차 이름에 대한 유효성 검사 테스트", (carNames, errorMessage) => {
-    expect(() => {
-      Validator.carName(carNames);
-    }).toThrow(errorMessage);
-  });
+  test.each([[" "], ["aaa,b b,ccc"], ["aaa, ,ccc"]])(
+    "자동차 이름에 공백이 포함될 경우 예외가 발생한다.",
+    (carNames) => {
+      expect(() => {
+        Validator.carName(carNames);
+      }).toThrow(MESSAGE.error.blank);
+    }
+  );
+
+  test.each([["aaa,bbbbbb,ccc"], ["aaaaaa,bbb,ccc"]])(
+    "자동차 이름의 길이가 4이하가 아닌경우 예외가 발생한다.",
+    (carNames) => {
+      expect(() => {
+        Validator.carName(carNames);
+      }).toThrow(MESSAGE.error.nameLength);
+    }
+  );
+
+  test.each([["aaa,bbb,Ccc"], ["aaa,bBb,ccc"]])(
+    "자동차 이름이 소문자로만 이루어지지 않은 경우 예외가 발생한다",
+    (carNames) => {
+      expect(() => {
+        Validator.carName(carNames);
+      }).toThrow(MESSAGE.error.lowerCase);
+    }
+  );
+
+  test.each([["aaa,aaa,ccc"], ["aaa,bbb,aaa"]])(
+    "자동차 이름이 중복된 경우 예외가 발생한다",
+    (carNames) => {
+      expect(() => {
+        Validator.carName(carNames);
+      }).toThrow(MESSAGE.error.duplicatedName);
+    }
+  );
+
+  test.each([[""], ["aaa,,ccc"]])(
+    "자동차 이름이 빈값인 경우 예외가 발생한다",
+    (carNames) => {
+      expect(() => {
+        Validator.carName(carNames);
+      }).toThrow(MESSAGE.error.null);
+    }
+  );
 });
 
-//수정필요할듯?
 describe("시도 횟수에 대한 유효성 검사", () => {
   test.each(["-2", "3.142592", "0"])(
     "양의 정수가 아닌 숫자가 입력되면 예외를 발생시킨다.",
