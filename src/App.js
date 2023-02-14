e2105c4671537474c92989fef2f40a410804efa9
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-02-15 00:04:04 +0900

    [2단계 - 자동차 경주 리팩터링] 제레미(김민석) 미션 제출합니다. (#218)
    * refactor(Validator): 함수 네이밍 수정
    
    * refactor(Validator): 자동차 이름길이에 대한 메서드 네이밍 수정
    
    * refactor(App): 메서드(createCarObject)  분리
    
    * docs: 2차 피드백 이후 리팩터링 및 기능추가
    
    * refactor: 컨트롤러 분리
    
    * test: 자동차 이름, 시도횟수에 대한 테스트 추가
    
    * test: 자동차 이름 유효성 검사 테스트 추가

--- src/App.js
+++ src/App.js
@@ -1,85 +1,15 @@
-const InputView = require("./view/InputView.js");
-const OutputView = require("./view/OutputView.js");
-const Validator = require("./domain/Validator.js");
-
-const Car = require("./domain/Car.js");
-const Console = require("./util/Console.js");
-const GameManager = require("./domain/GameManager.js");
+const Controller = require("./controller/Controller");
 
 class App {
-  #gameManager;
-  #cars = [];
-  #tryCount;
+  #controller;
 
   play() {
-    this.readCarName();
-  }
-
-  readCarName() {
-    InputView.readCarName((carName) => this.readCarNameCallback(carName));
-  }
-
-  readCarNameCallback(carNames) {
-    try {
-      Validator.carName(carNames);
-      this.createCarObject(carNames);
-      this.readTryCount();
-    } catch (error) {
-      OutputView.printErrorMessage(error);
-      this.readCarName();
-    }
-  }
-
-  createCarObject(carNames) {
-    carNames.split(",").forEach((carName) => {
-      this.#cars.push(new Car(carName));
-    });
-  }
-
-  readTryCount() {
-    InputView.readTryCount((tryCount) => this.readTryCountCallback(tryCount));
-  }
-
-  readTryCountCallback(tryCount) {
-    try {
-      Validator.tryCount(tryCount);
-      this.#tryCount = Number(tryCount);
-      this.moveCar();
-    } catch (error) {
-      OutputView.printErrorMessage(error);
-      this.readTryCount();
-    }
-  }
-
-  moveCar() {
-    this.#cars.forEach((car) => {
-      car.move(this.#tryCount);
-    });
-
-    this.printProcessResult();
-  }
-
-  printProcessResult() {
-    const carsStatus = this.#cars.map((car) => car.getStatus());
-    OutputView.printProcessResult(carsStatus, this.#tryCount);
-
-    this.printWinner(carsStatus);
-  }
-
-  printWinner(carsStatus) {
-    this.#gameManager = new GameManager(carsStatus);
-    const winner = this.#gameManager.winner;
-
-    OutputView.printWinner(winner);
-    this.quit();
-  }
-
-  quit() {
-    Console.close();
+    this.#controller = new Controller();
+    this.#controller.startGame();
   }
 }
 
+module.exports = App;
+
 const app = new App();
 app.play();
-
-module.exports = App;
