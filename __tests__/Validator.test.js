f5a535188abd88241bfec26f10039e6875411522
shackstack <kms1061@naver.com>
2023-02-14 10:00:16 +0900

    test: 자동차 이름, 시도횟수에 대한 테스트 추가

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -3,10 +3,13 @@ const Validator = require("../src/domain/Validator.js");
 
 describe("자동차 이름 입력값에 대한 유효성 검사", () => {
   test.each([
+    [" ", MESSAGE.error.blank],
     ["aaa,b b,ccc", MESSAGE.error.blank],
+    ["aaa, ,ccc", MESSAGE.error.blank],
     ["aaa,bbbbbb,ccc", MESSAGE.error.nameLength],
     ["aaa,bbb,Ccc", MESSAGE.error.lowerCase],
     ["aaa,aaa,ccc", MESSAGE.error.duplicatedName],
+    ["", MESSAGE.error.null],
     ["aaa,,ccc", MESSAGE.error.null],
   ])("자동차 이름에 대한 유효성 검사 테스트", (carNames, errorMessage) => {
     expect(() => {
@@ -18,7 +21,7 @@ describe("자동차 이름 입력값에 대한 유효성 검사", () => {
 //수정필요할듯?
 describe("시도 횟수에 대한 유효성 검사", () => {
   test.each(["-2", "3.142592", "0"])(
-    "%s가 입력되면 예외를 발생시킨다.",
+    "양의 정수가 아닌 숫자가 입력되면 예외를 발생시킨다.",
     (number) => {
       expect(() => {
         Validator.tryCount(number);
