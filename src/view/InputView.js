a07474f43c20c1e86671dd68a4674920a04ce06e
hanyoung <phanyoung@naver.com>
2024-02-14 20:03:02 +0900

    feat: InputView 구현(readCarNames, readRoundNumber 완성)
    Co-authored-by: Hain-tain <hain7ce@gmail.com>

---
+++ src/view/InputView.js
@@ -0,0 +1,13 @@
+import { readLineAsync } from "../utils/ConsoleUtil.js";
+
+export default class InputView {
+  static async readCarNames() {
+    return await readLineAsync(
+      "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)." + "\n"
+    );
+  }
+
+  static async readRoundNumber() {
+    return await readLineAsync("시도할 횟수는 몇 회인가요?" + "\n");
+  }
+}
