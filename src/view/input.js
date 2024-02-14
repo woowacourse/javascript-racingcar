b602e11127b86c0dd840801bed9f57c1ce4f01e4
GimGayeon <kky1373@naver.com>
2024-02-14 17:49:14 +0900

    feat: 시도 횟수 입력받기

--- src/view/input.js
+++ src/view/input.js
@@ -1,29 +1,30 @@
 import readLineAsync from "../readLineAsync.js";
 import Car from "../Car.js";
+import Validation from "../validation.js";
 
 const carNameInput = async () => {
   try {
     const carList = await readLineAsync(
       "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n"
     );
-
     const car = new Car(carList.split(","));
 
     return car.getCarList();
   } catch (error) {
-    console.log(error);
-    carNameInput();
+    console.log(error.message);
+    return carNameInput();
   }
 };
 
 const tryInput = async () => {
   try {
     const tryNumber = await readLineAsync("시도할 횟수는 몇번인가요?\n");
+    Validation.isNaturalNumber(tryNumber);
 
     return tryNumber;
   } catch (error) {
-    console.log(error);
-    tryInput();
+    console.log(error.message);
+    return tryInput();
   }
 };
 
