13c173eb36222091d9287a3a9366168fed39d103
hanyoung <phanyoung@naver.com>
2024-02-14 19:55:50 +0900

    feat: ConsoleUtil 구현(readLineAsync, print 완성)
    Co-authored-by: Hain-tain <hain7ce@gmail.com>

---
+++ src/utils/ConsoleUtil.js
@@ -0,0 +1,29 @@
+import readline from "readline";
+
+export default class ConsoleUtil {
+  static readLineAsync(query) {
+    return new Promise((resolve, reject) => {
+      if (arguments.length !== 1) {
+        reject(new Error("arguments must be 1"));
+      }
+
+      if (typeof query !== "string") {
+        reject(new Error("query must be string"));
+      }
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question(query, (input) => {
+        rl.close();
+        resolve(input);
+      });
+    });
+  }
+
+  print(query) {
+    console.log(query);
+  }
+}
