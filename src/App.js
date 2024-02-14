fb29e222a75163251f51bb3f3933d608b5bf44cc
ss0526100 <ss0526100@gmail.com>
2024-02-14 19:44:06 +0900

    feat: 실행결과 출력

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import readLineAsync from './utils/readLineAsync.js';
 import getRandomNumberInRange from './utils/getRandomNumberInRange.js';
 
 class App {
-  #carNames = [1232, 23];
-  #tryCount = 5;
+  #carNames;
+  #tryCount;
 
   async run() {
-    // this.#carNames = await this.readCarNames();
+    this.#carNames = await this.readCarNames();
 
-    // this.#tryCount = await this.readTryCount();
-    this.play();
+    this.#tryCount = await this.readTryCount();
+    const result = this.play();
+    this.printResult(result);
   }
 
   async readCarNames() {
     const answer = await readLineAsync(
       '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n'
-    ).split(',');
+    ).then(names => names.split(','));
 
     const result = this.validateCars(answer);
     if (!result) throw new Error('[ERROR] 유효한 차 이름 입력이 아님!');
@@ -45,6 +46,26 @@ class App {
       return obj;
     }, {});
     return result;
+  }
+
+  printResult(result, isLineBreak = true) {
+    if (isLineBreak) console.log('\n');
+    console.log('실행결과\n');
+
+    for (let nowTry = 0; nowTry < this.#tryCount; nowTry++) {
+      this.#carNames.forEach(name => {
+        console.log(`${name} : ${'-'.repeat(result[name][nowTry])}`);
+      });
+      console.log('\n');
+    }
+
+    const maxPosition = Math.max(
+      ...Object.values(result).map(array => array[this.#tryCount - 1])
+    );
+    const winners = Object.keys(result).filter(
+      name => result[name][this.#tryCount - 1] === maxPosition
+    );
+    console.log(`최종 우승자: ${winners.join(', ')}`);
   }
 
   validateCars(cars) {
