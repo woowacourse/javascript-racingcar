1139dd04bd852a451583a89072fa24f11c563342
anttiey <hanuuny@gmail.com>
2024-02-14 17:03:59 +0900

    test: 경주 횟수 유효성 검사 테스트 추가
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,5 +1,6 @@
 import CommonValidator from '../src/validator/CommonValidator';
 import CarNamesValidator from '../src/validator/CarNamesValidator';
+import TryCountValidator from '../src/validator/TryCountValidator';
 
 describe('공통 유효성 테스트', () => {
   test('입력 공백이 들어오면 에러가 발생한다.', () => {
@@ -25,4 +26,13 @@ describe('자동차 이름 유효성 테스트', () => {
     const input = ['파슬리', '파슬리', '쑤쑤'];
     expect(() => CarNamesValidator.isDuplicate(input)).toThrow();
   });
+});
+
+describe('경주 횟수 유효성 테스트', () => {
+  test.each(['ㄱ', 'a', '0', '-1', '5.5', '&'])(
+    '경주 횟수가 자연수가 아니라면 발생한다.',
+    (input) => {
+      expect(() => TryCountValidator.isNaturalNumber(Number(input))).toThrow();
+    }
+  );
 });
