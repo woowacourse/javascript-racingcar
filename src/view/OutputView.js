9f0265d4b1b1a95257c06d105b3b0031bbfa0ada
anttiey <hanuuny@gmail.com>
2024-02-14 21:34:31 +0900

    feat: 최종 우승자 출력 기능 구현
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,6 +4,12 @@ class OutputView {
       console.log(`${carName.getCarName()} : ${'-'.repeat(carName.getAdvance())}`);
     });
   }
+
+  winnerResult(winnerCarNamesMap) {
+    console.log(
+      `최종 우승자: ${winnerCarNamesMap.map((carName) => carName.getCarName()).join(', ')}`
+    );
+  }
 }
 
 export default OutputView;
