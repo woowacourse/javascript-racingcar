8ec0d803fb05c5ce903339a40cba0766d527c4e4
손진영 <sm099@naver.com>
2024-02-14 19:12:02 +0900

    test(CommonValidator): 공통 유효성 검사 관련 테스트
    Co-authored-by: chosim-dvlpr <bmj05378@gmail.com>

---
+++ src/validator/common/CommonValidator.test.js
@@ -0,0 +1,38 @@
+import AppError from '../../errors/AppError/module.js';
+import CommonValidator from './CommonValidator.js';
+
+describe('공통 유효성 검사 테스트', () => {
+  const startValidation = (inputValue) => () => CommonValidator.check(inputValue);
+  describe('예외 테스트', () => {
+    test.each([
+      {
+        input: '',
+        expectedErrorMessage: CommonValidator.validationTypes.emptyValues.errorMessage,
+      },
+      {
+        input: 'Some text with space',
+        expectedErrorMessage: CommonValidator.validationTypes.existSpaces.errorMessage,
+      },
+    ])(
+      '입력값이 "$input"일 때 "$expectedErrorMessage" 메시지와 함께 에러가 발생해야 한다.',
+      ({ input, expectedErrorMessage }) => {
+        // then
+        expect(startValidation(input)).toThrow(new AppError(expectedErrorMessage));
+      },
+    );
+  });
+
+  describe('비 예외 테스트', () => {
+    test.each([
+      {
+        input: 'abc',
+      },
+      {
+        input: '123',
+      },
+    ])('입력값이 "$input"일 때 에러가 발생하지 않는다.', ({ input }) => {
+      // then
+      expect(startValidation(input)).not.toThrow();
+    });
+  });
+});
