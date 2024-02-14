ff94ae0be5e2834230c90406fc98c8d633a1bf2b
Lee sang Yeop <mma7710@naver.com>
2024-02-14 19:20:10 +0900

    chore(Conditions): 상수 추가 - 이동을 위한 최소 임계값 - 랜덤값 지정 범위 - 이동 거리

--- src/constant/Conditions.js
+++ src/constant/Conditions.js
@@ -1,6 +1,10 @@
 const CONDITIONS = Object.freeze({
   delimiter: ',',
   distance: '-',
+  minMoveCondition: 4,
+  minRandomNumberRange: 0,
+  maxRandomNumberRange: 9,
+  moveDistance: 1,
 });
 
 export default CONDITIONS;
