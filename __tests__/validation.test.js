00a006bca41f94068486be2e2b79461f1bc0b351
anttiey <hanuuny@gmail.com>
2024-02-14 16:48:03 +0900

    test: 공통 유효성 검사 테스트 추가
    - 입력이 공백일 때 에러가 발생하는지 테스트
    
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,30 +1,10 @@
-import CarValidator from '../src/utils/CarValidator.js';
-import TryCountValidator from '../src/utils/TryCountValidator.js';
+import CommonValidator from '../src/validator/CommonValidator';
 
-describe('자동차 이름 유효성 테스트', () => {
-  test('자동차 대수가 2대 미만이면 에러가 발생한다.', () => {
-    const input = ['파슬리'];
-    expect(() => CarValidator.isValidCount(input)).toThrow();
+describe('공통 유효성 테스트', () => {
+  test('입력 공백이 들어오면 에러가 발생한다.', () => {
+    const input = '';
+    expect(() => CommonValidator.inputEmpty(input)).toThrow(
+      '[ERROR] 입력은 공백이 될 수 없습니다.'
+    );
   });
-
-  test.each(['', '파슬리쑤쑤쿠키썬데이'])(
-    '자동차 이름이 1자 미만 또는 5자 초과면 에러가 발생한다.',
-    (input) => {
-      expect(() => CarValidator.isValidNameRange(input)).toThrow();
-    }
-  );
-
-  test('자동차 이름이 중복되면 에러가 발생한다.', () => {
-    const input = ['파슬리', '파슬리', '쑤쑤'];
-    expect(() => CarValidator.isNameDuplicate(input)).toThrow();
-  });
-});
-
-describe('경주 횟수 유효성 테스트', () => {
-  test.each(['ㄱ', 'a', '0', '-1', '5.5', '&'])(
-    '경주 횟수가 자연수가 아니라면 발생한다.',
-    (input) => {
-      expect(() => TryCountValidator.isNaturalNumber(Number(input))).toThrow();
-    }
-  );
 });
