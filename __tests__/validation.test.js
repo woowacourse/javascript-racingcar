77f290cf83e1f2685423d8f3cb4a820341bd9349
anttiey <hanuuny@gmail.com>
2024-02-14 17:02:10 +0900

    test: 자동차 이름 유효성 검사 테스트 추가
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,10 +1,28 @@
 import CommonValidator from '../src/validator/CommonValidator';
+import CarNamesValidator from '../src/validator/CarNamesValidator';
 
 describe('공통 유효성 테스트', () => {
   test('입력 공백이 들어오면 에러가 발생한다.', () => {
     const input = '';
-    expect(() => CommonValidator.inputEmpty(input)).toThrow(
-      '[ERROR] 입력은 공백이 될 수 없습니다.'
-    );
+    expect(() => CommonValidator.inputEmpty(input)).toThrow();
+  });
+});
+
+describe('자동차 이름 유효성 테스트', () => {
+  test('자동차 대수가 2대 미만이면 에러가 발생한다.', () => {
+    const input = ['파슬리'];
+    expect(() => CarNamesValidator.isValidCount(input)).toThrow();
+  });
+
+  test.each(['', '파슬리쑤쑤쿠키썬데이'])(
+    '자동차 이름이 1자 미만 또는 5자 초과면 에러가 발생한다.',
+    (input) => {
+      expect(() => CarNamesValidator.isValidRange(input)).toThrow();
+    }
+  );
+
+  test('자동차 이름이 중복되면 에러가 발생한다.', () => {
+    const input = ['파슬리', '파슬리', '쑤쑤'];
+    expect(() => CarNamesValidator.isDuplicate(input)).toThrow();
   });
 });
