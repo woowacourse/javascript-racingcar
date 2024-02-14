fdef8e03bba2812b46bdebbc9b35d3665f989f82
Hyelim Choi <HyelimChoi01@gmail.com>
2024-02-14 17:36:49 +0900

    feat: 중복 입력 예외처리 에러 및 재입력
    Co-Authored-By: 월하 <52562061+vi-wolhwa@users.noreply.github.com>

--- src/model/CarGame.js
+++ src/model/CarGame.js
@@ -4,6 +4,8 @@ class CarGame {
   #carList = [];
 
   setCars(carNames) {
+    this.#validateCarNamesMulti(carNames);
+
     carNames.reduce((carList, name) => {
       this.#validateCarNamesLength(name);
       carList.push(new Car(name));
@@ -14,6 +16,14 @@ class CarGame {
   #validateCarNamesLength(carName) {
     if (carName.length > 5) {
       throw new Error('[ERROR] 자동차 이름은 5글자 이하만 가능합니다.');
+    }
+  }
+
+  #validateCarNamesMulti(carNames) {
+    this.validMulti = new Set(new Set(carNames)).size === carNames.length;
+
+    if (!this.validMulti) {
+      throw new Error('[ERROR] 자동차 이름은 중복될 수 없습니다.');
     }
   }
 }
