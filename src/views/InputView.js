4282a749f590194c3debd3978957f115767a8253
skiende74 <skiende74@gmail.com>
2024-02-14 14:50:53 +0900

    docs(REQUIREMENT): 구현할 기능 목록 추가

---
+++ src/views/InputView.js
@@ -0,0 +1,30 @@
+import readline from "readline";
+const Private = { readLineAsync(query) {
+    return new Promise((resolve, reject) => {
+      if (arguments.length !== 1) {
+        reject(new Error("arguments must be 1"));
+      }
+
+      if (typeof query !== "string") {
+        reject(new Error("query must be string"));
+      }
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question(query, (input) => {
+        rl.close();
+        resolve(input);
+      });
+    });
+  }};
+
+const InputView = {
+    readline() {
+        Private.readLineAsync();
+    }
+};
+
+export default InputView;
