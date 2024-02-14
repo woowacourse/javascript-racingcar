ca25209e8f6f92bb62fb24b5fb89829dcbb263b2
jaeml06 <jaeml0630@gmail.com>
2024-02-14 18:48:19 +0900

    feat: 시도 횟수 Vaildation 함수 추가
    시도 횟수가 음수거나 정수가 아닐 경우 에러 반환하는 함수 구현

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,12 +1,21 @@
 const Validation = {
   carNamesArrayValidate(carNamesArray) {
-    carNamesArray.every((carName) => {
+    carNamesArray.every((car) => {
       if (carName.length <= 0 || carName.length > 5) {
         throw new Error(
           '[ERROR] 각 자동차의 이름은 1 ~ 5자 이내로 입력해주세요.'
         );
       }
     });
+  },
+
+  tryCountValidate(tryCountString) {
+    if (
+      !Number.isInteger(Number(tryCountString)) ||
+      Number(tryCountString) < 0
+    ) {
+      throw new Error('[ERROR]');
+    }
   },
 };
 export default Validation;
