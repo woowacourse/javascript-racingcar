c26109cd669acb08addcf595916016a26b0e3d9b
Lee sang Yeop <mma7710@naver.com>
2024-02-14 19:21:12 +0900

    feat(Car, getRandomNumber): 무작위 값에 따라 자동차를 이동

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,8 +1,21 @@
+import CONDITIONS from '../constant/Conditions.js';
+import getRandomNumber from '../utils/getRandomNumber.js';
+
 class Car {
   #name;
+  #distance;
 
   constructor(name) {
     this.#name = name;
+    this.#distance = 0;
+  }
+
+  tryMove() {
+    const randomNumber = getRandomNumber(CONDITIONS.minRandomNumberRange, CONDITIONS.maxRandomNumberRange);
+
+    if (randomNumber >= CONDITIONS.minMoveCondition) {
+      this.#distance += CONDITIONS.moveDistance;
+    }
   }
 }
 
