d6921c6e622ce3e808df9f42772a7627de9221c0
pakxe <pigkill40@naver.com>
2024-02-14 20:38:03 +0900

    fix: async 키워드 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,11 +3,11 @@ import Console from '../utils/Console.js';
 const CAR_NAMES_PROMPT = '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n';
 const TRY_COUNT_PROMPT = '시도할 횟수는 몇 회인가요?\n';
 class InputView {
-  readCarNames() {
+  async readCarNames() {
     return Console.read(CAR_NAMES_PROMPT);
   }
 
-  readTryCount() {
+  async readTryCount() {
     return Console.read(TRY_COUNT_PROMPT);
   }
 }
