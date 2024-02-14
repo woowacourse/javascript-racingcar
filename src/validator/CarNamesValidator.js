e8cd20c5de59945db9f241633a6d5ace595acc7a
anttiey <hanuuny@gmail.com>
2024-02-14 16:55:21 +0900

    fix: 불필요한 유효성 검사 삭제
    - 자동차 이름 입력 포맷 유효성 검사 삭제
    
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- src/validator/CarNamesValidator.js
+++ src/validator/CarNamesValidator.js
@@ -1,10 +1,4 @@
 class CarNamesValidator {
-  static isValidFormat(carNames) {
-    if (!carNames.includes(',')) {
-      throw new Error('[ERROR] 자동차 이름이 유효하지 않은 형식입니다.');
-    }
-  }
-
   static isValidCount(carNames) {
     if (carNames.length < 2) {
       throw new Error('[ERROR] 자동차 대수는 2대 이상만 가능하다.');
