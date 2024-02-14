08abe4366531a8afdb9b9018cb0fcc97c650fce1
skiende74 <skiende74@gmail.com>
2024-02-14 15:59:56 +0900

    feat(InputView) : 경주할 자동차 이름 입력 기능추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,12 +1,26 @@
-import readline from "readline";
-const Private = { readLineAsync(query) {
+import readline from 'readline.js';
+import OutputView from 'OutputView.js';
+import MESSAGES from '../constants/messages.js';
+
+const Private = {
+  readline() {
+    Private.readLineAsync();
+  },
+  readCarNames() {
+    return Private.readLineAsync(MESSAGES.carNamesInput);
+  },
+
+  readTrialCount() {
+    return Private.readLineAsync(MESSAGES.trialCountInput);
+  },
+  readLineAsync(query) {
     return new Promise((resolve, reject) => {
       if (arguments.length !== 1) {
-        reject(new Error("arguments must be 1"));
+        reject(new Error('arguments must be 1'));
       }
 
-      if (typeof query !== "string") {
-        reject(new Error("query must be string"));
+      if (typeof query !== 'string') {
+        reject(new Error('query must be string'));
       }
 
       const rl = readline.createInterface({
@@ -14,17 +28,28 @@ const Private = { readLineAsync(query) {
         output: process.stdout,
       });
 
-      rl.question(query, (input) => {
+      rl.question(query, input => {
         rl.close();
         resolve(input);
       });
     });
-  }};
+  },
+
+  robustInput(readline, Object) {
+    while (true) {
+      try {
+        return new Object(readline());
+      } catch (error) {
+        print(error.message);
+      }
+    }
+  },
+};
 
 const InputView = {
-    readline() {
-        Private.readLineAsync();
-    }
+  readCarNames() {
+    return Private.robustInput(Private.readCarNames, Object);
+  },
 };
 
 export default InputView;
