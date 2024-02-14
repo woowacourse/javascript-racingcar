5c174e1aec2ac6b5c8c2dcf3524439230111bd85
jaeml06 <jaeml0630@gmail.com>
2024-02-14 20:32:37 +0900

    refactor: 출력 문장 수정
    출력 문장 양식 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,15 +1,16 @@
-import Console from './utils/Console';
+import Console from './utils/Console.js';
 
 const InputView = {
   async queryCarName() {
     const carNames = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).'
+      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n'
     );
     return carNames;
   },
 
   async queryTryCount() {
-    const tryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    const tryCount =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     return tryCount;
   },
 };
