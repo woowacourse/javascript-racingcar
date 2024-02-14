55c2e0f5c8efe915c57b0cac204c381a434ec563
jaeml06 <jaeml0630@gmail.com>
2024-02-14 19:24:58 +0900

    feat: 각 시도 횟수별 실행 결과 반환 함수 구현
    각 시도 횟수별 실행 결과 반환 함수 구현

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,9 @@
 class Car {
+  #name;
+  #distance;
   constructor(name) {
-    this.name = name;
-    this.distance = 0;
+    this.#name = name;
+    this.#distance = 0;
   }
 
   canMove() {
@@ -11,8 +13,11 @@ class Car {
 
   move() {
     if (this.canMove()) {
-      this.distance += 1;
+      this.#distance += 1;
     }
+  }
+  getDistanceString() {
+    return `${this.#name} : ${'-'.repeat(this.#distance)}\n`;
   }
 }
 
--- src/Cars.js
+++ src/Cars.js
@@ -5,6 +5,17 @@ class Cars {
   constructor(carNameArray) {
     this.#carList = carNameArray.map((name) => new Car(name));
   }
+
+  moveAllCars() {
+    this.#carList.forEach((car) => {
+      car.move();
+    });
+  }
+  getEachStepString() {
+    return this.#carList.reduce((pre, cur) => {
+      pre += cur.getDistanceString();
+    }, '');
+  }
 }
 
 export default Cars;
