42bbcb393fa579aed3dd03dee0dfb27455105794
pakxe <pigkill40@naver.com>
2024-02-14 18:53:25 +0900

    feat: Car 클래스에 전진 메서드 추가

--- src/model/Car.js
+++ src/model/Car.js
@@ -1,7 +1,9 @@
 import ERROR_MESSAGE from '../error/message.js';
+import Random from './Random.js';
 
 export const MIN_NAME_LENGTH = 1;
 export const MAX_NAME_LENGTH = 5;
+const FORWARD_CONDITION = 4;
 
 class Car {
   #name;
@@ -16,6 +18,12 @@ class Car {
     if (name.length < MIN_LENGTH || name.length > MAX_LENGTH) {
       throw new Error(`${ERROR_MESSAGE.nameLength} ${ERROR_MESSAGE.retry}`);
     }
+  }
+
+  forward() {
+    const randomNumber = new Random.create();
+
+    if (randomNumber >= FORWARD_CONDITION) this.#location += 1;
   }
 
   get name() {
