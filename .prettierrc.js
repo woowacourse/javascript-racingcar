e96e86e808eba39d9e6c817b59f4c85b56284709
simhorim <hori5020@gmail.com>
2024-02-14 15:30:06 +0900

    docs(racingcar): 기능목록 작성

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	endOfLine: 'lf',
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	tabWidth: 2,
+	useTabs: true,
+	trailingComma: 'all',
+	arrowParens: 'avoid',
+}
