23707693a0862e93448341aa65da4076f4eca45c
skiende74 <skiende74@gmail.com>
2024-02-14 18:51:36 +0900

    feat(Cars): 빈칸 입력 시 예외 처리

--- src/entities/Cars.js
+++ src/entities/Cars.js
@@ -2,13 +2,26 @@ import MESSAGES from '../constants/messages.js';
 import ERRORS from '../constants/errors.js';
 
 class Cars {
-  #validate(carStr) {
+  #validateLength(carStr) {
     if (carStr.split(',').some(car => car.length > 5)) {
       throw new Error(ERRORS.carNameLength);
     }
   }
 
-  constructor(carStr) {}
+  #validateBlank(carStr) {
+    if (carStr.split(',').some(car => car.trim().length === 0)) {
+      throw new Error(ERRORS.carNameBlank);
+    }
+  }
+
+  #validate(carStr) {
+    this.#validateLength(carStr);
+    this.#validateBlank(carStr);
+  }
+
+  constructor(carStr) {
+    this.#validate(carStr);
+  }
 }
 
 export default Cars;
