525515f44199bfc00ec0d9a5d75cd879ecc0f8ee
badahertz52 <badahertz52@gmail.com>
2024-02-14 16:48:25 +0900

    docs: 메세지 관련 상수 파일 추가

---
+++ src/constants/Message.js
@@ -0,0 +1,20 @@
+export const FORM_MESSAGE = Object.freeze({
+  name: '자동차 이름은 영문자,한글을 사용해 1자 이상 5자 이하로 해주세요.',
+  delimiter: '자동차 이름은 쉼표(,)를 기준으로 구분해주세요.',
+  round: '진행 횟수는 1이상 5이하의 정수만 가능해요.',
+});
+
+export const INPUT_MESSAGE = Object.freeze({
+  name: `경주할 자동차 이름을 입력하세요.\n${FORM_MESSAGE.name}\n${FORM_MESSAGE.delimiter}`,
+  round: `시도할 횟수는 몇 회인가요?\n${FORM_MESSAGE.round}`,
+});
+
+export const OUTPUT_MESSAGE_HEADER = Object.freeze({
+  roundResult: '실행 결과',
+  winner: '최종 우승자',
+});
+
+export const ERROR_MESSAGE = Object.freeze({
+  name: `유효하지 않은 이름 형식이에요.\n ${FORM_MESSAGE.name}\n${FORM_MESSAGE.delimiter}`,
+  round: `유효하지 않은 형식이에요.\n${FORM_MESSAGE.round}`,
+});
