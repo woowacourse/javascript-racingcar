4ca931f055b4b132d822aa342d8180e050463599
anttiey <hanuuny@gmail.com>
2024-02-14 17:31:35 +0900

    feat: 랜덤 숫자 생성 기능 추가
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- src/utils/Random.js
+++ src/utils/Random.js
@@ -1,10 +1,6 @@
-import Condition from '../constant/Condition.js';
-
-const { RANDOM } = Condition;
-
 class Random {
   static pickNumberZeroToNine() {
-    return Math.floor(Math.random() * RANDOM.RANGE);
+    return Math.floor(Math.random() * 10);
   }
 }
 
