8f68bb4b8e2c29ca57b6c674a7e5717941a07837
GimGayeon <kky1373@naver.com>
2024-02-14 16:33:22 +0900

    chore:초기 다운로드

---
+++ src/Car.js
@@ -0,0 +1,20 @@
+import Validation from "./validation.js";
+
+class Car {
+  #carList;
+
+  constructor(carList) {
+    this.#carList = this.#validation(carList);
+  }
+
+  #validation(carList) {
+    Validation.isDuplicate(carList);
+    return carList;
+  }
+
+  getCarList() {
+    return this.#carList;
+  }
+}
+
+export default Car;
