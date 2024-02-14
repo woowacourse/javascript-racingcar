dc1690890bb6e6ec262b2cad2f3728d7f88f6d68
pakxe <pigkill40@naver.com>
2024-02-14 20:37:29 +0900

    fix: this가 없어 호출되지 않았던 오류 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,9 @@
 import Console from '../utils/Console.js';
 
-const RESULT_TITLE = '실행 결과';
+const RESULT_TITLE = '\n실행 결과';
 const COLON = ':';
 const ADVANCE = '-';
-const COMMA = ',';
+export const COMMA = ',';
 const WINNER_TITLE = '최종 우승자';
 
 class OutputView {
@@ -17,12 +17,13 @@ class OutputView {
 
   printRacingResult(cars) {
     cars.forEach(({ name, location }) => {
-      printCarResult(name, location);
+      this.printCarResult(name, location);
     });
+    Console.print(' ');
   }
 
   printWinners(names) {
-    Console.print(`${WINNER_TITLE} ${COLON} ${names.join(COMMA + ' ')}`);
+    Console.print(`${WINNER_TITLE}${COLON} ${names.join(COMMA + ' ')}`);
   }
 }
 
