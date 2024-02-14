ea43f18023a2ed04bb07cca91ec38ae00c632fe0
Lee sang Yeop <mma7710@naver.com>
2024-02-14 23:22:11 +0900

    chore(index): 앱 실행 추가

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Controller from './controller/Controller.js';
+
+class App {
+  #controller;
+
+  async start() {
+    this.#controller = new Controller();
+    await this.#controller.getCarNames();
+    await this.#controller.getMoveCount();
+    this.#controller.race();
+    this.#controller.racingWinners();
+  }
+}
+
+const app = new App();
+app.start();
