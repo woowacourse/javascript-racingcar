bcbc64a0f7277aeaad3a445e52d86a3abcbdc36d
jaeml06 <jaeml0630@gmail.com>
2024-02-14 15:06:12 +0900

    feat: queryCarName 함수 구현
    경주할 자동차 이름 문자열을 입력 받는 함수 구현

---
+++ src/InputView.js
@@ -0,0 +1,12 @@
+import Console from './utils/Console';
+
+const InputView = {
+  async queryCarName() {
+    const carNames = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).'
+    );
+    return carNames;
+  },
+};
+
+export default InputView;
