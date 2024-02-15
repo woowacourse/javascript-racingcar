276f83691ebc96981c3c5fb9c08844d34b94e628
skiende74 <skiende74@gmail.com>
2024-02-15 16:45:05 +0900

    test(WinnerService): 승자 판단 테스트 구현

---
+++ __tests__/WinnerService.test.js
@@ -0,0 +1,23 @@
+import WinnerService from '../src/services/WinnerService.js';
+
+describe('승자 판단', () => {
+  test('승자가 한 명일 때', () => {
+    // Arrange
+    const CAR_NAMES = ['car1', 'car2', 'car3', 'car4'];
+    const POSITIONS = [0, 1, 4, 1];
+    // Act
+    const winners = new WinnerService([CAR_NAMES, POSITIONS]).findWinner();
+    // Assert
+    expect(winners).toEqual(['car3']);
+  });
+
+  test('승자가 여러 명일 때', () => {
+    // Arrange
+    const CAR_NAMES = ['car1', 'car2', 'car3', 'car4'];
+    const POSITIONS = [0, 4, 4, 2];
+    // Act
+    const winners = new WinnerService([CAR_NAMES, POSITIONS]).findWinner();
+    // Assert
+    expect(winners).toEqual(['car2', 'car3']);
+  });
+});
