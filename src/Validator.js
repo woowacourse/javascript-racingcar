f067923f284fa523999ecf2caa49e9b9f46091ca
rbgksqkr <rbgks1937@naver.com>
2024-02-15 13:46:24 +0900

    refactor: Validator 메소드 분리

--- src/Validator.js
+++ src/Validator.js
@@ -1,26 +1,43 @@
-// - [ ] 이동 횟수가 0 이하일 경우 예외 처리한다.
-// - [ ] 이동 횟수가 자연수가 아닌 경우 예외 처리한다.
 const Validator = {
   validateCarNameList(carNameList) {
-    if (carNameList.length <= 1) {
-      throw new Error('[ERROR] 자동차 목록은 2대 이상 입력해주세요.');
-    }
-
+    this.validateCarNameListLength(carNameList.length);
     carNameList.forEach((carName) => {
-      if (carName.length === 0 || carName.length > 5) {
-        throw new Error('[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.');
-      }
+      this.validateCarNameLength(carName.length);
     });
   },
 
   validateTurnCount(turnCountInput) {
     const turnCount = parseFloat(turnCountInput, 10);
+    this.validateFloatNumber(turnCount);
+    this.validateNumber(turnCount);
+    this.validateNaturalNumber(turnCount);
+  },
+
+  validateCarNameListLength(length) {
+    if (length <= 1) {
+      throw new Error('[ERROR] 자동차 목록은 2대 이상 입력해주세요.');
+    }
+  },
+
+  validateCarNameLength(length) {
+    if (length === 0 || length > 5) {
+      throw new Error('[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.');
+    }
+  },
+
+  validateFloatNumber(turnCount) {
     if (!Number.isInteger(turnCount)) {
       throw new Error('[ERROR] 시도할 횟수는 실수로 입력할 수 없습니다. 다시 입력해주세요.');
     }
+  },
+
+  validateNumber(turnCount) {
     if (Number.isNaN(turnCount)) {
       throw new Error('[ERROR] 시도할 횟수는 숫자로만 입력해주세요.');
     }
+  },
+
+  validateNaturalNumber(turnCount) {
     if (turnCount <= 0) {
       throw new Error('[ERROR] 시도할 횟수는 1 이상의 자연수로만 입력해주세요.');
     }
