dd1aca27c7c4f61d6e4b849ce7dec893c3310621
badahertz52 <badahertz52@gmail.com>
2024-02-15 14:04:09 +0900

    docs: controllers/index.js에서 Game 내보내기 추가

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,4 @@
-import InputController from './InputController';
+import InputController from './InputController.js';
+import Game from './Game.js';
 
-export { InputController };
+export { InputController, Game };
