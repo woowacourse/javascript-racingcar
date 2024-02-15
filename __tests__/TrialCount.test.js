1d11a7525dee42fcef922f7dc062942acbf654ef
skiende74 <skiende74@gmail.com>
2024-02-15 13:55:14 +0900

    test(TrialCount): TrialCount 테스트 추가

---
+++ __tests__/TrialCount.test.js
@@ -0,0 +1,24 @@
+import TrialCount from '../src/entities/TrialCount';
+
+describe('시도 횟수', () => {
+  test.each(['5', '1'])('정상 입력', countStr => {
+    // Arrange
+    const trialCount = new TrialCount(countStr);
+
+    // Act
+    const result = trialCount.getCount();
+
+    // Assert
+    expect(result).toBe(parseInt(countStr));
+  });
+
+  test.each(['', '  '])('공백 입력', countStr => {
+    expect(() => new TrialCount(countStr)).toThrow('[ERROR]');
+  });
+  test.each(['a', '_3'])('숫자형이어야 함', countStr => {
+    expect(() => new TrialCount(countStr)).toThrow('[ERROR]');
+  });
+  test.each(['-1', '0'])('1이상의 수여야 함', countStr => {
+    expect(() => new TrialCount(countStr)).toThrow('[ERROR]');
+  });
+});
