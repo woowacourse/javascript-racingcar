b01ff36c25db0fb96247d26d92fc24971e5c7b8a
Lee sang Yeop <mma7710@naver.com>
2024-02-15 11:06:56 +0900

    chore(Messages): 에러 메시지 상수 정의 - 자동차 갯수 범위 - 자동차 이름 최대 길이 - 숫자 유효성 검증 - 시도 횟수 범위

--- src/constant/Messages.js
+++ src/constant/Messages.js
@@ -1,3 +1,5 @@
+import CONDITIONS from './Conditions.js';
+
 export const INPUT_MESSSAGES = Object.freeze({
   carNames: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)',
   moveCount: '시도할 횟수는 몇 회인가요?',
@@ -6,4 +8,11 @@ export const INPUT_MESSSAGES = Object.freeze({
 export const OUTPUT_MESSAGES = Object.freeze({
   result: '실행 결과',
   winners: '최종 우승자: ',
+});
+
+export const ERROR_MESSAGES = Object.freeze({
+  invalidCarCountRange: `자동차 갯수는 ${CONDITIONS.minCarCount}개 이상 ${CONDITIONS.maxCarCount}개 이하여야 합니다.`,
+  invalidCarNameLength: `자동차 이름은 ${CONDITIONS.maxCarNameLength}자 이하여야 합니다.`,
+  NaN: `입력값은 숫자여야 합니다.`,
+  invalidTryCountRange: `시도할 횟수는 ${CONDITIONS.minTryCount}회 이상 ${CONDITIONS.maxTryCount}회 이하여야 합니다.`,
 });
