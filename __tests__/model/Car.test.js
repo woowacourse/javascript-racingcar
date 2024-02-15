399cd991828042042a32bbeb5ff5131b2b63b2f9
김진호 <rlawlsgh1227@gmail.com>
2024-02-15 17:01:03 +0900

    test: 자동차 전진 단위테스트
    Co-authored-by: useon <yoosk4986@naver.com>

--- __tests__/model/Car.test.js
+++ __tests__/model/Car.test.js
@@ -1,7 +1,15 @@
 import Car from "../../src/models/Car";
+import gameUtils from "../../src/utils/gameUtils";
 
-describe('자동차 생성테스트', () => {
-  describe('자동차가 생성되지 않는 경우', () => {
+const mockRandoms = (numbers) => {
+  gameUtils.pickRandomNumber = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, gameUtils.pickRandomNumber);
+}
+
+describe('자동차 테스트', () => {
+  describe('자동차 생성 테스트', () => {
     test('자동차 이름이 비었으면 에러를 발생시킨다.', () => {
       // given
       const invalidInput = '';
@@ -32,8 +40,6 @@ describe('자동차 생성테스트', () => {
         // then
       }).toThrow();
     });
-  });
-  describe('자동차 정상 생성테스트', () => {
     test('자동차 이름을 정상적으로 입력하면 자동차가 생성된다.', () => {
       // given
       const validInput = '썬데이';
@@ -43,6 +49,34 @@ describe('자동차 생성테스트', () => {
 
       // then
       expect(typeof car === 'object').toBeTruthy();
+    });
+  });
+  describe('자동차 전진테스트, (4이상이면 전진)', () => {
+    const sundayCar = new Car('썬데이');
+
+    test('랜덤한 수를 3을 받으면 전진하지 않습니다.', () => {
+      // given
+      const randomNumber = [3];
+      const answer = 0;
+      mockRandoms(randomNumber);
+
+      // when
+      const actResult = sundayCar.actCar();
+
+      // then
+      expect(actResult.score).toEqual(answer);
+    });
+    test('랜덤한 수를 4을 받으면 전진합니다.', () => {
+      // given
+      const randomNumber = [4];
+      const answer = 1;
+      mockRandoms(randomNumber);
+
+      // when
+      const actResult = sundayCar.actCar();
+
+      // then
+      expect(actResult.score).toEqual(answer);
     });
   });
 });
