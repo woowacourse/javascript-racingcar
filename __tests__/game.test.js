9f55a4cae4a4d58c982b2239d9740391797ef81d
anttiey <hanuuny@gmail.com>
2024-02-15 15:49:22 +0900

    test: 테스트 코드 리팩토링
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,9 +1,9 @@
-import GameController from '../src/controller/GameController';
+import Game from '../src/controller/Game';
 import Car from '../src/model/Car';
 
 describe('최종 우승자 선발 테스트', () => {
   test('가장 전진을 많이 한 자동차를 최종 우승자로 선발한다', () => {
-    const game = new GameController();
+    const game = new Game();
 
     const carA = new Car('파슬리');
     const carB = new Car('쑤쑤');
@@ -15,7 +15,7 @@ describe('최종 우승자 선발 테스트', () => {
   });
 
   test('최종 우승자는 한 명 이상일 수 있다', () => {
-    const game = new GameController();
+    const game = new Game();
 
     const carA = new Car('파슬리');
     const carB = new Car('쑤쑤');
