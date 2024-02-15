ac0f01f780d26a6a9f4ba6673df7299f8a3d6f23
GimGayeon <kky1373@naver.com>
2024-02-15 11:37:19 +0900

    feat:Race Controller 생성

---
+++ src/RaceController.js
@@ -0,0 +1,57 @@
+import { carNameInput, tryInput } from "./view/Input.js";
+import Move from "./Move.js";
+import Output from "./view/Output.js";
+import { count } from "./util/count.js";
+
+class RaceController {
+  #moveInstanceList = [];
+
+  async race() {
+    const { cars, tryNumber } = await this.input();
+    cars.map((car) => this.#moveInstanceList.push(new Move(car)));
+    this.calculateResult(tryNumber);
+    this.printResult(tryNumber);
+    const winner = this.calculateWinner();
+    Output.printWinner(winner);
+  }
+
+  async input() {
+    const cars = await carNameInput();
+    const tryNumber = await tryInput();
+    return { cars, tryNumber };
+  }
+
+  printResult(tryNumber) {
+    console.log("실행결과");
+    for (let i = 0; i < tryNumber; i++) {
+      this.#moveInstanceList.forEach((move) => {
+        Output.printMove(move.getInfo(), i + 1);
+      });
+      console.log("");
+    }
+  }
+
+  calculateResult(tryNumber) {
+    for (let i = 0; i <= tryNumber; i++) {
+      this.#moveInstanceList.forEach((move) => {
+        move.move();
+      });
+    }
+  }
+
+  calculateWinner() {
+    const maxMove = Math.max(
+      ...this.#moveInstanceList.map((moveInstance) =>
+        count(moveInstance.getInfo().moveTrace)
+      )
+    );
+    const result = this.#moveInstanceList
+      .filter(
+        (moveInstance) => count(moveInstance.getInfo().moveTrace) === maxMove
+      )
+      .map((moveInstance) => moveInstance.getInfo().carName);
+    return result;
+  }
+}
+
+export default RaceController;
