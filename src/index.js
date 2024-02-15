c7d8bf5a0e6ee1f1fa56674b10cc369b1b82d43c
GimGayeon <kky1373@naver.com>
2024-02-15 11:40:43 +0900

    fix:컨트롤러 실행

--- src/index.js
+++ src/index.js
@@ -1,42 +1,11 @@
 import { carNameInput, tryInput } from "./view/Input.js";
 import Move from "./Move.js";
 import Output from "./view/Output.js";
+import RaceController from "./RaceController.js";
 
 class App {
   async play() {
-    const cars = await carNameInput();
-    const tryNumber = await tryInput();
-    const carsMove = [];
-
-    cars.map((car) => carsMove.push(new Move(car)));
-
-    console.log("실행결과");
-
-    for (let i = 0; i <= tryNumber; i++) {
-      carsMove.forEach((carMove) => {
-        carMove.move();
-        Output.printMove(carMove.getInfo());
-      });
-      console.log("");
-    }
-    const winners = [];
-    carsMove.map((carMove) => {
-      winners.push(carMove.getInfo());
-    });
-    const maxMove = Math.max(
-      ...winners.map((winner) => {
-        return winner.moveCount;
-      })
-    );
-    const result = winners
-      .filter((winner) => {
-        return winner.moveCount === maxMove;
-      })
-      .map((winner) => {
-        return winner.carName;
-      });
-
-    Output.printWinner(result);
+    new RaceController().race();
   }
 }
 
