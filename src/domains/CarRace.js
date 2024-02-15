81fabbefd7b9a639c74d196fa0778b45426d0523
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-02-15 14:34:43 +0900

    refactor: findMaxPosition 메서드를 private으로 설정
    Co-Authored-By: hwinkr <dnddl8280@naver.com>

--- src/domains/CarRace.js
+++ src/domains/CarRace.js
@@ -3,18 +3,26 @@ import Car from './Car';
 class CarRace {
   #cars;
 
-  // view에서 전달되는 자동차 : "pobi, bingbong, harry"
   constructor(cars) {
     this.#cars = this.#initRaceCars(cars);
   }
 
-  // 문자열 -> Car 인스턴스 리스트 만들기
   #initRaceCars(cars) {
     const spliitedCars = cars.split(',').map((carName) => carName.trim());
     return spliitedCars.map((carName) => new Car(carName));
   }
 
-  // 각 레이스 결과 보드 만들기
+  #findMaxPosition() {
+    let maxPositionCar = this.#cars[0];
+    for (let index = 1; index < this.#cars.length; index++) {
+      if (!maxPositionCar.isAheadOf(this.#cars[index])) {
+        maxPositionCar = this.#cars[index];
+      }
+    }
+
+    return maxPositionCar;
+  }
+
   makesRoundResult() {
     const result = {};
 
@@ -26,21 +34,9 @@ class CarRace {
     return result;
   }
 
-  // 가장 멀리간 차 찾기
-  findMaxPosition() {
-    let maxPositionCar = this.#cars[0];
-    for (let index = 1; index < this.#cars.length; index++) {
-      if (!maxPositionCar.isAheadOf(this.#cars[index])) {
-        maxPositionCar = this.#cars[index];
-      }
-    }
-
-    return maxPositionCar;
-  }
-
   // 우승자 결정
   judgeWinners() {
-    const maxPositionCar = this.findMaxPosition();
+    const maxPositionCar = this.#findMaxPosition();
     const winners = this.#cars
       .filter((car) => maxPositionCar.isSamePosition(car))
       .map((winner) => winner.getName());
