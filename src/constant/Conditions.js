6ed98fb661b7e9953edb5264fccf0ec941d2e042
Lee sang Yeop <mma7710@naver.com>
2024-02-15 15:45:07 +0900

    refactor(Conditions): 상수 종류별 분리 CONDITIONS 세분화

--- src/constant/Conditions.js
+++ src/constant/Conditions.js
@@ -1,7 +1,10 @@
-const CONDITIONS = Object.freeze({
+const SYMBOL = Object.freeze({
   delimiter: ',',
   distance: '-',
   space: ' ',
+});
+
+const RULES = Object.freeze({
   minMoveCondition: 4,
   minRandomNumberRange: 0,
   maxRandomNumberRange: 9,
@@ -11,7 +14,16 @@ const CONDITIONS = Object.freeze({
   maxCarNameLength: 5,
   minTryCount: 1,
   maxTryCount: 1000,
+});
+
+const REGEXP = Object.freeze({
   numericPattern: /^\d+$/,
+});
+
+const CONDITIONS = Object.freeze({
+  ...SYMBOL,
+  ...RULES,
+  ...REGEXP,
 });
 
 module.exports = CONDITIONS;
