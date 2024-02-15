e41a3afe0349589fcd340950eebc982ceb42437d
anttiey <hanuuny@gmail.com>
2024-02-15 15:49:22 +0900

    test: 테스트 코드 리팩토링
    Co-authored-by: soosoo22 <soosoo22@users.noreply.github.com>

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,30 +1,22 @@
-import CommonValidator from '../src/utils/CommonValidator';
-import CarNamesValidator from '../src/utils/CarNamesValidator';
-import TryCountValidator from '../src/utils/TryCountValidator';
-
-describe('공통 유효성 테스트', () => {
-  test('입력 공백이 들어오면 에러가 발생한다.', () => {
-    const input = '';
-    expect(() => CommonValidator.inputEmpty(input)).toThrow();
-  });
-});
+import CarValidator from '../src/utils/CarValidator.js';
+import TryCountValidator from '../src/utils/TryCountValidator.js';
 
 describe('자동차 이름 유효성 테스트', () => {
   test('자동차 대수가 2대 미만이면 에러가 발생한다.', () => {
     const input = ['파슬리'];
-    expect(() => CarNamesValidator.isValidCount(input)).toThrow();
+    expect(() => CarValidator.isValidCount(input)).toThrow();
   });
 
   test.each(['', '파슬리쑤쑤쿠키썬데이'])(
     '자동차 이름이 1자 미만 또는 5자 초과면 에러가 발생한다.',
     (input) => {
-      expect(() => CarNamesValidator.isValidRange(input)).toThrow();
+      expect(() => CarValidator.isValidNameRange(input)).toThrow();
     }
   );
 
   test('자동차 이름이 중복되면 에러가 발생한다.', () => {
     const input = ['파슬리', '파슬리', '쑤쑤'];
-    expect(() => CarNamesValidator.isDuplicate(input)).toThrow();
+    expect(() => CarValidator.isNameDuplicate(input)).toThrow();
   });
 });
 
