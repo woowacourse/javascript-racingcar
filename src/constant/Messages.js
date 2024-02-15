cee9431e72bcca4d929ba6d28e843587896d2982
Lee sang Yeop <mma7710@naver.com>
2024-02-15 15:46:57 +0900

    refactor(Messages): 메시지 줄바꿈 추가

--- src/constant/Messages.js
+++ src/constant/Messages.js
@@ -1,12 +1,12 @@
 const CONDITIONS = require('./Conditions.js');
 
 const INPUT_MESSSAGES = Object.freeze({
-  carNames: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)',
-  moveCount: '시도할 횟수는 몇 회인가요?',
+  carNames: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)\n',
+  moveCount: '시도할 횟수는 몇 회인가요?\n',
 });
 
 const OUTPUT_MESSAGES = Object.freeze({
-  result: '실행 결과',
+  result: '\n실행 결과',
   winners: '최종 우승자: ',
 });
 
