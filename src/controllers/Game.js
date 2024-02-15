d1f1303bf52f7efe04b74cd4fdec0c05644d0a2a
badahertz52 <badahertz52@gmail.com>
2024-02-15 14:02:55 +0900

    refactor: Game.js 리팩토링

--- src/controllers/Game.js
+++ src/controllers/Game.js
@@ -1,23 +1,22 @@
 import { Car } from '../models/index.js';
-import { InputController } from './controllers.index.js';
+import OutputView from '../views/OutView.js';
+import InputController from './InputController.js';
+import { OUTPUT_MESSAGE } from '../constants/Message.js';
 
 class Game {
   #carList = [];
+
   #winner = [];
+
   #round = {
     total: 0,
     current: 0,
   };
 
-  constructor(string) {
-    this.#setCarList(string);
-    this.#getTotalRound();
-    this.#play();
-    this.#judgementWinner();
-  }
+  async #setCarList() {
+    const value = await InputController.getCarName();
 
-  #setCarList(string) {
-    this.#carList = string.split(',').map((name) => new Car(name));
+    if (value) this.#carList = value.split(',').map((name) => new Car(name));
   }
 
   async #getTotalRound() {
@@ -26,25 +25,56 @@ class Game {
     this.#round.total = Number(value);
   }
 
-  #play() {
-    while (this.#round.total < this.#round.current) {
-      this.#carList.forEach((car) => car.movement());
+  async setGame() {
+    await this.#setCarList();
+    await this.#getTotalRound();
+  }
+
+  #printRoundResult() {
+    this.#carList.forEach((car) => {
+      const { name, step } = car.getCarInfo();
+      const message = `${name} : ${Array.from({ length: step }, () => '-').join(
+        '',
+      )} `;
+
+      OutputView.printMessage(message);
+    });
+  }
+
+  play() {
+    OutputView.printMessage(`\n${OUTPUT_MESSAGE.roundResult}`);
+
+    while (this.#round.total > this.#round.current) {
+      this.#carList.forEach((car) => {
+        car.movement();
+      });
+      OutputView.printMessage('');
+      this.#printRoundResult();
+      this.#round.current += 1;
     }
   }
 
   #judgementWinner() {
     const winnerPoint = Math.max(
-      this.#carList.map((car) => car.getCarInfo().step),
+      ...this.#carList.map((car) => car.getCarInfo().step),
     );
 
     this.#carList.forEach((car) => {
       const { step, name } = car.getCarInfo();
-      if (step >= winnerPoint) this.#winner.push(name);
+
+      if (step === winnerPoint) this.#winner.push(name);
     });
   }
 
-  getWinner() {
-    return this.#winner;
+  #printWinner() {
+    const message = `\n${OUTPUT_MESSAGE.winner}: ${this.#winner.join(',')}`;
+
+    OutputView.printMessage(message);
+  }
+
+  getGameResult() {
+    this.#judgementWinner();
+    this.#printWinner();
   }
 }
 
