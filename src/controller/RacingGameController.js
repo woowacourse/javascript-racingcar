57b59e1aa5771ecbc9dc6bcb86e18f7009a10f96
bmj0053 <bmj05378@gmail.com>
2024-02-15 15:47:47 +0900

    feat(phase 4): 우승자 확인 기능 구현 1. RacingWinnerRecorder 모듈 추가 2. Controller 내 우승자 확인 로직 추가
    Co-authored-by: jinyoung234 <sm099@naver.com>

--- src/controller/RacingGameController.js
+++ src/controller/RacingGameController.js
@@ -2,6 +2,7 @@ import InputView from '../views/InputView.js';
 import ErrorHandler from '../errors/ErrorHandler/module.js';
 import RacingGame from '../domain/RacingGame/module.js';
 import RandomMoveCountMaker from '../domain/RandomMoveCountMaker/module.js';
+import RacingWinnerRecorder from '../domain/RacingWinnerRecorder/module.js';
 
 const RacingGameController = Object.freeze({
   async run() {
@@ -22,5 +23,7 @@ async function processUserInput() {
 function processRacingGame({ racingCarNames, tryCount }) {
   const randomMoveCounts = RandomMoveCountMaker.execute(tryCount, racingCarNames.length);
   const racingResult = RacingGame.startRace({ racingCarNames, tryCount, randomMoveCounts });
-  console.log(racingResult);
+
+  const finalRacingResult = racingResult.at(-1);
+  const racingWinners = RacingWinnerRecorder.createRacingWinners(finalRacingResult);
 }
