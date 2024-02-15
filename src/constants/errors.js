eb9a81e8d159fed840e03c0b0957fc458dbd375d
skiende74 <skiende74@gmail.com>
2024-02-15 13:57:46 +0900

    fix(errors): 에러메세지 trialPositive 수정 및 trialInteger 추가

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -3,7 +3,8 @@ const ERRORS = Object.freeze({
   carNameBlank: '[ERROR] 자동차 이름은 빈칸이 아니어야 합니다.',
   trialBlank: '[ERROR] 시도할 횟수는 빈칸이 아니어야 합니다.',
   trialNumber: '[ERROR] 시도할 횟수는 숫자여야 합니다.',
-  trialPositive: '[ERROR] 시도할 횟수는 자연수여야 합니다.',
+  trialPositive: '[ERROR] 시도할 횟수는 양수여야 합니다.',
+  trialInteger: '[ERROR] 시도할 횟수는 정수여야 합니다.',
 });
 
 export default ERRORS;
