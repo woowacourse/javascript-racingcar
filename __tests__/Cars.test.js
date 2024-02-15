bf66741a04c494a9438da807f8e1a4d5755ee964
skiende74 <skiende74@gmail.com>
2024-02-15 16:32:17 +0900

    test(Cars); 랜덤 값에 따른 전진 여부 테스트 구현

--- __tests__/Cars.test.js
+++ __tests__/Cars.test.js
@@ -1,3 +1,4 @@
+import CONDITIONS from '../src/constants/Conditions';
 import Cars from '../src/entities/Cars';
 
 describe('Cars', () => {
@@ -25,4 +26,17 @@ describe('Cars', () => {
       expect(() => new Cars(carsStr)).toThrow('[ERROR]');
     },
   );
+
+  test('랜덤값에 따른 전진 여부', ()=>{
+    // Arrange
+    const RANDOM_NUMBERS = [0,1,3,4,5,9];
+    const POSITIONS = RANDOM_NUMBERS.map(number=>number>=CONDITIONS.progressRandomThreshold?1:0);
+    const carsStr = 'pen, apple, car3, car4,car5,car6';
+    const cars = new Cars(carsStr);
+    cars.progress(RANDOM_NUMBERS);
+    // Act
+    const [_, positions] = cars.getState();
+    // Assert
+    expect(positions).toEqual(POSITIONS);
+  })
 });
