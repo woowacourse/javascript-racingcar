df821419e196a2cfa6dc0af4bd08bc6bff241bc8
badahertz52 <badahertz52@gmail.com>
2024-02-15 14:02:13 +0900

    fix: InputController에 대한 오류 수정
    - 이름, 진행 횟수가 유효하지 않을 때, 다음 게임 단계 진행이 되는 오류 수정
    - 오류 원인: catch 문에서 재귀함수를 비동기로 실행하지 않았음

--- src/controllers/InputController.js
+++ src/controllers/InputController.js
@@ -7,16 +7,15 @@ const InputController = {
     let result;
     try {
       const value = await InputView.readInput(INPUT_MESSAGE.name);
-      //유효성 검사 : 통과하지 못하면 throw Error
+
       CarValidator.confirm(value);
 
       result = value;
     } catch (error) {
       OutputView.printMessage(error.message);
 
-      this.getCarName();
+      await this.getCarName();
     }
-
     return result;
   },
 
@@ -24,16 +23,13 @@ const InputController = {
     let result;
     try {
       const value = await InputView.readInput(INPUT_MESSAGE.round);
-      //유효성 검사 -> 통과 못하면 throw error
       RoundValidator.confirmRound(value);
 
       result = value;
     } catch (error) {
       OutputView.printMessage(ERROR_MESSAGE);
-
-      this.getRoundNumber();
+      await this.getRoundNumber();
     }
-
     return result;
   },
 };
