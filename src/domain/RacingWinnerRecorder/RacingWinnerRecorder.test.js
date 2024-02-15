bff467f6e4f8fcc1915ef1e82f451b8d1ac80149
bmj0053 <bmj05378@gmail.com>
2024-02-15 15:52:23 +0900

    test(RacingWinnerRecorder): 레이싱 게임 우승자 생성 테스트 Co-authored-by: jinyoung234 <sm099@naver.com>

---
+++ src/domain/RacingWinnerRecorder/RacingWinnerRecorder.test.js
@@ -0,0 +1,33 @@
+import RacingWinnerRecorder from './module.js';
+
+describe('레이싱 게임 우승자 생성 테스트', () => {
+  // given
+  const testCases = [
+    {
+      description: '여러 차량이 동일한 최대 이동 횟수를 가질 때, 모든 해당 차량의 이름을 반환해야 한다',
+      finalRacingResults: [
+        { carName: 'CarA', moveCount: 4 },
+        { carName: 'CarB', moveCount: 4 },
+        { carName: 'CarC', moveCount: 2 },
+      ],
+      expectedRacingWinners: ['CarA', 'CarB'],
+    },
+    {
+      description: '단일 차량만 최대 이동 횟수를 가질 때, 해당 차량의 이름만 반환해야 한다',
+      finalRacingResults: [
+        { carName: 'CarD', moveCount: 1 },
+        { carName: 'CarE', moveCount: 1 },
+        { carName: 'CarF', moveCount: 3 },
+      ],
+      expectedRacingWinners: ['CarF'],
+    },
+  ];
+
+  test.each(testCases)('$description', ({ finalRacingResults, expectedRacingWinners }) => {
+    // when
+    const racingWinners = RacingWinnerRecorder.createRacingWinners(finalRacingResults);
+
+    // then
+    expect(racingWinners).toStrictEqual(expectedRacingWinners);
+  });
+});
