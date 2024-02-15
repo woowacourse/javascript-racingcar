fc9090fbcaf1c8e15518131a825a5cf3ca39c9ed
0jenn0 <jenn0.6n@gmail.com>
2024-02-15 14:09:25 +0900

    Refactor: Controller 메서드 분리
    Co-authored-by: lurgi <wahoo9040@naver.com>

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -5,27 +5,63 @@ import OutputView from "../View/OutputView.js";
 export default class Controller {
   #cars;
 
+  #input = new InputView();
+
+  #output = new OutputView();
+
   async run() {
-    const inputView = new InputView();
-    const outputView = new OutputView();
+    await this.#promptCarNames();
+    const tryNum = await this.#promptTry();
 
-    const carNames = await inputView.readCars();
-    this.#cars = carNames.map((name) => new Car(name));
+    this.#runRace(tryNum);
+    this.#output.printResultTitle();
 
-    const tryNum = await inputView.readTry();
-
-    for (let i = 0; i < tryNum; i++) {
-      this.#cars.forEach((car) => {
-        car.move(this.makeRandomNumber());
-        outputView.printCarCurrentDistance(car);
-      });
-      outputView.printBlank();
-    }
-    outputView.printResultTitle();
-    outputView.printWinner(this.#cars);
+    const calculValue = this.calculateWinners(this.cars);
+    this.#output.printWinner(calculValue);
   }
 
-  makeRandomNumber() {
+  async #promptCarNames() {
+    try {
+      const carNames = await this.#input.readCars();
+      this.#cars = carNames.map((name) => new Car(name));
+    } catch (error) {
+      //TODO: print Error
+      await this.#promptCarNames();
+    }
+  }
+
+  async #promptTry() {
+    try {
+      const tryNum = await this.#input.readTry();
+      return tryNum;
+    } catch (error) {
+      //TODO print Error
+      await this.#promptTry();
+    }
+  }
+
+  #runRace(tryNum) {
+    for (let i = 0; i < tryNum; i++) {
+      this.#cars.forEach((car) => {
+        car.move(this.#makeRandomNumber());
+        this.#output.printCarCurrentDistance(car);
+      });
+      this.#output.printBlank();
+    }
+  }
+
+  #makeRandomNumber() {
     return Math.floor(Math.random() * 10);
+  }
+
+  calculateWinners(cars) {
+    const maxDistance = Math.max(...cars.map((car) => car.getDistance()));
+    if (maxDistance) {
+      const winners = cars.filter((car) =>
+        car.getDistance() === maxDistance ? true : false
+      );
+      return { hasWinner: true, maxDistance, winners };
+    }
+    return { hasWinner: false, maxDistance, winners: [] };
   }
 }
