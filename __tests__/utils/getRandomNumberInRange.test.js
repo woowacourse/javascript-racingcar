9c9457ef76b4f7be726aeaa192adf04e790e851c
ss0526100 <ss0526100@gmail.com>
2024-02-15 11:28:56 +0900

    test: getRandomNumberInRange 테스트

---
+++ __tests__/utils/getRandomNumberInRange.test.js
@@ -0,0 +1,59 @@
+import getRandomNumberInRange from '../../src/utils/getRandomNumberInRange.js';
+
+const rangeCheckCase = [
+  [0, 9],
+  // [-Infinity, Infinity], *TODO: (-Infinity + Infinity) -> NaN
+  [1, 1],
+  [-1.123123123, -1.123123123],
+];
+const typeCheckCase = [
+  ['1', '1'],
+  [BigInt(10), BigInt(123)],
+  [null, null],
+  [undefined, undefined],
+  [Symbol(), Symbol()],
+  [true, false],
+  [{}, {}],
+  [[], []],
+];
+const lowerAndUpperCheckCase = [9, 0];
+describe('utils: getRandomNumberInRange 검증', () => {
+  test.skip.each(rangeCheckCase)(
+    '범위 내 숫자 나오는지 1만회 검증',
+    //Arrange
+    (lower, upper) => {
+      const maxCount = 10_000;
+      for (let count = 0; count < maxCount; count++) {
+        //Act
+        const result = getRandomNumberInRange(lower, upper);
+        //Assert
+        expect(result).not.toBeNaN();
+        expect(result).toBeGreaterThanOrEqual(lower);
+        expect(result).toBeLessThanOrEqual(upper);
+      }
+    }
+  );
+  describe('예외 검증', () => {
+    test.skip('대소 검증', () => {
+      // Arrange
+      const [lower, upper] = lowerAndUpperCheckCase;
+      //Assert
+      expect(
+        //Act
+        () => getRandomNumberInRange(lower, upper)
+      ).toThrow();
+    });
+
+    test.skip.each(typeCheckCase)(
+      '타입 검증',
+      //Arrange
+      (lower, upper) => {
+        //Assert
+        expect(
+          //Act
+          () => getRandomNumberInRange(lower, upper)
+        ).toThrow();
+      }
+    );
+  });
+});
--- src/utils/getRandomNumberInRange.js
+++ src/utils/getRandomNumberInRange.js
@@ -1,5 +1,8 @@
+// [-Infinity, Infinity], *TODO: (-Infinity + Infinity) -> NaN
 function getRandomNumberInRange(lower, upper) {
-  if (lower > upper) [lower, upper] = [upper, lower];
+  if (typeof lower !== 'Number' || typeof upper !== 'Number')
+    throw new Error('[ERROR]Number가 아닌 인자가 들어옴');
+  if (lower > upper) throw new Error('[ERROR]lower가 upper보다 큼');
 
   return lower + Math.random() * (upper - lower);
 }
