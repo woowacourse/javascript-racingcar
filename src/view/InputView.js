58c2acf34f73060b32d9f289e92b6f56c372b4af
Lee sang Yeop <mma7710@naver.com>
2024-02-15 12:12:15 +0900

    feat(InputView): 자동차 이름과 시도 횟수를 잘못  입력 시 다시 입력

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,21 +2,32 @@ import { INPUT_MESSSAGES } from '../constant/messages.js';
 import readLineAsync from '../utils/readLineAsync.js';
 import splitByDelimiter from '../utils/splitByDelimiter.js';
 import Validator from '../utils/validator.js';
+import OutputView from './OutputView.js';
 
 const InputView = {
   async readCarNames() {
-    const input = await readLineAsync(INPUT_MESSSAGES.carNames);
-    const splitCarNames = splitByDelimiter(input);
+    try {
+      const input = await readLineAsync(INPUT_MESSSAGES.carNames);
+      const splitCarNames = splitByDelimiter(input);
 
-    Validator.carNames(splitCarNames);
-    return splitCarNames;
+      Validator.carNames(splitCarNames);
+      return splitCarNames;
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.readCarNames();
+    }
   },
 
   async readMoveCount() {
-    const input = await readLineAsync(INPUT_MESSSAGES.moveCount);
+    try {
+      const input = await readLineAsync(INPUT_MESSSAGES.moveCount);
 
-    Validator.tryCount(input);
-    return Number(input);
+      Validator.tryCount(input);
+      return Number(input);
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.readMoveCount();
+    }
   },
 };
 
