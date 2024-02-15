f9064be702eaff39314301ff26de58c86f459515
jaeml06 <jaeml0630@gmail.com>
2024-02-15 11:30:11 +0900

    refactor: 불필요한 변수 선언 제거
    불필요한 변수 선언 제거

--- src/InputView.js
+++ src/InputView.js
@@ -3,13 +3,11 @@ import { INPUT_MESSAGE } from './constants';
 
 const InputView = {
   async queryCarName() {
-    const carNames = await Console.readLineAsync(INPUT_MESSAGE.CAR_NAME_INPUT);
-    return carNames;
+    return await Console.readLineAsync(INPUT_MESSAGE.CAR_NAME_INPUT);
   },
 
   async queryTryCount() {
-    const tryCount = await Console.readLineAsync(INPUT_MESSAGE.TRY_COUNT_INPUT);
-    return tryCount;
+    return await Console.readLineAsync(INPUT_MESSAGE.TRY_COUNT_INPUT);
   },
 };
 
