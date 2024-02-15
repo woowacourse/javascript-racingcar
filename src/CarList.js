b58654bf99e23ae030b3c2568b768b664cc56bb6
rbgksqkr <rbgks1937@naver.com>
2024-02-15 11:00:29 +0900

    refactor: Car를 인스턴스로 갖는 CarList 클래스를 구현하여 리팩토링

---
+++ src/CarList.js
@@ -0,0 +1,40 @@
+import Car from './Car';
+
+class CarList {
+  #carList;
+
+  constructor(carNameList) {
+    this.#carList = carNameList.map((carName) => new Car(carName));
+  }
+
+  printCurrentPosition() {
+    this.#carList.forEach((car) => {
+      car.move();
+      car.printPosition();
+    });
+    console.log();
+  }
+
+  #getMaxPosition() {
+    let finalPosition = 0;
+    this.#carList.forEach((car) => {
+      const carData = car.getData();
+      finalPosition = Math.max(finalPosition, carData.position);
+    });
+    return finalPosition;
+  }
+
+  #getFinalCarList() {
+    const finalPosition = this.#getMaxPosition();
+    return this.#carList.filter((car) => {
+      const carData = car.getData();
+      return carData.position === finalPosition;
+    });
+  }
+
+  getWinner() {
+    return this.#getFinalCarList().map((car) => car.getData().name);
+  }
+}
+
+export default CarList;
