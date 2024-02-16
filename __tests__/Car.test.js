883b9e0786d76f06fa0d79d512e20ba504fee1d7
Seongjin Hong <hongseongjin.to@gmail.com>
2024-02-17 01:35:15 +0900

    test: Car 클래스에 대한 단위 테스트 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,8 +1,7 @@
 import Car from '../src/Car';
-import { CAR_TEST_MESSAGE } from '../src/constants/testMessage';
 
-describe(CAR_TEST_MESSAGE.TITLE, () => {
-  test(CAR_TEST_MESSAGE.MOVE_FORWARD, () => {
+describe('Car 클래스 테스트', () => {
+  test('자동차는 이름을 가지며 move 메서드 실행 시 전진할 수 있어야 한다.', () => {
     // Arrange
     const car = new Car('아르');
 
