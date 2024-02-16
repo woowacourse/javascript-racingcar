cb1e88d022343a25bcedf47f424dda43e0bbf60a
Seongjin Hong <hongseongjin.to@gmail.com>
2024-02-16 22:04:41 +0900

    refactor: 전체 경주 로직을 전담하는 클래스로서 CarList를 Race로 재편

--- src/Race.js
+++ src/Race.js
@@ -2,7 +2,6 @@ import Car from './Car';
 import OutputView from './views/OutputView';
 import CONFIG from './constants/config';
 import { MESSAGE } from './constants/message';
-import pickRandomNumber from './utils/pickRandomNumber';
 
 class Race {
   #carList;
@@ -14,38 +13,38 @@ class Race {
   runRace(turnCount) {
     OutputView.print(MESSAGE.RACE_RESULT);
     Array.from({ length: turnCount }).forEach(() => {
-      this.proceedTurn(this.#carList);
-      OutputView.printTurnResult(this.getTurnResult(this.#carList));
+      const turnResult = this.#getTurnResult();
+      OutputView.printTurnResult(turnResult);
     });
   }
 
-  proceedTurn(carList) {
-    carList.forEach((car) => {
-      const randomNumber = pickRandomNumber();
-      this.moveCar(car, randomNumber);
+  #getTurnResult() {
+    return this.#carList.map((car) => {
+      this.#moveCar(car);
+      return {
+        name: car.name,
+        position: car.position,
+      };
     });
   }
 
-  getTurnResult(carList) {
-    return carList.map((car) => ({
-      name: car.name,
-      position: car.position,
-    }));
-  }
-
-  isMetConditionToMoveCar(pickedNumber) {
-    return pickedNumber >= CONFIG.CAR_MOVING_CONDITION;
-  }
-
-  moveCar(car, randomNumber) {
-    if (this.isMetConditionToMoveCar(randomNumber)) {
+  #moveCar(car) {
+    if (this.#isMetConditionToMoveCar(this.#pickRandomNumber())) {
       car.move();
     }
   }
 
-  getWinner(carList = this.#carList) {
-    const maxPosition = Math.max(...carList.map((car) => car.position));
-    const winners = carList.filter((car) => car.position === maxPosition);
+  #isMetConditionToMoveCar(pickedNumber) {
+    return pickedNumber >= CONFIG.CAR_MOVING_CONDITION;
+  }
+
+  #pickRandomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  get winner() {
+    const maxPosition = Math.max(...this.#carList.map((car) => car.position));
+    const winners = this.#carList.filter((car) => car.position === maxPosition);
     return winners.map((car) => car.name);
   }
 }
