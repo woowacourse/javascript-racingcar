90ca21327c2f39c0a52a444258e3eac79472d155
soosoo22 <lllllh00@naver.com>
2024-02-17 00:45:36 +0900

    refactor: 0에서 9사이의 랜덤 값을 추출하는 코드 수정

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,8 +1,8 @@
-import Random from '../src/utils/Random.js';
+import Random from "../src/utils/Random.js";
 
-describe('랜덤 숫자 생성 테스트', () => {
-  test('0에서 9 사이의 값을 생성한다', () => {
-    const result = Random.pickNumberZeroToNine();
+describe("랜덤 숫자 생성 테스트", () => {
+  test("0에서 9 사이의 값을 생성한다", () => {
+    const result = Random.pickNumber(0, 9);
 
     expect(Number.isInteger(result)).toBe(true);
     expect(result).toBeLessThanOrEqual(9);
