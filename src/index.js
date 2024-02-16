4bdd6a878b84a4e3330cc3fa52e041a20e7eb1f0
Seongjin Hong <hongseongjin.to@gmail.com>
2024-02-16 22:24:50 +0900

    refactor: App 클래스에서 불필요한 #carList 필드를 제거하고 play 메서드의 코드 흐름을 간소화

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-import CarList from './CarList';
-import { MESSAGE } from './constants/message';
+import Race from './Race';
 import InputView from './views/InputView';
 import OutputView from './views/OutputView';
 
 class App {
-  #carList;
-
   async play() {
     const carNameList = await InputView.readCarNameList();
-    this.#carList = new CarList(carNameList);
     const turnCount = await InputView.readTurnCount();
-    this.#race(turnCount);
-    const winners = this.#carList.getWinner();
-    OutputView.printWinners(winners);
-  }
 
-  #race(turnCount) {
-    OutputView.print(MESSAGE.RACE_RESULT);
-    Array.from({ length: turnCount }).forEach(() => {
-      this.#carList.printCurrentPosition();
-    });
+    const race = new Race(carNameList);
+    race.runRace(turnCount);
+
+    OutputView.printWinners(race.winner);
   }
 }
 
