33f61c776cc50b874c17196da9d044f5b29888f4
skiende74 <skiende74@gmail.com>
2024-02-16 16:27:01 +0900

    refactor(Car): Cars에서 Car 클래스를 분리

---
+++ src/entities/Car.js
@@ -0,0 +1,41 @@
+class Car {
+  #name;
+  #position;
+
+  constructor(name) {
+    this.#validate(name);
+    this.#name = name;
+    this.#position = 0;
+  }
+
+  #validate(carStr) {
+    this.#validateLength(carStr);
+    this.#validateBlank(carStr);
+  }
+
+  #validateLength(name) {
+    if (name.length > CONDITIONS.maxCarNameLength) {
+      throw new Error(ERRORS.carNameLength);
+    }
+  }
+
+  #validateBlank(name) {
+    if (name.length === 0) {
+      throw new Error(ERRORS.carNameBlank);
+    }
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getPosition() {
+    return this.#position;
+  }
+
+  moveForward() {
+    this.#position++;
+  }
+}
+
+export default Car;
