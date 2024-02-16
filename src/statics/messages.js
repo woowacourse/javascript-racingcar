5871ce86ccd7e698ae7eb25cbf8239344c93445a
jayming66 <mjw8941@gmail.com>
2024-02-16 20:02:31 +0900

    refactor: error message의 error prefix를 메시지와 분리

--- src/statics/messages.js
+++ src/statics/messages.js
@@ -16,9 +16,11 @@ export const OUTPUT_MESSAGES = Object.freeze({
   },
 });
 
+const PREFIX_ERROR = '[ERROR]';
+
 export const ERROR_MESSAGES = Object.freeze({
-  invalidAttemptNum: '[ERROR] 0 이상의 정수를 입력해 주세요.',
-  hasRedundantCarName: '[ERROR] 중복된 자동차 이름이 있습니다.',
-  invalidCarName: '[ERROR] 자동차 이름 입력이 올바르지 않습니다',
-  hasSingleCar: '[ERROR] 두 대 이상의 자동차를 입력해 주세요.',
+  invalidAttemptNum: `${PREFIX_ERROR} 0 이상의 정수를 입력해 주세요.`,
+  hasRedundantCarName: `${PREFIX_ERROR} 중복된 자동차 이름이 있습니다.`,
+  invalidCarName: `${PREFIX_ERROR} 자동차 이름 입력이 올바르지 않습니다`,
+  hasSingleCar: `${PREFIX_ERROR} 두 대 이상의 자동차를 입력해 주세요.`,
 });
