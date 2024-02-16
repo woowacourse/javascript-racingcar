af2f43162eacb6874c9d808076b6809f8f7e0f73
soosoo22 <lllllh00@naver.com>
2024-02-16 23:48:45 +0900

    refactor: UPPERCASE로 변경된 상수 객체 내부명을 CarValidator 클래스에 적용시킴

--- src/utils/CarValidator.js
+++ src/utils/CarValidator.js
@@ -1,25 +1,25 @@
-import Condition from '../constant/Condition.js';
-import Message from '../constant/Message.js';
+import Condition from "../constant/Condition.js";
+import Message from "../constant/Message.js";
 
 const { CAR, CAR_NAME } = Condition;
 const { ERROR } = Message;
 
 class CarValidator {
   static validCount(cars) {
-    if (cars.length < CAR.count.min) {
-      throw new Error(ERROR.car_count);
+    if (cars.length < CAR.COUNT.MIN) {
+      throw new Error(ERROR.CAR_COUNT);
     }
   }
 
   static duplicateName(cars) {
     if (new Set(cars.map((car) => car.getName())).size !== cars.length) {
-      throw new Error(ERROR.car_name_duplicate);
+      throw new Error(ERROR.CAR_NAME_DUPLICATE);
     }
   }
 
   static validNameRange(carName) {
-    if (carName.length < CAR_NAME.range.min || carName.length > CAR_NAME.range.max) {
-      throw new Error(ERROR.car_name_range);
+    if (carName.length < CAR_NAME.RANGE.MIN || carName.length > CAR_NAME.RANGE.MAX) {
+      throw new Error(ERROR.CAR_NAME_RANGE);
     }
   }
 }
