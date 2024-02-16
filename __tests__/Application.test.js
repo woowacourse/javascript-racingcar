4b03951a48ba7fedb6b1e395c4c7237e6a9db1c7
hwinkr <dnddl8280@naver.com>
2024-02-16 22:13:13 +0900

    test: 랜덤 함수 모킹 방법 변경 & 코드 중복 제거
    - reduce를 사용한 모킹 방법에서 forEach를 사용한 모킹 방법으로 변경
    - 모든 테스트 케이스에서 중복 호출되는 mockRandoms, mockQuestions를each 함수를 사용해서 한번만 호출하도록 변경

--- __tests__/Application.test.js
+++ __tests__/Application.test.js
@@ -1,16 +1,16 @@
-import App from '../src/App';
-import pickNumberInRange from '../src/utils/pickNumberInRange';
-import Console from '../src/utils/Console';
-import { ERROR_MESSAGES } from '../src/constants/car-race';
+import App from "../src/App";
+import pickNumberInRange from "../src/utils/pickNumberInRange";
+import Console from "../src/utils/Console";
+import { ERROR_MESSAGES } from "../src/constants/car-race";
 
-jest.mock('../src/utils/pickNumberInRange', () => {
+jest.mock("../src/utils/pickNumberInRange", () => {
   return jest.fn();
 });
 
 const mockRandoms = (numbers) => {
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, pickNumberInRange);
+  numbers.forEach((number) => {
+    pickNumberInRange.mockReturnValueOnce(number);
+  });
 };
 
 const mockQuestions = (inputs) => {
@@ -23,24 +23,24 @@ const mockQuestions = (inputs) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(Console, 'print');
+  const logSpy = jest.spyOn(Console, "print");
   logSpy.mockClear();
 
   return logSpy;
 };
 
-describe('자동차 경주 애플리케이션 테스트', () => {
-  it('전진-정지 출력 테스트', async () => {
+describe("자동차 경주 애플리케이션 테스트", () => {
+  it("정상적인 전진-정지의 경우 올바르게 출력 되는지 테스트", async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
     const randoms = [MOVING_FORWARD, STOP];
 
-    const inputs = ['harry,bong', '1'];
-    const harryOutput = ['harry : -'];
-    const bongOutput = ['bong :'];
-    const logSpy = getLogSpy();
+    const inputs = ["harry,bong", "1"];
+    const harryOutput = ["harry : -"];
+    const bongOutput = ["bong :"];
 
+    const logSpy = getLogSpy();
     // when
     mockQuestions(inputs);
     mockRandoms(randoms);
@@ -53,61 +53,46 @@ describe('자동차 경주 애플리케이션 테스트', () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
-
-  it('자동차 이름 중복에 대한 예외 테스트', async () => {
-    // given
-    const inputs = ['harry,harry', 'harry,bong', '5'];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(
-      expect.stringContaining(ERROR_MESSAGES.carNameUniqueness),
-    );
-  });
-
-  it('자동차 이름 길이에 대한 예외 테스트', async () => {
-    // given
-    const inputs = ['harrrrrry,harry', 'harry,bong', '5'];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(
-      expect.stringContaining(ERROR_MESSAGES.carNameLength),
-    );
-  });
-
-  it('자동차 경주 시도 횟수에 대한 예외 테스트', async () => {
-    // given
-    const inputs = ['harry,bong', 'a', '10'];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-    await app.run();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(
-      expect.stringContaining(ERROR_MESSAGES.tryCount),
-    );
-  });
 });
 
-/* 
-harry, harry -> ERROR
-undefined -> split method net exists
-harry, bong -> tryCount -> undefined ->  [ERROR] 시도할 횟수는 1이상의 숫자만 가능합니다.
-*/
+describe("자동차 경주 입력에 대한 예외 테스트", () => {
+  const TEST_CASES = [
+    {
+      inputs: ["harry,harry", "harry,bong", "5"],
+      expectedErrorMessage: ERROR_MESSAGES.carNameUniqueness,
+    },
+    {
+      inputs: ["harrrrrry,harry", "harry,bong", "5"],
+      expectedErrorMessage: ERROR_MESSAGES.carNameLength,
+    },
+    {
+      inputs: ["harry,bong", "a", "10"],
+      expectedErrorMessage: ERROR_MESSAGES.invalidNumberType,
+    },
+    {
+      inputs: ["harry,bong", "-1", "10"],
+      expectedErrorMessage: ERROR_MESSAGES.negativeTryCount,
+    },
+  ];
+
+  let logSpy;
+
+  beforeEach(() => {
+    logSpy = getLogSpy();
+  });
+
+  test.each(TEST_CASES)(
+    "각 예외 상황에 맞는 에러 메시지를 콘솔에 출력하는지 테스트",
+    async ({ inputs, expectedErrorMessage }) => {
+      console.log(inputs, expectedErrorMessage);
+      mockQuestions(inputs);
+
+      const app = new App();
+      await app.run();
+
+      expect(logSpy).toHaveBeenCalledWith(
+        expect.stringContaining(expectedErrorMessage)
+      );
+    }
+  );
+});
