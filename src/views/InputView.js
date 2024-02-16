dcc92453dea3ff6924f01fa4973abada94551642
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-02-17 00:40:09 +0900

    refactor: 메시지에서 개행 문자 분리 및 템플릿 리터럴 사용

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,21 +1,20 @@
+import { INPUT_QUERY } from '../constants/car-race';
 import Console from '../utils/Console';
 
 const InputView = {
   async readInput(query) {
-    const input = await Console.readLineAsync(query);
+    const input = await Console.readLineAsync(query + '\n');
     return input;
   },
 
   async readCarNames() {
-    const carNames = await this.readInput(
-      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
-    );
+    const carNames = await this.readInput(INPUT_QUERY.carNames);
 
     return carNames;
   },
 
   async readTryCount() {
-    const tryCount = await this.readInput('시도할 횟수는 몇 회인가요?');
+    const tryCount = await this.readInput(INPUT_QUERY.tryCount);
     return tryCount;
   },
 };
