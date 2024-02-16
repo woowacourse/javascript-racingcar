3e22483634a51365859e400159efa936bd58ade1
soosoo22 <lllllh00@naver.com>
2024-02-16 23:22:33 +0900

    refactor: 상수 객체 내부명을 UPPERCASE로 변경
    - 상수임을 뚜렷하게 드러낼 수 있게 UPPERCASE로 변경함
    - 자동차 이름의 길이 제한을 RANGE 폴더 내에 정의한 상수를 사용하여 변경함

--- src/constant/Message.js
+++ src/constant/Message.js
@@ -1,25 +1,26 @@
-const PREFIX = '[ERROR]';
+import Condition from "./Condition.js";
+
+const { CAR_NAME } = Condition;
+
+const PREFIX = "[ERROR]";
 
 const ERROR = {
-  query: `${PREFIX} 쿼리는 공백이 아닌 문자열이어야 한다.`,
-  null: `${PREFIX} 입력은 공백이 될 수 없습니다.`,
-  car_count: `${PREFIX} 자동차 대수는 2대 이상만 가능하다.`,
-  car_name_duplicate: `${PREFIX} 자동차 이름은 중복될 수 없다.`,
-  car_name_range: `${PREFIX} 자동차 이름은 1자 이상, 5자 이하만 가능하다.`,
-  try_count_range: `${PREFIX} 시도 횟수는 자연수여야 합니다.`,
+  QUERY: `${PREFIX} 쿼리는 공백이 아닌 문자열이어야 한다.`,
+  NULL: `${PREFIX} 입력은 공백이 될 수 없습니다.`,
+  CAR_COUNT: `${PREFIX} 자동차 대수는 2대 이상만 가능하다.`,
+  CAR_NAME_DUPLICATE: `${PREFIX} 자동차 이름은 중복될 수 없다.`,
+  CAR_NAME_RANGE: `${PREFIX} 자동차 이름은 ${CAR_NAME.RANGE.MIN}자 이상, ${CAR_NAME.RANGE.MAX}자 이하만 가능하다.`,
+  TRY_COUNT_RANGE: `${PREFIX} 시도 횟수는 자연수여야 합니다.`,
 };
 
 const INPUT = {
-  car_name: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
-  try_count: '시도할 횟수는 몇 회인가요?\n',
+  CAR_NAME: "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n",
+  TRY_COUNT: "시도할 횟수는 몇 회인가요?\n",
 };
 
 const OUTPUT = {
-  result: '\n실행 결과',
-  winner: '최종 우승자:',
-  colon: ':',
-  advance: '-',
-  combiner: ', ',
+  RESULT: "\n실행 결과",
+  WINNER: "최종 우승자:",
 };
 
 export default {
