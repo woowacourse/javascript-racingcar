8ed49f8a1f7c52e99f1efaeffe2427b104b814de
soosoo22 <lllllh00@naver.com>
2024-02-17 00:17:28 +0900

    refactor: 테스트 코드에 대한 설명을 변경
    에러가 발생하는 상황과 동작을 명확하게 명시함

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,28 +1,28 @@
-import CarValidator from '../src/utils/CarValidator.js';
-import TryCountValidator from '../src/utils/TryCountValidator.js';
+import CarValidator from "../src/utils/CarValidator.js";
+import TryCountValidator from "../src/utils/TryCountValidator.js";
 
-describe('자동차 이름 유효성 테스트', () => {
-  test('자동차 대수가 2대 미만이면 에러가 발생한다.', () => {
-    const input = ['파슬리'];
+describe("자동차 이름 유효성 테스트", () => {
+  test("자동차 대수가 2대 미만이면 에러가 발생한다.", () => {
+    const input = ["파슬리"];
     expect(() => CarValidator.isValidCount(input)).toThrow();
   });
 
-  test.each(['', '파슬리쑤쑤쿠키썬데이'])(
-    '자동차 이름이 1자 미만 또는 5자 초과면 에러가 발생한다.',
+  test.each(["", "파슬리쑤쑤쿠키썬데이"])(
+    "자동차 이름이 1자 미만 또는 5자 초과면 에러가 발생한다.",
     (input) => {
       expect(() => CarValidator.isValidNameRange(input)).toThrow();
     }
   );
 
-  test('자동차 이름이 중복되면 에러가 발생한다.', () => {
-    const input = ['파슬리', '파슬리', '쑤쑤'];
+  test("자동차 이름이 중복되면 에러가 발생한다.", () => {
+    const input = ["파슬리", "파슬리", "쑤쑤"];
     expect(() => CarValidator.isNameDuplicate(input)).toThrow();
   });
 });
 
-describe('경주 횟수 유효성 테스트', () => {
-  test.each(['ㄱ', 'a', '0', '-1', '5.5', '&'])(
-    '경주 횟수가 자연수가 아니라면 발생한다.',
+describe("경주 횟수 유효성 테스트", () => {
+  test.each(["ㄱ", "a", "0", "-1", "5.5", "&"])(
+    "경주 횟수로 자연수가 아닌 값이 입력되면 에러가 발생한다.",
     (input) => {
       expect(() => TryCountValidator.isNaturalNumber(Number(input))).toThrow();
     }
