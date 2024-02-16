a6dd83ca3ee6d50ad06af3be5252593637594724
ImxYJL <allensain14@gmail.com>
2024-02-16 15:53:57 +0900

    refactor: StringParser를 객체로 수정, splitCarNames 내장함수로 간편화

--- src/utils/StringParser.js
+++ src/utils/StringParser.js
@@ -1,20 +1,16 @@
 import MESSAGE from "../constants/Message.js";
 
-class StringParser {
-  static splitCarNames(input) {
-    const carNamesArray = input.split(",");
+const StringParser = {
+  splitCarNames: (input) => {
+    return input
+      .split(",")
+      .map((carName) => carName.trim())
+      .filter((name) => name !== ""); // // 공백으로만 이루어진 이름은 불허
+  },
 
-    const result = [];
-    carNamesArray.forEach((carName) => {
-      result.push(carName.trim());
-    });
-
-    return result;
-  }
-
-  static concatElements(array) {
+  concatElements: (array) => {
     return array.join(MESSAGE.championSeparator);
-  }
-}
+  },
+};
 
 export default StringParser;
