07f8514586affa2afbf5387d903189964fa8cc4a
hwinkr <dnddl8280@naver.com>
2024-02-16 22:18:00 +0900

    chore(split error message): 에러 메시지 세분화

--- src/validators/tryCountValidator.js
+++ src/validators/tryCountValidator.js
@@ -1,16 +1,16 @@
-import { ERROR_MESSAGES } from '../constants/car-race';
-import InvalidInputException from '../exceptions/InvalidInputException';
+import { ERROR_MESSAGES } from "../constants/car-race";
+import InvalidInputException from "../exceptions/InvalidInputException";
 
 const tryCountValidator = {
   isNumeric(tryCount) {
     if (!Number.isInteger(tryCount)) {
-      throw new InvalidInputException(ERROR_MESSAGES.tryCount);
+      throw new InvalidInputException(ERROR_MESSAGES.invalidNumberType);
     }
   },
 
   isPositive(tryCount) {
     if (tryCount <= 0) {
-      throw new InvalidInputException(ERROR_MESSAGES.tryCount);
+      throw new InvalidInputException(ERROR_MESSAGES.negativeTryCount);
     }
   },
 
