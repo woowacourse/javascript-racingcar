69ffcd845e0c2d6eca4418f5b7939912632388fb
Seongjin Hong <hongseongjin.to@gmail.com>
2024-02-16 22:38:21 +0900

    refactor: 입력값 검증 및 재입력 요청 역할을 InputView에서 App으로 위임

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,35 +1,16 @@
-import Validator from '../Validator';
+// import Validator from '../Validator';
 import { MESSAGE } from '../constants/message';
-
 import readLineAsync from '../utils/readLineAsync';
-import OutputView from './OutputView';
 
 const InputView = {
   async readCarNameList() {
-    try {
-      const carNameList = await this.getCarNameInput();
-      Validator.validateCarNameList(carNameList);
-      return carNameList;
-    } catch (error) {
-      OutputView.print(error.message);
-      return this.readCarNameList();
-    }
-  },
-
-  async getCarNameInput() {
     const carNameListInput = await readLineAsync(MESSAGE.CAR_NAME_LIST_INPUT);
     return carNameListInput.split(',').map((car) => car.trim());
   },
 
   async readTurnCount() {
-    try {
-      const turnCountInput = await readLineAsync(MESSAGE.TURN_COUNT_INPUT);
-      Validator.validateTurnCount(turnCountInput);
-      return parseInt(turnCountInput, 10);
-    } catch (error) {
-      OutputView.print(error.message);
-      return this.readTurnCount();
-    }
+    const turnCountInput = await readLineAsync(MESSAGE.TURN_COUNT_INPUT);
+    return turnCountInput;
   },
 };
 
