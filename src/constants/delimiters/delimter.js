5f929764e9d0c34a4a6d0c2566ce28372508a3be
brgndy <brgndy96@gmail.com>
2024-02-17 20:07:08 +0900

    Feat: 구분문자 관련 상수 객체 생성

--- src/constants/delimiters/delimter.js
+++ src/constants/delimiters/delimter.js
@@ -1,11 +1,17 @@
 import deepFreeze from '../../utils/deepFeeze.js';
 
 const DELIMITER = deepFreeze({
+<<<<<<< HEAD
   NONE: '',
   SPACE: ' ',
   COMMA: ',',
   HYPHEN: '-',
   COLON: ' : ',
+=======
+  SPACE: '',
+  COMMA: ',',
+  HYPHEN: '-',
+>>>>>>> 50117f4 (Feat: 구분문자 관련 상수 객체 생성)
 });
 
 export default DELIMITER;
