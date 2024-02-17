0e7d211c5c383253e079b48f7da23fab68748f5a
Kim Da Eun <midekuna@gmail.com>
2024-02-17 23:31:03 +0900

    test: Validator 테스트 코드 문구 수정 및 테스트 세분화

--- __tests__/domain/Validator.test.js
+++ __tests__/domain/Validator.test.js
@@ -5,67 +5,49 @@ import {
   isInvalidCarName,
 } from '../../src/domain/validate/validator';
 
-describe('Validator Test', () => {
-  const invalidCarNamesInput = [
-    {
-      input: 'aaaaaa,asdf',
-    },
-    {
-      input: 'asdf/asd',
-    },
-    {
-      input: 'as s, asd',
-    },
-    {
-      input: ' , asd, asdf',
-    },
-  ];
-  test.each(invalidCarNamesInput)(
-    'isInvalidCarName Test - 6자 이상, 쉼표가 아닌 구분자, 공백 문자를 포함한 입력이 들어올 경우 true를 반환한다.',
-    ({ input }) => {
-      expect(isInvalidCarName(input)).toBe(true);
+describe('자동차 이름 입력 Validator Test', () => {
+  test('isInvalidCarName Test - 이름이 6자 이상일 경우 true를 반환한다', () => {
+    const CARS_NAME = 'pobi,masterwee';
+    expect(isInvalidCarName(CARS_NAME)).toBe(true);
+  });
+
+  test('isInvalidCarName Test - 이름의 구분자가 쉼표(,)가 아닐 경우 true를 반환한다', () => {
+    const CARS_NAME = 'pobi/jay';
+    expect(isInvalidCarName(CARS_NAME)).toBe(true);
+  });
+
+  test.each(['po bi,jay', ' , ', ' '])(
+    'isInvalidCarName Test - 공백 문자를 포함한 입력이 들어올 경우 true를 반환한다.',
+    CARS_NAME => {
+      expect(isInvalidCarName(CARS_NAME)).toBe(true);
     },
   );
 
-  const redunDantCarNamesInput = [
-    {
-      input: 'pobi,pobi',
-    },
-    {
-      input: 'pobi,jay,pobi',
-    },
-  ];
-  test.each(redunDantCarNamesInput)('hasRedundantCarName Test - 이름이 중복됐을 경우 true를 반환한다.', ({ input }) => {
-    expect(hasRedundantCarName(input)).toBe(true);
-  });
-
-  const singleCarInput = [
-    {
-      input: 'pobi',
-    },
-  ];
-  test.each(singleCarInput)('hasSingleCar Test - 자동차가 1개만 입력됐을 경우 true를 반환한다.', ({ input }) => {
-    expect(hasSingleCar(input)).toBe(true);
-  });
-
-  const attemptNumInput = [
-    {
-      input: 's',
-    },
-    {
-      input: '0',
-    },
-    {
-      input: '-1',
-    },
-    {
-      input: '1.2',
-    },
-  ];
-  test.each(attemptNumInput)(
-    'isInvalidAttemptNum Test - 숫자가 아닌 문자, 1 이하의 정수, 소수가 입력됐을 경우 true를 반환한다.',
-    ({ input }) => {
-      expect(isInvalidAttemptNum(input)).toBe(true);
+  test.each(['pobi,pobi', 'pobi,jay,pobi'])(
+    'hasRedundantCarName Test - 이름이 중복됐을 경우 true를 반환한다.',
+    CARS_NAME => {
+      expect(hasRedundantCarName(CARS_NAME)).toBe(true);
     },
   );
+
+  test('hasSingleCar Test - 자동차가 1개만 입력됐을 경우 true를 반환한다.', () => {
+    const CAR_NAME = 'pobi';
+    expect(hasSingleCar(CAR_NAME)).toBe(true);
+  });
+});
+
+describe('시도 횟수 입력 Validator Test', () => {
+  test('isInvalidAttemptNum Test - 숫자가 아닌 문자가 입력됐을 경우 true를 반환한다.', () => {
+    const ATTEMPT_NUM = 's';
+    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBe(true);
+  });
+
+  test.each(['0', '-1'])('isInvalidAttemptNum Test - 1 이하의 정수가 입력됐을 경우 true를 반환한다.', ATTEMPT_NUM => {
+    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBe(true);
+  });
+
+  test('isInvalidAttemptNum Test - 소수가 입력됐을 경우 true를 반환한다.', () => {
+    const ATTEMPT_NUM = '1.2';
+    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBe(true);
+  });
 });
