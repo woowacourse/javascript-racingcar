59f1bc46387e7fbc81ee4e2178594d5cec4ff80e
useon <yoosk4986@naver.com>
2024-02-17 14:17:47 +0900

    fix: 10번 입력이 틀리면 프로그램이 종료되어야 하는데 다음 입력으로 넘어가는 문제 해결

--- src/utils/repeatFunctionUntilIsValid.js
+++ src/utils/repeatFunctionUntilIsValid.js
@@ -6,18 +6,17 @@ async function runMethod(method, params) {
     return result;
   } catch (error) {
     OutputView.printError(error);
-    return false;
+    return error;
   }
 }
 
 async function repeatFunctionUntilIsValid(method, params) {
-  let retryCount = 0;
-  while (retryCount < 10) {
+  const errorStack = [];
+  while (errorStack.length < 10) {
     const result = await runMethod(method, params);
-    if (result !== false) return result;
-    retryCount += 1;
+    errorStack.push(result);
   }
-  return OutputView.printError(new Error('\n입력 횟수를 초과하였습니다.'));
+  throw new Error('\n입력 횟수를 초과하였습니다.');
 }
 
 export default repeatFunctionUntilIsValid;
