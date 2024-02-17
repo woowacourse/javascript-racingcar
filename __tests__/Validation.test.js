6d50ab6d999bafd58f82896bd7b8e94e830d1837
greetings1012 <ahnqwerty@gmail.com>
2024-02-18 03:33:53 +0900

    refactor: Vaildation 테스트 코드에 상수 추가
    각 테스트가 무엇을 검증하기 위함인지 명확히 하기 위해 예외를 발생시키는 값을 상수로 선언

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,11 +1,15 @@
 import Validation from '../src/utils/Validation';
 import { ERROR_MESSAGE } from '../src/constants';
 
+const TOO_SHORT_NAME = ''
+
+const TOO_LONG_NAME = 'pobipobi'
+
 describe('사용자 입력 유효성 검사', () => {
-  test.each([[['pobi', 'jun', '']], [['pobipobi', 'jun', 'cron']]])(
+  test.each([[['pobi', 'jun', TOO_SHORT_NAME]], [[TOO_LONG_NAME, 'jun', 'cron']]])(
     '자동차 이름 길이 유효성 검사',
     (nameArray) => {
-      expect(() => Validation.carNamesArrayValidate(nameArray)).toThrow(
+      expect(() => Validation.carNamesValidate(nameArray)).toThrow(
         ERROR_MESSAGE.CAR_NAME_INPUT_ERROR.NOT_IN_RANGE
       );
     }
