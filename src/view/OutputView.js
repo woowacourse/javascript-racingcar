9ef322c49828673ba19984a5daeec474a45bf404
Largopie <106071687+Largopie@users.noreply.github.com>
2024-02-17 23:49:44 +0900

    chore(OutputView): 불필요한 주석 제거

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,7 +3,6 @@ const numberToDistanceSymbol = require('../utils/numberToDistanceSymbol.js');
 const { OUTPUT_MESSAGES } = require('../constant/messages.js');
 
 const OutputView = {
-  // 추후 수정
   printNewLine() {
     console.log();
   },
