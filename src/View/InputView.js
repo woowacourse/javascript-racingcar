ae053436addd973b361955808bc283d7dc873f2c
0jenn0 <jenn0.6n@gmail.com>
2024-02-17 14:23:36 +0900

    Refactor: InputView 내부 함수 은닉

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -5,31 +5,32 @@ import { SYMBOL } from "../Constants/Constants";
 const { PROMPT_CAR_NAMES, PROMPT_TRY } = VIEW_MESSAGES;
 const { DIVIDE_SYMBOL } = SYMBOL;
 
-const STD_OBJ = Object.freeze({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const readLineAsync = (prompt) => {
-  const rl = readLine.createInterface(STD_OBJ);
-  return new Promise((resolve) => {
-    rl.question(prompt, (answer) => {
-      resolve(answer);
-      rl.close();
-    });
+const InputView = (() => {
+  const STD_OBJ = Object.freeze({
+    input: process.stdin,
+    output: process.stdout,
   });
-};
 
-const InputView = {
-  async readCars() {
-    const carsName = await readLineAsync(PROMPT_CAR_NAMES);
-    return carsName.split(DIVIDE_SYMBOL);
-  },
-  async readTry() {
-    const tryNums = await readLineAsync(PROMPT_TRY);
+  const readLineAsync = (prompt) => {
+    const rl = readLine.createInterface(STD_OBJ);
+    return new Promise((resolve) => {
+      rl.question(prompt, (answer) => {
+        resolve(answer);
+        rl.close();
+      });
+    });
+  };
 
-    return Number(tryNums);
-  },
-};
+  return {
+    readCars: async () => {
+      const carsName = await readLineAsync(PROMPT_CAR_NAMES);
+      return carsName.split(DIVIDE_SYMBOL);
+    },
+    readTry: async () => {
+      const tryNums = await readLineAsync(PROMPT_TRY);
+      return Number(tryNums);
+    },
+  };
+})();
 
 export default InputView;
