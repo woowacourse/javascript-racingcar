ed07c680682ea89626549c5d3b67e74695d423b1
badahertz52 <badahertz52@gmail.com>
2024-02-17 23:16:57 +0900

    test: 우승자 판단 테스트에서 라운드 진행도 같이 태스트 하도록 수정

--- __tests__/Application.test.js
+++ __tests__/Application.test.js
@@ -4,12 +4,18 @@ import { SPACE } from '../src/constant';
 import { getLogSpy, mockQuestions, mockRandoms } from '../testUtils';
 
 describe('자동차 경주 테스트', () => {
-  test('랜덤 값에 따른 전진 판단과 우승자 선정', async () => {
+  test('입력한 횟수 만큼 게임 진행 후 우승자 선정', async () => {
     const carNameInput = 'pobi,woni,jun';
     const roundInput = '3';
     const randoms = [5, 3, 6, 2, 3, 9, 7, 8, 1];
     const winnerOutput = `최종 우승자: pobi,${SPACE}jun`;
-    const outputs = ['pobi : --', 'woni : -', 'jun : --', winnerOutput];
+    const outputs = [
+      `라운드:${roundInput}`,
+      'pobi : --',
+      'woni : -',
+      'jun : --',
+      winnerOutput,
+    ];
 
     const logSpy = getLogSpy();
 
