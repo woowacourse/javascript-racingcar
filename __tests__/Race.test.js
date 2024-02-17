cdd4d1a8927af07dbf5d3fb35983f52e2e3078b3
rbgksqkr <rbgks1937@naver.com>
2024-02-18 00:34:19 +0900

    refactor: 최종 우승자가 여러 명일 경우 우승자 테스트 케이스 작성

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -30,7 +30,7 @@ describe('자동차 전진 로직 검증', () => {
 });
 
 describe('자동차 경주 로직 검증', () => {
-  test('자동차 경주 진행하여 우승자를 판별한다.', () => {
+  test('현재 위치값이 가장 큰 최종 우승자 1명을 판별한다.', () => {
     // Arrange
     const CAR_NAME_LIST = ['마루', '아르'];
     const CAR_LIST = CAR_NAME_LIST.map((carName) => new Car(carName));
@@ -39,6 +39,26 @@ describe('자동차 경주 로직 검증', () => {
     const MAX_POSITION = 1;
     const race = new RaceController(CAR_NAME_LIST, TURN_COUNT);
     const RESULT_WINNER = ['마루'];
+
+    // Act
+    CAR_LIST.forEach((car, index) => {
+      race.moveCar(car, RANDOM_NUMBERS[index]);
+    });
+    const winners = raceWinnerController.getWinner(CAR_LIST, MAX_POSITION);
+
+    // Assert
+    expect(winners).toEqual(RESULT_WINNER);
+  });
+
+  test('최종 우승자가 여러 명일 경우 쉼표로 구분한다.', () => {
+    // Arrange
+    const CAR_NAME_LIST = ['마루', '아르'];
+    const CAR_LIST = CAR_NAME_LIST.map((carName) => new Car(carName));
+    const TURN_COUNT = CONFIG.MIN_TURN_COUNT;
+    const RANDOM_NUMBERS = [CONFIG.CAR_MOVING_CONDITION, CONFIG.CAR_MOVING_CONDITION];
+    const MAX_POSITION = 1;
+    const race = new RaceController(CAR_NAME_LIST, TURN_COUNT);
+    const RESULT_WINNER = ['마루', '아르'];
 
     // Act
     CAR_LIST.forEach((car, index) => {
