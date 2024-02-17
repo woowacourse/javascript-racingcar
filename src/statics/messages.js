6027bbb9a233b990469c791aa53edf151c75f230
Kim Da Eun <midekuna@gmail.com>
2024-02-17 22:46:09 +0900

    refactor: messages의 매직 넘버를 상수로 교체

--- src/statics/messages.js
+++ src/statics/messages.js
@@ -1,12 +1,12 @@
-import { SYMBOLS } from './constants';
+import { PREFIX, RULES, SYMBOLS } from './constants';
 
 export const INPUTS = Object.freeze({
-  carsName: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분). \n',
-  attemptNum: '시도할 횟수는 몇 회인가요? \n',
+  carsName: `경주할 자동차 이름을 입력하세요(이름은 쉼표(${SYMBOLS.nameSeperator})를 기준으로 구분). ${SYMBOLS.newLine}`,
+  attemptNum: `시도할 횟수는 몇 회인가요? ${SYMBOLS.newLine}`,
 });
 
 export const OUTPUTS = Object.freeze({
-  resultGuide: '\n실행 결과',
+  resultGuide: `${SYMBOLS.newLine}실행결과`,
 });
 export const OUTPUT_MESSAGE_GENERATORS = Object.freeze({
   nameAndCarPosition: ([name, position]) => {
@@ -19,8 +19,8 @@ export const OUTPUT_MESSAGE_GENERATORS = Object.freeze({
 });
 
 export const ERRORS = Object.freeze({
-  invalidAttemptNum: '[ERROR] 0 이상의 정수를 입력해 주세요.',
-  hasRedundantCarName: '[ERROR] 중복된 자동차 이름이 있습니다.',
-  invalidCarName: '[ERROR] 자동차 이름 입력이 올바르지 않습니다',
-  hasSingleCar: '[ERROR] 두 대 이상의 자동차를 입력해 주세요.',
+  invalidAttemptNum: `${PREFIX.error} ${RULES.minAttemptNum} 이상의 정수를 입력해 주세요.`,
+  hasRedundantCarName: `${PREFIX.error} 중복된 자동차 이름이 있습니다.`,
+  invalidCarName: `${PREFIX.error} 자동차 이름 입력이 올바르지 않습니다`,
+  hasSingleCar: `${PREFIX.error} ${RULES.minCarNameCnt}대 이상의 자동차를 입력해 주세요.`,
 });
