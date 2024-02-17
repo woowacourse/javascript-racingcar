a6ef2a6b77ebe3262a569f551307cb80899d737f
brgndy <brgndy96@gmail.com>
2024-02-17 20:07:57 +0900

    Feat: 비동기 제어 유틸함수 생성

--- src/utils/asyncFunctionHandlerWithError.js
+++ src/utils/asyncFunctionHandlerWithError.js
@@ -1,5 +1,3 @@
-import AppError from '../errors/AppError.js';
-import ERROR_MESSAGE from '../constants/messages/errorMessage.js';
 /**
  * 비동기 함수 핸들러 입니다.
  * 에러 발생시, 해당 에러 메세지를 출력하고 재귀적으로 비동기 함수를 다시 호출합니다.
@@ -8,31 +6,13 @@ import ERROR_MESSAGE from '../constants/messages/errorMessage.js';
  * @returns {Promise<*>}
  */
 
-const validateOfTypeFromInput = (input, type) => {
-  return new Promise((resolve, reject) => {
-    if (type === 'number') {
-      const num = Number(input);
-      if (isNaN(num)) {
-        return reject(new AppError(ERROR_MESSAGE.not_number));
-      }
-      return resolve(Number(num));
-    }
-
-    return resolve(input);
-  });
-};
-
-const asyncFunctionHandlerWithError = (query, type, resolve, rl) => {
-  rl.question(query, async (input) => {
-    try {
-      const validatedInput = await validateOfTypeFromInput(input, type);
-      rl.close();
-      resolve(validatedInput);
-    } catch (error) {
-      console.error(error.message);
-      asyncFunctionHandlerWithError(query, type, resolve);
-    }
-  });
+const asyncFunctionHandlerWithError = async (asyncFn, context) => {
+  try {
+    return await asyncFn.call(context);
+  } catch (error) {
+    console.error(error.message);
+    return asyncFunctionHandlerWithError(asyncFn, context);
+  }
 };
 
 export default asyncFunctionHandlerWithError;
