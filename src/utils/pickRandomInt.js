2fa91817ecc591aaa61fafe5e4f7f94901487256
0jenn0 <jenn0.6n@gmail.com>
2024-02-18 12:52:38 +0900

    Refactor: 함수의 불필요한 코드블락 return 삭제

--- src/utils/pickRandomInt.js
+++ src/utils/pickRandomInt.js
@@ -1,5 +1,3 @@
-const pickRandomNumberInRange = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const pickRandomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default pickRandomNumberInRange;
