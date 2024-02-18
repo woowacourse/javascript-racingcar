7af3646dd3ee3c1d5994b27744871e27148e9e13
skiende74 <skiende74@gmail.com>
2024-02-19 02:00:45 +0900

    test(WinnerService): cars를 만드는 공통로직을 함수로 뽑아냄

--- __tests__/WinnerService.test.js
+++ __tests__/WinnerService.test.js
@@ -1,11 +1,14 @@
 import WinnerService from '../src/services/WinnerService.js';
 
 describe('승자 판단 테스트', () => {
+  const makeCars = (carNames, positions) =>
+    [...Array(carNames.length)].map((_, i) => ({ name: carNames[i], position: positions[i] }));
+
   test('승자가 한 명일 때, 승자를 잘 반환한다.', () => {
     // Arrange
     const CAR_NAMES = ['car1', 'car2', 'car3', 'car4'];
     const POSITIONS = [0, 1, 4, 1];
-    const cars = [...Array(CAR_NAMES.length)].map((_, i) => ({ name: CAR_NAMES[i], position: POSITIONS[i] }));
+    const cars = makeCars(CAR_NAMES, POSITIONS);
     // Act
     const winners = new WinnerService(cars).findWinners();
     // Assert
@@ -16,7 +19,7 @@ describe('승자 판단 테스트', () => {
     // Arrange
     const CAR_NAMES = ['car1', 'car2', 'car3', 'car4'];
     const POSITIONS = [0, 4, 4, 2];
-    const cars = [...Array(CAR_NAMES.length)].map((_, i) => ({ name: CAR_NAMES[i], position: POSITIONS[i] }));
+    const cars = makeCars(CAR_NAMES, POSITIONS);
 
     // Act
     const winners = new WinnerService(cars).findWinners();
