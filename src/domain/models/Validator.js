0aabf410d6e90b978579c2e4e11622a95fdc1059
soi-ha <soy2302ten@gmail.com>
2024-02-18 20:11:13 +0900

    fix: 시도 횟수의 유효한 최소 값 상수 추가

--- src/domain/models/Validator.js
+++ src/domain/models/Validator.js
@@ -1,7 +1,7 @@
 import REGEX_CONFIG from '../../constants/configs/regexConfig.js';
 import AppError from '../../errors/AppError.js';
 import ERROR_MESSAGE from '../../constants/messages/errorMessage.js';
-import VALID_LIMIT_CONSTANTS_CONFIG from '../../constants/configs/validLimitConstantsConfig.js';
+import VALID_CONSTANTS_CONFIG from '../../constants/configs/validConstantsConfig.js';
 
 export class Validator {
 	static validateCarNames(carNames) {
@@ -20,38 +20,36 @@ export class Validator {
 		return count;
 	}
 
-	// 문자열에 특수문자가 존재하면 error 발생
 	static validateSpecialCharacter(car) {
 		if (REGEX_CONFIG.SPECIAL_CHARACTER.test(car)) {
 			throw new AppError(ERROR_MESSAGE.HAVE_SPECIAL_CHARACTERS);
 		}
 	}
 
-	// 5글자 초과일떄
 	static validateOutOfRange(car) {
-		if (car.length > VALID_LIMIT_CONSTANTS_CONFIG.CAR_NAME_LIMIT_RANGE) {
+		if (car.length > VALID_CONSTANTS_CONFIG.CAR_NAME_LIMIT_RANGE) {
 			throw new AppError(ERROR_MESSAGE.OUT_OF_RANGE);
 		}
 	}
 
-	// 중복된 이름이 존재할때
 	static validateDuplication(carNames) {
 		if (new Set(carNames).size !== carNames.length) {
 			throw new AppError(ERROR_MESSAGE.HAVE_DUPLICATION);
 		}
 	}
 
-	// 자연수를 입력하지 않았을 때
 	static validateNaturalNumber(count) {
-		if (!Number.isInteger(count) || count < 1) {
-			throw new AppError(ERROR_MESSAGE.NOT_NATURAL_NUMBER);
+		if (!Number.isInteger(count)) {
+			throw new AppError(ERROR_MESSAGE.NOT_INTEGER);
 		}
 	}
 
-	// 시도횟수가 20을 넘었을때
 	static validateOverLimitCount(count) {
-		if (count > VALID_LIMIT_CONSTANTS_CONFIG.ATTEMPT_LIMIT_RANGE) {
-			throw new AppError(ERROR_MESSAGE.OVER_LIMIT_COUNT);
+		if (
+			count < VALID_CONSTANTS_CONFIG.ATTEMPT_MIN_LIMIT_RANGE ||
+			count > VALID_CONSTANTS_CONFIG.ATTEMPT_MAX_LIMIT_RANGE
+		) {
+			throw new AppError(ERROR_MESSAGE.OVER_LIMIT_COUNT_RANGE);
 		}
 	}
 }
