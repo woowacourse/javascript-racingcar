52ec8688535fa5ee32f0a4927b4699c7f4d02df6
skiende74 <skiende74@gmail.com>
2024-02-18 19:15:12 +0900

    test(CarList): 4이상인 경우와 ,4미만인 경우의 테스트를 각각 분리

--- __tests__/CarList.test.js
+++ __tests__/CarList.test.js
@@ -1,17 +1,39 @@
 import CONDITIONS from '../src/constants/Conditions';
 import CarList from '../src/entities/CarList';
 
-describe('Cars', () => {
-  test('랜덤값에 따른 전진 여부', () => {
-    // Arrange
-    const RANDOM_NUMBERS = [0, 1, 3, 4, 5, 9];
-    const POSITIONS = RANDOM_NUMBERS.map(number => (number >= CONDITIONS.progressRandomThreshold ? 1 : 0));
-    const carsStr = 'pen, apple, car3, car4,car5,car6';
-    const carList = new CarList(carsStr);
-    carList.progress(RANDOM_NUMBERS);
-    // Act
-    const positions = carList.getPositions();
-    // Assert
-    expect(positions).toEqual(POSITIONS);
+describe('CarList 테스트', () => {
+  describe('랜덤 값이 주어졌을때,', () => {
+    let carList;
+
+    beforeEach(() => {
+      const CAR_NAMES = 'pen, apple, car3';
+      carList = new CarList(CAR_NAMES);
+    });
+
+    test('4 미만의 값이 주어지면, 정지하여야 한다.', () => {
+      // Arrange
+      const MOVE_RANDOM_NUMBERS = [0, 1, 3];
+      const POSITIONS = [0, 0, 0];
+      carList.progress(MOVE_RANDOM_NUMBERS);
+
+      // Act
+      const positions = carList.getPositions();
+
+      // Assert
+      expect(positions).toEqual(POSITIONS);
+    });
+
+    test('4 이상의 값이 주어지면, 전진하여야 한다.', () => {
+      // Arrange
+      const STOP_RANDOM_NUMBERS = [4, 5, 9];
+      const POSITIONS = [1, 1, 1];
+      carList.progress(STOP_RANDOM_NUMBERS);
+
+      // Act
+      const positions = carList.getPositions();
+
+      // Assert
+      expect(positions).toEqual(POSITIONS);
+    });
   });
 });
