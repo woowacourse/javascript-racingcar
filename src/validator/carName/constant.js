6993a861d41e2da74821fad2704d81b40591b9cf
손진영 <sm099@naver.com>
2024-02-19 08:43:25 +0900

    refactor(CarNameValidator): 자동차 이름을 구분하는 정규 표현식 수정
    불필요한 문자 클래스를 제거([,] -> ,)

--- src/validator/carName/constant.js
+++ src/validator/carName/constant.js
@@ -1,6 +1,6 @@
 // CarNameValidator 디렉터리 내에서만 사용할 것!
 
-export const CAR_NAME_REGEX = /^[a-z0-9A-Z가-힣]+([,][a-z0-9A-Z가-힣]*)*[,]?$/;
+export const CAR_NAME_REGEX = /^[a-z0-9A-Z가-힣]+(,[a-z0-9A-Z가-힣]*)*,?$/;
 
 export const CAR_LENGTH_RANGE = Object.freeze({
   min: 2,
