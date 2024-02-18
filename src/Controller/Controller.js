0595a846d6e0c8ef3360bfc46a056ce793af9fd0
lurgi <wahoo9040@naver.com>
2024-02-18 10:26:28 +0900

    refactor: 불필요한 rule 제거 및 접근 제한자 수정

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -21,14 +21,14 @@ export default class Controller {
     this.#runRace(tryNum);
     OutputView.printMessage(RESULT_MESSAGE);
 
-    const calculValue = Controller.calculateWinners(this.#cars);
+    const calculValue = this.#calculateWinners(this.#cars);
     OutputView.printWinner(calculValue);
   }
 
   async #makeCars() {
     try {
       const carNames = await InputView.readCars();
-      Controller.checkCarDuplicate(carNames);
+      this.#checkCarDuplicate(carNames);
       return carNames.map((name) => new Car(name));
     } catch (error) {
       const returnValue = await this.#printErrorAndRetry(error, this.#makeCars);
@@ -36,7 +36,7 @@ export default class Controller {
     }
   }
 
-  static checkCarDuplicate(carNames) {
+  #checkCarDuplicate(carNames) {
     if (carNames.length !== new Set([...carNames]).size) {
       throw new AppError(ERROR_MESSAGES.DUPLICATE_NAME);
     }
@@ -45,7 +45,7 @@ export default class Controller {
   async #makeTryNum() {
     try {
       const tryNum = await InputView.readTry();
-      Controller.checkTryNum(tryNum);
+      this.#checkTryNum(tryNum);
       return tryNum;
     } catch (error) {
       const returnValue = await this.#printErrorAndRetry(error, this.#makeTryNum);
@@ -59,7 +59,7 @@ export default class Controller {
     return returnValue;
   }
 
-  static checkTryNum(number) {
+  #checkTryNum(number) {
     if (Number.isNaN(number)) {
       throw new AppError(ERROR_MESSAGES.ONLY_NUM);
     }
@@ -72,18 +72,14 @@ export default class Controller {
   #runRace(tryNum) {
     for (let i = 0; i < tryNum; i += 1) {
       this.#cars.forEach((car) => {
-        car.move(Controller.makeRandomNumber0to9());
+        car.move(this.#makeRandomNumber0to9());
         OutputView.printCarCurrentDistance(car);
       });
       OutputView.printMessage(BLANK_STR);
     }
   }
 
-  static makeRandomNumber0to9() {
-    return Math.floor(Math.random() * 10);
-  }
-
-  static calculateWinners(cars) {
+  #calculateWinners(cars) {
     const MIN_DISTACNE = 0;
     const maxDistance = Math.max(...cars.map((car) => car.getDistance()));
 
@@ -92,5 +88,9 @@ export default class Controller {
       return { hasWinner: true, winners };
     }
     return { hasWinner: false, winners: [] };
+  }
+
+  #makeRandomNumber0to9() {
+    return Math.floor(Math.random() * 10);
   }
 }
