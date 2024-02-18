1e60db7f3fab29461a1eceb6520deaf37d80cc93
anttiey <hanuuny@gmail.com>
2024-02-18 18:47:08 +0900

    refactor: util 함수 정의를 객체 리터럴로 변경

--- src/utils/Console.js
+++ src/utils/Console.js
@@ -3,8 +3,8 @@ import Message from '../constant/Message.js';
 
 const { ERROR } = Message;
 
-class Console {
-  static validateQuery(query) {
+const Console = {
+  validateQuery(query) {
     return new Promise((resolve, reject) => {
       if (!query || typeof query !== 'string') {
         reject(new Error(ERROR.query));
@@ -12,9 +12,9 @@ class Console {
 
       resolve();
     });
-  }
+  },
 
-  static makeReadLineQuestion(query, rl) {
+  makeReadLineQuestion(query, rl) {
     return new Promise((resolve, reject) => {
       rl.question(query, (input) => {
         if (input === '') reject(new Error(ERROR.null));
@@ -22,9 +22,9 @@ class Console {
         resolve(input);
       });
     });
-  }
+  },
 
-  static async readLineAsync(query) {
+  async readLineAsync(query) {
     await this.validateQuery(query);
 
     const rl = readline.createInterface({
@@ -33,9 +33,9 @@ class Console {
     });
 
     return await this.makeReadLineQuestion(query, rl);
-  }
+  },
 
-  static async retryUntilSuccess(getFunc) {
+  async retryUntilSuccess(getFunc) {
     while (true) {
       try {
         const result = await getFunc();
@@ -44,7 +44,7 @@ class Console {
         console.log(err.message);
       }
     }
-  }
-}
+  },
+};
 
 export default Console;
