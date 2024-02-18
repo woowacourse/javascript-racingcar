8178ffd038beb0999fe7b0f42208d7eae8a144c5
brgndy <brgndy96@gmail.com>
2024-02-18 11:29:09 +0900

    Test: 테스트코드 재작성

--- __tests__/ScoreBoard.test.js
+++ __tests__/ScoreBoard.test.js
@@ -1,26 +1,17 @@
-import ScoreBoard from '../src/models/ScoreBoard.js';
+import ScoreBoard from '../src/domains/ScoreBoard.js';
 
-describe('스코어보드에 대한 유효성 테스트', () => {
-	test('유효한 값읋 넣으면 0으로 초기화된 Map 객체를 반환한다', () => {
-		const input = ['pobi', 'jun'];
+describe('초기화된 점수판에 대한 유효성 테스트', () => {
+  test('유효한 자동차 이름 입력값읋 넣으면 0으로 초기화된 Map 객체(초기화 된 점수판)를 반환한다', () => {
+    const input = ['pobi', 'jun'];
 
-		const expectedResult = new Map();
-		input.forEach((name) => {
-			expectedResult.set(name, 0);
-		});
+    const expectedResult = new Map();
+    input.forEach((name) => {
+      expectedResult.set(name, 0);
+    });
 
-		const scoreBoard = new ScoreBoard(input);
-		const initializedBoard = scoreBoard.getScoreBoard();
+    const scoreBoard = new ScoreBoard(input);
+    const initializedBoard = scoreBoard.getScoreBoard();
 
-		expect(initializedBoard).toEqual(expectedResult);
-	});
-
-	test.each([[['!df', 'soha']], [['brgndy', 'pobi']], [['왼손', '왼손', '공원']]])(
-		'유효하지 않은 값을 넣으면 에러를 발생시킨다.',
-		(inputs) => {
-			expect(() => {
-				new ScoreBoard(inputs);
-			}).toThrow('[ERROR]');
-		}
-	);
+    expect(initializedBoard).toEqual(expectedResult);
+  });
 });
