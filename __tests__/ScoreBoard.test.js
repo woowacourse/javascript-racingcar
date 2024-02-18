450b0d2028dd53c67ace06cbc36ff893660a4586
brgndy <brgndy96@gmail.com>
2024-02-18 11:56:22 +0900

    Fix: test 도메인 import 추가

--- __tests__/ScoreBoard.test.js
+++ __tests__/ScoreBoard.test.js
@@ -1,5 +1,6 @@
 import GAME_CONDITION from '../src/constants/configs/gameCondition.js';
 import RANDOM_CAR_NAME_CONFIG from '../src/constants/configs/randomCarNameConfig.js';
+import ScoreBoard from '../src/domains/ScoreBoard.js';
 
 describe('초기화된 점수판에 대한 유효성 테스트', () => {
   test('유효한 자동차 이름 입력값을 넣으면 0으로 초기화된 Map 객체(초기화 된 점수판)를 반환한다', () => {
