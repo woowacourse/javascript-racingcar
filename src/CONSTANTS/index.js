85c64d5680f65314e2db7a214a1987379144f958
cys4585 <dev.youngsu@gmail.com>
2024-02-18 22:07:22 +0900

    rename: 폴더명 대소문자 이슈로 재커밋

--- src/CONSTANTS/index.js
+++
@@ -1,7 +0,0 @@
-import MESSAGE from "./message";
-import NUMERIC from "./numeric";
-import SEPARATOR from "./separator";
-
-const CONSTANT = Object.freeze({ MESSAGE, NUMERIC, SEPARATOR });
-
-export default CONSTANT;
