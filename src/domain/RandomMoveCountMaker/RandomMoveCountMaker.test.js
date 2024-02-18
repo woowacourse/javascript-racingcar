41e157514aa19fcd50f52e2c394a9d4b7e295193
bmj0053 <bmj05378@gmail.com>
2024-02-18 23:53:53 +0900

    refactor: jest.fn 함수 대체

--- src/domain/RandomMoveCountMaker/RandomMoveCountMaker.test.js
+++ src/domain/RandomMoveCountMaker/RandomMoveCountMaker.test.js
@@ -1,35 +1,28 @@
 import RandomMoveCountMaker from './module.js';
 
-jest.mock('../../utils/random.js', () => ({
-  pickUniqueNumbersInRange: jest.fn((min, max, length) => Array.from({ length }, (_, index) => index)),
-}));
-
 describe('시도 횟수와 총 자동차 대수가 주어졌을 때 알맞은 크기의 랜덤 이동 횟수 생성 테스트', () => {
   // given
   const testCases = [
     {
       tryCount: 2,
       racingCarNamesLength: 3,
-      expectedRandomMoveCounts: [
-        [0, 1, 2],
-        [0, 1, 2],
-      ],
     },
     {
       tryCount: 1,
       racingCarNamesLength: 5,
-      expectedRandomMoveCounts: [[0, 1, 2, 3, 4]],
     },
   ];
 
   test.each(testCases)(
-    'tryCount가 $tryCount이고, racingCarNamesLength가 $racingCarNamesLength일 때, racingCarNamesLength 길이의 tryCount개의 배열이 나와야 하며 예상한 결과 값 $expectedRandomMoveCounts이 도출된다.',
-    ({ tryCount, racingCarNamesLength, expectedRandomMoveCounts }) => {
+    'tryCount가 $tryCount이고, racingCarNamesLength가 $racingCarNamesLength일 때, randomMoveCounts 각 배열의 길이는 racingCarNamesLength 길이인 $racingCarNamesLength와 같다.',
+    ({ tryCount, racingCarNamesLength }) => {
       // when
       const randomMoveCounts = RandomMoveCountMaker.execute(tryCount, racingCarNamesLength);
 
       // then
-      expect(randomMoveCounts).toStrictEqual(expectedRandomMoveCounts);
+      randomMoveCounts.forEach((moveCountArray) => {
+        expect(moveCountArray.length).toBe(racingCarNamesLength);
+      });
     },
   );
 });
--- src/errors/ErrorHandler/ErrorHandler.test.js
+++ src/errors/ErrorHandler/ErrorHandler.test.js
@@ -1,37 +1,53 @@
-import Console from '../../utils/console.js';
 import ErrorHandler from './module.js';
 
-jest.mock('../../utils/console.js', () => ({
-  print: jest.fn(),
-}));
+let consoleOutput = [];
+const mockPrint = (output) => consoleOutput.push(output);
+console.log = mockPrint;
 
 describe('입력 관련 예외 처리 테스트', () => {
-  let executeTest;
-
   beforeEach(() => {
-    jest.clearAllMocks();
-    // given
-    executeTest = jest.fn().mockRejectedValueOnce(new Error('Test Error')).mockResolvedValueOnce('Success');
+    consoleOutput = [];
   });
 
   test('함수가 두 번 호출된다.', async () => {
+    // given
+    let callCount = 0;
+    const executeTest = async () => {
+      callCount++;
+      if (callCount === 1) throw new Error('Test Error');
+      return 'Success';
+    };
     // when
     await ErrorHandler.retryOnErrors(executeTest);
     // then
-    expect(executeTest).toHaveBeenCalledTimes(2);
+    expect(callCount).toBe(2);
   });
 
   test('첫 번째 호출은 실패 후 에러 로깅이 발생한다.', async () => {
+    // given
+    let callCount = 0;
+    const executeTest = async () => {
+      callCount++;
+      if (callCount === 1) throw new Error('Test Error');
+      return 'Success';
+    };
     // when
     await ErrorHandler.retryOnErrors(executeTest);
     // then
-    expect(Console.print).toHaveBeenCalledWith('Test Error');
+    expect(consoleOutput).toEqual(['Test Error']);
   });
 
   test('두 번째 호출은 성공적인 결과를 반환한다.', async () => {
+    // given
+    let callCount = 0;
+    const executeTest = async () => {
+      callCount++;
+      if (callCount === 1) throw new Error('Test Error');
+      return 'Success';
+    };
     // when
     const result = await ErrorHandler.retryOnErrors(executeTest);
     // then
-    expect(result).toMatch('Success');
+    expect(result).toBe('Success');
   });
 });
