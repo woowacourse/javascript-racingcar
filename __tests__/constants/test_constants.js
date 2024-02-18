fde65e2cafca71e20538e7757d1f790107869d15
jayming66 <mjw8941@gmail.com>
2024-02-19 08:36:21 +0900

    test: Application.test 파일 삭제

--- __tests__/constants/test_constants.js
+++ __tests__/constants/test_constants.js
@@ -3,23 +3,3 @@ export const TEST_RULES = {
   movingForward: 5,
   stop: 1,
 };
-
-export const RANDOMCASES = {
-  firstWinOfTwoCarRandomCase: [
-    TEST_RULES.stop,
-    TEST_RULES.stop,
-    TEST_RULES.movingForward,
-    TEST_RULES.movingForward,
-    TEST_RULES.movingForward,
-    TEST_RULES.stop,
-  ],
-  drawOfTwoCarRandomCase: [
-    TEST_RULES.stop,
-    TEST_RULES.movingForward,
-    TEST_RULES.movingForward,
-    TEST_RULES.movingForward,
-    TEST_RULES.movingForward,
-    TEST_RULES.stop,
-  ],
-  moveTwoTimeOfOneCarRandomCase: [TEST_RULES.movingForward, TEST_RULES.movingForward, TEST_RULES.stop, TEST_RULES.stop],
-};
