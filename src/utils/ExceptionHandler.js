48b5019b873fdadea1085522d4c434f04bd134dc
vi-wolhwa <cerezo00@naver.com>
2024-02-18 23:02:55 +0900

    feat: 예외처리 모듈

---
+++ src/utils/ExceptionHandler.js
@@ -0,0 +1,33 @@
+import { ERROR } from '../constants/strings.js';
+
+const ExceptionHandler = {
+  throwError(errorMessage) {
+    throw new Error(`${ERROR.prefix} ${errorMessage}`);
+  },
+
+  throwErrorIfInvalid(condition, errorMessage) {
+    if (!condition) ExceptionHandler.throwError(errorMessage);
+  },
+
+  // eslint-disable-next-line max-lines-per-function
+  async retryAsyncWithErrorLogging(asyncFunc, maxRetries = Infinity) {
+    let retries = 0;
+    let errorMessage;
+
+    while (retries <= maxRetries) {
+      try {
+        return await asyncFunc();
+      } catch (error) {
+        errorMessage = error.message;
+        console.log(error.message);
+        retries += 1;
+      }
+    }
+
+    this.throwError(errorMessage);
+
+    return null;
+  }
+};
+
+export default ExceptionHandler;
