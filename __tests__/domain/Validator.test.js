df8ec7a812dd94515a9dbcb417b4b308007ebb09
Kim Da Eun <midekuna@gmail.com>
2024-02-18 22:51:16 +0900

    test: Validator 테스트 특수 문자, 한글 테스트 케이스 추가

--- __tests__/domain/Validator.test.js
+++ __tests__/domain/Validator.test.js
@@ -8,46 +8,58 @@ import {
 describe('자동차 이름 입력 Validator Test', () => {
   test('isInvalidCarNameForm Test - 이름이 6자 이상일 경우 true를 반환한다', () => {
     const CARS_NAME = 'pobi,masterwee';
-    expect(isInvalidCarNameForm(CARS_NAME)).toBe(true);
+    expect(isInvalidCarNameForm(CARS_NAME)).toBeTruthy();
   });
 
   test('isInvalidCarNameForm Test - 이름의 구분자가 쉼표(,)가 아닐 경우 true를 반환한다', () => {
     const CARS_NAME = 'pobi/jay';
-    expect(isInvalidCarNameForm(CARS_NAME)).toBe(true);
+    expect(isInvalidCarNameForm(CARS_NAME)).toBeTruthy();
   });
 
   test.each(['po bi,jay', ' , ', ' '])(
     'isInvalidCarNameForm Test - 공백 문자를 포함한 입력이 들어올 경우 true를 반환한다.',
     CARS_NAME => {
-      expect(isInvalidCarNameForm(CARS_NAME)).toBe(true);
+      expect(isInvalidCarNameForm(CARS_NAME)).toBeTruthy();
     },
   );
 
   test.each(['pobi,pobi', 'pobi,jay,pobi'])(
-    'hasRedundantCarName Test - 이름이 중복됐을 경우 true를 반환한다.',
+    'hasRedundantCarName Test - 이름이 중복된 경우 true를 반환한다.',
     CARS_NAME => {
-      expect(hasRedundantCarName(CARS_NAME)).toBe(true);
+      expect(hasRedundantCarName(CARS_NAME)).toBeTruthy();
     },
   );
 
-  test('hasSingleCarName Test - 자동차가 1개만 입력됐을 경우 true를 반환한다.', () => {
+  test('hasSingleCarName Test - 자동차가 1개만 입력된 경우 true를 반환한다.', () => {
     const CAR_NAME = 'pobi';
-    expect(hasSingleCarName(CAR_NAME)).toBe(true);
+    expect(hasSingleCarName(CAR_NAME)).toBeTruthy();
+  });
+
+  test('isInvalidCarNameForm Test - 특수문자가 입력된 경우 true를 반환한다', () => {
+    const CARS_NAME = '@#/,&&';
+    expect(isInvalidCarNameForm(CARS_NAME)).toBeTruthy();
+  });
+
+  test('hasSingleCarName Test - 한글이 입력된 경우 false를 반환한다.', () => {
+    const CAR_NAME = '가나다,ㅔㅜㅑ,pobi';
+    expect(hasSingleCarName(CAR_NAME)).toBeFalsy();
   });
 });
 
 describe('시도 횟수 입력 Validator Test', () => {
-  test('isInvalidAttemptNum Test - 숫자가 아닌 문자가 입력됐을 경우 true를 반환한다.', () => {
-    const ATTEMPT_NUM = 's';
-    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBe(true);
+  test.each(['s', '#', '  ', ' '])(
+    'isInvalidAttemptNum Test - 숫자가 아닌 문자가 입력된 경우 true를 반환한다.',
+    ATTEMPT_NUM => {
+      expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBeTruthy();
+    },
+  );
+
+  test.each(['0', '-1'])('isInvalidAttemptNum Test - 1 이하의 정수가 입력된 경우 true를 반환한다.', ATTEMPT_NUM => {
+    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBeTruthy();
   });
 
-  test.each(['0', '-1'])('isInvalidAttemptNum Test - 1 이하의 정수가 입력됐을 경우 true를 반환한다.', ATTEMPT_NUM => {
-    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBe(true);
-  });
-
-  test('isInvalidAttemptNum Test - 소수가 입력됐을 경우 true를 반환한다.', () => {
+  test('isInvalidAttemptNum Test - 소수가 입력된 경우 true를 반환한다.', () => {
     const ATTEMPT_NUM = '1.2';
-    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBe(true);
+    expect(isInvalidAttemptNum(ATTEMPT_NUM)).toBeTruthy();
   });
 });
