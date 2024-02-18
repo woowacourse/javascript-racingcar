0eea1e41bbc04db49f764a212631ec39b79770ed
jinsil <jinsil378900@gmail.com>
2024-02-19 02:30:21 +0900

    Refactor: 불필요한 메서드 제거

--- src/domain/Cars.js
+++ src/domain/Cars.js
@@ -1,26 +1,23 @@
-import ERROR_MESSAGE from '../error/message.js';
-import Car from './Car.js';
+import ERROR_MESSAGE from "../error/message.js";
+import Car from "./Car.js";
 
 class Cars {
   #cars;
 
   constructor(names) {
-    const parsedNames = this.#parse(names);
+    const parsedNames = names.split(",").map((name) => name.trim());
 
     this.#validate(parsedNames);
 
     this.#cars = parsedNames.map((name) => new Car(name));
   }
 
-  #parse(names) {
-    return names.split(',').map((name) => name.trim());
-  }
-
   #validate(names) {
     const uniqueNames = new Set(names);
 
-    if (uniqueNames.size !== names.length)
+    if (uniqueNames.size !== names.length) {
       throw new Error(`${ERROR_MESSAGE.duplicated} ${ERROR_MESSAGE.retry}`);
+    }
   }
 
   play() {
@@ -36,9 +33,7 @@ class Cars {
 
   winners() {
     const maxLocation = Math.max(...this.#cars.map((car) => car.getLocation()));
-    return this.#cars
-      .filter((car) => car.getLocation() === maxLocation)
-      .map((car) => car.getName());
+    return this.#cars.filter((car) => car.getLocation() === maxLocation).map((car) => car.getName());
   }
 }
 
