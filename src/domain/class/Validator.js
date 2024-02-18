310cb7caea46331f8d167a9464ecc815fb540eca
ss0526100 <ss0526100@gmail.com>
2024-02-18 21:57:32 +0900

    refactor: validator 로직 분리

--- src/domain/class/Validator.js
+++ src/domain/class/Validator.js
@@ -1,20 +1,34 @@
 import CONSTANTS from '../../CONSTANTS';
 
-const { numeric } = CONSTANTS;
+const { numeric, message } = CONSTANTS;
 
 class Validator {
-  static validateCars(cars) {
+  static validateCarNames(carNames) {
+    if (!this.#isValidCarNames(carNames))
+      throw new Error(message.INVALID_CAR_NAME);
+  }
+
+  static validateMaxTryCountString(maxTryCount) {
+    if (!this.#isValidMaxTryCountString(maxTryCount))
+      throw new Error(message.INVALID_MAX_TRY_COUNT);
+  }
+
+  static #isValidCarNames(carNames) {
     return (
-      cars.every(
-        name =>
-          name.length >= numeric.CAR_NAME_LENGTH_LOWER &&
-          name.length <= numeric.CAR_NAME_LENGTH_UPPER
-      ) && cars.length === new Set(cars).size
+      carNames.every(name => this.#isValidCarName(name)) &&
+      carNames.length === new Set(carNames).size
     );
   }
 
-  static validateTryCount(tryCount) {
-    return /^[0-9]+$/.test(tryCount);
+  static #isValidCarName(name) {
+    return (
+      name.length >= numeric.CAR_NAME_LENGTH_LOWER &&
+      name.length <= numeric.CAR_NAME_LENGTH_UPPER
+    );
+  }
+
+  static #isValidMaxTryCountString(maxTryCountString) {
+    return /^[0-9]+$/.test(maxTryCountString);
   }
 }
 
