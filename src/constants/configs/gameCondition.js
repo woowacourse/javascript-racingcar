99ec03b8bfbaa24c3b13956f1554344b1b003e36
brgndy <brgndy96@gmail.com>
2024-02-19 20:31:52 +0900

    Refactor: 게임 조건 상수값 관리

--- src/constants/configs/gameCondition.js
+++ src/constants/configs/gameCondition.js
@@ -2,27 +2,12 @@ import deepFreeze from '../../utils/deepFeeze.js';
 
 const GAME_CONDITION = deepFreeze({
   MIN_CAR_NAME_LENGTH: 5,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 178d167 (Refactor: 상수 옵션들 추가)
-=======
->>>>>>> c7816a0d72ed889c8e8bdebae53c88bf89886d4c
   MIN_COUNT_OF_ATTEMPT: 0,
   MAX_COUNT_OF_ATTEMPT: 20,
   MOVE_FORWARD: 4,
   INITIALIZED_SCORE: 0,
   MIN_RANDOM_NUMBER: 0,
   MAX_RANDOM_NUMBER: 9,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f6a2668 (Refactor: 상수 키값 대문자로 변경 및 상수 추가)
-=======
->>>>>>> 178d167 (Refactor: 상수 옵션들 추가)
-=======
->>>>>>> c7816a0d72ed889c8e8bdebae53c88bf89886d4c
 });
 
 export default GAME_CONDITION;
