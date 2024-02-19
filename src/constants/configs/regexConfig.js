fcce2ba6b7f456da0aceed6323954b7f5488c22e
brgndy <brgndy96@gmail.com>
2024-02-19 20:32:14 +0900

    Refactor: 정규표현식 value 값 수정

--- src/constants/configs/regexConfig.js
+++ src/constants/configs/regexConfig.js
@@ -2,7 +2,7 @@ import deepFreeze from '../../utils/deepFeeze.js';
 
 /* eslint-disable no-useless-escape */
 const REGEX_CONFIG = deepFreeze({
-  SPECIAL_CHARACTER: /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/,
+  SPECIAL_CHARACTER: /^[A-Za-z0-9가-힣]+$/,
 });
 
 export default REGEX_CONFIG;
