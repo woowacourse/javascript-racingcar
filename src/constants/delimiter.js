fd8384542dcaee8d6ffa29205579408f8068b45e
brgndy <brgndy96@gmail.com>
2024-02-19 18:19:51 +0900

    refactor: Repush

--- src/constants/delimiter.js
+++ src/constants/delimiter.js
@@ -1,29 +1,11 @@
 import deepFreeze from '../utils/deepFeeze.js';
 
 const DELIMITER = deepFreeze({
-<<<<<<< HEAD
-<<<<<<< HEAD
   NONE: '',
   SPACE: ' ',
   CAR_NAME_SEPARATOR: ',',
   SCORE_MARK: '-',
   COLON: ' : ',
-=======
-  SPACE: '',
-  COMMA: ',',
-  HYPHEN: '-',
-<<<<<<< HEAD
->>>>>>> 50117f4 (Feat: 구분문자 관련 상수 객체 생성)
-=======
-  COLON: ':',
->>>>>>> f6a2668 (Refactor: 상수 키값 대문자로 변경 및 상수 추가)
-=======
-  NONE: '',
-  SPACE: ' ',
-  COMMA: ',',
-  HYPHEN: '-',
-  COLON: ' : ',
->>>>>>> 178d167 (Refactor: 상수 옵션들 추가)
 });
 
 export default DELIMITER;
