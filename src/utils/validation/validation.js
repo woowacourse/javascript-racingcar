a081e9252444b4cc6ac714b4a31daf18d8f8c67e
vi-wolhwa <cerezo00@naver.com>
2024-02-19 23:06:12 +0900

    comment: validation 불필요한 주석 제거

--- src/utils/validation/validation.js
+++ src/utils/validation/validation.js
@@ -1,8 +1,8 @@
 const validation = {
-  isInteger: (value) => Number.isInteger(value), // 값이 정수인지 검증하는 함수
-  isUnique: (array) => new Set(array).size === array.length, // 배열에 중복이 없는지 검증하는 함수
-  isPositive: (value) => value > 0, // 값이 양수인지 검증하는 함수
-  isInRange: (value, min, max) => value >= min && value <= max // 값이 주어진 범위 내에 있는지 검증하는 함수
+  isInteger: (value) => Number.isInteger(value),
+  isUnique: (array) => new Set(array).size === array.length,
+  isPositive: (value) => value > 0,
+  isInRange: (value, min, max) => value >= min && value <= max
 };
 
 export default validation;
