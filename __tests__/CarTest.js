48ddd2835ca5498dd37c650594f1ee7c456043c8
ImxYJL <111052302+ImxYJL@users.noreply.github.com>
2024-02-20 08:03:36 +0900

    [2단계 - 자동차 경주] 올리(이예진) 미션 제출합니다. (#323)
    * structure: 커스텀 에러 삭제
    
    * test: Car, GarageTest 테스트 설명 명확하게 개선
    
    * refactor: 유효한 입력을 받을 때까지 루프하는 함수 추상화

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -4,12 +4,14 @@ import randomNumber from "../src/utils/randomNumber.js";
 jest.mock("../src/utils/randomNumber");
 
 describe("Car", () => {
+  let car;
+
   beforeEach(() => {
+    car = new Car("testCar");
     randomNumber.mockClear();
   });
 
   test("car 객체가 지정한 이름과 position 0으로 생성되는지 테스트", () => {
-    const car = new Car("testCar");
     const status = car.getCarStatus();
 
     expect(status.name).toBe("testCar");
@@ -17,15 +19,13 @@ describe("Car", () => {
   });
 
   test("getCarStatus가 car 객체의 현재 상태를 리턴하는지 테스트", () => {
-    const car = new Car("testCar");
     const carStatus = car.getCarStatus();
 
     expect(carStatus).toEqual({ name: "testCar", position: 0 });
   });
 
-  test("position 이동이 불가능한 경우 이동하지 않는지 테스트", () => {
+  test("난수가 4 미만일 때 이동하지 않는지 테스트", () => {
     randomNumber.mockReturnValue(3);
-    const car = new Car("testCar");
 
     car.tryToMove();
 
@@ -33,9 +33,8 @@ describe("Car", () => {
     expect(status.position).toBe(0);
   });
 
-  it("position 이동이 가능한 경우 이동하는지 테스트", () => {
+  it("난수가 4 이상일 때 이동하는지 테스트", () => {
     randomNumber.mockReturnValue(4);
-    const car = new Car("testCar");
 
     car.tryToMove();
 
--- src/utils/StringParser.js
+++ src/utils/StringParser.js
@@ -3,7 +3,6 @@ import MESSAGE from "../constants/Message.js";
 const StringParser = {
   splitCarNames: (input) => {
     return input.split(",").map((carName) => carName.trim());
-    //.filter((name) => name !== ""); // // 공백으로만 이루어진 이름은 불허
   },
 
   concatElements: (array) => {
