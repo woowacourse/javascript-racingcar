db526c480c97193827888f3f0be4a503755e197d
useon <yoosk4986@naver.com>
2024-02-19 15:18:12 +0900

    refactor: Race 코드가 수정됨에 따라 불필요한 테스트를 삭제

--- __tests__/domain/Race.test.js
+++ __tests__/domain/Race.test.js
@@ -1,16 +1,6 @@
-import Cars from '../../src/collection/Cars';
-import Car from '../../src/domain/Car';
 import Race from '../../src/domain/Race';
-import gameUtils from '../../src/utils/gameUtils';
 
-const mockRandoms = (numbers) => {
-  gameUtils.pickRandomNumber = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, gameUtils.pickRandomNumber);
-};
-
-describe('라운드 카운트 검증', () => {
+describe('레이스 검증', () => {
   describe('라운드 카운트 생성 검증', () => {
     test('라운드 입력이 빌 경우 에러 발생', () => {
       // given
@@ -65,29 +55,6 @@ describe('라운드 카운트 검증', () => {
 
       // then
       expect(typeof race === 'object').toBeTruthy();
-    });
-  });
-  describe('자동차 전진 통합테스트', () => {
-    // given
-    const sundayCar = new Car('썬데이');
-    const cookieCar = new Car('쿠키');
-    const cars = new Cars([sundayCar, cookieCar]);
-    const race = new Race('2');
-    const randomNumbers = [3, 4, 4, 5];
-    mockRandoms(randomNumbers);
-
-    test('게임이 두 번 진행되고, 썬데이가 3, 4 쿠키가 4, 5의 수를 받으면 스코어가 썬데이는 1, 쿠키는 2다.', () => {
-      // given
-      const answer = [
-        ['썬데이 : ', '쿠키 : -'],
-        ['썬데이 : -', '쿠키 : --'],
-      ];
-
-      // when
-      race.raceStart(cars);
-
-      // then
-      expect(race.makeRaceResultOutput()).toEqual(answer);
     });
   });
 });
