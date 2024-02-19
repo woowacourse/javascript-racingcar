36a0fc508a134b21dbb99ec77df3f5ea29a4b926
pakxe <pigkill40@naver.com>
2024-02-19 12:40:55 +0900

    test: 이해하기 쉬운 테스트 명으로 수정

--- __tests__/TryCount.test.js
+++ __tests__/TryCount.test.js
@@ -1,16 +1,18 @@
 import ERROR_MESSAGE from '../src/error/message.js';
-import TryCount from '../src/model/TryCount.js';
+import TryCount, { MAX_TRY_COUNT, MIN_TRY_COUNT } from '../src/model/TryCount.js';
 
 describe('tryCount 횟수 입력 테스트', () => {
   // 올바른 테스트
   test.each`
-    testTitle                        | tryCount | expected
-    ${'올바른 횟수가 들어온 경우 1'} | ${'1'}   | ${1}
-    ${'올바른 횟수가 들어온 경우 2'} | ${'10'}  | ${10}
+    testTitle                        | tryNum  | expected
+    ${'올바른 횟수가 들어온 경우 1'} | ${'1'}  | ${1}
+    ${'올바른 횟수가 들어온 경우 2'} | ${'10'} | ${10}
   `(
-    '$testTitle테스트는 $tryCount이 입력되면 $expected 횟수를 출력한다.',
-    ({ tryCount, expected }) => {
-      expect(new TryCount(tryCount).getTryCount()).toEqual(expected);
+    `${MIN_TRY_COUNT}~${MAX_TRY_COUNT} 사이의 올바른 시도 횟수가 입력되면 저장한다.`,
+    ({ tryNum, expected }) => {
+      const tryCount = new TryCount(tryNum);
+
+      expect(tryCount.getTryCount()).toEqual(expected);
     },
   );
 
