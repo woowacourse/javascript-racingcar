98a2867c93017c0074b84b399e1255e3a4da9b71
ss0526100 <ss0526100@gmail.com>
2024-02-20 10:36:58 +0900

    refactor: CarMover에 giveManyTry for 문으로 변경

--- src/domain/class/CarMover.js
+++ src/domain/class/CarMover.js
@@ -11,21 +11,7 @@ class CarMover {
 
   giveManyTry(car, maxTryCount) {
     this.#validateTryCount(maxTryCount);
-    this.#giveManyTryRecursive(car, maxTryCount);
-  }
-
-  // #giveManyTryFor(car, maxTryCount) {
-  //   for (let tryCount = 0; tryCount < maxTryCount; tryCount++)
-  //     this.#giveOneTry(car);
-  // }
-
-  // 스택 오버플로우 방지를 위해 2 이상의 카운트가 들어올 경우 분기를 나눠서 실행
-  #giveManyTryRecursive(car, leftCount) {
-    if (leftCount >= 2) {
-      this.#giveManyTryRecursive(car, Math.floor(leftCount / 2));
-      this.#giveManyTryRecursive(car, leftCount - Math.floor(leftCount / 2));
-    }
-    if (leftCount === 1) this.#giveOneTry(car, leftCount - 1);
+    Array({ length: maxTryCount }).forEach(() => this.#giveOneTry(car));
   }
 
   #giveOneTry(car) {
