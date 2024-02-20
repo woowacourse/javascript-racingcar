ace34950a82310483ec1d5fe3f012140f46c0578
cys4585 <dev.youngsu@gmail.com>
2024-02-20 14:13:12 +0900

    refactor: 출력 로직 가독성 개선(상수 구조분해, 표현되는 값 변수 네이밍)

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,40 +1,43 @@
 import Console from "../utils/Console";
 import CONSTANT from "../constants";
 
+const { MESSAGE } = CONSTANT;
+
 class OutputView {
   static printResult(tryCount, cars) {
     Console.print("");
-    Console.print(CONSTANT.MESSAGE.resultOutput);
+    Console.print(MESSAGE.resultOutput);
     Console.print(this.#convertResultToString(tryCount, cars));
   }
 
   static printWinners(winners) {
+    const header = MESSAGE.winnerOutputHeader;
+    const content = winners.join(MESSAGE.winnerConnectionMark);
+
     Console.print("");
-    Console.print(
-      `${CONSTANT.MESSAGE.winnerOutputHeader}${winners.join(
-        CONSTANT.MESSAGE.winnerConnectionMark
-      )}`
-    );
+    Console.print(`${header}${content}`);
   }
 
   static #convertResultToString(tryCount, cars) {
     return Array.from({ length: tryCount }, (_, i) => i)
       .map((currentRound) => this.#generateResultInRace(currentRound, cars))
-      .join(CONSTANT.MESSAGE.resultLineBreakMarkInRace);
+      .join(MESSAGE.resultLineBreakMarkInRace);
   }
 
   static #generateResultInRace(currentRound, cars) {
     return cars
       .map((car) => this.#generateResultInRound(currentRound, car))
-      .join(CONSTANT.MESSAGE.resultLineBreakMarkInRound);
+      .join(MESSAGE.resultLineBreakMarkInRound);
   }
 
   static #generateResultInRound(currentRound, car) {
-    return `${car.getName()}${
-      CONSTANT.MESSAGE.resultConnectionMark
-    }${CONSTANT.MESSAGE.distanceMark.repeat(
+    const header = car.getName();
+    const connectionMark = MESSAGE.resultConnectionMark;
+    const content = MESSAGE.distanceMark.repeat(
       car.getPositionWhen(currentRound)
-    )}`;
+    );
+
+    return `${header}${connectionMark}${content}`;
   }
 }
 
