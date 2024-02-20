f812937573bf111507d3243c3dc52c400a821a14
greetings1012 <ahnqwerty@gmail.com>
2024-02-20 11:17:51 +0900

    refactor: Validation 테스트 수정
    import문 경로 수정
    이름의 길이가 경계값(6)이 되도록 수정

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,15 +1,17 @@
-import Validation from '../src/utils/Validation';
-import { ERROR_MESSAGE } from '../src/constants';
+import Cars from '../src/domain/Cars';
+import { ERROR_MESSAGE } from '../src/constants/constants';
 
 const TOO_SHORT_NAME = ''
 
-const TOO_LONG_NAME = 'pobipobi'
+const TOO_LONG_NAME = 'junjun'
 
 describe('사용자 입력 유효성 검사', () => {
-  test.each([[['pobi', 'jun', TOO_SHORT_NAME]], [[TOO_LONG_NAME, 'jun', 'cron']]])(
-    '자동차 이름 길이 유효성 검사',
-    (nameArray) => {
-      expect(() => Validation.carNamesValidate(nameArray)).toThrow(
+  test.each([
+    [['pobi', 'jun', TOO_SHORT_NAME]],
+     [[TOO_LONG_NAME, 'jun', 'cron']]
+    ]
+  )( '자동차 이름 길이 유효성 검사',(nameArray) => {
+      expect(() => Cars.validateCarNames(nameArray)).toThrow(
         ERROR_MESSAGE.CAR_NAME_INPUT_ERROR.NOT_IN_RANGE
       );
     }
@@ -17,7 +19,7 @@ describe('사용자 입력 유효성 검사', () => {
   test.each([-1, -999999, 0.232, 21.5])(
     '시도 횟수 숫자 유효성 검사',
     (nameArray) => {
-      expect(() => Validation.tryCountValidate(nameArray)).toThrow(
+      expect(() => Cars.validateTryCount(nameArray)).toThrow(
         ERROR_MESSAGE.TRY_COUNT_INPUT_ERROR.SHOULD_BE_POSITIVE
       );
     }
