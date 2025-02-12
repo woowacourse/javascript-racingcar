e790a104ef0f2e4ac9be1cee4c845ea49e70f6ac
ha-kuku <hakukudh@gmail.com>
2025-02-12 20:03:26 +0900

    feat: 입력 값 유효성 검사 연결

--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
 import readLineAsync from "./readLine.js";
+import {
+  validationCarNameForm,
+  validationDuplicatedCarName,
+  validationGameCountRange,
+  validationGameCountType,
+  validationInputLength,
+} from "./validation.js";
 
 class App {
   async getCarNameInput() {
     const input = await readLineAsync(
       "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n"
     );
-    return input;
+    try {
+      validationCarNameForm(input); // 문자열 나눠서 배열로 알아서 검사
+      validationDuplicatedCarName(input); // 문자열 나눠서 배열로 알아서 검사
+      input.split(",").forEach((carname) => {
+        validationInputLength(carname);
+      });
+      return input;
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 
   async getGameCount() {
     const input = await readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    return input;
+    try {
+      validationGameCountType(input);
+      validationGameCountRange(input);
+      return input;
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 
   async run() {
