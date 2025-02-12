546aac3da45c80bb56527ac7a252d1f781f8638d
guesung <gueit214@naver.com>
2025-02-12 17:19:34 +0900

    feat: 유효성 검사

---
+++ src/validator.js
@@ -0,0 +1,14 @@
+export default class Validator {
+  static validateCarName(rawName) {
+    const nameList = rawName.split(',');
+    for (const name of nameList) {
+      if (name.length > 5 || name.length === 0) throw new Error();
+    }
+  }
+
+  static validateCount(rawCount) {
+    const count = Number(rawCount);
+
+    if (Number.isNaN(count)) throw new Error();
+  }
+}
