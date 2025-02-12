701ab82e80e72a90a43d0e376e7ecb1545fc2665
ha-kuku <hakukudh@gmail.com>
2025-02-12 18:59:22 +0900

    feat: 자동차 이름 형식 관련 test 추가
    자동차 이름 올바른 형식, 틀린 형식 각각 추가

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,4 +1,7 @@
-import { validationInputLength } from "../src/validation.js";
+import {
+  validationInputLength,
+  validationCarNameForm,
+} from "../src/validation.js";
 
 describe("자동차 이름 유효성 검사", () => {
   test("자동차 이름 길이 5자 이하", () => {
@@ -12,6 +15,20 @@ describe("자동차 이름 유효성 검사", () => {
     const carName = "hakuuu";
     expect(() => {
       validationInputLength(carName);
+    }).toThrow("[Error]");
+  });
+
+  test("자동차 이름 올바른 형식", () => {
+    const carList = "haku,logun";
+    expect(() => {
+      validationCarNameForm(carList);
+    }).not.toThrow("[Error]");
+  });
+
+  test("자동차 이름 틀린 형식", () => {
+    const carList = "haku,";
+    expect(() => {
+      validationCarNameForm(carList);
     }).toThrow("[Error]");
   });
 });
