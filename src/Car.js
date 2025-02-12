65b173b22cf261c0e3b2caca6084603de5adc943
ha-kuku <hakukudh@gmail.com>
2025-02-12 17:15:36 +0900

    feat: 자동차 움직이기 추가
    getRandomNumber를 통해 0에서 9사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 전진

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,18 @@
+import { getRandomNumber } from "./util.js";
+
+const MOVE_FORWARD = 4;
+
 class Car {
   constructor(name) {
     this.name = name;
     this.position = 0;
+  }
+
+  move() {
+    const moveCondition = getRandomNumber();
+    if (moveCondition >= MOVE_FORWARD) {
+      this.position += 1;
+      return;
+    }
   }
 }
