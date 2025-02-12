5fc9f7d422f1804de03af6383fe748915902e385
spoyodevelop <kjy9505@gmail.com>
2025-02-12 18:08:01 +0900

    test: 인풋 validation 테스트 코드 작성
    Co-authored-by: hanheel <hanhee630630@gmail.com>

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -13,7 +13,7 @@ describe("input validation test", () => {
 
   test("이름이 5자 이상인 경우 false를 반환한다", () => {
     //given
-    const INPUT_VALUE = "abcdef".split(",");
+    const INPUT_VALUE = "abcdfe".split(",");
 
     //when
     const RESULT = Validation.isNameTooLong(INPUT_VALUE);
@@ -32,6 +32,33 @@ describe("input validation test", () => {
     expect(RESULT).toBe(false);
   });
 
-  test("숫자가 아닌 경우 false를 반환한다", () => {});
-  test("숫자가 음수인 경우 false를 반환한다", () => {});
+  test("문자가 입력된 경우 false를 반환한다", () => {
+    //given
+    const INPUT_VALUE = "a";
+
+    //when
+    const RESULT = Validation.isInteger(INPUT_VALUE);
+
+    //then
+    expect(RESULT).toBe(false);
+  });
+  test("정수가 아닌 경우 false를 반환한다", () => {
+    //given
+    const INPUT_VALUE = "1.5";
+
+    //when
+    const RESULT = Validation.isInteger(INPUT_VALUE);
+
+    //then
+    expect(RESULT).toBe(false);
+  });
+  test("숫자가 음수인 경우 false를 반환한다", () => {
+    const INPUT_VALUE = "-1";
+
+    //when
+    const RESULT = Validation.isNegative(INPUT_VALUE);
+
+    //then
+    expect(RESULT).toBe(false);
+  });
 });
