f129c0a507a6c3e5c02bde86e252c14f65728d3a
sanghee01 <sang6299@naver.com>
2025-02-12 17:26:12 +0900

    feat: 자연수 입력이 아닌 것에 대한 예외처리
    Co-authored-by: jaeyoung-kwon <wodud2443@gmail.com>
    Co-authored-by: aydenote <dslgpgh@gmail.com>

--- src/validates/tryCountValidates.js
+++ src/validates/tryCountValidates.js
@@ -1,3 +1,7 @@
 export function checkTryCountRange(tryCount) {
   if (tryCount < 1 || tryCount > 20) throw new Error('[ERROR] 시도 횟수는 1 ~ 20 사이여야 합니다.');
 }
+
+export function checkIsInteger(tryCount) {
+  if (!Number.isInteger(tryCount)) throw new Error('[ERROR] 시도 횟수는 자연수로 입력되어야 합니다.');
+}
