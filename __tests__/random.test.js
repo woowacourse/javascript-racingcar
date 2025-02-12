14912521512de44796d03077e0d3377a885a9902
eunoia-jaxson <eunoia@handong.ac.kr>
2025-02-12 17:33:22 +0900

    feat: 랜덤 값 생성

---
+++ __tests__/random.test.js
@@ -0,0 +1,8 @@
+import { getRandomValue } from '../src/utils/random';
+
+describe('랜덤 값 테스트', () => {
+  test('랜덤으로 생성한 변수가 지정한 범위 내에 있는지 확인한다.', () => {
+    expect(getRandomValue()).toBeGreaterThanOrEqual(0);
+    expect(getRandomValue()).toBeLessThanOrEqual(9);
+  });
+});
---
+++ src/utils/random.js
@@ -0,0 +1,3 @@
+export function getRandomValue() {
+  return Math.floor(Math.random() * 10);
+}
