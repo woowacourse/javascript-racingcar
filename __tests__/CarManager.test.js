cda2ea67b8dde717964781cf707f0b8ad1293da9
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-12 16:18:20 +0900

    test: 객체를 생성하는 부분 중복 제거

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -1,8 +1,11 @@
 import CarManager from '../src/CarManager.js';
 
+let carManager;
+beforeEach(() => {
+  carManager = new CarManager();
+});
 test('콤마를 기준으로 자동차 이름을 분리한다', () => {
   // given
-  const carManager = new CarManager();
   // when
   const cars = carManager.splitCarName('except, hailey, jun');
 
@@ -12,7 +15,6 @@ test('콤마를 기준으로 자동차 이름을 분리한다', () => {
 
 test('한 대의 자동차를 생성한다.', () => {
   // given
-  const carManager = new CarManager();
   // when
   carManager.createCars('except');
   // then
