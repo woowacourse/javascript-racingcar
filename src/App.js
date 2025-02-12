fff035147c643df27929f57eca483e46b7c25602
sanghee01 <sang6299@naver.com>
2025-02-12 16:57:18 +0900

    feat: 빈 입력에 대한 예외처리
    Co-authored-by: jaeyoung-kwon <wodud2443@gmail.com>
    Co-authored-by: aydenote <dslgpgh@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { checkCarNameLength, checkCarNameDuplicate } from './validates/carValida
 class App {
   async run() {
     const carNamesInput = await readLineAsync('경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n');
+    checkIsEmpty(carNamesInput);
     const carNames = splitString(carNamesInput);
-
     checkCarNameLength(carNames);
     checkCarNameDuplicate(carNames);
   }
