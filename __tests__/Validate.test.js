446af5b79d9bebc26ab836fe94779785bf656455
Oliy <eunsoa0310@gmail.com>
2025-02-12 17:26:36 +0900

    test: 자동차 이름을 입력받은 후 자동차 생성 에 대한 예외 사항 테스트
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- __tests__/Validate.test.js
+++ __tests__/Validate.test.js
@@ -7,37 +7,9 @@ test("공백 입력 시 에러 발생", () => {
   }).toThrow("[ERROR] 공백이 입력되었습니다.");
 })
 
-test("',' 입력 시 에러 발생", () => {
-  const validate = new Validate();
-  expect(() => {
-    validate.isValidCarNames(',');
-  }).toThrow("[ERROR] 공백이 입력되었습니다.");
-})
-
 test("자동차 이름 글자수 5자 초과시 에러 발생", () =>{
     const validate = new Validate();
     expect(() => {
-        validate.isValidCarNames('ABCDEF');
+        validate.carNameLength('ABCDEF');
     }).toThrow("[ERROR] 이름 글자수가 5자를 초과하였습니다.");
-})
-
-test("숫자가 아닌 입력 시 에러 발생", () =>{
-  const validate = new Validate();
-  expect(() => {
-      validate.isNumber(Number("ABCDEF"));
-  }).toThrow("[ERROR] 숫자가 아닙니다.");
-})
-
-test("0 이하의 숫자 입력 시 에러 발생", () =>{
-  const validate = new Validate();
-  expect(() => {
-      validate.isPositiveNumber(0);
-  }).toThrow("[ERROR] 0 이하의 숫자 입니다.");
-})
-
-test("정수가 아닌 입력 시 에러 발생", () =>{
-  const validate = new Validate();
-  expect(() => {
-      validate.isInteger(3.1);
-  }).toThrow("[ERROR] 정수가 아닙니다.");
 })
