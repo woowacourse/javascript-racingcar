ea7774b143ab700995c674be12cc7b223f791602
Oliy <eunsoa0310@gmail.com>
2025-02-12 17:40:36 +0900

    refactor: 자동차 이름 명령문 문구 수정
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- src/InputView.js
+++ src/InputView.js
@@ -23,8 +23,12 @@ function readLineAsync(query) {
 
 class InputView {  
  async readCarNames() {
-    const name = await readLineAsync("자동차 이름을 입력하세요 > ");
+    const name = await readLineAsync("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n");
     return name;
+  }
+
+  async readAttemps(){
+    const attempts = await readLineAsync("시도할 횟수는 몇 회인가요? ");
   }
 }
   
