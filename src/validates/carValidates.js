cfb9b846c865b9b43c114c3abb625d77a77624b9
sanghee01 <sang6299@naver.com>
2025-02-12 16:53:15 +0900

    feat: 이름 중복 예외처리
    Co-authored-by: jaeyoung-kwon <wodud2443@gmail.com>
    Co-authored-by: aydenote <dslgpgh@gmail.com>

--- src/validates/carValidates.js
+++ src/validates/carValidates.js
@@ -3,3 +3,9 @@ export function checkCarNameLength(carNames) {
     if (carName.length < 1 || carName.length > 5) throw new Error('[ERROR] 자동차 이름은 1에서 5자 사이여야 합니다.');
   });
 }
+
+export function checkCarNameDuplicate(carNames) {
+  const carNameSet = new Set(carNames);
+
+  if (carNameSet.size !== carNames.length) throw new Error('[ERROR] 자동차 이름은 중복되면 안됩니다.');
+}
