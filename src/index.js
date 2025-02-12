e647f553142cc9ad6258e1a88cc0686d5c3215e7
minji2219 <aabb0219@naver.com>
2025-02-12 15:38:58 +0900

    feat: MVC 패턴을 위한 Controller 클래스 생성 및 App 초기화

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Controller from "./controller/Controller.js";
+class App {
+  run() {
+    const controller = new Controller();
+    controller.run();
+  }
+}
+
+const app = new App();
+app.run();
