c067ce3c0b624d17319f9a0b807697b8c6efff6a
youdame <112458620+youdame@users.noreply.github.com>
2025-02-12 17:18:06 +0900

    test: 랜덤생성 함수 반복문 안에서 실행

--- __tests__/getRandomNumber.test.js
+++ __tests__/getRandomNumber.test.js
@@ -5,8 +5,10 @@ test("getRandomNumber는 min과 max 사이의 숫자를 반환해야 한다.", (
   const max = RANDOM_NUMBER.MAX;
   const min = RANDOM_NUMBER.MIN;
 
-  const randomNum = getRandomNumber(max, min);
+  for (let i = 0; i < 100; i++) {
+    const randomNum = getRandomNumber(min, max);
 
-  expect(randomNum).toBeGreaterThanOrEqual(min);
-  expect(randomNum).toBeLessThanOrEqual(max);
+    expect(randomNum).toBeGreaterThanOrEqual(min);
+    expect(randomNum).toBeLessThanOrEqual(max);
+  }
 });
