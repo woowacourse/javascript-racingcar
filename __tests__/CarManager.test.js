a7be38720a76c6168a28cc877a1bca2bd057c393
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-12 15:58:18 +0900

    test: 한 대의 자동차를 생성하는 테스트 코드 작성

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -9,3 +9,12 @@ test('콤마를 기준으로 자동차 이름을 분리한다', () => {
   // then
   expect(cars).toEqual(['except', 'hailey', 'jun']);
 });
+
+test('한 대의 자동차를 생성한다.', () => {
+  // given
+  const carManager = new CarManager();
+  // when
+  carManager.createCars('except');
+  // then
+  expect(carManager.cars).toHaveLength(1);
+});
