e9e5390a70b0b6f015df1d4c5aa893c9b72df21c
youdame <112458620+youdame@users.noreply.github.com>
2025-02-12 16:05:01 +0900

    feat: 자동차 전진 기능 구현

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,8 +1,14 @@
 class Car {
   #name;
+  #position;
+
   constructor(name) {
     this.#name = name;
-    this.position = 0;
+    this.#position = 0;
+  }
+
+  move() {
+    this.#position++;
   }
 
   get name() {
