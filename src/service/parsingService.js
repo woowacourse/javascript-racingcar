ae4301b17ca65546f2b1faa1027ae038e72f41ad
spoyodevelop <kjy9505@gmail.com>
2025-02-12 16:32:12 +0900

    feat : 반복할 횟수 입력 및 유효성 검사
    Co-authored-by: hanheel <hanhee630630@gmail.com>

--- src/service/parsingService.js
+++ src/service/parsingService.js
@@ -1,24 +1,26 @@
 const INVALID = false;
+import { errorMessage } from "../settings/ErrorMessage.js";
 
 export const parsingService = {
   parseNames(input) {
     let parsedString = input.split(",");
     if (isNameEmpty(parsedString)) {
-    
-      throw new Error()
+      throw new Error(errorMessage.HAS_EMPTY_NAME);
     }
     if (isNameTooLong(parsedString)) {
-      return INVALID;
+      throw new Error(errorMessage.NAME_TOO_LONG);
     }
     if (isNameDuplicate(parsedString)) {
-      return INVALID;
+      throw new Error(errorMessage.DUPLICATE_NAME);
     }
     return parsedString;
   },
   parseRound(input) {
-    if(isNumber(input)){
-        return INVALID;
+    if (isNumber(input)) {
+      throw new Error(errorMessage.NOT_A_NUMBER);
     }
-    if ()
+    if (isNegative(input)) {
+      throw new Error(errorMessage.NOT_POSITIVE);
+    }
   },
 };
