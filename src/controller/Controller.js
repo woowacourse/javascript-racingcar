7ee00b919978e980492527be612d7c5d705bd397
minji2219 <aabb0219@naver.com>
2025-02-12 15:53:46 +0900

    feat: 자동차 이름 및 시도 횟수 입력받기

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,3 +1,15 @@
+import InputView from "../view/InputView.js";
+
 export default class Controller {
-  run() {}
+  async run() {
+    const inputView = new InputView();
+    const cars = await inputView.readLineAsync(
+      "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    );
+    const carNames = cars.split(",").map((carName) => carName.trim());
+
+    const tryCount = await inputView.readLineAsync(
+      "시도할 횟수는 몇 회인가요?"
+    );
+  }
 }
