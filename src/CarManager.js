8c360d31329c3a4fa68ade9e3a14e9a9f57c8a05
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-12 17:15:54 +0900

    feat: 4 이상 9 이하의 값이 나오면 1칸을 전진한다

--- src/CarManager.js
+++ src/CarManager.js
@@ -1,6 +1,7 @@
 // @ts-check
 import Car from './Car.js';
 import { CONFIG } from './constants/config.js';
+import pickRandomNumber from './utils/pickRandomNumber.js';
 
 class CarManager {
   constructor() {
@@ -16,6 +17,17 @@ class CarManager {
 
   splitCarName(names) {
     return names.split(`${CONFIG.COMMA} `);
+  }
+
+  isMoveCondition(pickedRandomNumber) {
+    return pickedRandomNumber >= 4 && pickedRandomNumber <= 9;
+  }
+
+  moveForwardCar(car, pickedRandomNumber) {
+    const moveCondition = this.isMoveCondition(pickedRandomNumber);
+    if (moveCondition) {
+      return car.move();
+    }
   }
 }
 
