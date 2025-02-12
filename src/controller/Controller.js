2ee37ea924a234f568fb87f33a38aa9dec5cb524
minji2219 <aabb0219@naver.com>
2025-02-12 16:25:59 +0900

    refactor: Car 객체 선언 위치 변경

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -4,28 +4,29 @@ import Car from "../model/Car.js";
 export default class Controller {
   async run() {
     const inputView = new InputView();
-    const cars = await inputView.readLineAsync(
+    const carsInput = await inputView.readLineAsync(
       "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     );
-    const carNames = cars.split(",").map((carName) => carName.trim());
+    const carNames = carsInput.split(",").map((carName) => carName.trim());
 
     const tryCount = await inputView.readLineAsync(
       "시도할 횟수는 몇 회인가요?"
     );
 
-    this.runRace(carNames, tryCount);
-  }
-
-  runRace(carNames, tryCount) {
     const cars = [];
     carNames.forEach((carName) => {
       cars.push(new Car(carName));
     });
+    this.runRace(cars, tryCount);
+  }
+
+  runRace(cars, tryCount) {
     console.log("실행 결과");
     for (let i = 0; i < tryCount; i++) {
       this.gameRound(cars);
     }
   }
+
   gameRound(cars) {
     cars.forEach((car) => {
       if (this.isMove(this.getRandomNumber())) car.move();
