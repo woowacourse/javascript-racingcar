555b5bf2b87a3759d88c7fe99187b91cc9dec887
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-12 17:46:07 +0900

    fix: 랜덤 숫자 생성 함수로 변경

--- src/util/random.js
+++ src/util/random.js
@@ -1,5 +1,7 @@
 import { MAX_RANDOM_NUMBER } from "../constant/constant.js";
 
-const randomNumber = Math.floor(Math.random() * MAX_RANDOM_NUMBER);
+const randomNumber = () => {
+  return Math.floor(Math.random() * MAX_RANDOM_NUMBER);
+};
 
 export default randomNumber;
