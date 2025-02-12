fdea96099aba25eeab0a982d9a52299a318c65ba
spoyodevelop <kjy9505@gmail.com>
2025-02-12 18:41:08 +0900

    feat: error를 출력하는 함수 제작
    Co-authored-by: hanheel <hanhee630630@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import { InputView } from "./view/InputView.js";
+import { parsingService } from "./Service/parsingService.js";
 
+const carInput = await InputView.getCarName();
+let parsedCars;
+// try {
+//   parsedCars = parsingService.parseNames(carInput);
+// } catch (error) {
+//   console.error(error.message);
+// }
+
+function printError(parser, input) {
+  let parsedValue;
+  try {
+    parsedValue = parser(input);
+  } catch (error) {
+    console.error(error.message);
+  }
+  return parsedValue;
+}
+
+// const roundInput = await InputView.getRound();
+// const parsedRound = parsingService.parseRound(roundInput);
