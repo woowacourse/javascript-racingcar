43ea6344f5bd870cf6e2a847326365937f8f8aa4
youdame <112458620+youdame@users.noreply.github.com>
2025-02-12 16:47:57 +0900

    feat: Race 클래스 생성

---
+++ src/domain/Race.js
@@ -0,0 +1,14 @@
+import Car from "./Car.js";
+import { RANDOM_NUMBER } from "../constants/Constants.js";
+import { getRandomNumber } from "../utils/getRandomNumber.js";
+
+class Race {
+  #carList = [];
+
+  constructor(nameList, attemptCount) {
+    this.#carList = nameList.map((name) => new Car(name));
+    this.attemptCount = attemptCount;
+  }
+}
+
+export default Race;
