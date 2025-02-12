5ec590d372f3989cd6355de42b2a6f93f44e6e09
sanghee01 <sang6299@naver.com>
2025-02-12 17:52:42 +0900

    feat: 랜덤 숫자가 4 이상인지 판단
     Co-authored-by: jaeyoung-kwon <wodud2443@gmail.com>
    Co-authored-by: aydenote <dslgpgh@gmail.com>

--- src/Controllers/CarController.js
+++ src/Controllers/CarController.js
@@ -1,6 +1,12 @@
 class CarController {
-  async getRandomNumber() {
+  getRandomNumber() {
     return Math.floor(Math.random() * 10);
+  }
+
+  canMove() {
+    const randomNumber = this.getRandomNumber();
+
+    return randomNumber >= 4;
   }
 }
 
--- src/Models/Car.js
+++ src/Models/Car.js
@@ -1,5 +1,3 @@
 class Car {
   constructor() {}
-
-  setRandomNumber() {}
 }
