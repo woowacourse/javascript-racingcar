05992f4907b3c4cd9d0929a3ea016f84c6911fab
ha-kuku <hakukudh@gmail.com>
2025-02-12 16:52:03 +0900

    feat: 자동차 이름 입력받기 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
+import readLineAsync from "./readLine.js";
+
 class App {
-  run() {
-    console.log("hi");
+  async getCarNameInput() {
+    const input = await readLineAsync(
+      "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n"
+    );
+    return input;
+  }
+
+  async run() {
+    const carNames = await this.getCarNameInput();
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import App from "./app.js";
 
 const app = new App();
-app.run();
+await app.run();
