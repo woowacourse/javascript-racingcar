f5e6b5b753d9bfd82a7eb2b795d552182f30f431
Ko Daeun <drechsu@gmail.com>
2025-02-12 16:44:32 +0900

    0~9 사이의 무작위 값을 구한다

---
+++ __tests__/Util.test.js
@@ -0,0 +1,10 @@
+import { getRandomNumber } from "../src/Util";
+
+test("0-9까지의 랜덤 숫자를 반환한다", () => {
+  //when
+  const randomNum = getRandomNumber();
+
+  //then
+  expect(randomNum).toBeLessThanOrEqual(9);
+  expect(randomNum).toBeGreaterThanOrEqual(0);
+});
---
+++ src/Util.js
@@ -0,0 +1,5 @@
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+export { getRandomNumber };
