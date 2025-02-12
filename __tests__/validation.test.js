7b8707d2474027db9b265e53cbaa5760c8fe2635
ha-kuku <hakukudh@gmail.com>
2025-02-12 19:15:46 +0900

    feat: 시도 횟수 타입 관련 test 추가 소수, 문자열, NaN, Infinity 각각 검사 추가

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -2,6 +2,7 @@ import {
   validationInputLength,
   validationCarNameForm,
   validationDuplicatedCarName,
+  validationGameCountType,
 } from "../src/validation.js";
 
 describe("자동차 이름 유효성 검사", () => {
@@ -28,6 +29,7 @@ describe("자동차 이름 유효성 검사", () => {
 
   test("자동차 이름 틀린 형식", () => {
     const carList = "haku,";
+
     expect(() => {
       validationCarNameForm(carList);
     }).toThrow("[Error]");
@@ -38,5 +40,18 @@ describe("자동차 이름 유효성 검사", () => {
     expect(() => {
       validationDuplicatedCarName(carList);
     }).toThrow("[Error]");
+  });
+});
+
+describe("시도 횟수 유효성 검사", () => {
+  test.each([
+    [0.1, "[Error]"],
+    ["abc", "[Error]"],
+    [NaN, "[Error]"],
+    [Infinity, "[Error]"],
+  ])("시도 횟수 타입", (input, errorMessage) => {
+    expect(() => {
+      validationGameCountType(input);
+    }).toThrow(errorMessage);
   });
 });
