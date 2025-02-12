4741fbb777199b687f6be436e454dad150b3777f
ha-kuku <hakukudh@gmail.com>
2025-02-12 18:35:30 +0900

    feat: 시도 횟수 유효성 검사 추가

--- src/validation.js
+++ src/validation.js
@@ -1,8 +1,10 @@
 const MAX_NAME_LENGTH = 5;
+const MIN_GAME_COUNT = 0;
+const MAX_GAME_COUNT = 100;
 
 const validationInputLength = (input) => {
   if (input.length > MAX_NAME_LENGTH) {
-    throw new Error("자동차 이름은 5자 이하입니다.");
+    throw new Error(`자동차 이름은 ${MAX_NAME_LENGTH}자 초과 안됩니다.`);
   }
 };
 
@@ -11,5 +13,27 @@ const validationCarNameForm = (input) => {
   const carCount = input.split(",").length;
   if (input.trim() === "" || commaCount - 1 !== carCount) {
     throw new Error("자동차 이름이 올바르지 않습니다.");
+  }
+};
+
+const validationDuplicatedCarName = (input) => {
+  const target = Array.from(Set(input.split(",")));
+  const origin = input.split(",");
+  if (target.length !== origin.length) {
+    throw new Error("중복된 자동차 이름은 안됩니다.");
+  }
+};
+
+const validationGameCountType = (input) => {
+  const isInteger = Number.isInteger(Number(input));
+  if (isInteger === false) {
+    throw new Error("시도 횟수는 소수,문자열,NaN,Infinity 안됩니다.");
+  }
+};
+
+const validationGameCountRange = (input) => {
+  const gameCount = Number(input);
+  if (gameCount <= MIN_GAME_COUNT || gameCount >= MAX_GAME_COUNT) {
+    throw new Error("시도 횟수는 0이하, 100이상 안됩니다.");
   }
 };
