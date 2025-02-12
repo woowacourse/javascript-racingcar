9c5b53987fdc5894c4e93c8784760ed0732945d9
yeongipark <yeongi0111@naver.com>
2025-02-12 16:58:15 +0900

    chore: 실행 아이콘 및 숫자 상수화

---
+++ src/constants/setting.js
@@ -0,0 +1,29 @@
+export const SCORE_ICON = "-";
+
+export const CAR_SETTING = {
+  maxCarName: 5,
+  minCarCount: 2,
+  maxCarCount: 100,
+  numberLength: 6,
+};
+
+export const CAR_SETTING = {
+  maxCarName: 5,
+  minCarCount: 2,
+  maxCarCount: 100,
+  numberLength: 6,
+
+  minNumber: 1,
+  maxNumber,
+};
+
+// carNameLengthOverFive: `${ERROR_PREFIX} 자동차 이름은 5글자 이하이어야 합니다.`,
+// carCountUnderTwo: `${ERROR_PREFIX} 자동차 갯수는 2개 이상이어야 합니다.`,
+// carCountOverHundred: `${ERROR_PREFIX} 자동차 갯수는 100개 이하이어야 합니다.`,
+// duplicateCarName: `${ERROR_PREFIX} 중복된 자동차 이름은 입력할 수 없습니다.`,
+
+// // 시도 횟수
+// tryNumberUnderOne: `${ERROR_PREFIX} 시도 횟수는 1회 이상이어야 합니다.`,
+// tryNumberOverHundred: `${ERROR_PREFIX} 시도 횟수는 100회 이하이어야 합니다.`,
+// tryNumberNotPositiveInteger: `${ERROR_PREFIX} 시도 횟수는 양의 정수여야 합니다.`,
+// };
