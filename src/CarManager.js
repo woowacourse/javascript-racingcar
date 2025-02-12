1d87d6a082d6d87a5dad07cd132697d989e6e9ea
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-12 15:57:58 +0900

    feat: 한 대의 자동차를 생성한다

--- src/CarManager.js
+++ src/CarManager.js
@@ -1,6 +1,18 @@
 // @ts-check
+import Car from './Car.js';
 
 class CarManager {
+  constructor() {
+    this.cars = [];
+  }
+
+  createCars(names) {
+    const carNames = this.splitCarName(names);
+
+    this.cars = carNames.map((carName) => new Car(carName));
+    return this.cars;
+  }
+
   splitCarName(names) {
     return names.split(', ');
   }
