476818a110b03aa0056e2e3a72feacd3a383f2ac
thgml05 <aisohii@naver.com>
2025-02-12 17:34:18 +0900

    feat: input attemp and validationt

--- src/Race.js
+++ src/Race.js
@@ -3,14 +3,39 @@ import { ERROR_MESSAGE, INFO_MESSAGE } from "./constants.js";
 import Car from "./Car.js";
 
 export const start = async () => {
+  await getCarNames();
+  await getAttempt();
+};
+
+const getCarNames = async () => {
   while (true) {
     const name = await readLineAsync(INFO_MESSAGE.CAR_NAMR_INPUT);
     const carNames = name.split(",");
     try {
       let cars = carNames.map((name) => new Car(name));
-      if (cars.length > 10) throw Error(ERROR_MESSAGE.CAR_COUNT);
-      if (carNames.length !== new Set(carNames).size)
-        throw Error(ERROR_MESSAGE.CAR_NAME_DUPLICATE);
+      validateCarNames(cars, carNames);
+      break;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+};
+
+const validateCarNames = (cars, carNames) => {
+  if (cars.length > 10) throw Error(ERROR_MESSAGE.CAR_COUNT);
+  if (carNames.length !== new Set(carNames).size)
+    throw Error(ERROR_MESSAGE.CAR_NAME_DUPLICATE);
+};
+
+const getAttempt = async () => {
+  while (true) {
+    const attempt = Number(await readLineAsync(INFO_MESSAGE.ATTEMPT_INPUT));
+    try {
+      if (Number.isNaN(attempt))
+        throw Error(ERROR_MESSAGE.INVALID_ATTEMPT_NUMBER);
+      else if (attempt <= 0) throw Error(ERROR_MESSAGE.ATTEMPT_TOO_LOW);
+      else if (attempt > 50) throw Error(ERROR_MESSAGE.ATTEMPT_TOO_HIGH);
+      break;
     } catch (error) {
       console.log(error.message);
     }
--- src/constants.js
+++ src/constants.js
@@ -2,11 +2,15 @@ export const MOVE_THRESHOLD = 4;
 
 export const INFO_MESSAGE = {
   CAR_NAMR_INPUT:
-    "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분) : ",
+    "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n",
+  ATTEMPT_INPUT: "시도할 횟수는 몇 회인가요?\n",
 };
 
 export const ERROR_MESSAGE = {
-  CAR_NAME_LENGTH: "자동차 이름은 5자 이하여야 합니다.",
-  CAR_COUNT: "경주 가능한 최대 자동차 개수는 10개입니다.",
-  CAR_NAME_DUPLICATE: "자동차 이름이 중복됩니다.",
+  CAR_NAME_LENGTH: "[ERROR] 자동차 이름은 5자 이하여야 합니다.",
+  CAR_COUNT: "[ERROR] 경주 가능한 최대 자동차 개수는 10개입니다.",
+  CAR_NAME_DUPLICATE: "[ERROR] 자동차 이름이 중복됩니다.",
+  INVALID_ATTEMPT_NUMBER: "[ERROR] 시도 횟수는 숫자여야 합니다.",
+  ATTEMPT_TOO_LOW: "[ERROR] 시도 횟수는 1 이상의 정수여야 합니다.",
+  ATTEMPT_TOO_HIGH: "[ERROR] 시도 횟수는 최대 50회까지 가능합니다.",
 };
