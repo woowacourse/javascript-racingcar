85f05f2ded1ee3f0df4b75b31b4d892e70e6743a
Ko Daeun <drechsu@gmail.com>
2025-02-12 16:14:05 +0900

    [INPUT] 시도할 횟수를 입력받는다

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import Validate from "./Validate.js";
 // 입출력 예시
 async function run() {
   const name = await readLineAsync(
-    "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n"
   );
 
-  const names = name.split(",");
-  const validate = new Validate();
-  console.log(validate.carNameLength("aaaaaaaa", 3));
+  //TODO: 자동차 이름 길이를 검증하는 코드 필요요
+  // const names = name.split(",");
+  // const validate = new Validate();
+
+  const count = await readLineAsync("시도할 횟수는 몇 회인가요?\n");
 }
 
 run();
