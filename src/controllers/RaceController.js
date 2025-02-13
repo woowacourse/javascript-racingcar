4e09bec9a71d1786f8e75ac2f660c35e93b604bf
H0ngJu <hongju4385@gmail.com>
2025-02-13 15:43:26 +0900

    refactor: controller 함수 분리

--- src/controllers/RaceController.js
+++ src/controllers/RaceController.js
@@ -4,37 +4,68 @@ import CarNameValidator from "../validators/CarNameValidator.js";
 import TryCountValidator from "../validators/TryCountValidator.js";
 
 class RaceController {
-    #inputView;
-    #outputView;
+  #inputView;
+  #outputView;
 
-    constructor(views) {
-        this.#inputView = views.inputView; 
-        this.#outputView = views.outputView;
+  constructor(views) {
+    this.#inputView = views.inputView;
+    this.#outputView = views.outputView;
+  }
+
+  async race() {
+    const parseCarNames = this.#initCarNames();
+    const parseTryCount = this.#initTryCount();
+
+    const cars = new Cars(parseCarNames);
+
+    this.#processRacing(cars);
+    this.#processWinner(cars);
+  }
+
+  async #initCarNames() {
+    try {
+      const carNames = await this.#inputView.getCarNames();
+      const parseCarNames = carNames
+        .split(",")
+        .map((carName) => carName.trim());
+      new CarNameValidator().valiateNames(parseCarNames);
+
+      return parseCarNames;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async race() {
-        const carNames = await this.#inputView.getCarNames();
-        const parseCarNames = carNames.split(",").map(carName => carName.trim());
-        new CarNameValidator().valiateNames(parseCarNames);
+  async #initTryCount() {
+    try {
+      const tryCount = await this.#inputView.getTryCount();
+      const parseTryCount = Number(tryCount);
+      new TryCountValidator().validateNumber(parseTryCount);
 
-        const tryCount = await this.#inputView.getTryCount();
-        const parseTryCount = Number(tryCount);
-        new TryCountValidator().validateNumber(parseTryCount);
-
-        this.#outputView.printResultHeader();
-
-        const cars = new Cars(parseCarNames);
-        const carList = cars.cars;
-
-        for(let i=0; i<parseTryCount; i++) {
-            cars.moveCars();
-            this.#outputView.printRaceResult(carList);
-            this.#outputView.printNewLine();
-        }
-
-        const maxPosition = cars.getMaxPosition();
-        this.#outputView.printWinners(new Winner().getWinners(carList, maxPosition));
+      return parseTryCount;
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  #processRacing(cars) {
+    this.#outputView.printResultHeader();
+    const carList = cars.cars;
+
+    for (let i = 0; i < parseTryCount; i++) {
+      cars.moveCars();
+      this.#outputView.printRaceResult(carList);
+      this.#outputView.printNewLine();
+    }
+  }
+
+  #processWinner(cars) {
+    const maxPosition = cars.getMaxPosition();
+
+    this.#outputView.printWinners(
+      new Winner().getWinners(carList, maxPosition)
+    );
+  }
 }
 
 export default RaceController;
