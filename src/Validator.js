8193db7e0ea891a71b9e438ba5c1c7101da1cba5
yeongipark <122340316+yeongipark@users.noreply.github.com>
2025-02-13 18:00:59 +0900

    [1단계 - 자동차 경주] 우디(박연기) 제출합니다. (#361)
    * docs: README에 기능 명세 추가
    
    * chore: 에러 메시지 상수화
    
    * chore: 실행 아이콘 및 숫자 상수화
    
    * docs: README에 자동차 전진 조건 추가
    
    * chore: 자동차 이름 최소 길이 상수 추가
    
    * feat: input 클래스 구현
    
    * feat: output 클래스 구현
    
    * refactor: input, output 메시지 상수화
    
    * refactor: Input default 추가
    
    * feat: 0에서 9 사이 무작위 값 구하는 유틸 함수 및 테스트 코드 작성
    
    * feat: car 객체 생성 및 이동 구현
    
    * feat: Validator 클래스 구현 및 테스트
    
    - 자동차 유효성 검사
    - 시도횟수 유효성 검사
    
    * refactor: Car 클래스 export 컨벤션 수정
    
    * fix: import 에러 수정
    
    * feat: race 클래스 자동차 입력, 시도 횟수 입력 구현
    
    * feat: Output 클래스 빈칸 출력 메서드 추가
    
    * feat: Race 클래스 회차별 레이스 구현
    
    * feat: Race 클래스 우승자 출력 구현
    
    * style: Output 클래스 우승자 출력 포맷 수정
    
    * refactor: Race 클래스 메서드 분리
    
    * feat: 잘못된 입력 값 입력시 다시 입력받도록 구현
    
    * refactor: 매직 넘버 상수화 및 변수명 변경
    
    - tryCount을 tryNumber로 변경
    
    * refactor: 불필요 주석 제거
    
    * refactor: tryCount -> tryNumber로 변경
    
    * fix: import 에러 수정
    
    * refactor: 자동차 이동 거리 상수화
    
    * refactor: Car 클래스 내부 프라이빗 필드로 변경

---
+++ src/Validator.js
@@ -0,0 +1,71 @@
+import Output from "./view/Output.js";
+import { ERROR_MESSAGE } from "./constants/message.js";
+import { CAR_SETTING, TRY_NUMBER_SETTING } from "./constants/setting.js";
+
+export default class Validator {
+  static carNamesAndCount(carNames) {
+    this.#carNameLengthOverFive(carNames);
+    this.#carNameLengthUnderOne(carNames);
+    this.#carCountUnderTwo(carNames.length);
+    this.#carCountOverHundred(carNames.length);
+    this.#duplicateCarName(carNames);
+  }
+
+  static tryNumber(tryNumber) {
+    this.#tryNumberUnderOne(tryNumber);
+    this.#tryNumberOverHundred(tryNumber);
+    this.#tryNumberNotPositiveInteger(tryNumber);
+  }
+
+  static #carNameLengthOverFive = (carNames) => {
+    carNames.forEach((carName) => {
+      if (carName.length > CAR_SETTING.maxCarName) {
+        Output.error(ERROR_MESSAGE.carNameLengthOverFive);
+      }
+    });
+  };
+
+  static #carNameLengthUnderOne = (carNames) => {
+    carNames.forEach((carName) => {
+      if (carName.length < CAR_SETTING.minCarName) {
+        Output.error(ERROR_MESSAGE.carNameLengthUnderOne);
+      }
+    });
+  };
+
+  static #carCountUnderTwo = (carCount) => {
+    if (carCount < CAR_SETTING.minCarCount) {
+      Output.error(ERROR_MESSAGE.carCountUnderTwo);
+    }
+  };
+
+  static #carCountOverHundred = (carCount) => {
+    if (carCount > CAR_SETTING.maxCarCount) {
+      Output.error(ERROR_MESSAGE.carCountOverHundred);
+    }
+  };
+
+  static #duplicateCarName = (carNames) => {
+    if (new Set(carNames).size !== carNames.length) {
+      Output.error(ERROR_MESSAGE.duplicateCarName);
+    }
+  };
+
+  static #tryNumberUnderOne = (tryNumber) => {
+    if (tryNumber < TRY_NUMBER_SETTING.minTryNumber) {
+      Output.error(ERROR_MESSAGE.tryNumberUnderOne);
+    }
+  };
+
+  static #tryNumberOverHundred = (tryNumber) => {
+    if (tryNumber > TRY_NUMBER_SETTING.maxTryNumber) {
+      Output.error(ERROR_MESSAGE.tryNumberOverHundred);
+    }
+  };
+
+  static #tryNumberNotPositiveInteger = (tryNumber) => {
+    if (!Number.isInteger(tryNumber)) {
+      Output.error(ERROR_MESSAGE.tryNumberNotPositiveInteger);
+    }
+  };
+}
