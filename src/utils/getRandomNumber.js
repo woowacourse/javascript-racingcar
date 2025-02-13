ff306c143ee729adc120cf8b54f15fffe17b2e85
Jeongeun Lee <leeju1009@gmail.com>
2025-02-13 16:04:05 +0900

    chore(getRandomNumber): 주석 제거

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,7 +1,7 @@
 function getRandomNumber(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // 최댓값도 포함, 최솟값도 포함
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
 export default getRandomNumber;
