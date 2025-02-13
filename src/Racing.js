83cf9284ed4bffa247ae64a2c987c95f4adc9c98
Jeongeun Lee <leeju1009@gmail.com>
2025-02-13 14:01:24 +0900

    feat(racing): 최종 우승자를 계산하는 함수 추가

--- src/Racing.js
+++ src/Racing.js
@@ -1,4 +1,5 @@
 import getRandomNumber from './utils/getRandomNumber.js';
+import Printer from './Printer.js';
 
 class Racing {
   #cars;
@@ -15,6 +16,35 @@ class Racing {
         car.move();
       });
     }
+  }
+
+  runRace(tryCount) {
+    let currentCount = 0;
+
+    while (currentCount < tryCount) {
+      this.raceTurn();
+      const result = this.getCarInfo(this.#cars);
+
+      Printer.printRacingResult(result);
+      currentCount += 1;
+    }
+  }
+
+  getCarInfo() {
+    const result = this.#cars.map((car) => {
+      const position = car.getCarPosition();
+      const name = car.getName();
+      return { name, position };
+    });
+
+    return result;
+  }
+
+  decideWinner() {
+    const results = this.getCarInfo();
+    const positions = results.map((result) => result.position);
+    const maxPosition = Math.max(...positions);
+    return results.filter((result) => result.position === maxPosition);
   }
 }
 
