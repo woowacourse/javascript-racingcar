a461fba63149979efa20446dc814a25da3a5da75
yeongipark <yeongi0111@naver.com>
2025-02-13 14:15:26 +0900

    feat: Output 클래스 빈칸 출력 메서드 추가

--- src/view/Output.js
+++ src/view/Output.js
@@ -1,6 +1,11 @@
 import { SCORE_ICON } from "../constants/setting.js";
+import { OUTPUT_MESSAGE } from "../constants/message.js";
 
 export default class Output {
+  static newLine() {
+    console.log("");
+  }
+
   static print(message) {
     console.log(message);
   }
@@ -9,8 +14,8 @@ export default class Output {
     throw new Error(errorMessage);
   }
 
-  static scoreByRace(car) {
-    this.print(`pobi : ${SCORE_ICON}`);
+  static scoreByRace(carName, carPositions) {
+    this.print(`${carName} : ${SCORE_ICON.repeat(carPositions)}`);
   }
 
   static raceResult() {
