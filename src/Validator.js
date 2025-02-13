dd5d9bbccd8e308843490ba94f31507ef5e77074
yeongipark <yeongi0111@naver.com>
2025-02-13 15:18:53 +0900

    fix: import 에러 수정

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,6 @@
 import Output from "./view/Output.js";
 import { ERROR_MESSAGE } from "./constants/message.js";
-import { CAR_SETTING } from "./constants/setting.js";
+import { CAR_SETTING, TRY_NUMBER_SETTING } from "./constants/setting.js";
 
 export default class Validator {
   static carNamesAndCount(carNames) {
