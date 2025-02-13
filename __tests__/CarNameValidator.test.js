c90539a16875ed5acbccc9ce1409ec67afe019b2
H0ngJu <hongju4385@gmail.com>
2025-02-13 15:44:16 +0900

    test: 로직 변경으로 인한 테스트 코드 수정

--- __tests__/CarNameValidator.test.js
+++ __tests__/CarNameValidator.test.js
@@ -6,19 +6,19 @@ describe('자동차 이름 유효성 클래스 테스트', () => {
     describe('자동차 이름 유효성 클래스 정상 케이스',  () => {
         test('자동차 이름은 1이상 5이하이다.', () => {
             const carNames = ["일", "화성", "토성이", "목성목성", "금성금성금"]
-            expect(() => carNameValidator.valiateCarNameLength(carNames)).not.toThrow();
+            expect(() => carNameValidator.valiateNames(carNames)).not.toThrow();
         });
     });
 
     describe('자동차 이름 유효성 클래스 예외 케이스',  () => {
         test('1글자 미만의 자동차 이름이 포함되어 있으면 에러가 발생한다.', () => {
             const carNames = ["", "화성", "토성이", "목성목성"]
-            expect(() => carNameValidator.valiateCarNameLength(carNames)).toThrow();
+            expect(() => carNameValidator.valiateNames(carNames)).toThrow();
         });
 
         test('5글자를 초과하는 자동차 이름이 포함되어 있으면 에러가 발생한다.', () => {
             const carNames = ["화성", "토성이", "목성목성", "금성금성금성"]
-            expect(() => carNameValidator.valiateCarNameLength(carNames)).toThrow();
+            expect(() => carNameValidator.valiateNames(carNames)).toThrow();
         });
     });
 });
