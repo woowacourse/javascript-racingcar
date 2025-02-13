f43484f41d2361f3af3db0bafafd66a9ec90bebb
Jeongeun Lee <leeju1009@gmail.com>
2025-02-13 15:19:19 +0900

    refactor(app, ioMessage): app 클래스의 run 메서드 로직 분리

--- src/app.js
+++ src/app.js
@@ -7,27 +7,39 @@ import parseToNumber from './utils/parseToNumber.js';
 import Car from './Car.js';
 import Racing from './Racing.js';
 import Printer from './Printer.js';
+import IOMessage from './constants/IOMessage.js';
 
 class App {
   async run() {
+    const cars = await this.getCar();
+    const tryCount = await this.getTryCount();
+    const racing = new Racing(cars);
+    racing.runRace(tryCount);
+    const raceResult = racing.decideWinner();
+    Printer.printWinner(raceResult);
+  }
+
+  async getCar() {
     try {
-      const carName = await InputView.readLineAsync(
-        '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
-      );
-      const parsedCarName = splitInput(carName);
+      const carNameInput = await InputView.readLineAsync(IOMessage.getCarName);
+      const parsedCarName = splitInput(carNameInput);
       NameValidator.isValid(parsedCarName);
-      const cars = parsedCarName.map((carName) => new Car(carName));
-      const tryCount =
-        await InputView.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-      const parsedTryCount = parseToNumber(tryCount);
-      CountValidator.isValid(parsedTryCount);
-      const racing = new Racing(cars);
-      racing.runRace(tryCount);
-      const raceResult = racing.decideWinner();
-      Printer.printWinner(raceResult);
+      return parsedCarName.map((carName) => new Car(carName));
     } catch (error) {
       OutputView.print(error.message);
-      await this.run();
+      return this.getCar();
+    }
+  }
+
+  async getTryCount() {
+    try {
+      const tryCount = await InputView.readLineAsync(IOMessage.getTryCount);
+      const parsedTryCount = parseToNumber(tryCount);
+      CountValidator.isValid(parsedTryCount);
+      return parsedTryCount;
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.getTryCount();
     }
   }
 }
---
+++ src/constants/IOMessage.js
@@ -0,0 +1,7 @@
+const IOMessage = Object.freeze({
+  getCarName:
+    '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
+  getTryCount: '시도할 횟수는 몇 회인가요?\n',
+});
+
+export default IOMessage;
