4847b6c9340715665f5eebc14381883c83a1b1b0
guesung <gueit214@naver.com>
2025-02-13 16:58:19 +0900

    refactor: 상수 분리

--- src/validator.js
+++ src/validator.js
@@ -1,14 +1,17 @@
+import { ERROR_MESSAGES } from './constants.js';
+
 export default class Validator {
   static validateCarName(rawName) {
     const nameList = rawName.split(',');
     for (const name of nameList) {
-      if (name.length > 5 || name.length === 0) throw new Error();
+      if (name.length > 5 || name.length === 0)
+        throw new Error(ERROR_MESSAGES.carName);
     }
   }
 
   static validateCount(rawCount) {
     const count = Number(rawCount);
-    if (Number.isNaN(count)) throw new Error();
-    if (count <= 0) throw new Error();
+    if (Number.isNaN(count)) throw new Error(ERROR_MESSAGES.countNotANumber);
+    if (count <= 0) throw new Error(ERROR_MESSAGES.countNotPositive);
   }
 }
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,21 +1,22 @@
+import { INPUT_MESSAGES } from '../constants.js';
 import { readLineAsync, retryUntilSuccess } from '../utils.js';
 import Validator from '../validator.js';
 
 export default class InputView {
   static async getNameList() {
     return retryUntilSuccess(async () => {
-      const rawName = await readLineAsync(
-        '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
-      );
+      const rawName = await readLineAsync(INPUT_MESSAGES.carName);
       Validator.validateCarName(rawName);
+
       return rawName.split(',');
     });
   }
 
   static async getCount() {
     return retryUntilSuccess(async () => {
-      const rawCount = await readLineAsync('시도할 횟수는 몇 회인가요?\n');
+      const rawCount = await readLineAsync(INPUT_MESSAGES.count);
       Validator.validateCount(rawCount);
+
       return Number(rawCount);
     });
   }
