e26bd77f444fa65403644260a0ce86c45e804d52
keemsebeen <sebin0580@gmail.com>
2025-02-13 11:58:21 +0900

    feat(constants): OUTPUT 상수 객체 정의, ERROR IS_NOT_NUMBER 추가
    Co-authored-by: EunWoo <eunwoo1341@gmail.com>

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,10 +1,16 @@
 export const INPUT = Object.freeze({
-  CAR_NAMES: "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).",
-  TRY_COUNT: "시도할 횟수는 몇 회인가요?",
+  CAR_NAMES: `경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n`,
+  TRY_COUNT: `시도할 횟수는 몇 회인가요?\n`,
+});
+
+export const OUTPUT = Object.freeze({
+  RESULT: "실행 결과",
+  FINAL_WINNER: "최종 우승자:",
 });
 
 export const ERROR = Object.freeze({
   IS_CAR_NAME_EMPTY: "자동차 이름이 비어있습니다.",
   IS_LENGTH_LONGER_THAN_FIVE: "자동차 이름은 다섯 글자 이하로 정해주세요.",
   IS_TRY_COUNT_EMPTY: "시도할 횟수가 비어있습니다.",
+  IS_NOT_NUMBER: "숫자를 입력해주세요.",
 });
