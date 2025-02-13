3589323561ecd3a0e83357ab71952f27a1793c1a
guesung <gueit214@naver.com>
2025-02-13 17:43:54 +0900

    refactor: print를 private필드로 변경

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,28 +1,28 @@
 import { OUTPUT_MESSAGE } from '../helpers/constants.js';
 
 export default class OutputView {
-  static print(message) {
+  static #print(message) {
     console.log(message);
   }
 
   static printBlank() {
-    this.print('');
+    this.#print('');
   }
 
   static printResult() {
-    OutputView.print(OUTPUT_MESSAGE.result);
+    OutputView.#print(OUTPUT_MESSAGE.result);
   }
 
   static printEachGame(nameList, cars) {
     for (let i = 0; i < nameList.length; i++) {
       const carOutput = '-'.repeat(cars[i].position);
-      this.print(`${nameList[i]} : ${carOutput}`);
+      this.#print(`${nameList[i]} : ${carOutput}`);
     }
     this.printBlank();
   }
 
   static printWinner(winners) {
     const winnerOutput = winners.join(', ');
-    this.print(`${winnerOutput}${OUTPUT_MESSAGE.winner}`);
+    this.#print(`${winnerOutput}${OUTPUT_MESSAGE.winner}`);
   }
 }
