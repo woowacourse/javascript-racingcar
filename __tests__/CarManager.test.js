101e32d058b9061631c32d8c2e43c1f20c736987
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-13 16:09:19 +0900

    test: 자동차 생성 시 사용하는 입력 값 수정

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -5,19 +5,11 @@ let carManager;
 beforeEach(() => {
   carManager = new CarManager();
 });
-test('콤마를 기준으로 자동차 이름을 분리한다', () => {
-  // given
-  // when
-  const cars = carManager.splitCarName('except,hailey,jun');
-
-  // then
-  expect(cars).toEqual(['except', 'hailey', 'jun']);
-});
 
 test('한 대의 자동차를 생성한다.', () => {
   // given
   // when
-  carManager.createCars('except');
+  carManager.createCars(['except']);
   // then
   expect(carManager.cars).toHaveLength(1);
 });
