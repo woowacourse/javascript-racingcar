2e045dacc1275872cfcd382e5c0d46014cbf8014
Oliy <eunsoa0310@gmail.com>
2025-02-13 11:19:46 +0900

    feat: 각 자동차의 전진 조건 구하는 기능 추가
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- src/Car.js
+++ src/Car.js
@@ -1,8 +1,17 @@
 class Car {
     name
+    position
     constructor(name) {
         this.name = name;
+        this.position = 0;
     }
+
+    move(randomNumber) {
+        if(randomNumber > 3) {
+            this.position += 1;
+        }
+    }
+
 }
 
 export default Car;
