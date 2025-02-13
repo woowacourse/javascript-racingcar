3373bc564f67dc792443a4dc8b94aa0312ffe44a
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-13 11:02:16 +0900

    refactor: 입력받는 부분 각 메소드로 분리

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -7,10 +7,20 @@ class Controller {
   }
 
   async process() {
-    const carNames = await readLineAsync('경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n');
+    const carNames = await this.readCarNames();
     this.carManager.createCars(carNames);
-    const attempts = await readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const attempts = await this.readAttempts();
     this.carManager.race(attempts);
+  }
+
+  async readCarNames() {
+    const carNames = await readLineAsync('경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n');
+    return carNames;
+  }
+
+  async readAttempts() {
+    const attempts = await readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return attempts;
   }
 }
 
