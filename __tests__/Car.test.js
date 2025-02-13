5bddb1a42529f65baa4ffba840e5e1b38580f2f8
Oliy <eunsoa0310@gmail.com>
2025-02-13 11:21:02 +0900

    test: 각 자동차의 전진 조건에 대한 단위 테스트
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -4,3 +4,15 @@ test("자동차를 만들 수 있다.", ()=>{
     const car = new Car("자동차");
     expect(car).toBeDefined(); 
 });
+
+test("무작위 값이 4이상일 때 자동차가 전진한다.", ()=>{
+    const car = new Car("자동차");
+    car.move(4);
+    expect(car.position).toBe(1);
+});
+
+test("무작위 값이 3이하일 때 자동차가 전진하지 않는다.", ()=>{
+    const car = new Car("자동차");
+    car.move(3);
+    expect(car.position).toBe(0);
+});
