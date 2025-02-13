6386451a7d2c1da52676f68f4dedacfda736b186
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-13 10:54:25 +0900

    feat: 시도할 횟수를 입력

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -9,7 +9,8 @@ class Controller {
   async process() {
     const carNames = await readLineAsync('경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n');
     this.carManager.createCars(carNames);
-    this.carManager.race(3);
+    const attempts = await readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    this.carManager.race(attempts);
   }
 }
 
