daf2476349fe46fa3d622767549c05cfd4daba2c
ahhyun <wndkgus0225@naver.com>
2025-02-13 13:43:21 +0900

    feat:중복이 있는 차 이름 입력에 대한 예외처리 구현과 테스트 코드 작성

--- __tests__/utils/validateCarNames.test.js
+++ __tests__/utils/validateCarNames.test.js
@@ -1,23 +1,38 @@
-import { validateEmptyString, validateCarNameLength } from "../../src/utils/validateCarNames.js";
+import {
+  validateEmptyString,
+  validateCarNameLength,
+  validateDuplicate
+} from "../../src/utils/validateCarNames.js";
 
-describe('utils/valideCarNames', () => {
-    test("이름 입력에 빈 값이 들어온 경우", () => {
-        // given
-        const names = ['A', 'B', 'C', '']
+describe("utils/valideCarNames", () => {
+  test.each([[["일이삼사오", "일이삼"]], [["aaa", "aaa"]]]);
 
-        // when & then
-        expect(() => validateEmptyString(names)).toThrow('이름이 비어있습니다.');
-    });
+  test("이름 입력에 빈 값이 들어온 경우", () => {
+    // given
+    const names = ["A", "B", "C", ""];
 
-    test.each([
-        [['일이삼사오', '일이삼'], null],
-        [['일이삼사오육', '일이삼'], '5자 이하로 설정해주세요.'] 
-    ])('%s가 5자 이하인지 확인', (input, expectedError) => {
-        if (expectedError) {
-            expect(() => validateCarNameLength(input)).toThrow(expectedError);
-        } else {
-            expect(() => validateCarNameLength(input)).not.toThrow();
-        }
-    });
+    // when & then
+    expect(() => validateEmptyString(names)).toThrow("이름이 비어있습니다.");
+  });
 
-})
+  test.each([
+    [["일이삼사오", "일이삼"], null],
+    [["일이삼사오육", "일이삼"], "5자 이하로 설정해주세요."]
+  ])("%s가 5자 이하인지 확인", (input, expectedError) => {
+    if (expectedError) {
+      expect(() => validateCarNameLength(input)).toThrow(expectedError);
+    } else {
+      expect(() => validateCarNameLength(input)).not.toThrow();
+    }
+  });
+
+  test("차 이름이 중복인 경우", () => {
+    // given
+    const names = ["aa", "aa", "b", "c"];
+
+    // when & then
+    expect(() => validateDuplicate(names)).toThrow(
+      "중복된 이름은 사용할 수 없습니다."
+    );
+  });
+});
--- src/utils/validateCarNames.js
+++ src/utils/validateCarNames.js
@@ -1,33 +1,43 @@
-
 export function validateCarNames(names) {
-    // 글자 수 5개 초과 금지
-    // 구분자는 쉼표
+  // 글자 수 5개 초과 금지
+  // 구분자는 쉼표
 
-    // A, B, C, D
-    // ,A, B
-    const namesArr = names.split(',').map(name => name.trim())
-    // ['A', ' B', ' C', ' D']
-    // ['', 'A', 'B']
-    validateEmptyString(namesArr)
-    validateCarNameLength (namesArr)
+  // A, B, C, D
+  // ,A, B
+  const namesArr = names.split(",").map((name) => name.trim());
+  // ['A', ' B', ' C', ' D']
+  // ['', 'A', 'B']
+  validateEmptyString(namesArr);
+  validateCarNameLength(namesArr);
+  validateDuplicate(namesArr);
 
-    
-    return namesArr
+  return namesArr;
 }
 
-export function validateEmptyString (names) {
-    names.forEach(name => {
-        if (name.length === 0) {
-            throw new Error('이름이 비어있습니다.')
-        }
-    })
+export function validateEmptyString(names) {
+  names.forEach((name) => {
+    if (name.length === 0) {
+      throw new Error("이름이 비어있습니다.");
+    }
+  });
 }
 
-export function validateCarNameLength (names) {
-    names.forEach(name => {
-        if (name.length > 5) {
-            throw new Error('5자 이하로 설정해주세요.')
-        }
-    })
+export function validateCarNameLength(names) {
+  names.forEach((name) => {
+    if (name.length > 5) {
+      throw new Error("5자 이하로 설정해주세요.");
+    }
+  });
+}
 
+export function validateDuplicate(names) {
+  let uniqueCars = []; // 중복 없는 자동차들
+
+  for (let name of names) {
+    if (uniqueCars.includes(name)) {
+      // 중복일 때
+      throw new Error("중복된 이름은 사용할 수 없습니다.");
+    }
+    uniqueCars.push(name);
+  }
 }
