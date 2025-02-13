4b170f6c98181b9ab38443197728864519699e13
keemsebeen <sebin0580@gmail.com>
2025-02-13 14:14:14 +0900

    refactor(validation): 파일 구조 변경
    Co-authored-by: EunWoo <eunwoo1341@gmail.com>

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,22 +1,29 @@
 import { ERROR } from "../constants/messages.js";
 
-export const validateCarNames = (carNames) => {
-  const splitCars = carNames.split(",");
-
-  if (isArrEmpty(splitCars)) {
-    throw new Error(ERROR.IS_CAR_NAME_EMPTY);
-  }
-
-  if (!isLengthLongerThanFive(splitCars)) {
-    throw new Error(ERROR.IS_LENGTH_LONGER_THAN_FIVE);
-  }
-  return splitCars;
-};
-
 const isArrEmpty = (arr) => {
   return arr.some((item) => item === "");
 };
 
 const isLengthLongerThanFive = (arr) => {
   return arr.every((item) => item.length < 5);
+};
+
+export const validateCarNames = (carNamesArr) => {
+  if (isArrEmpty(carNamesArr)) {
+    throw new Error(ERROR.IS_CAR_NAME_EMPTY);
+  }
+
+  if (!isLengthLongerThanFive(carNamesArr)) {
+    throw new Error(ERROR.IS_LENGTH_LONGER_THAN_FIVE);
+  }
+};
+
+export const validateCount = (number) => {
+  if (isNaN(number)) {
+    throw new Error(ERROR.IS_NOT_NUMBER);
+  }
+
+  if (number === "") {
+    throw new Error(ERROR.IS_TRY_COUNT_EMPTY);
+  }
 };
