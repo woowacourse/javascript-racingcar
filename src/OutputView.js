e653746fb8778e5011d0e5e8f8d1c5dfce9bcf63
Oliy <eunsoa0310@gmail.com>
2025-02-13 11:40:43 +0900

    feat: 한 회차 실행 결과를 출력하는 기능 추가
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,6 +2,17 @@ class OutputView {
     printErrorMessage(errorMessage){
         console.log(errorMessage);
     }
+
+    printResultMessage(){
+        console.log("실행 결과");
+    }
+
+    printOneRoundResult(cars){
+        cars.forEach(car => {
+            console.log(`${car.name} : ${'-'.repeat(car.position)}`);
+        });
+        console.log();
+    }
 }
 
 export default OutputView;
