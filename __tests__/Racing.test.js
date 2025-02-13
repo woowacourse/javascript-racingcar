8f54f26f7d1155350f0335e40fd42c6e93c3bca5
Jeongeun Lee <leeju1009@gmail.com>
2025-02-13 14:52:41 +0900

    test(racing.test): 우승자 판단 메서드 테스트 추가

--- __tests__/Racing.test.js
+++ __tests__/Racing.test.js
@@ -4,23 +4,63 @@ import getRandomNumber from '../src/utils/getRandomNumber.js';
 
 jest.mock('../src/utils/getRandomNumber');
 
-test.each([
-  [4, [1, 1]],
-  [0, [0, 0]],
-])(
-  '랜덤 숫자가 %s 이면 자동차의 위치는 %s가 된다.',
-  (randomNumber, positions) => {
-    getRandomNumber.mockReturnValue(randomNumber);
+describe('랜덤 숫자에 따른 자동차 경주 테스트', () => {
+  test.each([
+    [4, [1, 1]],
+    [0, [0, 0]],
+  ])(
+    '랜덤 숫자가 %s 이면 자동차의 위치는 %s가 된다.',
+    (randomNumber, positions) => {
+      getRandomNumber.mockReturnValue(randomNumber);
 
-    getRandomNumber();
-    getRandomNumber();
+      getRandomNumber();
+      getRandomNumber();
 
+      const NAMES = ['jenna', 'mato'];
+      const cars = NAMES.map((name) => new Car(name));
+      const racing = new Racing(cars);
+      racing.raceTurn();
+
+      const positionResult = cars.map((car) => car.getCarPosition());
+      expect(positionResult).toEqual(positions);
+    },
+  );
+});
+
+describe('우승자 판단 메서드 테스트', () => {
+  test('우승자가 한 명일 수 있다.', () => {
     const NAMES = ['jenna', 'mato'];
     const cars = NAMES.map((name) => new Car(name));
-    const racing = new Racing(cars);
-    racing.raceTurn();
 
-    const positionResult = cars.map((car) => car.getCarPosition());
-    expect(positionResult).toEqual(positions);
-  },
-);
+    const racing = new Racing(cars);
+    racing.getCarInfo = jest.fn().mockReturnValue([
+      { name: 'jenna', position: 3 },
+      { name: 'mato', position: 2 },
+    ]);
+
+    const winners = racing.decideWinner();
+
+    const expectedWinner = [{ name: 'jenna', position: 3 }];
+
+    expect(winners).toEqual(expectedWinner);
+  });
+
+  test('우승자가 두 명 이상일 수 있다.', () => {
+    const NAMES = ['jenna', 'mato'];
+    const cars = NAMES.map((name) => new Car(name));
+
+    const racing = new Racing(cars);
+    racing.getCarInfo = jest.fn().mockReturnValue([
+      { name: 'jenna', position: 2 },
+      { name: 'mato', position: 2 },
+    ]);
+    const winners = racing.decideWinner();
+
+    const expectedWinner = [
+      { name: 'jenna', position: 2 },
+      { name: 'mato', position: 2 },
+    ];
+
+    expect(winners).toEqual(expectedWinner);
+  });
+});
