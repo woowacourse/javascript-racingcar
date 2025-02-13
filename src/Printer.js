81a6e1a84a235e0ebc266560b3c34b3c9ce4d4f8
Jeongeun Lee <leeju1009@gmail.com>
2025-02-13 14:05:03 +0900

    chore(printer): 주석 제거

--- src/Printer.js
+++ src/Printer.js
@@ -1,7 +1,6 @@
 import OutputView from './view/OutputView.js';
 
 class Printer {
-  // 실행 결과 출력
   static printRacingResult(results) {
     results.forEach((result) => {
       const { name, position } = result;
@@ -13,7 +12,6 @@ class Printer {
     OutputView.print('');
   }
 
-  //   // 최종 우승자 출력
   static printWinner(results) {
     const winner = results.map((result) => result.name).join(', ');
 
