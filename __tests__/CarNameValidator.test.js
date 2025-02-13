26e989c3d21373203ff1c5f2b10da8c42fea9787
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-13 15:24:19 +0900

    test: 자동차 이름이 중복 유효성 검사하는 테스트 코드 작성

--- __tests__/CarNameValidator.test.js
+++ __tests__/CarNameValidator.test.js
@@ -1,4 +1,4 @@
-import CarNameValidator from '../src/validator/CarNameValidator';
+import CarNameValidator from '../src/validator/CarNameValidator.js';
 
 test('자동차 이름은 1자 이상 5자 이하가 아니면 에러를 띄운다.', () => {
   // given
@@ -15,4 +15,10 @@ test('자동차 이름에 공백이 들어가면 에러를 띄운다', () => {
 
   // then
   expect(() => CarNameValidator.checkBlank(carName)).toThrow('[ERROR]');
+});
+
+test('자동차 이름이 중복되면 에러를 띄운다.', () => {
+  const carNames = ['pobi', 'pobi', 'jun'];
+
+  expect(() => CarNameValidator.checkDuplicatedCarName(carNames)).toThrow('[ERROR]');
 });
