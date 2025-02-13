3838d96a379ec1e251fff57dd15f6b5f6cb688e0
hwanheejung <hwanheejung117@gmail.com>
2025-02-14 08:42:57 +0900

    refactor: playGame 코드 축약

--- src/domain/playGame.js
+++ src/domain/playGame.js
@@ -5,18 +5,14 @@ import { printResultHeader, printRoundScore } from "../view/output.js";
 // 3
 
 const playGame = (carNames, rounds) => {
-  let cars = [];
-  for (let name of carNames) {
-    cars.push({ name, count: 0 });
-  }
-
   printResultHeader();
-  for (let i = 0; i < rounds; i++) {
-    const updatedCars = playRound(cars);
-    cars = updatedCars;
-    // 각 라운드 결과 출력
+
+  let cars = carNames.map((name) => ({ name, count: 0 }));
+  Array.from({ length: rounds }).forEach(() => {
+    cars = playRound(cars);
     printRoundScore(cars);
-  }
+  });
+
   return cars;
 };
 
