36fa4209b71871ce777e7a30394154b7fae7ae0d
minji2219 <aabb0219@naver.com>
2025-02-13 11:57:55 +0900

    test: 자동차 경주 한 라운드 진행 테스트 작성

---
+++ __tests__/Controller.test.js
@@ -0,0 +1,39 @@
+import Controller from "../src/controller/Controller.js";
+import { getRandomNumber } from "../src/utils/getRandomNumber.js";
+import Car from "../src/model/Car.js";
+
+jest.mock("../src/utils/getRandomNumber", () => ({
+  getRandomNumber: jest.fn(),
+}));
+
+describe("자동차 경주 테스트", () => {
+  test("자동차 한 라운드 진행 후 결과 출력한다.(한대일 경우)", () => {
+    getRandomNumber.mockReturnValue(5);
+    const logSpy = jest.spyOn(console, "log");
+
+    const CARS = [new Car("머핀")];
+
+    const controller = new Controller();
+
+    controller.gameRound(CARS);
+
+    expect(logSpy).toHaveBeenCalledWith("머핀 : -");
+  });
+
+  test("자동차 한 라운드 진행 후 결과 출력한다.(여러대일 경우)", () => {
+    getRandomNumber.mockReturnValue(5);
+    getRandomNumber.mockReturnValue(3);
+    const logSpy = jest.spyOn(console, "log");
+    const logs = ["머핀 : -", "데이지 : "];
+
+    const CARS = [new Car("머핀"), new Car("데이지")];
+
+    const controller = new Controller();
+
+    controller.gameRound(CARS);
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(log);
+    });
+  });
+});
