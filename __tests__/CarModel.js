04fa7f45e7fe37ea487d7b7a44995cc86ef5f382
guesung <gueit214@naver.com>
2025-02-13 17:12:35 +0900

    refactor: 차를 beforeEach에서 생성

--- __tests__/CarModel.js
+++ __tests__/CarModel.js
@@ -2,19 +2,20 @@ import { mockRandom } from '../src/helpers/testUtils.js';
 import { CarModel } from '../src/models/index.js';
 
 describe('CarModel', () => {
+  let car;
+  beforeEach(() => {
+    car = new CarModel();
+  });
+
   test('CarModel을 생성한다.', () => {
-    const car = new CarModel();
     expect(car).toBeDefined();
   });
 
   test('CarModel은 초기에 position은 0이다.', () => {
-    const car = new CarModel();
-
     expect(car.position).toBe(0);
   });
 
   test('CarModel은 랜덤 숫자가 4이상일 시 앞으로 전진한다.', () => {
-    const car = new CarModel();
     for (let value = 4; value <= 9; value++) {
       mockRandom(value);
       car.go();
@@ -24,8 +25,6 @@ describe('CarModel', () => {
   });
 
   test('CarModel은 랜덤 숫자가 4이하일시 정지한다.', () => {
-    const car = new CarModel();
-
     for (let value = 0; value < 4; value++) {
       mockRandom(value);
       car.go();
