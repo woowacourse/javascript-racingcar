df796412a15013a473419b6018b74fa77d373b55
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-13 15:39:13 +0900

    test: 시도 횟수는 양의 정수 유효성 검사하는 테스트 코드 작성

---
+++ __tests__/AttemptsValidator.js
@@ -0,0 +1,8 @@
+import AttemptsValidator from '../src/validator/AttemptsValidator.js';
+
+test.each([
+  [-1],
+  [0],
+])('시도할 횟수가 양의 정수가 아니면 에러를 띄운다', (attempts) => {
+  expect(() => AttemptsValidator.checkPositiveNumber(attempts)).toThrow(Error);
+});
