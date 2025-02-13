c0997e1e6fbc86f5eadfbc60189e4734d95a755c
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-13 14:56:09 +0900

    test: 자동차 이름 입력 형식에 맞게 입력 값 수정

--- __tests__/CarManager.test.js
+++ __tests__/CarManager.test.js
@@ -8,7 +8,7 @@ beforeEach(() => {
 test('콤마를 기준으로 자동차 이름을 분리한다', () => {
   // given
   // when
-  const cars = carManager.splitCarName('except, hailey, jun');
+  const cars = carManager.splitCarName('except,hailey,jun');
 
   // then
   expect(cars).toEqual(['except', 'hailey', 'jun']);
