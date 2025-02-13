ffb1dac0a8fa68064dd740d0082c270a4efe8fc2
ahhyun <wndkgus0225@naver.com>
2025-02-13 14:22:43 +0900

    feat:moveCar에 대한 테스트코드 작성

--- src/domain/playGame.js
+++ src/domain/playGame.js
@@ -1,14 +1,13 @@
 import playRound from "./playRound.js";
-import { printRoundScore } from "../view/output.js";
+import { printResultHeader, printRoundScore } from "../view/output.js";
 
 const playGame = (carNames, rounds) => {
-  // 초기화
   let cars = [];
   for (let name of carNames) {
     cars.push({ name, count: 0 });
   }
 
-  console.log("\n실행 결과");
+  printResultHeader();
   for (let i = 0; i < rounds; i++) {
     const updatedCars = playRound(cars);
     cars = updatedCars;
--- src/view/output.js
+++ src/view/output.js
@@ -1,3 +1,7 @@
+export const printResultHeader = () => {
+  console.log("\n실행 결과");
+};
+
 export const printRoundScore = (cars) => {
   for (let car of cars) {
     console.log(`${car.name}: ${"-".repeat(car.count)}`);
