2219d90b58a8f7b98d41d03b504e121ac9cb18b8
guesung <gueit214@naver.com>
2025-02-14 16:58:00 +0900

    feat: 실패 시 에러 메시지 출력

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -25,7 +25,8 @@ export async function readLineAsync(query) {
 export async function retryUntilSuccess(callbackFn) {
   try {
     return await callbackFn();
-  } catch {
+  } catch (error) {
+    console.log(error.message);
     return await retryUntilSuccess(callbackFn);
   }
 }
