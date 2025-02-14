68c842c607f0820db2a65a6ffa6284c5b2487e96
Jeongeun Lee <leeju1009@gmail.com>
2025-02-14 18:13:39 +0900

    refactor(app): 가독성을 고려하여 줄바꿈 수정

--- src/app.js
+++ src/app.js
@@ -19,8 +19,10 @@ class App {
   async run() {
     const cars = await this.getCar();
     const tryCount = await this.getTryCount();
+
     const racing = new Racing(cars);
     racing.runRace(tryCount, App.getIsMoveList(cars));
+
     const raceResult = racing.decideWinner();
     Printer.printWinner(raceResult);
   }
@@ -31,7 +33,9 @@ class App {
         InputMessage.getCarName,
       );
       const parsedCarName = splitInput(carNameInput);
+
       NameValidator.isValid(parsedCarName);
+
       return parsedCarName.map((carName) => new Car(carName));
     } catch (error) {
       OutputView.print(error.message);
@@ -43,7 +47,9 @@ class App {
     try {
       const tryCount = await InputView.readLineAsync(InputMessage.getTryCount);
       const parsedTryCount = parseToNumber(tryCount);
+
       CountValidator.isValid(parsedTryCount);
+
       return parsedTryCount;
     } catch (error) {
       OutputView.print(error.message);
