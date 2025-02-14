15a813e705d24046b9e84fe80e6a9ccf9a3a53b5
Jeongeun Lee <leeju1009@gmail.com>
2025-02-14 16:53:26 +0900

    refactor(OutputMessage): 상수와 포맷팅 함수를 별도의 객체로 분리

--- src/constants/OutputMessage.js
+++ src/constants/OutputMessage.js
@@ -1,8 +1,9 @@
-const OutputMessage = Object.freeze({
+export const OutputMessage = Object.freeze({
   resultHeader: '\n실행 결과',
-  getRaceResult: (name, raceResult) => `${name} : ${raceResult}`,
-  getWinner: (winner) => `최종 우승자: ${winner}`,
   lineBreak: '',
 });
 
-export default OutputMessage;
+export const OutputFormat = Object.freeze({
+  getRaceResult: (name, raceResult) => `${name} : ${raceResult}`,
+  getWinner: (winner) => `최종 우승자: ${winner}`,
+});
