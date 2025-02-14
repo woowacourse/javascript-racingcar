963110b125440c909c4a92bfba32948644aa9cc8
마 <126178440+shuyeon@users.noreply.github.com>
2025-02-14 23:43:53 +0900

    refactor : 소수와 문자열 구분하여 테스트

--- __tests__/ValicateCarNameList.test.js
+++ __tests__/ValicateCarNameList.test.js
@@ -35,8 +35,12 @@ describe("시도 횟수 유효성 검사", () => {
   test("빈 값을 입력할 경우 에러 발생", () => {
     expect(() => validateAttemptCount("")).toThrow("[ERROR] 입력이 비어 있습니다.");
   });
+
+  test.each(["도레미도레미도", "마이턴"])("시도 횟수가 문자열인 경우 에러 발생", (input) => {
+    expect(() => validateAttemptCount(input)).toThrow("[ERROR] 정수를 입력해주세요.");
+  });
   
-  test.each([-1.3, 342.3, "도레미도레미도", "마이턴"])("시도 횟수가 정수가 아닌 경우 에러 발생", (input) => {
+  test.each([-1.3, 342.3])("시도 횟수가 정수가 아닌 경우 에러 발생", (input) => {
     expect(() => validateAttemptCount(input)).toThrow("[ERROR] 정수를 입력해주세요.");
   });
   
