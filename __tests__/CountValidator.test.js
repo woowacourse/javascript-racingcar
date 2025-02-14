6315e928ff21c3e04555b79187cede7685f8da5a
Jeongeun Lee <leeju1009@gmail.com>
2025-02-14 17:00:22 +0900

    refactor(CountValidator.test): 시도 횟수와 관련된 테스트를 describe로 그룹화

--- __tests__/CountValidator.test.js
+++ __tests__/CountValidator.test.js
@@ -1,35 +1,36 @@
 import CountValidator from '../src/utils/validator/CountValidator.js';
 import ErrorMessage from '../src/constants/ErrorMessage.js';
 
-test('시도횟수가 숫자가 아니면 에러 처리한다.', () => {
-  const TRY_COUNT = NaN;
+describe('시도 횟수의 예외 상황 테스트', () => {
+  test('시도횟수가 숫자가 아니면 에러 처리한다.', () => {
+    const TRY_COUNT = NaN;
 
-  expect(() => CountValidator.isNumber(TRY_COUNT)).toThrow(
-    ErrorMessage.isNotNumber,
-  );
-});
+    expect(() => CountValidator.isNumber(TRY_COUNT)).toThrow(
+      ErrorMessage.isNotNumber,
+    );
+  });
 
-// todo: test.each
-test('시도횟수가 0 이하이면 에러 처리한다.', () => {
-  const TRY_COUNT = 0;
+  test('시도횟수가 정수가 아니면 에러 처리한다.', () => {
+    const TRY_COUNT = 0.5;
 
-  expect(() => CountValidator.isAtLeastOne(TRY_COUNT)).toThrow(
-    ErrorMessage.isInvalidNumber,
-  );
-});
+    expect(() => CountValidator.isInteger(TRY_COUNT)).toThrow(
+      ErrorMessage.isNotInteger,
+    );
+  });
 
-test('시도횟수가 정수가 아니면 에러 처리한다.', () => {
-  const TRY_COUNT = 0.5;
+  test('시도횟수가 0 이하이면 에러 처리한다.', () => {
+    const TRY_COUNT = 0;
 
-  expect(() => CountValidator.isInteger(TRY_COUNT)).toThrow(
-    ErrorMessage.isNotInteger,
-  );
-});
+    expect(() => CountValidator.isAtLeastOne(TRY_COUNT)).toThrow(
+      ErrorMessage.isInvalidNumber,
+    );
+  });
 
-test('시도횟수가 100만번을 넘으면 에러 처리한다.', () => {
-  const TRY_COUNT = 2000000;
+  test('시도횟수가 100만번을 넘으면 에러 처리한다.', () => {
+    const TRY_COUNT = 2000000;
 
-  expect(() => CountValidator.isNotBigNumber(TRY_COUNT)).toThrow(
-    ErrorMessage.isBigNumber,
-  );
+    expect(() => CountValidator.isNotBigNumber(TRY_COUNT)).toThrow(
+      ErrorMessage.isBigNumber,
+    );
+  });
 });
