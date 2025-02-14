205e3a6b75c5740725e43384401baf1445397383
spoyodevelop <kjy9505@gmail.com>
2025-02-15 00:18:51 +0900

    chores(index): import 순서 조정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import { raceInit } from "./service/raceService.js";
-
 import { InputView } from "./view/InputView.js";
 import { OutputView } from "./view/OutputView.js";
 import { parsingService } from "./service/parsingService.js";
-import { startRace } from "./service/raceService.js";
-import { systemMessage } from "./settings/SystemMessage.js";
+import { startRace, raceInit } from "./service/raceService.js";
 import { getWinner } from "./service/statisticsService.js";
+import { systemMessage } from "./settings/SystemMessage.js";
 
 const { cars, round } = await raceInit(InputView, parsingService);
 
