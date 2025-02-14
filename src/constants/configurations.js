df078248b50e35f6ed34e49bc33ce8fb6c244120
Minje Sung <minje813@dgu.ac.kr>
2025-02-14 19:06:12 +0900

    refactor: 매직넘버 상수 분리
    최소 이름 길이, 최소 참가 자동차, 최소 횟수, 최대 횟수 분리

--- src/constants/configurations.js
+++ src/constants/configurations.js
@@ -1,5 +1,8 @@
-const MOVING_THRESHOLD = 4;
+export const MOVING_THRESHOLD = 4;
+export const MOVING_DISTANCE = 1;
 
-const MOVING_DISTANCE = 1;
+export const MIN_NAME_LENGTH = 5;
+export const MIN_CARS_FOR_RACE = 2;
 
-export { MOVING_THRESHOLD, MOVING_DISTANCE };
+export const MIN_RACING_COUNT = 1;
+export const MAX_RACING_COUNT = 1000;
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,24 +1,33 @@
+import {
+  MAX_RACING_COUNT,
+  MIN_CARS_FOR_RACE,
+  MIN_NAME_LENGTH,
+  MIN_RACING_COUNT,
+} from '../constants/configurations.js';
 import { ERROR_MESSAGE } from '../constants/systemMessages.js';
 
 const validator = Object.freeze({
   carNames(carNames) {
     if (carNames.length !== new Set(carNames).size)
       throw new Error(ERROR_MESSAGE.DUPLICATE_NAME);
-    if (carNames.some((name) => name.length > 5))
+    if (carNames.some((name) => name.length > MIN_NAME_LENGTH))
       throw new Error(ERROR_MESSAGE.INVALID_NAME_LENGTH);
     if (carNames.some((name) => name.includes(' ')))
       throw new Error(ERROR_MESSAGE.INVALID_NAME_SPACE);
     if (carNames.some((name) => !/^[a-zA-Z]+$/.test(name)))
       throw new Error(ERROR_MESSAGE.INVALID_NAME_CHARACTER);
-    if (carNames.length < 2) throw new Error(ERROR_MESSAGE.INVALID_CAR_COUNT);
+    if (carNames.length < MIN_CARS_FOR_RACE)
+      throw new Error(ERROR_MESSAGE.INVALID_CAR_COUNT);
   },
 
   count(count) {
     if (count.includes(' ')) throw new Error(ERROR_MESSAGE.INVALID_COUNT_SPACE);
     if (!/^[0-9]+$/.test(count))
       throw new Error(ERROR_MESSAGE.INVALID_COUNT_CHARACTER);
-    if (Number(count) < 1) throw new Error(ERROR_MESSAGE.INVALID_COUNT_MIN);
-    if (Number(count) > 1000) throw new Error(ERROR_MESSAGE.INVALID_COUNT_MAX);
+    if (Number(count) < MIN_RACING_COUNT)
+      throw new Error(ERROR_MESSAGE.INVALID_COUNT_MIN);
+    if (Number(count) > MAX_RACING_COUNT)
+      throw new Error(ERROR_MESSAGE.INVALID_COUNT_MAX);
   },
 });
 
