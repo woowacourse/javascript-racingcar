09aed44d742864997729a32ae4fc3169cda13801
guesung <gueit214@naver.com>
2025-02-14 16:59:55 +0900

    refactor: 에러 메시지 명시

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,8 @@
+const DEFAULT_ERROR_MESSAGE = '[ERROR]';
 export const ERROR_MESSAGES = {
-  carName: '1글자 이상 5글자 이하의 자동차를 입력해주세요.',
-  countNotANumber: '숫자를 입력해주세요.',
-  countNotPositive: '양의 정수를 입력해주세요.',
+  carName: `${DEFAULT_ERROR_MESSAGE} 1글자 이상 5글자 이하의 자동차를 입력해주세요.`,
+  countNotANumber: `${DEFAULT_ERROR_MESSAGE} 숫자를 입력해주세요.`,
+  countNotPositive: `${DEFAULT_ERROR_MESSAGE} 양의 정수를 입력해주세요.`,
 };
 
 export const INPUT_MESSAGES = {
