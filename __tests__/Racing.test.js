246a2cc62e62d21a86bc74ab1271a6736cd8b4d6
Jeongeun Lee <leeju1009@gmail.com>
2025-02-14 19:01:29 +0900

    refactor(Racing.test): 랜덤 숫자를 받는 로직의 분리에 따른 모킹 함수 제거

--- __tests__/Racing.test.js
+++ __tests__/Racing.test.js
@@ -1,42 +1,62 @@
 import Car from '../src/Car.js';
 import Racing from '../src/Racing.js';
-import getRandomNumber from '../src/utils/getRandomNumber.js';
-
-jest.mock('../src/utils/getRandomNumber');
 
 describe('랜덤 숫자에 따른 자동차 경주 테스트', () => {
   test.each([
-    [4, [1, 1]],
-    [0, [0, 0]],
+    [
+      [true, false],
+      [1, 0],
+    ],
+    [
+      [false, false],
+      [0, 0],
+    ],
+    [
+      [true, true],
+      [1, 1],
+    ],
   ])(
     '랜덤 숫자가 %s 이면 자동차의 위치는 %s가 된다.',
-    (randomNumber, positions) => {
-      getRandomNumber.mockReturnValue(randomNumber);
-
-      getRandomNumber();
-      getRandomNumber();
-
+    (carsMoveList, positions) => {
       const NAMES = ['jenna', 'mato'];
       const cars = NAMES.map((name) => new Car(name));
       const racing = new Racing(cars);
-      racing.raceTurn();
+      racing.raceTurn(carsMoveList);
 
       const positionResult = cars.map((car) => car.getCarPosition());
       expect(positionResult).toEqual(positions);
     },
   );
 
-  test('시도 횟수만큼 raceTurn을 호출하는지 runRace 메서드를 테스트한다.', () => {
-    const NAMES = ['jenna', 'mato'];
-    const TRY_COUNT = 5;
-    const cars = NAMES.map((name) => new Car(name));
+  test.each([
+    [
+      [
+        [true, true],
+        [true, true],
+        [true, true],
+      ],
+      3,
+    ],
+    [
+      [
+        [true, true],
+        [true, true],
+      ],
+      2,
+    ],
+  ])(
+    '시도 횟수만큼 raceTurn을 호출하는지 runRace 메서드를 테스트한다.',
+    (totalRaceMoves, expectedCalledTimes) => {
+      const NAMES = ['jenna', 'mato'];
+      const cars = NAMES.map((name) => new Car(name));
 
-    const racing = new Racing(cars);
-    const raceTurnSpy = jest.spyOn(racing, 'raceTurn');
-    racing.runRace(TRY_COUNT);
+      const racing = new Racing(cars);
+      const raceTurnSpy = jest.spyOn(racing, 'raceTurn');
+      racing.runRace(totalRaceMoves);
 
-    expect(raceTurnSpy).toHaveBeenCalledTimes(TRY_COUNT);
-  });
+      expect(raceTurnSpy).toHaveBeenCalledTimes(expectedCalledTimes);
+    },
+  );
 });
 
 describe('우승자 판단 메서드 테스트', () => {
