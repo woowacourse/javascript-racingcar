223bf18e99a8948991668ea4731b9ee6d30d57e0
bunju20 <85238126+bunju20@users.noreply.github.com>
2025-02-14 15:18:48 +0900

    Update src/index.js
    Co-authored-by: SONG WON LEE <swon3210@gmail.com>

--- src/index.js
+++ src/index.js
@@ -30,10 +30,8 @@ export async function run() {
     printOneGame(nameList, cars);
   }
 
-  let winnerPosition = 0;
-  cars.forEach(car => {
-    winnerPosition = Math.max(car.position, winnerPosition);
-  });
+  const winnerPosition = cars.reduce((maxPosition, car) => 
+    Math.max(car.position, maxPosition), 0);
 
   const winnerList = [];
   for (let i = 0; i < nameList.length; i++) {
