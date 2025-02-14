5a096fb07b88ef24857068ea7d822a6ec709bace
Jeongeun Lee <leeju1009@gmail.com>
2025-02-14 17:15:24 +0900

    refactor(NameValidator.test): 자동차 이름과 관련된 테스트를 describe로 그룹화

--- __tests__/NameValidator.test.js
+++ __tests__/NameValidator.test.js
@@ -1,34 +1,36 @@
 import NameValidator from '../src/utils/validator/NameValidator.js';
 import ErrorMessage from '../src/constants/ErrorMessage.js';
 
-test(`자동차 이름이 빈 값이면 ${ErrorMessage.isEmpty} 메시지를 포함한 에러를 throw 한다.`, () => {
-  const CAR_NAME = '';
+describe('자동차 이름의 예외 상황 테스트', () => {
+  test(`자동차 이름이 빈 값이면 ${ErrorMessage.isEmpty} 메시지를 포함한 에러를 throw 한다.`, () => {
+    const CAR_NAME = '';
 
-  expect(() => NameValidator.isNotEmpty(CAR_NAME)).toThrow(
-    ErrorMessage.isEmpty,
-  );
-});
+    expect(() => NameValidator.isNotEmpty(CAR_NAME)).toThrow(
+      ErrorMessage.isEmpty,
+    );
+  });
 
-test(`자동차가 2대 미만이면 ${ErrorMessage.isLessThanTwo} 메시지를 포함한 에러를 throw 한다.`, () => {
-  const CAR_NAMES = ['jenna'];
+  test(`자동차가 2대 미만이면 ${ErrorMessage.isLessThanTwo} 메시지를 포함한 에러를 throw 한다.`, () => {
+    const CAR_NAMES = ['jenna'];
 
-  expect(() => NameValidator.isMoreThanTwo(CAR_NAMES)).toThrow(
-    ErrorMessage.isLessThanTwo,
-  );
-});
+    expect(() => NameValidator.isMoreThanTwo(CAR_NAMES)).toThrow(
+      ErrorMessage.isLessThanTwo,
+    );
+  });
 
-test(`자동차 이름이 5자를 초과하면 ${ErrorMessage.isLongerThanFive} 메시지를 포함한 에러를 throw 한다.`, () => {
-  const CAR_NAMES = ['jenna', 'mato', 'gongwon'];
+  test(`자동차 이름이 5자를 초과하면 ${ErrorMessage.isLongerThanFive} 메시지를 포함한 에러를 throw 한다.`, () => {
+    const CAR_NAMES = ['jenna', 'mato', 'gongwon'];
 
-  expect(() => NameValidator.isBelowFive(CAR_NAMES)).toThrow(
-    ErrorMessage.isLongerThanFive,
-  );
-});
+    expect(() => NameValidator.isBelowFive(CAR_NAMES)).toThrow(
+      ErrorMessage.isLongerThanFive,
+    );
+  });
 
-test(`자동차 이름이 중복되면 ${ErrorMessage.isDuplicated} 메시지를 포함한 에러를 throw 한다.`, () => {
-  const CAR_NAMES = ['jenna', 'jenna', 'mato'];
+  test(`자동차 이름이 중복되면 ${ErrorMessage.isDuplicated} 메시지를 포함한 에러를 throw 한다.`, () => {
+    const CAR_NAMES = ['jenna', 'jenna', 'mato'];
 
-  expect(() => NameValidator.isNotDuplicated(CAR_NAMES)).toThrow(
-    ErrorMessage.isDuplicated,
-  );
+    expect(() => NameValidator.isNotDuplicated(CAR_NAMES)).toThrow(
+      ErrorMessage.isDuplicated,
+    );
+  });
 });
