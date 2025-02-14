c60639645c0a83f79732f17ccdf743587ae1e87a
spoyodevelop <kjy9505@gmail.com>
2025-02-15 00:53:54 +0900

    refactor: errorHandler를 고차함수를 이용하여 분리

---
+++ src/util/errorHandler.js
@@ -0,0 +1,8 @@
+import { OutputView } from "../view/OutputView.js";
+export const withTryCatch = (fn) => (input) => {
+  try {
+    return fn(input);
+  } catch (error) {
+    OutputView.printError(error);
+  }
+};
