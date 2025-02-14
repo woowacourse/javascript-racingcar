f0f5297087622038432027a8d546724db7d68ab1
eunoia-jaxson <eunoia@handong.ac.kr>
2025-02-14 16:36:04 +0900

    refactor: 상수화 및 오타 수정

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,24 +1,39 @@
+import {
+  MAX_NAME_LENGTH,
+  MAX_RACE_COUNT,
+  MIN_CAR_COUNT,
+  MIN_RACE_COUNT,
+  NAME_PATTERN,
+  NUMBER_PATTERN,
+} from '../constants/configurations.js';
 import { ERROR_MESSAGE } from '../constants/systemMessages.js';
 
 const validator = Object.freeze({
   carNames(carNames) {
     if (carNames.length !== new Set(carNames).size)
       throw new Error(ERROR_MESSAGE.DUPLICATE_NAME);
-    if (carNames.some((name) => name.length > 5))
+
+    if (carNames.some((name) => name.length > MAX_NAME_LENGTH))
       throw new Error(ERROR_MESSAGE.INVALID_NAME_LENGTH);
+
     if (carNames.some((name) => name.includes(' ')))
       throw new Error(ERROR_MESSAGE.INVALID_NAME_SPACE);
-    if (carNames.some((name) => !/^[a-zA-Z]+$/.test(name)))
+
+    if (carNames.some((name) => !NAME_PATTERN.test(name)))
       throw new Error(ERROR_MESSAGE.INVALID_NAME_CHARACTER);
-    if (carNames.length < 2) throw new Error(ERROR_MESSAGE.INVALID_CAR_COUNT);
+
+    if (carNames.length < MIN_CAR_COUNT) throw new Error(ERROR_MESSAGE.INVALID_CAR_COUNT);
   },
 
   count(count) {
     if (count.includes(' ')) throw new Error(ERROR_MESSAGE.INVALID_COUNT_SPACE);
-    if (!/^[0-9]+$/.test(count))
+
+    if (!NUMBER_PATTERN.test(count))
       throw new Error(ERROR_MESSAGE.INVALID_COUNT_CHARACTER);
-    if (Number(count) < 1) throw new Error(ERROR_MESSAGE.INVALID_COUNT_MIN);
-    if (Number(count) > 1000) throw new Error(ERROR_MESSAGE.INVALID_COUNT_MAX);
+
+    if (Number(count) < MIN_RACE_COUNT) throw new Error(ERROR_MESSAGE.INVALID_COUNT_MIN);
+
+    if (Number(count) > MAX_RACE_COUNT) throw new Error(ERROR_MESSAGE.INVALID_COUNT_MAX);
   },
 });
 
