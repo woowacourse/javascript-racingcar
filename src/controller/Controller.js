3ac7aced6c8bb2357d81069dde94744e7af78e69
서민지 <119796600+minji2219@users.noreply.github.com>
2025-02-14 18:50:40 +0900

    [1단계 - 자동차 경주] 머핀(서민지) 제출합니다.  (#349)
    * docs: 기능 구현 사항 작성
    
    * feat: MVC 패턴을 위한 Controller 클래스 생성 및 App 초기화
    
    * feat: 입력 기능 구현
    
    * docs: 기능 구현 사항 체크박스 추가
    
    * feat: Car 모델 생성
    
    * feat: 자동차 이름 및 시도 횟수 입력받기
    
    * chore: export문 추가
    
    * feat: 자동차 수 만큼 객체 생성 및 경주 진행
    
    * feat: 한 라운드를 진행하는 함수 구현
    
    * feat: 전체 경주 결과 출력
    
    * refactor: Car 객체 선언 위치 변경
    
    * feat: 최종 우승자 뽑기 및 출력
    
    * docs: 구현 완료 체크 표시
    
    * docs: 구현 사항 추가
    
    * feat: 상수화 적용
    
    * docs: 기능 추가
    
    * feat: validation 함수 생성
    
    * feat: 자동차 입력에 관한 예외 처리 및 재입력 기능 구현
    
    * feat: 시도 횟수 입력에 관한 예외처리 및 재입력 구현
    
    * refactor: 함수 선언 순서 변경
    
    - 우선순위를 고려하여 함수 순서를 변경
    
    * refactor: 재사용될 수 있는 함수를 utils로 분리
    
    - getRandomNumber utils로 분리
    
    * test: 자동차 모델 테스트 작성
    
    * test: 자동차 경주 한 라운드 진행 테스트 작성
    
    * test: describe 제목 변경
    
    * test: 경주 완료 후 전진 정보 변화 테스트 작성
    
    * test: getRandomNumber함수의 다중 호출 반환값 설정 방식 개선
    
    - 모킹을 mockReturnValueOnce 체이닝으로 변경함
    
    * test: 최종 우승자 선별 테스트 작성
    
    * test: 예외 테스트 작성
    
    - 잘못된 자동차 이름 입력시
    - 잘못된 시도 횟수 입력시
    
    * refactor: 잘못된 입력시 재입력 받는 기능 공통 함수로 분리
    
    - validateAndRetry공통 함수 생성
    
    * refactor: 상수 객체 동결
    
    외부에서의 수정을 막기 위해 상수 객체 동결
    
    * refactor: 변수 네이밍 의미있게 변경
    
    * refactor: 코드컨벤션에 맞춰 수정
    
    - position++ 으로 증가 시키던 것을 position+=1로 변경
    
    * refactor: 가독성을 위한 줄바꿈 추가
    
    * refactor: 피드백 고려 후 수정
    
    - inputView를 constructor로 빼 한번만 생성하도록 구현
    - map을 사용하여 배열 리턴
    
    * refactor: outputview 생성
    
    * refactor: 불필요한 코드 삭제
    
    isNaN의 기능으로 인해 대체되는 코드 삭제
    
    * test: 예외테스트 정확한 에러메시지 사용으로 변경
    
    * refactor: 함수명 변경

---
+++ src/controller/Controller.js
@@ -0,0 +1,69 @@
+import InputView from "../view/InputView.js";
+import Car from "../model/Car.js";
+import { INPUT_MESSAGE, OUTPUT_MESSAGE } from "../constants/constants.js";
+import { CAR } from "../constants/constants.js";
+import { validateCarNames, validateTryCount } from "../utils/validation.js";
+import { getRandomNumber } from "../utils/getRandomNumber.js";
+import OutputView from "../view/OutputView.js";
+
+export default class Controller {
+  constructor() {
+    this.inputView = new InputView();
+    this.outputView = new OutputView();
+  }
+
+  async run() {
+    const carNameInput = await this.validateAndRetry(
+      INPUT_MESSAGE.CAR_NAMES,
+      validateCarNames,
+    );
+    const carNames = carNameInput.split(",").map((carName) => carName.trim());
+    const tryCount = await this.validateAndRetry(
+      INPUT_MESSAGE.TRY_COUNT,
+      validateTryCount,
+    );
+
+    const cars = carNames.map((carName) => new Car(carName));
+
+    this.runRace(cars, tryCount);
+    this.findWinner(cars);
+  }
+
+  runRace(cars, tryCount) {
+    console.log(OUTPUT_MESSAGE.RESULT);
+    for (let i = 0; i < tryCount; i++) {
+      this.runRound(cars);
+    }
+  }
+
+  findWinner(cars) {
+    const max = Math.max(...cars.map((car) => car.position));
+    const winners = cars.filter((car) => car.position === max);
+    this.outputView.printWinner(winners);
+  }
+
+  runRound(cars) {
+    cars.forEach((car) => {
+      if (this.isMove(getRandomNumber())) {
+        car.move();
+      }
+      this.outputView.printProgressResult(car.name, car.position);
+    });
+    console.log("");
+  }
+
+  async validateAndRetry(message, validateFn) {
+    try {
+      const input = await this.inputView.readLineAsync(message);
+      validateFn(input);
+      return input;
+    } catch (e) {
+      console.log(e.message);
+      return this.validateAndRetry(message, validateFn);
+    }
+  }
+
+  isMove(number) {
+    return number >= CAR.PROGRESS_CRITERIA;
+  }
+}
