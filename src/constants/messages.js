0101933fd0c6cadb9e2f2a5b639a32653c6c961f
keemsebeen <sebin0580@gmail.com>
2025-02-14 17:49:22 +0900

    feat(message): ERROR  `IS_LENGTH_LONGER_THAN_FIVE` 상수 포함하도록 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,3 +1,5 @@
+import { MAX_CAR_NAME_LENGTH } from "./race.js";
+
 export const INPUT = Object.freeze({
   CAR_NAMES: `경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n`,
   TRY_COUNT: `시도할 횟수는 몇 회인가요?\n`,
@@ -10,7 +12,7 @@ export const OUTPUT = Object.freeze({
 
 export const ERROR = Object.freeze({
   IS_CAR_NAME_EMPTY: "자동차 이름이 비어있습니다.",
-  IS_LENGTH_LONGER_THAN_FIVE: "자동차 이름은 다섯 글자 이내로 작성주세요.",
+  IS_LENGTH_LONGER_THAN_FIVE: `자동차 이름은 ${MAX_CAR_NAME_LENGTH}글자 이내로 작성주세요.`,
   IS_TRY_COUNT_EMPTY: "시도할 횟수가 비어있습니다.",
   IS_NOT_NUMBER: "숫자를 입력해주세요.",
 });
