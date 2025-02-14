16fbcce5602d54762b27422e2680e3ea0941e01f
Minje Sung <minje813@dgu.ac.kr>
2025-02-14 21:26:16 +0900

    refactor: Checkers 이용 InputValidator 조건 변경

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -5,28 +5,32 @@ import {
   MIN_RACING_COUNT,
 } from '../constants/configurations.js';
 import { ERROR_MESSAGE } from '../constants/systemMessages.js';
+import { ListChecker, NumberChecker, StringChecker } from './Checkers.js';
 
 const InputValidator = Object.freeze({
   carNames(carNames) {
-    if (carNames.length !== new Set(carNames).size)
+    if (ListChecker.isNoRepeatValue(carNames))
       throw new Error(ERROR_MESSAGE.DUPLICATE_NAME);
-    if (carNames.some((name) => name.length > MIN_NAME_LENGTH))
-      throw new Error(ERROR_MESSAGE.INVALID_NAME_LENGTH);
-    if (carNames.some((name) => name.includes(' ')))
-      throw new Error(ERROR_MESSAGE.INVALID_NAME_SPACE);
-    if (carNames.some((name) => !/^[a-zA-Z]+$/.test(name)))
-      throw new Error(ERROR_MESSAGE.INVALID_NAME_CHARACTER);
-    if (carNames.length < MIN_CARS_FOR_RACE)
+    carNames.forEach((name) => {
+      if (StringChecker.isMoreThanValue(name, MIN_NAME_LENGTH))
+        throw new Error(ERROR_MESSAGE.INVALID_NAME_LENGTH);
+      if (StringChecker.isIncludeBlank(name))
+        throw new Error(ERROR_MESSAGE.INVALID_NAME_SPACE);
+      if (StringChecker.isNotRegString(name, /^[a-zA-Z]+$/))
+        throw new Error(ERROR_MESSAGE.INVALID_NAME_CHARACTER);
+    });
+    if (ListChecker.isLessThanValue(carNames, MIN_CARS_FOR_RACE))
       throw new Error(ERROR_MESSAGE.INVALID_CAR_COUNT);
   },
 
   count(count) {
-    if (count.includes(' ')) throw new Error(ERROR_MESSAGE.INVALID_COUNT_SPACE);
-    if (!/^[0-9]+$/.test(count))
+    if (StringChecker.isIncludeBlank(count))
+      throw new Error(ERROR_MESSAGE.INVALID_COUNT_SPACE);
+    if (StringChecker.isNotRegString(count, /^[0-9]+$/))
       throw new Error(ERROR_MESSAGE.INVALID_COUNT_CHARACTER);
-    if (Number(count) < MIN_RACING_COUNT)
+    if (NumberChecker.isLessThanValue(Number(count), MIN_RACING_COUNT))
       throw new Error(ERROR_MESSAGE.INVALID_COUNT_MIN);
-    if (Number(count) > MAX_RACING_COUNT)
+    if (NumberChecker.isMoreThanValue(Number(count), MAX_RACING_COUNT))
       throw new Error(ERROR_MESSAGE.INVALID_COUNT_MAX);
   },
 });
