ff3e947bdad96671238a0296d95cd380630199a7
spoyodevelop <kjy9505@gmail.com>
2025-02-15 00:27:44 +0900

    test(validation): 테스트 케이스 보충

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,64 +1,59 @@
 import { Validation } from "../src/validation/Validation.js";
+
 describe("input validation test", () => {
   test("이름에 공백이 존재하는 경우 false를 반환한다", () => {
-    //given
-    const INPUT_VALUE = "1,2,,".split(",");
-
-    //when
-    const RESULT = Validation.isNameNotEmpty(INPUT_VALUE);
-
-    //then
-    expect(RESULT).toBe(false);
+    const INPUT_VALUES = [" ", "", "   "];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isNameNotEmpty(value.split(","))).toBe(false);
+    });
   });
 
-  test("이름이 5자 이상인 경우 false를 반환한다", () => {
-    //given
-    const INPUT_VALUE = "abcdfe".split(",");
-
-    //when
-    const RESULT = Validation.isNameTooLong(INPUT_VALUE);
-
-    //then
-    expect(RESULT).toBe(false);
+  test("이름이 5자 초과인 경우 false를 반환한다", () => {
+    const INPUT_VALUES = ["abcdef", "abcdefgh"];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isNameTooLong(value.split(","))).toBe(false);
+    });
   });
+
   test("이름에 중복이 존재하는 경우 false를 반환한다", () => {
-    //given
-    const INPUT_VALUE = "a,a,b,c".split(",");
-
-    //when
-    const RESULT = Validation.isNameDuplicate(INPUT_VALUE);
-
-    //then
-    expect(RESULT).toBe(false);
+    const INPUT_VALUES = ["a,a", "b,b,b", "c,d,c"];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isNameDuplicate(value.split(","))).toBe(false);
+    });
   });
 
   test("문자가 입력된 경우 false를 반환한다", () => {
-    //given
-    const INPUT_VALUE = "a";
-
-    //when
-    const RESULT = Validation.isInteger(INPUT_VALUE);
-
-    //then
-    expect(RESULT).toBe(false);
+    const INPUT_VALUES = ["a", "abc", "A", "Z", "1a"];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isInteger(value)).toBe(false);
+    });
   });
+
   test("정수가 아닌 경우 false를 반환한다", () => {
-    //given
-    const INPUT_VALUE = "1.5";
-
-    //when
-    const RESULT = Validation.isInteger(INPUT_VALUE);
-
-    //then
-    expect(RESULT).toBe(false);
+    const INPUT_VALUES = ["1.5", "0.1", "-1.2", "3.14159"];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isInteger(value)).toBe(false);
+    });
   });
+
   test("숫자가 음수인 경우 false를 반환한다", () => {
-    const INPUT_VALUE = "-1";
+    const INPUT_VALUES = ["-1", "-100", "-99999"];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isPositive(value)).toBe(false);
+    });
+  });
 
-    //when
-    const RESULT = Validation.isPositive(INPUT_VALUE);
+  test("유효한 정수인 경우 true를 반환한다", () => {
+    const INPUT_VALUES = ["0", "1", "10", "99999"];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isInteger(value)).toBe(true);
+    });
+  });
 
-    //then
-    expect(RESULT).toBe(false);
+  test("양의 정수인 경우 true를 반환한다", () => {
+    const INPUT_VALUES = ["1", "10", "99999"];
+    INPUT_VALUES.forEach((value) => {
+      expect(Validation.isPositive(value)).toBe(true);
+    });
   });
 });
