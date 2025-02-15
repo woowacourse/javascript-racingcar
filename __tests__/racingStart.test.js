76fec81e6b50bbe4f6d1375363e8df408f526bac
thgml05 <aisohii@naver.com>
2025-02-15 18:50:26 +0900

    test: toThrow에 new Error 사용

--- __tests__/racingStart.test.js
+++ __tests__/racingStart.test.js
@@ -9,7 +9,7 @@ describe("예외 처리", () => {
 
       expect(() => {
         validateCars(cars, carNames);
-      }).toThrow(ERROR_MESSAGE.CAR_NAME_DUPLICATE);
+      }).toThrow(new Error(ERROR_MESSAGE.CAR_NAME_DUPLICATE));
     });
 
     test("자동차 개수 예외 처리", () => {
@@ -18,7 +18,7 @@ describe("예외 처리", () => {
 
       expect(() => {
         validateCars(cars, carNames);
-      }).toThrow(ERROR_MESSAGE.CAR_MAX_COUNT);
+      }).toThrow(new Error(ERROR_MESSAGE.CAR_MAX_COUNT));
     });
   });
 
@@ -26,19 +26,19 @@ describe("예외 처리", () => {
     test("시도 횟수가 숫자가 아닌 경우 예외 처리", () => {
       expect(() => {
         validateAttempt(Number("p"));
-      }).toThrow(ERROR_MESSAGE.INVALID_ATTEMPT_NUMBER);
+      }).toThrow(new Error(ERROR_MESSAGE.INVALID_ATTEMPT_NUMBER));
     });
 
     test("시도 횟수가 음수인 경우 예외 처리", () => {
       expect(() => {
         validateAttempt(-1);
-      }).toThrow(ERROR_MESSAGE.ATTEMPT_TOO_LOW);
+      }).toThrow(new Error(ERROR_MESSAGE.ATTEMPT_TOO_LOW));
     });
 
     test("시도 횟수가 50회를 초과할 경우 예외 처리", () => {
       expect(() => {
         validateAttempt(51);
-      }).toThrow(ERROR_MESSAGE.ATTEMPT_TOO_HIGH);
+      }).toThrow(new Error(ERROR_MESSAGE.ATTEMPT_TOO_HIGH));
     });
   });
 });
