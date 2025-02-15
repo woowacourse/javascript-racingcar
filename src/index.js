5aed53d2a64d99e03e40a844099ce1936017c450
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-02-15 12:41:00 +0900

    ♻️ refactor: Index 클래스 제거

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import Game from './controllers/Game.js';
 
-class Index {
-  async start() {
-    const game = new Game();
-    await game.start();
-  }
-}
+const start = async () => {
+  const game = new Game();
+  await game.start();
+};
 
-const index = new Index();
-index.start();
+start();
