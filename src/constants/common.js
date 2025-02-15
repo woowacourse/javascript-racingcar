f73c81ecbc12033d280a1bad9f10b41cf3ba9f85
aydenote <dslgpgh@gmail.com>
2025-02-15 19:48:31 +0900

    refactor: magic number 상수 분리

---
+++ src/constants/common.js
@@ -0,0 +1,10 @@
+// 자동차 이름
+export const MIN_CAR_NAME = 1;
+export const MAX_CAR_NAME = 5;
+export const MIN_CAR_COUNT = 2;
+
+// 시도 횟수
+export const MIN_TRY_COUNT = 1;
+export const MAX_TRY_COUNT = 20;
+
+export const MIN_FORWARD_NUMBER = 4;
