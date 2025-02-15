6ced6b77aae5f9a2a6a90cbc9a1025e846fdfc29
yeongipark <yeongi0111@naver.com>
2025-02-15 21:36:44 +0900

    test: Car 테스트 코드 추가 및 테스트 이름 변경
    - 자동차 이름 예외 상황 아닌 경우 테스트
    
    - 자동차 이름 예외 상황인 경우 테스트 이름 직관적으로 변경

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -12,10 +12,19 @@ test.each([
 });
 
 test.each([
-  ["자동차의 이름은 1글자 이하여야 한다.", ""],
-  ["자동차 이름은 5글자 이하여야 한다.", "여섯글자임다"],
+  ["자동차의 이름은 1글자 이하인 경우 에러가 발생한다.", ""],
+  ["자동차 이름은 6글자 이상인 경우 에러가 발생한다.", "여섯글자임다"],
 ])("%s", (_, carName) => {
   expect(() => {
     const car = new Car(carName);
   }).toThrow();
 });
+
+test.each([["2자"], ["세글자"], ["pobi"], ["pobii"]])(
+  "자동차의 이름은 1글자 이상, 5글자 이하이어야 한다.",
+  (carName) => {
+    expect(() => {
+      const car = new Car(carName);
+    }).not.toThrow();
+  }
+);
