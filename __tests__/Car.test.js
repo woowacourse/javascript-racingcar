4133ad94a543024f35fb99dc6dc2d20be075e712
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-02-15 20:57:20 +0900

    ♻️ refactor: 테스트명 변경

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -8,23 +8,23 @@ test('Car.js의 객체가 잘 생성되는가?', () => {
   expect(car).not.toBeUndefined();
 });
 
-test('Car.js가 4이상일 때 전진을 하는가?', () => {
+test('랜덤 값이 4 이상일때 자동차가 전진을 하는가?', () => {
   //given
+  const car = new Car();
   const randomValue = 4;
 
   //when
-  const car = new Car();
   car.moveForward(randomValue);
 
   //then
   expect(car.position).toBe(1);
 });
 
-test('Car.js가 4이하일 때 멈추는가?', () => {
+test('랜덤 값이 4이하일 때 멈추는가?', () => {
   //given
+  const car = new Car();
   const randomValue = 3;
   //when
-  const car = new Car();
   car.moveForward(randomValue);
   //then
   expect(car.position).toBe(0);
