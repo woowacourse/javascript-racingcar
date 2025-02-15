513aa8bb4fd3ef1ca73db8d38a07b0e9a30630dc
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-15 19:21:43 +0900

    refactor(constants): 구체적인 예외 조건을 위해 에러 메시지 구체화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -5,12 +5,12 @@ export const INPUT = Object.freeze({
 
 export const OUTPUT = Object.freeze({
   RESULT: `실행 결과\n`,
-  FINAL_WINNER: "최종 우승자:",
+  FINAL_WINNER: '최종 우승자:',
 });
 
 export const ERROR = Object.freeze({
-  IS_CAR_NAME_EMPTY: "자동차 이름이 비어있습니다.",
-  IS_LENGTH_LONGER_THAN_FIVE: "자동차 이름은 다섯 글자 이내로 작성주세요.",
-  IS_TRY_COUNT_EMPTY: "시도할 횟수가 비어있습니다.",
-  IS_NOT_NUMBER: "숫자를 입력해주세요.",
+  IS_CAR_NAME_EMPTY: '자동차 이름이 비어있습니다.',
+  IS_LENGTH_LONGER_THAN_FIVE: '자동차 이름은 다섯 글자 이내로 작성주세요.',
+  IS_TRY_COUNT_EMPTY: '시도할 횟수가 비어있습니다.',
+  IS_NOT_NUMBER: '0보다 큰 정수를 입력해주세요.',
 });
