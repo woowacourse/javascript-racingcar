9d63d5c8ab7f602dcdb21cf1ba2bdb8e7e5a6d4b
H0ngJu <hongju4385@gmail.com>
2025-02-15 21:24:33 +0900

    chore: ESLint, prettier 수정

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"], // 파일 대상은 .js
+    rules: {
+      eqeqeq: ["error", "always"],
+      "no-multi-spaces": "error",
+      "comma-dangle": ["error", "never"],
+    },
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+];
