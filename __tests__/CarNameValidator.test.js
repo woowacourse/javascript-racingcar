2726677f2db272d505357adbdc2f736384ede661
장정안 <wjddks96@naver.com>
2025-02-15 22:08:01 +0900

    refactor: 유효성 검증 테스트 코드 내에 테스트 제목 구체화 및 구체적인 에러 메세지로 테스트 변경

--- __tests__/CarNameValidator.test.js
+++ __tests__/CarNameValidator.test.js
@@ -1,19 +1,24 @@
+import { ERROR } from '../src/constants/messages.js';
 import CarNameValidator from '../src/validator/CarNameValidator.js';
 
-test('자동차 이름은 1자 이상 5자 이하가 아니면 에러를 띄운다.', () => {
+test('checkCarNameLength함수는 자동차 이름이 1자 이상 5자 이하가 아니면 에러를 띄운다.', () => {
   const carName = 'exceptqwer';
 
-  expect(() => CarNameValidator.checkCarNameLength(carName)).toThrow('[ERROR]');
+  expect(() => CarNameValidator.checkCarNameLength(carName)).toThrow(
+    ERROR.CAR_NAME_LENGTH
+  );
 });
 
-test('자동차 이름에 공백이 들어가면 에러를 띄운다', () => {
+test('checkBlank함수는 자동차 이름에 공백이 들어가면 에러를 띄운다', () => {
   const carName = 'po bi';
 
-  expect(() => CarNameValidator.checkBlank(carName)).toThrow('[ERROR]');
+  expect(() => CarNameValidator.checkBlank(carName)).toThrow(ERROR.BLANK);
 });
 
-test('자동차 이름이 중복되면 에러를 띄운다.', () => {
+test('checkDuplicatedCarName함수는 자동차 이름이 중복되면 에러를 띄운다.', () => {
   const carNames = ['pobi', 'pobi', 'jun'];
 
-  expect(() => CarNameValidator.checkDuplicatedCarName(carNames)).toThrow('[ERROR]');
+  expect(() => CarNameValidator.checkDuplicatedCarName(carNames)).toThrow(
+    ERROR.DUPLICATED_CAR_NAME
+  );
 });
