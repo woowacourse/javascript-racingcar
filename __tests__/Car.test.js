b16aef6475238d3ca21d70309b773c8820a1a0bb
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-16 02:04:06 +0900

    refactor: 문자열에 포함된 매직 넘버를 상수로 변경

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,4 +1,5 @@
 import Car from "../src/model/Car.js";
+import { MIN } from "../src/constant/range.js";
 
 describe("Car", () => {
   let car;
@@ -15,7 +16,7 @@ describe("Car", () => {
     expect(car.getPosition()).toBe(0);
   });
 
-  test("4이상의 숫자가 나오면 자동차는 전진할 수 있다.", () => {
+  test(`${MIN.MOVE_CONDITION}이상의 숫자가 나오면 자동차는 전진할 수 있다.`, () => {
     const randomNumber = 4;
 
     car.move(randomNumber);
@@ -23,7 +24,7 @@ describe("Car", () => {
     expect(car.getPosition()).toBe(1);
   });
 
-  test("4미만의 숫자가 나오면 자동차는 전진하지 않는다.", () => {
+  test(`${MIN.MOVE_CONDITION}미만의 숫자가 나오면 자동차는 전진하지 않는다.`, () => {
     const randomNumber = 3;
 
     car.move(randomNumber);
--- src/constant/error.js
+++ src/constant/error.js
@@ -1,9 +1,10 @@
+import { MIN, MAX } from "./range.js";
+
 const ERROR = Object.freeze({
   HEADER: "[⛔️WARNING⛔️]",
-  MIN_CAR_COUNT:
-    "자동차는 최소 2대 이상 필요합니다. 쉼표(,)를 기준으로 구분해주세요.",
-  NAME_LENGTH: "이름은 1자 이상 5자 이하만 가능합니다.",
-  RACE_COUNT: "횟수는 1이상 필요합니다.",
+  MIN_CAR_COUNT: `자동차는 최소 ${MIN.CAR_COUNT}대 이상 필요합니다. 쉼표(,)를 기준으로 구분해주세요.`,
+  NAME_LENGTH: `이름은 ${MIN.NAME_LENGTH}자 이상 ${MAX.NAME_LENGTH}자 이하만 가능합니다.`,
+  RACE_COUNT: `횟수는 ${MIN.RACE_COUNT}이상 필요합니다.`,
   RACE_COUNT_INTEGER: "횟수는 정수여야 합니다.",
   RACE_COUNT_NUMBER: "횟수는 숫자여야 합니다.",
 });
