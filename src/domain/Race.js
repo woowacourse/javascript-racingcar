db44eaa50f44c668f4db72db7d3b11ada1f696ef
jin123457 <jin_ox@naver.com>
2025-02-15 19:00:14 +0900

    Rename : controller -> Race로 파일명  수정
    [수정된 파일]
    Race.js : controller(기존)에서 Race(신규)로 좀 더 명확하게 파일명을 수정했습니다.

--- src/domain/Race.js
+++ src/domain/Race.js
@@ -6,7 +6,7 @@ import tryInput from "../view/tryInput.js";
 import { validateCarNames, validateRaceCount } from "./validation.js";
 import { SPLITTER } from "../constant/constant.js";
 
-class Controller {
+class Race {
   #carList;
   #raceCount;
 
@@ -70,4 +70,4 @@ class Controller {
   }
 }
 
-export default Controller;
+export default Race;
