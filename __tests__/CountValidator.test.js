bd12ebe687486503ffa8e6942bf4aac42ce16653
deokbin <deokbin@naver.com>
2025-02-15 21:32:37 +0900

    test(CountValidator) : 테스트 코드 내 매직넘버 반영 및 경계값 변경

--- __tests__/CountValidator.test.js
+++ __tests__/CountValidator.test.js
@@ -9,8 +9,7 @@ test('시도횟수가 숫자가 아니면 에러 처리한다.', () => {
   );
 });
 
-// todo: test.each
-test('시도횟수가 0 이하이면 에러 처리한다.', () => {
+test(`시도횟수가 ${MIN_TRY_COUNT} 미만이면 에러 처리한다.`, () => {
   const TRY_COUNT = 0;
 
   expect(() => CountValidator.isAtLeastOne(TRY_COUNT)).toThrow(
@@ -26,8 +25,8 @@ test('시도횟수가 정수가 아니면 에러 처리한다.', () => {
   );
 });
 
-test('시도횟수가 100만번을 넘으면 에러 처리한다.', () => {
-  const TRY_COUNT = 2000000;
+test(`시도횟수가 ${MAXIMUM_COUNT}을 넘으면 에러 처리한다.`, () => {
+  const TRY_COUNT = 1_000_001;
 
   expect(() => CountValidator.isNotBigNumber(TRY_COUNT)).toThrow(
     ErrorMessage.isBigNumber,
