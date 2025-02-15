1cc2a1948f8d556549b3a42f7ccb6ba610aa84cf
장정안 <wjddks96@naver.com>
2025-02-15 22:05:06 +0900

    refactor: 랜덤 숫자를 뽑는 함수 테스트 시 변수에 담아서 여러번 호출 방지

--- __tests__/pickRandomNumber.test.js
+++ __tests__/pickRandomNumber.test.js
@@ -1,8 +1,8 @@
 import pickRandomNumber from '../src/utils/pickRandomNumber.js';
 
 test('0에서 9사이 숫자를 하나 반환한다.', () => {
-  pickRandomNumber();
+  const randomNumber = pickRandomNumber();
 
-  expect(pickRandomNumber()).toBeLessThanOrEqual(9);
-  expect(pickRandomNumber()).toBeGreaterThanOrEqual(0);
+  expect(randomNumber).toBeLessThanOrEqual(9);
+  expect(randomNumber).toBeGreaterThanOrEqual(0);
 });
