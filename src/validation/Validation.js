e6cc418cb12706990a6b86ccd6d5ec952b213da2
hanheel <hanhee630630@gmail.com>
2025-02-15 20:44:30 +0900

    refactor : every를 some으로 변경
    불필요한 연산 감소를 위해 every메서드를 some으로 변경

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -1,9 +1,9 @@
 export const Validation = {
   isNameNotEmpty(parsedString) {
-    return parsedString.every((name) => name.length > 0);
+    return !parsedString.some((name) => name.length === 0);
   },
   isNameTooLong(parsedString) {
-    return parsedString.every((name) => name.length <= 5);
+    return !parsedString.some((name) => name.length > 5);
   },
   isNameDuplicate(parsedString) {
     return parsedString.length === new Set(parsedString).size;
