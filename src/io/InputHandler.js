3458cfd29171c21dfde7df047e47f9872eef24e7
thgml05 <aisohii@naver.com>
2025-02-15 16:41:31 +0900

    refactor: inputHandler.js 입력 처리에 trim() 추가

--- src/io/InputHandler.js
+++ src/io/InputHandler.js
@@ -3,12 +3,14 @@ import { validateAttempt } from "../utils/validate.js";
 import readLineAsync from "./input.js";
 
 export const getCarNames = async () => {
-  return await readLineAsync(INFO_MESSAGE.CAR_NAME_INPUT);
+  return (await readLineAsync(INFO_MESSAGE.CAR_NAME_INPUT)).trim();
 };
 
 export const getAttempt = async () => {
   while (true) {
-    const attempt = Number(await readLineAsync(INFO_MESSAGE.ATTEMPT_INPUT));
+    const attempt = Number(
+      (await readLineAsync(INFO_MESSAGE.ATTEMPT_INPUT)).trim()
+    );
     try {
       validateAttempt(attempt);
       return attempt;
