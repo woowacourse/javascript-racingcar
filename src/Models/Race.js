661c26e577a07c58f54f7664bc57deaa6daca87f
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-02-16 00:45:16 +0900

    fix: tryCount가 1 ~ 20 사이인지 검증하는 위치 변경

--- src/Models/Race.js
+++ src/Models/Race.js
@@ -1,4 +1,4 @@
-import { isDuplicated, isLessThanMin } from '../utils/validations.js';
+import { isDuplicated, isInRange, isLessThanMin } from '../utils/validations.js';
 import Car from './Car.js';
 
 class Race {
@@ -7,6 +7,7 @@ class Race {
 
   constructor(carNames, tryCount) {
     this.#validateCarNames(carNames);
+    this.#validateTryCount(tryCount);
 
     this.cars = carNames.map((car) => new Car(car));
     this.tryCount = tryCount;
@@ -15,6 +16,10 @@ class Race {
   #validateCarNames(carNames) {
     if (isLessThanMin(carNames.length, 2)) throw new Error('[ERROR] 자동차는 2대 이상이여야 합니다.');
     if (isDuplicated(carNames)) throw new Error('[ERROR] 자동차 이름은 중복되면 안됩니다.');
+  }
+
+  #validateTryCount(tryCount) {
+    if (!isInRange(tryCount, 1, 20)) throw new Error('[ERROR] 시도 횟수는 1 ~ 20 사이여야 합니다.');
   }
 }
 
--- src/validates/ValidatorModule.js
+++ src/validates/ValidatorModule.js
@@ -10,7 +10,6 @@ class ValidateModule {
 
     if (!isNumber(tryCount)) throw new Error('[ERROR] 시도 횟수는 숫자여야 합니다.');
     if (!isInteger(tryCount)) throw new Error('[ERROR] 시도 횟수는 정수여야 합니다.');
-    if (!isInRange(tryCount, 1, 20)) throw new Error('[ERROR] 시도 횟수는 1 ~ 20 사이여야 합니다.');
   }
 }
 
