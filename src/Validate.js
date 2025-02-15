223d17683a7a0d5facf7514011653db012a6c43f
Ko Daeun <drechsu@gmail.com>
2025-02-15 15:34:25 +0900

    [REFACTOR] : throwIfInValid의 파라미터를 객체 형태로 변경

--- src/Validate.js
+++ src/Validate.js
@@ -1,7 +1,7 @@
 import { ERROR_MESSAGE, MAX_CAR_LENGTH } from "./Const.js";
 
 class Validate {
-  #throwIfInValid(condition, errorMessage) {
+  #throwIfInValid({ condition, errorMessage }) {
     if (condition) {
       throw new Error(errorMessage);
     }
@@ -9,29 +9,38 @@ class Validate {
   }
 
   isLimitLength(name) {
-    return this.#throwIfInValid(
-      name.length > MAX_CAR_LENGTH,
-      ERROR_MESSAGE.belowLimit
-    );
+    return this.#throwIfInValid({
+      condition: name.length > MAX_CAR_LENGTH,
+      errorMessage: ERROR_MESSAGE.belowLimit,
+    });
   }
 
   isPositiveLength(name) {
-    return this.#throwIfInValid(name.length <= 0, ERROR_MESSAGE.positiveLength);
+    return this.#throwIfInValid({
+      condition: name.length <= 0,
+      errorMessage: ERROR_MESSAGE.positiveLength,
+    });
   }
 
   isPositiveNumber(number) {
-    return this.#throwIfInValid(number <= 0, ERROR_MESSAGE.positiveNumber);
+    return this.#throwIfInValid({
+      condition: number <= 0,
+      errorMessage: ERROR_MESSAGE.positiveNumber,
+    });
   }
 
   isNumeric(number) {
-    return this.#throwIfInValid(Number.isNaN(number), ERROR_MESSAGE.numeric);
+    return this.#throwIfInValid({
+      condition: Number.isNaN(number),
+      errorMessage: ERROR_MESSAGE.numeric,
+    });
   }
 
   isInteger(number) {
-    return this.#throwIfInValid(
-      !Number.isInteger(number),
-      ERROR_MESSAGE.integer
-    );
+    return this.#throwIfInValid({
+      condition: !Number.isInteger(number),
+      errorMessage: ERROR_MESSAGE.integer,
+    });
   }
 }
 
