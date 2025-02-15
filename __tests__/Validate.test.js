7a7beaa489fa4a81fc401813162e682e8af89b4f
aydenote <dslgpgh@gmail.com>
2025-02-15 21:31:31 +0900

    refactor: 여러 자동차 이름 검증 테스트를 각각 테스트로 분리

--- __tests__/Validate.test.js
+++ __tests__/Validate.test.js
@@ -1,5 +1,10 @@
-import Car from '../src/Models/Car.js';
 import Race from '../src/Models/Race.js';
+import {
+  checkCarCount,
+  checkCarNameDuplicate,
+  checkCarNameLength,
+  checkIsEmpty,
+} from '../src/validates/carValidates.js';
 
 export const getLogSpy = () => {
   const logSpy = jest.spyOn(console, 'log');
@@ -8,16 +13,30 @@ export const getLogSpy = () => {
 };
 
 describe('자동차 이름 입력 검증 테스트', () => {
-  const FAIL_CASE = [[['재오상추앵버', '']], [['재오', '상추', '상추']], [['앵버']]];
-  const SUCCESS_CASE = [[['재오', '상추', '앵버']], [['재', '상', '앵']]];
-
-  test.each(SUCCESS_CASE)('자동차 이름 성공 테스트', (carNames) => {
-    expect(() => carNames.forEach((name) => new Car(name, carNames))).not.toThrow('[ERROR]');
+  test('자동차 이름 빈 값 오류 검증 테스트', () => {
+    //then
+    expect(() => checkIsEmpty('')).toThrow('[ERROR]');
   });
 
-  test.each(FAIL_CASE)('자동차 이름 실패 테스트', (carNames) => {
+  test.each([['앵버앵버앵버', '상']])('자동차 이름 길이(1~5자) 오류 검증 테스트', (carName) => {
+    //then
+    expect(() => checkCarNameLength(carName)).toThrow('[ERROR]');
+  });
+
+  test('자동차 이름 중복 오류 검증 테스트', () => {
+    // given
+    const carNames = ['상추', '상추'];
+
+    //then
+    expect(() => checkCarNameDuplicate(carNames)).toThrow('[ERROR]');
+  });
+
+  test('최소 자동차 대수(2대) 미만 입력 검증 테스트', () => {
+    // given
+    const carNames = ['앵버'];
+
     // then
-    expect(() => carNames.forEach((name) => new Car(name, carNames))).toThrow('[ERROR]');
+    expect(() => checkCarCount(carNames)).toThrow('[ERROR]');
   });
 });
 
