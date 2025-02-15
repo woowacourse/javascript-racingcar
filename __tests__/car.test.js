f1dcfc90ad34847ec4f5c33dac5b7efbab5352ea
hoyyChoi <hoyeon8165@gmail.com>
2025-02-15 15:41:48 +0900

    refactor: Car 도메인 단위테스트

--- __tests__/car.test.js
+++ __tests__/car.test.js
@@ -1,24 +1,34 @@
-import Car from "../src/models/Car.js";
+import Car from "../src/domains/Car";
 
 describe("Car 모델 클래스 테스트", () => {
-  test.each(["Niya", "Hoyychoi"])("Car 클래스 생성 시 name 값이 잘 반환되고 count가 0으로 초기화 되는지는지 확인한다.", (name) => {
+  test("자동차가 생성되는지 확인한다.", () => {
+    const car = new Car();
+    expect(car).toBeInstanceOf(Car);
+  });
+
+  test.each(["Niya", "Hoyychoi"])("자동차의 이름이 맞게 생성되는지 확인한다.", (name) => {
     // given
     // when
     const car = new Car(name);
     // then
-    expect(car.name).toEqual(name);
+    expect(car.name).toBe(name);
+  });
+
+  test("자동차가 생성되면 멈춰있다.", () => {
+    const car = new Car("호이초이");
+    // then
     expect(car.count).toBe(0);
   });
 
   test.each([
-    ["무작위 랜덤 값이 4 이상일 경우 count가 1 증가한다.", 9, 1],
-    ["무작위 랜덤 값이 4 미만일 경우 count가 그대로 유지된다.", 3, 0],
-  ])("%s", (_, randomNumber, expectedNumber) => {
+    ["무작위 랜덤 값이 4 이상일 경우 자동차는 움직인다.", 9, 1],
+    ["무작위 랜덤 값이 4 미만일 경우 자동차는 움직이지 않는다.", 3, 0],
+  ])("%s", (_, randomNumber, expectedCount) => {
     // given
     // when
     const car = new Car("Niya");
     car.move(randomNumber);
     // then
-    expect(car.count).toEqual(expectedNumber);
+    expect(car.count).toEqual(expectedCount);
   });
 });
