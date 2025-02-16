c96be5f2fd127e994c9004a08770ea41c6b872c9
Minje Sung <minje813@dgu.ac.kr>
2025-02-16 19:03:32 +0900

    test: 테스트 명세를 명확하게 변경

--- __tests__/InputValidator.test.js
+++ __tests__/InputValidator.test.js
@@ -10,7 +10,7 @@ describe('경주에 참여하는 자동차의 이름들을 검증한다.', () =>
     }).toThrow(ERROR_MESSAGE.DUPLICATE_NAME);
   });
 
-  test('자동차 이름이 5자 이상이면 안된다.', () => {
+  test('자동차 이름이 5자 이상이면 오류가 발생한다.', () => {
     const carNames = ['a', 'b', 'cdefgd'];
 
     expect(() => {
@@ -18,7 +18,7 @@ describe('경주에 참여하는 자동차의 이름들을 검증한다.', () =>
     }).toThrow(ERROR_MESSAGE.INVALID_NAME_LENGTH);
   });
 
-  test('자동차 이름 중 공백이 있으면 안된다.', () => {
+  test('자동차 이름 중 공백이 있으면 오류가 발생한다.', () => {
     const carNames = ['a', 'b b', 'c'];
 
     expect(() => {
@@ -26,7 +26,7 @@ describe('경주에 참여하는 자동차의 이름들을 검증한다.', () =>
     }).toThrow(ERROR_MESSAGE.INVALID_NAME_SPACE);
   });
 
-  test('자동차 이름은 알파벳으로 구성되어야 한다.', () => {
+  test('자동차 이름 중 알파벳이 아닌 문자가 있다면 오류가 발생한다.', () => {
     const carNames = ['a', 'b', 'c1'];
 
     expect(() => {
@@ -34,7 +34,7 @@ describe('경주에 참여하는 자동차의 이름들을 검증한다.', () =>
     }).toThrow(ERROR_MESSAGE.INVALID_NAME_CHARACTER);
   });
 
-  test('자동차는 두 대 이상이어야 한다.', () => {
+  test('출전하는 자동차가 두 대 이상이 아니면 오류가 발생한다.', () => {
     const carNames = ['a'];
 
     expect(() => {
@@ -42,7 +42,7 @@ describe('경주에 참여하는 자동차의 이름들을 검증한다.', () =>
     }).toThrow(ERROR_MESSAGE.INVALID_CAR_COUNT);
   });
 
-  test('자동차 입력을 정상적으로 입력한 경우 에러를 내지 않는다.', () => {
+  test('자동차 입력을 정상적으로 입력한 경우 오류가 발생하지 않는다.', () => {
     const carNames = ['asdf', 'qwer'];
 
     expect(() => {
@@ -52,35 +52,35 @@ describe('경주에 참여하는 자동차의 이름들을 검증한다.', () =>
 });
 
 describe('경주를 진행할 횟수를 검증한다.', () => {
-  test('횟수는 문자를 입력받으면 안된다.', () => {
+  test('횟수로 문자를 입력받으면 오류가 발생한다.', () => {
     const count = 'a';
     expect(() => {
       InputValidator.count(count);
     }).toThrow(ERROR_MESSAGE.INVALID_COUNT_CHARACTER);
   });
 
-  test('횟수는 공백을 입력받으면 안된다.', () => {
+  test('횟수로 공백을 입력받으면 오류가 발생한다.', () => {
     const count = '1 2';
     expect(() => {
       InputValidator.count(count);
     }).toThrow(ERROR_MESSAGE.INVALID_COUNT_SPACE);
   });
 
-  test('횟수가 1보다 작으면 안된다.', () => {
+  test('횟수가 1보다 작으면 오류가 발생한다.', () => {
     const count = '0';
     expect(() => {
       InputValidator.count(count);
     }).toThrow(ERROR_MESSAGE.INVALID_COUNT_MIN);
   });
 
-  test('횟수는 최대 1000이다.', () => {
+  test('횟수가 1000보다 크면 오류가 발생한다.', () => {
     const count = '1001';
     expect(() => {
       InputValidator.count(count);
     }).toThrow(ERROR_MESSAGE.INVALID_COUNT_MAX);
   });
 
-  test('정상 입력', () => {
+  test('횟수를 정상적으로 입력한 경우 오류가 발생하지 않는다.', () => {
     const count = '12';
     expect(() => {
       InputValidator.count(count);
--- __tests__/Racing.test.js
+++ __tests__/Racing.test.js
@@ -6,13 +6,13 @@ describe('자동차 경주 객체 테스트', () => {
   const carList = carNames.map((carName, i) => new Car(carName, i));
   const racing = new Racing(carList);
 
-  test('자동차 객체 정보를 담는다.', () => {
+  test('자동차 경주 객체는 자동차 객체 정보를 저장한다.', () => {
     expect(racing.carList.map((car) => car.getCarStatus().name)).toEqual(
       carNames,
     );
   });
 
-  test('자동차 객체 중 가장 멀리 간 자동차를 배열에 담아 반환한다..', () => {
+  test('자동차 객체 중 가장 멀리 간 자동차를 배열에 담아 반환한다.', () => {
     expect(racing.getWinner()).toEqual(['efg']);
   });
   test('우승자가 여러 명인 경우 여러 개의 자동차를 배열에 담아 반환한다.', () => {
