fcd7cec9a720b899c4a9007c2fd4001f5087596c
Minje Sung <minje813@dgu.ac.kr>
2025-02-16 17:28:25 +0900

    refactor: Car 내 필드 공개 범위 수정
    필드의 공개 범위를 수정하고 필드 정보를 반환하는 함수 구현
    이를 통해 외부 사용 및 테스트 코드 작성 가능

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -5,12 +5,20 @@ describe('자동차 객체 테스트', () => {
   const ENOUGH_VALUE = MOVING_THRESHOLD + 1;
   const NOT_ENOUGH_VALUE = MOVING_THRESHOLD - 1;
 
+  test('자동차는 자신의 이름과 위치를 표시할 수 있다.', () => {
+    const car = new Car('test', 3);
+    const { name, position } = car.getCarStatus();
+
+    expect(name).toBe('test');
+    expect(position).toBe(3);
+  });
+
   test(`자동차는 주어지는 값이 ${MOVING_THRESHOLD} 이상인 경우 움직인다.`, () => {
     const car = new Car('car', 0);
 
     car.move(ENOUGH_VALUE);
 
-    expect(car.position).toBe(1);
+    expect(car.getCarStatus().position).toBe(1);
   });
 
   test(`자동차는 주어지는 값이 ${MOVING_THRESHOLD} 미만인 경우 움직이지 않는다.`, () => {
@@ -18,16 +26,6 @@ describe('자동차 객체 테스트', () => {
 
     car.move(NOT_ENOUGH_VALUE);
 
-    expect(car.position).toBe(0);
-  });
-
-  test('자동차는 현재 이동 거리를 문자열로 반환할 수 있다.', () => {
-    const car = new Car('car', 0);
-
-    car.move(ENOUGH_VALUE);
-    car.move(ENOUGH_VALUE);
-    car.move(NOT_ENOUGH_VALUE);
-
-    expect(car.getRacingStatus()).toBe('car : --');
+    expect(car.getCarStatus().position).toBe(0);
   });
 });
--- src/Car.js
+++ src/Car.js
@@ -2,23 +2,22 @@ import {
   MOVING_DISTANCE,
   MOVING_THRESHOLD,
 } from './constants/configurations.js';
-import { FORWARD_DASH } from './constants/systemMessages.js';
 
 class Car {
-  name;
-  position;
+  #name;
+  #position;
 
   constructor(name, position) {
-    this.name = name;
-    this.position = position;
+    this.#name = name;
+    this.#position = position;
+  }
+
+  getCarStatus() {
+    return { name: this.#name, position: this.#position };
   }
 
   move(value) {
-    if (value >= MOVING_THRESHOLD) this.position += MOVING_DISTANCE;
-  }
-
-  getRacingStatus() {
-    return `${this.name} : ${FORWARD_DASH.repeat(this.position)}`;
+    if (value >= MOVING_THRESHOLD) this.#position += MOVING_DISTANCE;
   }
 }
 
