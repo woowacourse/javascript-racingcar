d25a46671c185c4be1e4acf758be4757f273b161
mlnwns <mlnwns@naver.com>
2025-02-16 20:11:22 +0900

    fix: ATTEMPT_COUNT 메시지에서 시도 횟수 표현 개선

--- src/constants/messages/invalid.js
+++ src/constants/messages/invalid.js
@@ -6,5 +6,5 @@ export const INVALID_MESSAGE = Object.freeze({
   CAR_COUNT: `자동차는 ${CAR_RULES.MIN_COUNT}대 이상 ${CAR_RULES.MAX_COUNT}대 이하만 가능합니다.`,
   DUPLICATE_CAR_NAME: "자동차 이름은 중복되지 않아야 합니다.",
   INTEGER_FORMAT: "정수만 입력 가능합니다.",
-  ATTEMPT_COUNT: `시도 횟수는 ${GAME_RULES.MIN_ATTEMPT_COUNT} 이상 ${GAME_RULES.MAX_ATTEMPT_COUNT}이어야 합니다.`,
+  ATTEMPT_COUNT: `시도 횟수는 ${GAME_RULES.MIN_ATTEMPT_COUNT}회 이상 ${GAME_RULES.MAX_ATTEMPT_COUNT}회 이하만 가능합니다.`,
 });
