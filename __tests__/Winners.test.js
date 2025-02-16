d71b89fccca16ac84e9551d9f0dfa9f3d076086f
kimyou1102 <yoosk5485@naver.com>
2025-02-16 15:31:19 +0900

    test: private 필드로 변경한 것에 맞춰 테스트 수정

--- __tests__/Winners.test.js
+++ __tests__/Winners.test.js
@@ -1,11 +1,9 @@
 import Winners from "../src/domain/Winners.js";
+import Car from "../src/domain/Car.js";
 
 describe("최종 우승자 선별 테스트", () => {
   test("최종 우승자를 뽑는다.(우승자가 1명인 경우)", () => {
-    const CARS = [
-      { name: "데이지", position: 3 },
-      { name: "머핀", position: 1 },
-    ];
+    const CARS = [new Car("데이지", 3), new Car("머핀", 1)];
 
     const winners = new Winners(CARS);
 
@@ -13,14 +11,9 @@ describe("최종 우승자 선별 테스트", () => {
   });
 
   test("최종 우승자를 뽑는다.(우승자가 2명 이상인 경우", () => {
-    const CARS = [
-      { name: "데이지", position: 5 },
-      { name: "머핀", position: 5 },
-      { name: "메리", position: 2 },
-    ];
+    const CARS = [new Car("데이지", 5), new Car("머핀", 5), new Car("메리", 1)];
 
     const winners = new Winners(CARS);
-
     expect(winners.getWinners()).toEqual(["데이지", "머핀"]);
   });
 });
