5be92492e9fce10aaf4e51edd56dd56f3da5bbde
kimyou1102 <yoosk5485@naver.com>
2025-02-16 15:49:03 +0900

    refactor: 자동차 객체 생성시 오류 발생되면 재입력 받도록 로직 수정

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -7,26 +7,42 @@ import Winners from "../domain/Winners.js";
 
 export default class Controller {
   async run() {
-    const carNameInput = await this.validateAndRetry(
-      INPUT_MESSAGE.CAR_NAMES,
-      validateCarNames
-    );
-    const carNames = carNameInput.split(",").map((carName) => carName.trim());
+    const cars = await this.getCars();
+
     const tryCount = await this.validateAndRetry(
       INPUT_MESSAGE.TRY_COUNT,
       validateTryCount
     );
 
-    const cars = [];
-    carNames.forEach((carName) => {
-      cars.push(new Car(carName));
-    });
     const race = new Race(cars, tryCount);
     race.runRace();
 
     const winners = new Winners(cars);
     const winnerNames = winners.getWinners();
     console.log(`최종 우승자: ${winnerNames.join(", ")}`);
+  }
+
+  async getCars() {
+    const carNameInput = await this.validateAndRetry(
+      INPUT_MESSAGE.CAR_NAMES,
+      validateCarNames
+    );
+    const carNames = carNameInput.split(",").map((carName) => carName.trim());
+    const cars = this.createCars(carNames);
+    return cars;
+  }
+
+  async createCars(carNames) {
+    try {
+      const cars = [];
+      carNames.forEach((carName) => {
+        cars.push(new Car(carName));
+      });
+      return cars;
+    } catch (e) {
+      console.log(e.message);
+      return await this.getCars();
+    }
   }
 
   async validateAndRetry(message, validateFn) {
