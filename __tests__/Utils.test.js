b8e083de7ee94a18d7d9838557e1424004a95f0d
장정안 <wjddks96@naver.com>
2025-02-16 20:20:54 +0900

    style: util 테스트 코드 내import 문 수정

--- __tests__/Utils.test.js
+++ __tests__/Utils.test.js
@@ -1,4 +1,4 @@
-import splitStringToArray from '../src/utils/utils.js';
+import { splitStringToArray } from '../src/utils/utils.js';
 
 test('콤마를 기준으로 자동차 이름을 분리한다', () => {
   const cars = splitStringToArray('except,hailey,jun', ',');
