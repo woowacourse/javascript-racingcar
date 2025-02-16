b4b1a304ac91deeb5ee8cd8121f4e9f2ba802b55
aydenote <dslgpgh@gmail.com>
2025-02-16 22:12:48 +0900

    test: 이동 가능 여부 판단에 대한 유닛 테스트 추가

--- __tests__/Winner.test.js
+++ __tests__/Winner.test.js
@@ -3,12 +3,28 @@ import Race from '../src/Models/Race.js';
 import { mockRandom } from './Car.test.js';
 
 describe('우승자 선별 테스트', () => {
-  test('우승자가 잘 선별되는지 테스트한다.', () => {
+  let carNames;
+  let tryCount;
+  let cars;
+  let race;
+
+  beforeEach(() => {
+    carNames = ['재오', '앵버', '상추'];
+    tryCount = 2;
+    cars = carNames.map((name) => new Car(name, carNames));
+    race = new Race(cars, tryCount);
+  });
+
+  test('이동 가능 여부 판단 테스트', () => {
     // given
-    const carNames = ['재오', '앵버', '상추'];
-    const tryCount = 2;
-    const cars = carNames.map((name) => new Car(name, carNames));
-    const race = new Race(cars, tryCount);
+    mockRandom([3]);
+
+    // then
+    expect(race.canMove()).toBe(false);
+  });
+
+  test('우승자가 잘 선별되는지 테스트', () => {
+    // given;
     mockRandom([3, 4, 3, 3, 4, 3]);
 
     // when
