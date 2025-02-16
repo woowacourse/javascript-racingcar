1c072883a6b2da1fdb97b9c583a6e2fd75efe638
minji2219 <aabb0219@naver.com>
2025-02-16 15:03:46 +0900

    refactor: inputview 클래스 내 함수 우선순위 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,28 +3,6 @@ import { INPUT_MESSAGE } from "../constants/constants.js";
 import { validateCarNames, validateTryCount } from "../utils/validation.js";
 
 export default class InputView {
-  readLineAsync(query) {
-    return new Promise((resolve, reject) => {
-      if (arguments.length !== 1) {
-        reject(new Error("arguments must be 1"));
-      }
-
-      if (typeof query !== "string") {
-        reject(new Error("query must be string"));
-      }
-
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
-
-      rl.question(query, (input) => {
-        rl.close();
-        resolve(input);
-      });
-    });
-  }
-
   async readCarNames() {
     return await this.validateAndRetry(
       INPUT_MESSAGE.CAR_NAMES,
@@ -48,5 +26,27 @@ export default class InputView {
       console.log(e.message);
       return this.validateAndRetry(message, validateFn);
     }
+  }
+
+  readLineAsync(query) {
+    return new Promise((resolve, reject) => {
+      if (arguments.length !== 1) {
+        reject(new Error("arguments must be 1"));
+      }
+
+      if (typeof query !== "string") {
+        reject(new Error("query must be string"));
+      }
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question(query, (input) => {
+        rl.close();
+        resolve(input);
+      });
+    });
   }
 }
