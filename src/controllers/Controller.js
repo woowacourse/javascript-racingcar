9030c42cc11ef9c8d68fe02b3bc3b775770f89e8
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-02-16 13:06:08 +0900

    ♻️ refactor: start() 메서드 분리

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -3,30 +3,32 @@ import OutputView from '../views/OutputView.js';
 import InputView from '../views/InputView.js';
 
 export default class Controller {
-  async start() {
-    let inputName, inputTryNumber;
+  async getValidateName() {
     while (true) {
       try {
-        inputName = await InputView.inputName();
+        const inputName = await InputView.inputName();
         new Game(inputName, 1); // 검증을 위한 임시 값 1 입니다.
+        return inputName;
         break;
       } catch (error) {
         OutputView.printInput(error.message);
       }
     }
+  }
+  async getValidateTryNumber(inputName) {
     while (true) {
       try {
-        inputTryNumber = await InputView.inputTryNumber();
+        const inputTryNumber = await InputView.inputTryNumber();
         new Game(inputName, inputTryNumber); // tryNumber 검증
+        return inputTryNumber;
         break;
       } catch (error) {
         OutputView.printInput(error.message);
       }
     }
+  }
 
-    const game = new Game(inputName, inputTryNumber);
-    const { winners, roundResults } = game.race();
-
+  print(winners, roundResults) {
     // 결과 출력
     OutputView.printInput(`\n실행결과`);
     roundResults.forEach(round => {
@@ -37,7 +39,14 @@ export default class Controller {
     });
     OutputView.gameResult(winners);
   }
-  catch(error) {
-    OutputView.printInput(error.message);
+
+  async start() {
+    const inputName = await this.getValidateName();
+    const inputTryNumber = await this.getValidateTryNumber(inputName);
+
+    const game = new Game(inputName, inputTryNumber);
+    const { winners, roundResults } = game.race();
+
+    this.print(winners, roundResults);
   }
 }
