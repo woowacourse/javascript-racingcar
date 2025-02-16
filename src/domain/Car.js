d02105df3a4a9abda3a40469feca49e881d5bb7d
minji2219 <aabb0219@naver.com>
2025-02-16 16:31:24 +0900

    refacotr: Car 인스턴스 생성시 유효성 검사 추가

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,9 +1,18 @@
+import { CAR } from "../constants/domain.js";
+import { getRandomNumber } from "../utils/getRandomNumber.js";
+import { validateCarNames } from "../utils/validation.js";
+
 export default class Car {
   #name;
   #position = 0;
 
   constructor(name) {
+    this.validate(name);
     this.#name = name;
+  }
+
+  validate(name) {
+    validateCarNames(name);
   }
 
   move() {
