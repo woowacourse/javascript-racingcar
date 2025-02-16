ed997ad21b32e09254287f26d038c6d7e5fc5074
sooyeoniya <tndus1306403@naver.com>
2025-02-16 20:15:37 +0900

    test: 행위 주도 개발 방식으로 테스트 명세 수정
    - 우리가 개발하고자 하는 것은 어떻게(how)보다 무엇(what)이 더 중요함.
    - count라는 변수명이 바뀔 수도 있으므로 이런 방식보다는 자동차의 움직임, 즉 행위를 위주로 서술하는 것이 좋음.

--- __tests__/car.test.js
+++ __tests__/car.test.js
@@ -2,8 +2,8 @@ import Car from "../src/domains/Car.js";
 
 describe("Car 모델 클래스 테스트", () => {
   test.each([
-    ["무작위 랜덤 값이 4 이상일 경우 count가 1 증가한다.", 4, 1],
-    ["무작위 랜덤 값이 4 미만일 경우 count가 그대로 유지된다.", 3, 0],
+    ["무작위 랜덤 값이 4 이상일 경우 자동차가 전진한다.", 4, 1],
+    ["무작위 랜덤 값이 4 미만일 경우 자동차가 멈춰있다.", 3, 0],
   ])("%s", (_, randomNumber, expectedNumber) => {
     // given
     // when
