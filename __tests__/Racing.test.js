878641100b325be4529ff7e51e375ed036a173ae
deokbin <deokbin@naver.com>
2025-02-16 16:39:42 +0900

    refactor(Racing): 함수 mock 코드 삭제

--- __tests__/Racing.test.js
+++ __tests__/Racing.test.js
@@ -1,50 +1,14 @@
 import Car from '../src/domain/Car.js';
 import Racing from '../src/domain/Racing.js';
-import getRandomNumber from '../src/utils/getRandomNumber.js';
-
-jest.mock('../src/utils/getRandomNumber');
-
-describe('랜덤 숫자에 따른 자동차 경주 테스트', () => {
-  test.each([
-    [4, [1, 1]],
-    [0, [0, 0]],
-  ])(
-    '랜덤 숫자가 %s 이면 자동차의 위치는 %s가 된다.',
-    (randomNumber, positions) => {
-      getRandomNumber.mockReturnValue(randomNumber);
-
-      getRandomNumber();
-      getRandomNumber();
-
-      const NAMES = ['jenna', 'mato'];
-      const cars = NAMES.map((name) => new Car(name));
-      const racing = new Racing(cars);
-      racing.raceTurn();
-
-      const positionResult = cars.map((car) => car.getCarPosition());
-      expect(positionResult).toEqual(positions);
-    },
-  );
-
-  test('시도 횟수만큼 raceTurn을 호출하는지 runRace 메서드를 테스트한다.', () => {
-    const NAMES = ['jenna', 'mato'];
-    const TRY_COUNT = 5;
-    const cars = NAMES.map((name) => new Car(name));
-
-    const racing = new Racing(cars);
-    const raceTurnSpy = jest.spyOn(racing, 'raceTurn');
-    racing.runRace(TRY_COUNT);
-
-    expect(raceTurnSpy).toHaveBeenCalledTimes(TRY_COUNT);
-  });
-});
 
 describe('우승자 판단 메서드 테스트', () => {
+  //todo: beforeEach 쓰기
   test('우승자가 한 명일 수 있다.', () => {
     const NAMES = ['jenna', 'mato'];
     const cars = NAMES.map((name) => new Car(name));
 
     const racing = new Racing(cars);
+    //todo: 테스트 할때마다 이렇게 만들어줘야 하나 (beforeEach 쓰면 안되나)
     racing.getCarInfo = jest.fn().mockReturnValue([
       { name: 'jenna', position: 3 },
       { name: 'mato', position: 2 },
