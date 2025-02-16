7b1ab86e62d3cfc59552683154cf35b44bb18e8b
kimyou1102 <yoosk5485@naver.com>
2025-02-16 11:17:00 +0900

    test: Race 클래스 테스트 작성

---
+++ __tests__/Race.test.js
@@ -0,0 +1,40 @@
+import Race from "../src/model/Race.js";
+import Car from "../src/model/Car.js";
+import { getRandomNumber } from "../src/utils/getRandomNumber.js";
+
+jest.mock("../src/utils/getRandomNumber", () => ({
+  getRandomNumber: jest.fn(),
+}));
+
+describe("자동차 경주 한 라운드 테스트", () => {
+  test("자동차 한 라운드 진행 후 결과 출력한다.(한대일 경우)", () => {
+    getRandomNumber.mockReturnValue(5);
+    const logSpy = jest.spyOn(console, "log");
+
+    const CARS = [new Car("머핀")];
+    const TRY_COUNT = 1;
+
+    const race = new Race(CARS, TRY_COUNT);
+
+    race.gameRound();
+
+    expect(logSpy).toHaveBeenCalledWith("머핀 : -");
+  });
+
+  test("자동차 한 라운드 진행 후 결과 출력한다.(여러대일 경우)", () => {
+    getRandomNumber.mockReturnValueOnce(5).mockReturnValue(3);
+    const logSpy = jest.spyOn(console, "log");
+    const logs = ["머핀 : -", "데이지 : "];
+
+    const CARS = [new Car("머핀"), new Car("데이지")];
+    const TRY_COUNT = 1;
+
+    const race = new Race(CARS, TRY_COUNT);
+
+    race.gameRound();
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(log);
+    });
+  });
+});
