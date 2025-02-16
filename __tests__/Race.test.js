bf1fe946522f76605e5dcad384acec5bc842d466
kimyou1102 <yoosk5485@naver.com>
2025-02-16 20:31:00 +0900

    test: 외부에서 주입하도록 변경한 메서드를 테스트하는 방식 변경
    - 외부에서 주입하도록 변경하여 getRandomNumber 유틸함수 모킹 제거
    - 출력 검증이 아닌 변경된 자동차객체값을 검증하도록 변경

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -1,40 +1,61 @@
 import Race from "../src/domain/Race.js";
 import Car from "../src/domain/Car.js";
-import { getRandomNumber } from "../src/utils/getRandomNumber.js";
-
-jest.mock("../src/utils/getRandomNumber", () => ({
-  getRandomNumber: jest.fn(),
-}));
 
 describe("자동차 경주 한 라운드 테스트", () => {
-  test("자동차 한 라운드 진행 후 결과 출력한다.(한대일 경우)", () => {
-    getRandomNumber.mockReturnValue(5);
-    const logSpy = jest.spyOn(console, "log");
-
-    const CARS = [new Car("머핀")];
-    const TRY_COUNT = 1;
-
-    const race = new Race(CARS, TRY_COUNT);
-
-    race.gameRound();
-
-    expect(logSpy).toHaveBeenCalledWith("머핀 : -");
-  });
-
-  test("자동차 한 라운드 진행 후 결과 출력한다.(여러대일 경우)", () => {
-    getRandomNumber.mockReturnValueOnce(5).mockReturnValue(3);
-    const logSpy = jest.spyOn(console, "log");
-    const logs = ["머핀 : -", "데이지 : "];
-
+  test("자동차 한 라운드를 진행하면 자동차의 위치가 변경된다.", () => {
+    const RANDOM_NUMBERS = [5, 3];
     const CARS = [new Car("머핀"), new Car("데이지")];
     const TRY_COUNT = 1;
 
     const race = new Race(CARS, TRY_COUNT);
+    const result = race.getGameRoundResult(RANDOM_NUMBERS);
 
-    race.gameRound();
+    expect(result).toEqual([
+      { name: "머핀", position: 1 },
+      { name: "데이지", position: 0 },
+    ]);
+  });
+  test("자동차 경주를 시도횟수 만큼 진행하면 자동차의 위치가 변경된다.(시도횟수 1회)", () => {
+    const RANDOM_NUMBERS = [[5, 3]];
+    const CARS = [new Car("머핀"), new Car("데이지")];
+    const TRY_COUNT = 1;
 
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(log);
-    });
+    const race = new Race(CARS, TRY_COUNT);
+    const result = race.getRaceResult(RANDOM_NUMBERS);
+
+    expect(result).toEqual([
+      [
+        { name: "머핀", position: 1 },
+        { name: "데이지", position: 0 },
+      ],
+    ]);
+  });
+
+  test("자동차 경주를 시도횟수 만큼 진행하면 자동차의 위치가 변경된다.(시도횟수 3회)", () => {
+    const RANDOM_NUMBERS = [
+      [5, 3],
+      [7, 8],
+      [9, 0],
+    ];
+    const CARS = [new Car("머핀"), new Car("데이지")];
+    const TRY_COUNT = 3;
+
+    const race = new Race(CARS, TRY_COUNT);
+    const result = race.getRaceResult(RANDOM_NUMBERS);
+
+    expect(result).toEqual([
+      [
+        { name: "머핀", position: 1 },
+        { name: "데이지", position: 0 },
+      ],
+      [
+        { name: "머핀", position: 2 },
+        { name: "데이지", position: 1 },
+      ],
+      [
+        { name: "머핀", position: 3 },
+        { name: "데이지", position: 1 },
+      ],
+    ]);
   });
 });
