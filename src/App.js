cd01b88b9072bd7b0e6cc28deede0ab4da6602e2
Oliy <eunsoa0310@gmail.com>
2025-02-17 00:44:35 +0900

    refactor: RacingcarManager.js 로직을 분리
    - 기존의 RacingcarManager.js의 ui로직을 app.js로 이동
    - 기존의 RacingcarManager.js의 로직을 domain 기능만 하도록 축소

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
+import InputView from './ui/InputView.js';
+import OutputView from './ui/OutputView.js';
+import Validate from './utils/Validate.js';
 import RacingcarManager from './domain/RacingcarManager.js'
 
 class App {
-  async run() {
-    const racingcarManager = new RacingcarManager();
-    await racingcarManager.start();
+  #inputView
+  #outputView
+  #validate
+  #manager;
+
+  constructor() {
+    this.#inputView = new InputView();
+    this.#outputView = new OutputView();
+    this.#validate = new Validate();
+    this.#manager = new RacingcarManager();
   }
+
+  async run() {
+    const carNames = await this.#getCarNames();
+    const attempts = await this.#getAttempts();
+
+    const cars = this.#manager.createCars(carNames);
+    this.#outputView.printResultMessage();
+
+    for(let i = 0; i < attempts; i++) {
+      this.#manager.oneRound(cars);
+      this.#outputView.printOneRoundResult(cars);
+    }
+
+    const winners = this.#manager.getWinners(cars);
+    this.#outputView.printWinners(winners);
+  }
+
+  async #getCarNames() {
+    while (true) {
+      try {
+        const carNamesInput = await this.#inputView.readCarNames();
+        this.#validateCarNames(carNamesInput);
+        return carNamesInput.split(',');
+      } catch (error) {
+        this.#outputView.printErrorMessage(error.message);
+      }
+    }
+  }
+  
+  #validateCarNames(carNamesInput) {
+    this.#validate.isEmpty(carNamesInput);
+    this.#validate.isValidCarNames(carNamesInput);
+  }
+
+  async #getAttempts()  {
+    while(true) {
+        try {
+            const attempts = await this.#inputView.readAttempts();
+            this.#validateAttempts(attempts);
+            return Number(attempts);
+        } catch (error) {
+            this.#outputView.printErrorMessage(error.message);
+        }
+    }
+  }
+
+  #validateAttempts(attempts) {
+    try {
+        this.#validate.isEmpty(attempts);
+        const numAttempts = Number(attempts);
+        this.#validate.isNumber(numAttempts);
+        this.#validate.isPositiveNumber(numAttempts);
+        this.#validate.isInteger(numAttempts);
+    } catch (error) {
+        throw error;
+    }
+  }
+
 }
 
 export default App;
--- src/domain/RacingcarManager.js
+++ src/domain/RacingcarManager.js
@@ -1,68 +1,9 @@
-import InputView from '../ui/InputView.js';
-import OutputView from '../ui/OutputView.js';
 import Car from './Car.js';
-import Validate from '../utils/Validate.js'
 import getRandomNumber from '../utils/Random.js';
 
 class RacingcarManager{
-    #inputView
-    #outputView
-    #validate
-
-    constructor(){
-        this.#inputView = new InputView();
-        this.#outputView = new OutputView();
-        this.#validate = new Validate();
-    }
-
-    async start(){
-        const carNames = await this.#getCarNames();
-        const cars = this.createCars(carNames);
-        const attempts = await this.#getAttempts();
-        this.roundOfRacing(cars, attempts);
-        const winners = this.getWinners(cars);
-        this.#outputView.printWinners(winners);
-    }
-    
-    async #getCarNames()  {
-        while(true) {
-            try {
-                const carNames = await this.#inputView.readCarNames();
-                this.#validate.isEmpty(carNames);
-                this.#validate.isValidCarNames(carNames);
-                return carNames.split(',');
-            } catch (error) {
-                this.#outputView.printErrorMessage(error.message);
-            }
-        }
-    }
-
     createCars(carNames){
         return carNames.map(name => new Car(name));
-    }
-
-    async #getAttempts()  {
-        while(true) {
-            try {
-                const attempts = await this.#inputView.readAttempts();
-                this.#validateAttempts(attempts);
-                return Number(attempts);
-            } catch (error) {
-                this.#outputView.printErrorMessage(error.message);
-            }
-        }
-    }
-
-    #validateAttempts(attempts) {
-        try {
-            this.#validate.isEmpty(attempts);
-            const numAttempts = Number(attempts);
-            this.#validate.isNumber(numAttempts);
-            this.#validate.isPositiveNumber(numAttempts);
-            this.#validate.isInteger(numAttempts);
-        } catch (error) {
-            throw error;
-        }
     }
     
     oneRound(cars){
@@ -71,23 +12,8 @@ class RacingcarManager{
         });
     }
 
-    roundOfRacing(cars, attempts){
-        this.#outputView.printResultMessage();
-        for(let i = 0; i< attempts; i++) {
-            this.oneRound(cars);
-            this.#outputView.printOneRoundResult(cars);
-        }
-    }
-
     getMaxPosition(cars) {
-        const hasMaxPositionCar = cars.reduce((prev, value) => {
-            if(prev.position >= value.position){
-                return prev;
-            } else {
-                return value;
-            }  
-        });
-        return hasMaxPositionCar.position;
+        return Math.max(...cars.map((car) => car.position));
     }
 
     getWinners(cars) {
