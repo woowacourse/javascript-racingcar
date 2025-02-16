40cb9ac488b383b84938b373e0af945bb54be52b
suhwa <tnghk9611@naver.com>
2025-02-16 23:04:06 +0900

    refactor: 중첩 객체의 불변성을 유지하기 위한 deepFreeze 구현 및 적용

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,9 +1,11 @@
+import { deepFreeze } from '../utils/deepFreeze.js';
+
 export const PROMPT = Object.freeze({
   GET_CAR_NAMES: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
   GET_TRY_COUNT: '시도할 횟수는 몇 회인가요?\n',
 });
 
-export const ERROR = Object.freeze({
+export const ERROR = deepFreeze({
   CAR_NAME: {
     INVALID_LENGTH: '자동차 이름은 1자 이상 5자 이하만 가능합니다.',
     DUPLICATE: '자동차 이름은 중복될 수 없습니다.',
