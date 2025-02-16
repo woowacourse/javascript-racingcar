49d7e9f80e8ffbc823658a89f0530ccabeb21b7f
Minje Sung <minje813@dgu.ac.kr>
2025-02-16 18:11:01 +0900

    feat: Domain과 UI 통합 로직 App에 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { GAME_MESSAGE } from './constants/systemMessages.js';
 import Racing from './domain/Racing.js';
 import loopWhileValid from './utils/loopWhileValid.js';
 import InputView from './view/InputView.js';
+import Printer from './view/printMessage.js';
 
 class App {
   async run() {
@@ -8,7 +10,13 @@ class App {
     const count = await loopWhileValid(InputView.enterCount);
     const racing = new Racing(carList, count);
 
-    racing.start();
+    Printer.printMessage(GAME_MESSAGE.RACING_RESULT);
+    for (let i = 0; i < count; i++) {
+      racing.raceOnce();
+      Printer.printRaceStatus(racing);
+      Printer.printBlank();
+    }
+    Printer.printRaceWinner(racing);
   }
 }
 
