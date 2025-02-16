6f1a7eec48521fd7ff3d820da50755787e030855
Oliy <eunsoa0310@gmail.com>
2025-02-17 00:50:12 +0900

    refactor: Car 클래스 필드 선언 방식 변경

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,8 +1,6 @@
 import RacingcarConstants from "../constants/RacingCarConstants.js";
 
 class Car {
-    name
-    position
     constructor(name) {
         this.name = name;
         this.position = 0;
