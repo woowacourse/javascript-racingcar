80523f2e694e9cf989cea4234d23100ea599b2a6
deokbin <deokbin@naver.com>
2025-02-16 17:10:30 +0900

    refactor(Racing): 모킹 제거 후 테스트 코드 재구성

--- __tests__/Racing.test.js
+++ __tests__/Racing.test.js
@@ -2,19 +2,22 @@ import Car from '../src/domain/Car.js';
 import Racing from '../src/domain/Racing.js';
 
 describe('우승자 판단 메서드 테스트', () => {
-  //todo: beforeEach 쓰기
-  test('우승자가 한 명일 수 있다.', () => {
-    const NAMES = ['jenna', 'mato'];
-    const cars = NAMES.map((name) => new Car(name));
+  let racing;
+  let cars;
 
-    const racing = new Racing(cars);
-    //todo: 테스트 할때마다 이렇게 만들어줘야 하나 (beforeEach 쓰면 안되나)
-    racing.getCarInfo = jest.fn().mockReturnValue([
+  beforeEach(() => {
+    const NAMES = ['jenna', 'mato'];
+    cars = NAMES.map((name) => new Car(name));
+    racing = new Racing(cars);
+  });
+
+  test('우승자가 한 명일 수 있다.', () => {
+    const results = [
       { name: 'jenna', position: 3 },
       { name: 'mato', position: 2 },
-    ]);
+    ];
 
-    const winners = racing.decideWinner();
+    const winners = racing.decideWinner(results);
 
     const expectedWinner = [{ name: 'jenna', position: 3 }];
 
@@ -22,15 +25,12 @@ describe('우승자 판단 메서드 테스트', () => {
   });
 
   test('우승자가 두 명 이상일 수 있다.', () => {
-    const NAMES = ['jenna', 'mato'];
-    const cars = NAMES.map((name) => new Car(name));
-
-    const racing = new Racing(cars);
-    racing.getCarInfo = jest.fn().mockReturnValue([
+    const results = [
       { name: 'jenna', position: 2 },
       { name: 'mato', position: 2 },
-    ]);
-    const winners = racing.decideWinner();
+    ];
+
+    const winners = racing.decideWinner(results);
 
     const expectedWinner = [
       { name: 'jenna', position: 2 },
