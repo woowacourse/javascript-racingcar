00d5466e083864980b90932bd92462260c5e5ae1
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-16 17:35:00 +0900

    refactor: 우승자 로직 분리에 따른 파일 이름 변경
    - 기존 Controller test에 우승자 관련 테스트만 있었기에 테스트 이름을 변경한다.

--- __tests__/winner.test.js
+++ __tests__/winner.test.js
@@ -1,24 +1,20 @@
-
-import CarRacingController from "../src/controller/CarRacingController.js";
 import Car from "../src/model/Car.js";
+import { getWinners, getWinnerPosition } from "../src/util/winner.js";
 
-describe("controller", () => {
-  let controller;
+describe("우승자 선별 관련 테스트", () => {
   let car1;
   let car2;
 
   beforeEach(() => {
     car1 = new Car("seo");
     car2 = new Car("ohgus");
-    controller = new CarRacingController();
-
   });
 
   test("우승자 리스트를 가져올 수 있다.", () => {
     car1.move(1);
     car2.move(4);
 
-    const winners = controller.getWinners([car1, car2]);
+    const winners = getWinners([car1, car2]);
 
     expect(winners.length).toBe(1);
   });
@@ -27,7 +23,7 @@ describe("controller", () => {
     car1.move(4);
     car2.move(4);
 
-    const winners = controller.getWinners([car1, car2]);
+    const winners = getWinners([car1, car2]);
 
     expect(winners.length).toBe(2);
   });
@@ -36,7 +32,7 @@ describe("controller", () => {
     car1.move(4);
     car1.move(4);
 
-    const winnerPosition = controller.getWinnerPosition([car1, car2]);
+    const winnerPosition = getWinnerPosition([car1, car2]);
 
     expect(winnerPosition).toBe(2);
   });
