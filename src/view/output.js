f8aff81080fef381a7c5778f547128adc22e7791
mlnwns <mlnwns@naver.com>
2025-02-16 18:52:16 +0900

    fix: 우승자 출력 구분자 사이에 공백 추가

--- src/view/output.js
+++ src/view/output.js
@@ -2,7 +2,7 @@ import { VIEW_MESSAGE, RULE } from "../constants/index.js";
 
 export const printWinner = (winners) => {
   console.log(
-    VIEW_MESSAGE.WINNER_RESULT + winners.join(`${RULE.CAR_NAME_SEPARATOR}`)
+    VIEW_MESSAGE.WINNER_RESULT + winners.join(`${RULE.CAR_NAME_SEPARATOR} `)
   );
 };
 
