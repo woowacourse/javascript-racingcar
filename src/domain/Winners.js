f7b581a05dc3987d6628af8a889793bae4d8cdf9
kimyou1102 <yoosk5485@naver.com>
2025-02-16 16:56:59 +0900

    refactor: Winners클래스 속성 private 필드로 변경

--- src/domain/Winners.js
+++ src/domain/Winners.js
@@ -1,11 +1,13 @@
 export default class Winners {
+  #cars;
+
   constructor(cars) {
-    this.cars = cars;
+    this.#cars = cars;
   }
 
   getWinners() {
-    const max = Math.max(...this.cars.map((car) => car.getPosition()));
-    const winners = this.cars.filter((car) => car.getPosition() === max);
+    const max = Math.max(...this.#cars.map((car) => car.getPosition()));
+    const winners = this.#cars.filter((car) => car.getPosition() === max);
 
     return winners.map((winner) => winner.getName());
   }
