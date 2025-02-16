ae721c63da23ebf14f5bc35d9ee5ca2ce75d566f
sooyeoniya <tndus1306403@naver.com>
2025-02-16 20:05:08 +0900

    chore: 시도 횟수 테스트 케이스 명세 통일
    - 미만, 초과 / 이상, 이하 로 묶어서 통일시키기

--- __tests__/validateTryCount.test.js
+++ __tests__/validateTryCount.test.js
@@ -14,7 +14,7 @@ describe("TryCount 유효성 테스트", () => {
       errorMessage: ERROR_TRY_COUNT_MESSAGE.INVAILD_INTEGER,
     },
     {
-      description: "시도 횟수가 0 이하인 경우",
+      description: "시도 횟수가 1 미만인 경우",
       input: "0",
       errorMessage: ERROR_TRY_COUNT_MESSAGE.INVALID_RANGE,
     },
@@ -31,7 +31,7 @@ describe("TryCount 유효성 테스트", () => {
 
   test.each([
     {
-      description: "시도 횟수가 숫자 및 정수이며, 0 초과인 경우",
+      description: "시도 횟수가 숫자 및 정수이며, 1 이상인 경우",
       input: "1",
     },
     {
