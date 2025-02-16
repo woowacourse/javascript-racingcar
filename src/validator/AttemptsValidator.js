1753c0caea33f282d9cb977f30df02fab2d2a9d5
장정안 <wjddks96@naver.com>
2025-02-16 20:20:22 +0900

    refactor: 함수형 프로그래밍 _pipe로 유효성 검증 부분 리팩토링

--- src/validator/AttemptsValidator.js
+++ src/validator/AttemptsValidator.js
@@ -1,14 +1,15 @@
 import { CONFIG } from '../constants/config.js';
 import { ERROR } from '../constants/messages.js';
+import { _pipe } from '../utils/utils.js';
 
-const AttemptsValidator = {
-  checkPositiveNumber(attempts) {
-    if (!Number.isInteger(attempts) || attempts <= CONFIG.ZERO) {
-      throw new Error(ERROR.NOT_POSITIVE_NUMBER);
-    }
+const checkPositiveNumber = (attempts) => {
+  if (!Number.isInteger(attempts) || attempts <= CONFIG.ZERO) {
+    throw new Error(ERROR.NOT_POSITIVE_NUMBER);
+  }
 
-    return attempts;
-  },
+  return attempts;
 };
 
-export default AttemptsValidator;
+const attemptsValidator = _pipe(checkPositiveNumber);
+
+export default attemptsValidator;
--- src/validator/CarNameValidator.js
+++ src/validator/CarNameValidator.js
@@ -1,29 +1,34 @@
 /* eslint-disable operator-linebreak */
 import { CONFIG } from '../constants/config.js';
 import { ERROR } from '../constants/messages.js';
+import { _pipe } from '../utils/utils.js';
 
-const CarNameValidator = {
-  checkCarNameLength(carName) {
-    if (
-      carName.length > CONFIG.MAXIMUM_CAR_NAME ||
-      carName.length < CONFIG.MINIMUM_CAR_NAME
-    ) {
-      throw new Error(ERROR.CAR_NAME_LENGTH);
-    }
-    return carName;
-  },
-  checkBlank(carName) {
-    if (carName.includes(CONFIG.BLANK)) {
-      throw new Error(ERROR.BLANK);
-    }
-    return carName;
-  },
-  checkDuplicatedCarName(carNames) {
-    if (carNames.length !== new Set(carNames).size) {
-      throw new Error(ERROR.DUPLICATED_CAR_NAME);
-    }
-    return carNames;
-  },
+const checkCarNameLength = (carName) => {
+  if (
+    carName.length > CONFIG.MAXIMUM_CAR_NAME ||
+    carName.length < CONFIG.MINIMUM_CAR_NAME
+  ) {
+    throw new Error(ERROR.CAR_NAME_LENGTH);
+  }
+  return carName;
+};
+const checkBlank = (carName) => {
+  if (carName.includes(CONFIG.BLANK)) {
+    throw new Error(ERROR.BLANK);
+  }
+  return carName;
+};
+const checkDuplicatedCarName = (carNames) => {
+  if (carNames.length !== new Set(carNames).size) {
+    throw new Error(ERROR.DUPLICATED_CAR_NAME);
+  }
+  return carNames;
 };
 
-export default CarNameValidator;
+const carNameValidator = _pipe(
+  checkCarNameLength,
+  checkBlank,
+  checkDuplicatedCarName
+);
+
+export default carNameValidator;
